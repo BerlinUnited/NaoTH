4e63f9b6d1fd04c02530fc3245a19c9d0de7211c
Thomas Krause <thomas@berlin-reloaded.de>
2010-08-17 16:03:55 +0200

    - exit script if enviroment variable not set (insider joke: https://bugs.launchpad.net/upstart/+bug/557177)

--- Extern/install_scripts/sfsexp.sh
+++ Extern/install_scripts/sfsexp.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "$EXTERN_DIR" == "" ]; then
+  echo ".::ERROR::. enviroment variable EXTERN_DIR was not set"
+  echo ".::ERROR::. will exit"
+  exit -1
+fi
+
 wget http://sourceforge.net/projects/sexpr/files/sexpr/1.2.1/sexpr_1.2.1.tar.gz/download
 tar xvzf sexpr_1.2.1.tar.gz
 cd sexpr_1.2.1/
