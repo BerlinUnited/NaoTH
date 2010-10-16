e2b4402b779411c29e7765e11a8f31d5b6a96262
mellmann <mellmann@informatik.hu-berlin.de>
2010-10-16 20:07:30 +0200

    basic structure for the naoth soccer agent

---
+++ SoccerNaoTH/Source/Core/Motion/Motion.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file Motion.cpp
+ *
+ * @author <a href="mailto:xu@informatik.hu-berlin.de">Xu Yuan</a>
+ *
+ */
+
+#include "Motion.h"
+
+using namespace std;
+using namespace naoth;
+
+
+Motion::Motion()
+{
+  
+}
+
+void Motion::call()
+{
+  
+}//end call
+
+Motion::~Motion()
+{
+}
