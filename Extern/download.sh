f1ec9d6334f6b8df2c6f5c4b14c42ceddd3397ef
Thomas Krause <thomas@berlin-reloaded.de>
2011-02-03 11:09:24 +0100

    - using the "-N" option of wget in order to only download the external file if the there is no local one or the remote one is newer

--- Extern/download.sh
+++ Extern/download.sh
@@ -3,11 +3,11 @@
 mkdir -p downloads
 cd downloads
 
-wget http://ftp.gnome.org/pub/gnome/sources/glib/2.26/glib-2.26.0.tar.gz
-wget http://sourceforge.net/projects/sexpr/files/sexpr/1.2.1/sexpr_1.2.1.tar.gz
-wget http://protobuf.googlecode.com/files/protobuf-2.3.0.tar.gz
-wget http://googletest.googlecode.com/files/gtest-1.5.0.zip
-wget http://googlemock.googlecode.com/files/gmock-1.5.0.zip
+wget -N http://ftp.gnome.org/pub/gnome/sources/glib/2.26/glib-2.26.0.tar.gz
+wget -N http://sourceforge.net/projects/sexpr/files/sexpr/1.2.1/sexpr_1.2.1.tar.gz
+wget -N http://protobuf.googlecode.com/files/protobuf-2.3.0.tar.gz
+wget -N http://googletest.googlecode.com/files/gtest-1.5.0.zip
+wget -N http://googlemock.googlecode.com/files/gmock-1.5.0.zip
 # add more source files to download above
 
 cd ..
