96ef65ba79f14897650afc6f759b1890fb5de08f
Thomas Krause <thomas@berlin-reloaded.de>
2011-02-18 17:11:26 +0100

    activating the debug command part of ParameterListDebugLoader

--- NaoTHSoccer/Source/Core/Cognition/Modules/Infrastructure/Debug/ParameterListDebugLoader.cpp
+++ NaoTHSoccer/Source/Core/Cognition/Modules/Infrastructure/Debug/ParameterListDebugLoader.cpp
@@ -4,7 +4,8 @@
  *
  */
 #include "ParameterListDebugLoader.h"
-
+#include <Representations/Infrastructure/Configuration.h>
+#include <PlatformInterface/Platform.h>
 #include "Core/Cognition/CognitionDebugServer.h"
 #include <Tools/Debug/DebugImageDrawings.h>
 
@@ -36,39 +37,43 @@ void ParameterListDebugLoader::execute()
 
 void ParameterListDebugLoader::executeDebugCommand(const std::string& command, const std::map<std::string,std::string>& arguments, std::stringstream& outstream)
 {
-//  naoth::Configuration& config =  naoth::Platform::getInstance().theConfiguration;
-//
-//  for(int i=0; i < )
-//  if (command == std::string(parentClassName).append(":set"))
-//  {
-//    // save the old values
-//    saveToConfig();
-//
-//    for (std::map<std::string, std::string>::const_iterator iter = arguments.begin(); iter != arguments.end(); iter++)
-//    {
-//      // update global config
-//      config.setRawValue(parentClassName, iter->first, iter->second);
-//    }
-//    // load from the changed config
-//    loadFromConfig();
-//
-//    // always success
-//    outstream<<"set " << parentClassName << " successfully"<< std::endl;
-//  }
-//  else if (command == std::string(parentClassName).append(":list"))
-//  {
-//    std::list<std::string> keys = config.getKeys(parentClassName);
-//    for(std::list<std::string>::const_iterator it = keys.begin(); it != keys.end(); it++)
-//    {
-//      std::string val = config.getRawValue(parentClassName, *it);
-//      outstream << *it << "=" << val << std::endl;
-//    }
-//  }
-//  else if (command == std::string(parentClassName).append(":store"))
-//  {
-//    config.save(naoth::Platform::getInstance().theConfigDirectory);
-//    outstream << "saved to " << naoth::Platform::getInstance().theConfigDirectory << std::endl;
-//  }
+  naoth::Configuration& config =  naoth::Platform::getInstance().theConfiguration;
+
+  std::map<std::string,ParameterList*>::const_iterator itParamList = paramlists.begin();
+  while(itParamList != paramlists.end())
+  {
+    if (command == std::string(itParamList->first).append(":set"))
+    {
+      // save the old values
+      itParamList->second->saveToConfig();
+      
+      for (std::map<std::string, std::string>::const_iterator iter = arguments.begin(); iter != arguments.end(); iter++)
+      {
+        // update global config
+        config.setRawValue(itParamList->first, iter->first, iter->second);
+      }
+      // load from the changed config
+      itParamList->second->loadFromConfig();
+
+      // always success
+      outstream<<"set " << itParamList->first << " successfully"<< std::endl;
+    }
+    else if (command == std::string(itParamList->first).append(":list"))
+    {
+      std::list<std::string> keys = config.getKeys(itParamList->first);
+      for(std::list<std::string>::const_iterator it = keys.begin(); it != keys.end(); it++)
+      {
+        std::string val = config.getRawValue(itParamList->first, *it);
+        outstream << *it << "=" << val << std::endl;
+      }
+    }
+    else if (command == std::string(itParamList->first).append(":store"))
+    {
+      config.save(naoth::Platform::getInstance().theConfigDirectory);
+      outstream << "saved to " << naoth::Platform::getInstance().theConfigDirectory << std::endl;
+    }
+    itParamList++;
+  }
 }
 
 ParameterListDebugLoader::~ParameterListDebugLoader()
