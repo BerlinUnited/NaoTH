ebff523c883a9ac0dbb9d16c2f8cadad9ec12775
Yuan Xu <xu@informatik.hu-berlin.de>
2011-06-27 11:53:26 +0200

    9vs.9 field size in simspark

--- RobotControl/RobotControlGUI/src/de/hu_berlin/informatik/ki/nao/dialogs/drawings/FieldDrawing.java
+++ RobotControl/RobotControlGUI/src/de/hu_berlin/informatik/ki/nao/dialogs/drawings/FieldDrawing.java
@@ -87,43 +87,43 @@ public class FieldDrawing implements Drawable
   {
     // draw gree pane
     g2d.setColor(fieldColor);
-    g2d.fillRect(-9500, -6500, 19000, 13000);
+    g2d.fillRect(-11000, -7500, 22000, 15000);
 
     // draw lines
     g2d.setColor(lineColor);
     g2d.setStroke(new BasicStroke(50f)); // line width 50mm
 
-    g2d.drawLine(-9000, -6000, 9000, -6000); // bottom side line
-    g2d.drawLine(-9000, 6000, 9000, 6000); // top side line
-    g2d.drawLine(-9000, -6000, -9000, 6000); // left side line
-    g2d.drawLine(9000, -6000, 9000, 6000); // right side line
-    g2d.drawLine(0, -6000, 0, 6000); // center line
+    g2d.drawLine(-10500, -7000, 10500, -7000); // bottom side line
+    g2d.drawLine(-10500, 7000, 10500, 7000); // top side line
+    g2d.drawLine(-10500, -7000, -10500, 7000); // left side line
+    g2d.drawLine(10500, -7000, 10500, 7000); // right side line
+    g2d.drawLine(0, -7000, 0, 7000); // center line
 
-    g2d.drawLine(-7200, -2100, -7200, 2100); // blue front goal line
-    g2d.drawLine(-9000, -2100, -7200, -2100); // blue bottom goal line
-    g2d.drawLine(-9000, 2100, -7200, 2100); // blue top goal line
+    g2d.drawLine(-8700, -1950, -8700, 1950); // blue front goal line
+    g2d.drawLine(-10500, -1950, -8700, -1950); // blue bottom goal line
+    g2d.drawLine(-10500, 1950, -8700, 1950); // blue top goal line
 
-    g2d.drawLine(7200, -2100, 7200, 2100); // yellow front goal line
-    g2d.drawLine(9000, -2100, 7200, -2100); // yellow bottom goal line
-    g2d.drawLine(9000, 2100, 7200, 2100); // yellow top goal line
+    g2d.drawLine(8700, -1950, 8700, 1950); // yellow front goal line
+    g2d.drawLine(10500, -1950, 8700, -1950); // yellow bottom goal line
+    g2d.drawLine(10500, 1950, 8700, 1950); // yellow top goal line
 
     // draw center circle
     // radius is FreeKickDistance*FreeKickDistance
-    g2d.drawOval(-1690, -1690, 3380, 3380);
+    g2d.drawOval(-1800, -1800, 3600, 3600);
 
 
     g2d.setStroke(new BasicStroke(50.0f));
     // draw left goal
     g2d.setColor(leftGoalColor);
-    g2d.drawLine(-9600, -1050, -9000, -1050);
-    g2d.drawLine(-9600, 1050, -9000, 1050);
-    g2d.drawLine(-9600, -1050, -9600, 1050);
+    g2d.drawLine(-11000, -1050, -10500, -1050);
+    g2d.drawLine(-11000, 1050, -10500, 1050);
+    g2d.drawLine(-11000, -1050, -11000, 1050);
 
     // draw right goal
     g2d.setColor(rightGoalColor);
-    g2d.drawLine(9600, -1050, 9000, -1050);
-    g2d.drawLine(9600, 1050, 9000, 1050);
-    g2d.drawLine(9600, -1050, 9600, 1050);
+    g2d.drawLine(11000, -1050, 10500, -1050);
+    g2d.drawLine(11000, 1050, 10500, 1050);
+    g2d.drawLine(11000, -1050, 11000, 1050);
 
   }//end draw
 
