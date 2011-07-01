af59ee1e06941d7dfed2dc2a963503bb6246c73e
mellmann <mellmann@informatik.hu-berlin.de>
2011-07-01 15:36:36 +0200

    added missing files

---
+++ NaoTHSoccer/Source/Core/Representations/Modeling/BehaviorStatus.h
@@ -0,0 +1,36 @@
+/* 
+ * File:   BehaviorStatus.h
+ * Author: thomas
+ *
+ * Created on 22. März 2010, 09:10
+ */
+
+#ifndef _BEHAVIORSTATUS_H
+#define	_BEHAVIORSTATUS_H
+
+//#include "PlatformInterface/PlatformInterchangeable.h"
+#include "Tools/DataStructures/Printable.h"
+
+
+#include "Messages/Messages.pb.h"
+
+/**
+ * This is a wrapper Representation for naothmessages::BehaviorStatus in
+ * order to make the naothmessages::BehaviorStatus loggable
+ */
+class BehaviorStatus : public naoth::Printable
+{
+public:
+  BehaviorStatus();
+  virtual ~BehaviorStatus();
+
+  virtual void toDataStream(ostream& stream) const;
+  virtual void fromDataStream(istream& stream);
+
+  virtual void print(ostream& stream) const;
+
+  naothmessages::BehaviorStatus status;
+
+};
+#endif	/* _BEHAVIORSTATUS_H */
+
