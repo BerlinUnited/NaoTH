e1749ccdf2559c3b3c0f38059ea795cd9a5ef74c
Thomas Krause <thomas@berlin-reloaded.de>
2011-07-03 22:40:48 +0200

    - Video2Log now also is able to convert directories of images to to log files (non-recursive)

--- Utils/Video2Log/src/Video2Log.cpp
+++ Utils/Video2Log/src/Video2Log.cpp
@@ -1,89 +1,107 @@
 #include "Video2Log.h"
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <glib.h>
 
 #include <iostream>
 
 #include <Tools/Debug/NaoTHAssert.h>
 #include <Representations/Infrastructure/Image.h>
-#include <Core/Tools/Logfile/LogfileManager.h>
 #include <Tools/DataStructures/Serializer.h>
 
 Video2Log::Video2Log(std::string fileName, std::string outFile)
-  : path(fileName), outFile(outFile)
+  : path(fileName), outFile(outFile), mgr(true), frameNumber(0)
 {
-
+  mgr.openFile(outFile.c_str());
 }
 
 void Video2Log::generateLogFromVideoFile()
 {
   cv::VideoCapture capture(path);
-  LogfileManager<10> mgr(true);
-  mgr.openFile(outFile.c_str());
 
-  unsigned int frameNumber = 0;
   while(capture.isOpened())
   {
     cv::Mat matImgOrig;
-    cv::Mat matImg;
     capture >> matImgOrig;
-
-    ASSERT(matImgOrig.type() == CV_8SC3);
 
     if(matImgOrig.rows == 0)
     {
       break;
     }
+    logImage(matImgOrig);
+  }
+}
 
-    naoth::Image repImg;
-    // always resize to 320x240
-    ASSERT(repImg.width() == 320);
-    ASSERT(repImg.height() == 240);
-
-    if(matImgOrig.rows != repImg.height() || matImgOrig.cols != repImg.width())
+void Video2Log::generateLogFromImageDir()
+{
+  GDir* dir = g_dir_open(path.c_str(), 0, NULL);
+  if(dir != NULL)
+  {
+    const gchar* name;
+    while ((name = g_dir_read_name(dir)) != NULL)
     {
-      cv::resize(matImgOrig, matImg, cv::Size(repImg.width(),repImg.height()));
-    }
-    else
-    {
-      matImg = matImgOrig;
-    }
-    ASSERT(matImg.rows == repImg.height());
-    ASSERT(matImg.cols == repImg.width());
-
-    // we need YCbCr color space
-    cv::cvtColor(matImg, matImg, CV_RGB2YCrCb);
-
-    // convert the classical way
-    for(unsigned int x=0; x < matImg.cols && x < repImg.width(); x++)
-    {
-      for(unsigned int y=0; y < matImg.rows && y < repImg.height(); y++)
+      std::string completeFileName = path + "/" + name;
+      if (g_file_test(completeFileName.c_str(), G_FILE_TEST_IS_REGULAR))
       {
-        cv::Vec3b v = matImg.at<cv::Vec3b>(y,x);
-        Pixel p;
-        p.y = v[0];
-        p.u = v[1];
-        p.v = v[2];
-        repImg.set(x, y, p);
+        std::cout << "adding \"" << completeFileName << "\"" << std::endl;
+        cv::Mat matImgOrig = cv::imread(completeFileName);
+        logImage(matImgOrig);
       }
     }
-
-    // write out
-    std::stringstream& stream = mgr.log(frameNumber, "Image");
-    naoth::Serializer<naoth::Image>::serialize(repImg, stream);
-
-    frameNumber++;
-
+    g_dir_close(dir);
   }
-  mgr.flush();
-  mgr.closeFile();
+}
+
+void Video2Log::logImage(cv::Mat &matImgOrig)
+{
+  cv::Mat matImg;
+  naoth::Image repImg;
+
+
+  ASSERT(matImgOrig.type() == CV_8SC3);
+
+  // always resize to 320x240
+  ASSERT(repImg.width() == 320);
+  ASSERT(repImg.height() == 240);
+
+  if(matImgOrig.rows != repImg.height() || matImgOrig.cols != repImg.width())
+  {
+    cv::resize(matImgOrig, matImg, cv::Size(repImg.width(),repImg.height()));
+  }
+  else
+  {
+    matImg = matImgOrig;
+  }
+  ASSERT(matImg.rows == repImg.height());
+  ASSERT(matImg.cols == repImg.width());
+
+  // we need YCbCr color space
+  cv::cvtColor(matImg, matImg, CV_RGB2YCrCb);
+
+  // convert the classical way
+  for(unsigned int x=0; x < matImg.cols && x < repImg.width(); x++)
+  {
+    for(unsigned int y=0; y < matImg.rows && y < repImg.height(); y++)
+    {
+      cv::Vec3b v = matImg.at<cv::Vec3b>(y,x);
+      Pixel p;
+      p.y = v[0];
+      p.u = v[1];
+      p.v = v[2];
+      repImg.set(x, y, p);
+    }
+  }
+
+  // write out
+  std::stringstream& stream = mgr.log(frameNumber, "Image");
+  naoth::Serializer<naoth::Image>::serialize(repImg, stream);
+
+  frameNumber++;
 }
 
 Video2Log::~Video2Log()
 {
-
+  mgr.flush();
+  mgr.closeFile();
 }
 
 int main(int argc, char** argv)
@@ -99,7 +117,14 @@ int main(int argc, char** argv)
 
     std::cout << "opening \"" << fileName << "\"" << std::endl;
     Video2Log converter(fileName, outFile);
-    converter.generateLogFromVideoFile();
+    if(g_file_test(fileName.c_str(), G_FILE_TEST_IS_DIR))
+    {
+      converter.generateLogFromImageDir();
+    }
+    else
+    {
+      converter.generateLogFromVideoFile();
+    }
   }
   return 0;
 }
--- Utils/Video2Log/src/Video2Log.h
+++ Utils/Video2Log/src/Video2Log.h
@@ -3,17 +3,27 @@
 
 #include <string>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <Core/Tools/Logfile/LogfileManager.h>
 
 class Video2Log
 {
 public:
     Video2Log(std::string fileName, std::string outFile);
     virtual void generateLogFromVideoFile();
+    virtual void generateLogFromImageDir();
 
     virtual ~Video2Log();
 private:
     std::string path;
     std::string outFile;
+    LogfileManager<10> mgr;
+    unsigned int frameNumber;
+
+    virtual void logImage(cv::Mat& matImgOrig);
 };
 
 #endif // VIDEO2LOG_H
