3faba1630ac06a7f2bc3157ea4f2f38f1ff3f586
Florian Holzhauer <holzhaue@informatik.hu-berlin.de>
2011-07-05 22:11:01 +0430

    AL_CROSS is no longer used, apparently it is NAO_CROSSCOMPILE now.

--- Misc/NaoAdminScripts/nao-check-and-set-environment.sh
+++ Misc/NaoAdminScripts/nao-check-and-set-environment.sh
@@ -16,15 +16,15 @@ verify_aldir ()
 
 verify_alcross () 
 {
-  DEFAULT_AL_CROSS="$HOME/Roboter/Aldebaran/crosscompiletoolchain"
-  if  [ "$AL_CROSS" == "" ]
+  DEFAULT_NAO_CROSSCOMPILE="$HOME/Roboter/Aldebaran/crosscompiletoolchain"
+  if  [ "$NAO_CROSSCOMPILE" == "" ]
   then
-        echo "  * AL_CROSS has not been set, using default path '$DEFAULT_AL_CROSS'"
-        export AL_CROSS=$DEFAULT_AL_CROSS
+        echo "  * NAO_CROSSCOMPILE has not been set, using default path '$DEFAULT_NAO_CROSSCOMPILE'"
+        export NAO_CROSSCOMPILE=$DEFAULT_NAO_CROSSCOMPILE
   fi
-  if  [ ! -d $AL_CROSS/staging/geode-linux/usr/lib ]
+  if  [ ! -d $NAO_CROSSCOMPILE/staging/geode-linux/usr/lib ]
   then
-        echo "WARN: I was unable to find staging/usr/lib in AL_CROSS - the path is probably invalid" 
+        echo "WARN: I was unable to find staging/usr/lib in NAO_CROSSCOMPILE - the path is probably invalid" 
   fi
 }
 verify_naothsvn() 
@@ -75,7 +75,7 @@ verify_naothsvn
 verify_webotshome
 verify_naoimagedir
 echo "   AL_DIR is $AL_DIR"
-echo "   AL_CROSS is $AL_CROSS"
+echo "   NAO_CROSSCOMPILE is $NAO_CROSSCOMPILE"
 echo "   NAOTH_BZR is $NAOTH_BZR"
 echo "   WEBOTS_HOME is $WEBOTS_HOME"
 echo "   NAO_IMAGE_DIR is $NAO_IMAGE_DIR"
--- Misc/NaoAdminScripts/nao-create-stick.sh
+++ Misc/NaoAdminScripts/nao-create-stick.sh
@@ -220,7 +220,7 @@ echo "Setting Ethernet-Address on $ETH_ADDR and WLAN-Address on $WLAN_ADDR"
 sed -e 's/'ETH_ADDR'/'${ETH_ADDR}'/' -e 's/'ETH_NET'/'${ETH_PREFIX}0'/' -e 's/'ETH_GATE'/'${ETH_PREFIX}1'/' -e 's/'WLAN_ADDR'/'${WLAN_ADDR}'/' -e 's/'WLAN_NET'/'${WLAN_PREFIX}0'/' -e 's/'WLAN_GATE'/'${WLAN_PREFIX}1'/'   $NAOTH_BZR/Misc/NaoConfigFiles/etc/network/interfaces > $TMP_MOUNT_POINT/etc/network/interfaces
 
 echo "Copying staging area"
-cp -R $AL_CROSS/staging/usr/ $TMP_MOUNT_POINT/
+cp -R $NAO_CROSSCOMPILE/staging/usr/ $TMP_MOUNT_POINT/
 
     echo "put the stick into the nao, boot once and then run this script again without burning the image onto it"
 else # if burn
