996959e075ecc1850d7584d75b2d6e9161645930
Thomas Krause <thomas@berlin-reloaded.de>
2011-08-25 12:13:54 +0200

    forget to add some files

---
+++ ExampleSoccerAgent/Source/Motion/MotionBlackBoard.cpp
@@ -0,0 +1,23 @@
+/*
+ * File:   MotionBlackBoard.cpp
+ * Author: Oliver Welter
+ *
+ * Created on 26. Januar 2009, 01:39
+ */
+
+#include "MotionBlackBoard.h"
+
+
+MotionBlackBoard::MotionBlackBoard():
+currentlyExecutedMotion(MotionRequest::stand)
+{
+}
+
+void MotionBlackBoard::init()
+{
+  theSensorJointData.init();
+}
+
+MotionBlackBoard::~MotionBlackBoard()
+{
+}
