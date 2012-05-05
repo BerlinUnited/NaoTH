100683d25b672bb83885b73a3fa5533fa64851a1
Thomas Krause <thomas@berlin-reloaded.de>
2012-05-05 21:15:29 +0200

    test case improved

--- NaoTHSoccer/Tests/Core/Cognition/Modules/Modeling/PotentialFieldProvider/PotentialFieldProviderTest.cpp
+++ NaoTHSoccer/Tests/Core/Cognition/Modules/Modeling/PotentialFieldProvider/PotentialFieldProviderTest.cpp
@@ -11,6 +11,7 @@ PROVIDE(SelfLocGoalModel)
 PROVIDE(CompassDirection)
 PROVIDE(BallModel)
 PROVIDE(RawAttackDirection)
+PROVIDE(FrameInfo)
 END_DECLARE_MODULE(PotentialFieldProviderTest)
 
 class PotentialFieldProviderTest : public testing::Test,
@@ -34,10 +35,12 @@ protected:
 
 TEST_F(PotentialFieldProviderTest, StableOnMovedRobot)
 {
-  Vector2<double> simulatedBall(100.0,250.0);
+  getFrameInfo().setTime(5000);
 
-  Vector2<double> simulatedLeftPost(500, 3000);
-  Vector2<double> simulatedRightPost(-500, 3000);
+  Vector2<double> simulatedBall(820.0,250.0);
+
+  Vector2<double> simulatedLeftPost(3000, -250);
+  Vector2<double> simulatedRightPost(3000, 250);
 
   getCompassDirection().angle = 0;
 
@@ -51,8 +54,8 @@ TEST_F(PotentialFieldProviderTest, StableOnMovedRobot)
   potentialFieldProvider->execute();
   Vector2<double> result1 = getRawAttackDirection().attackDirection;
 
-  ASSERT_NE(0, result1.x);
-  ASSERT_NE(0, result1.y);
+  ASSERT_NE(0.0, result1.x);
+  ASSERT_NE(0.0, result1.y);
 
   // phase 1: only translation is changed
 
@@ -66,8 +69,7 @@ TEST_F(PotentialFieldProviderTest, StableOnMovedRobot)
   potentialFieldProvider->execute();
   Vector2<double> result2 = getRawAttackDirection().attackDirection;
 
-  ASSERT_NE(0, result2.x);
-  ASSERT_NE(0, result2.y);
-  ASSERT_EQ(result1.x, result2.x);
-  ASSERT_EQ(result1.y, result2.y);
+  ASSERT_NE(0.0, result2.x);
+  ASSERT_NE(0.0, result2.y);
+  ASSERT_NEAR(result1.angle(), result2.angle(), 0.001);
 }
