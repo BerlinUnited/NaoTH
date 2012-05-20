0836920878283d9a60533ac4f84694f23a689cbc
Thomas Krause <thomas@berlin-reloaded.de>
2012-05-20 21:47:38 +0200

    do not set autoexposure since it is a seperate calibration process

--- NaoTHSoccer/Source/Core/Cognition/Modules/Infrastructure/Debug/CameraDebug.cpp
+++ NaoTHSoccer/Source/Core/Cognition/Modules/Infrastructure/Debug/CameraDebug.cpp
@@ -33,7 +33,7 @@ void CameraDebug::execute()
   if(isAutoCalibratingCamera)
   {
     //disabled auto exposure time setting
-    getCameraSettingsRequest().data[CameraSettings::AutoExposition] = 1;
+    //getCameraSettingsRequest().data[CameraSettings::AutoExposition] = 1;
     getCameraSettingsRequest().data[CameraSettings::AutoGain] = 1;
     getCameraSettingsRequest().data[CameraSettings::AutoWhiteBalancing] = 1;
 
@@ -62,7 +62,7 @@ void CameraDebug::execute()
 
     // deactivate the auto stuff
     //disabled auto exposure time setting
-    getCameraSettingsRequest().data[CameraSettings::AutoExposition] = 0;
+    //getCameraSettingsRequest().data[CameraSettings::AutoExposition] = 0;
     getCameraSettingsRequest().data[CameraSettings::AutoGain] = 0;
     getCameraSettingsRequest().data[CameraSettings::AutoWhiteBalancing] = 0;
 
