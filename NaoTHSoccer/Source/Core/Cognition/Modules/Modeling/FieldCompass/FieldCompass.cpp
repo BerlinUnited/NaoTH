e331d31ede49b253495689f33fc02842a1720764
mellmann <mellmann@informatik.hu-berlin.de>
2012-06-11 13:01:31 -0700

    syntactic clean

--- NaoTHSoccer/Source/Core/Cognition/Modules/Modeling/FieldCompass/FieldCompass.cpp
+++ NaoTHSoccer/Source/Core/Cognition/Modules/Modeling/FieldCompass/FieldCompass.cpp
@@ -2,28 +2,16 @@
  * @file ActiveGoalLocator.h
  *
  * @author <a href="mailto:scheunem@informatik.hu-berlin.de">Marcus Scheunemann</a>
- * Implementation of class ActiveGoalLocator
+ * Implementation of class FieldCompass
  */
 
 #include "FieldCompass.h"
 
-//for MODIFY
-#include "Tools/Debug/DebugBufferedOutput.h"
-#include "Tools/Debug/DebugModify.h"
-#include "Tools/Debug/Stopwatch.h"
+FieldCompass::FieldCompass()
+{
+}//Constructor
 
-//MATH
-#include "Tools/Math/Probabilistics.h"
-#include "Representations/Modeling/GoalModel.h"
-//#include "Cognition/Modules/Modeling/SelfLocator/MonteCarloSelfLocator/SampleSet.h"
-#include <cmath>
-
-FieldCompass::FieldCompass() {
-
-} //Constructor
-
-void FieldCompass::execute() {
-
-    getCompassDirection().angle = getRobotPose().rotation;
-
+void FieldCompass::execute()
+{
+  getCompassDirection().angle = getRobotPose().rotation;
 }//end FieldCompass::execute()
