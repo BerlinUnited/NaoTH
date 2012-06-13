7a841d2b3817c75cb133162445239f91a6ae7efb
Marcus Scheunemann <scheunem@informatik.hu-berlin.de>
2012-06-13 20:51:24 +0200

    error in commit before

--- NaoTHSoccer/Source/Core/Cognition/Modules/Modeling/GoalLocator/ActiveGoalLocator/ActiveGoalLocator.cpp
+++ NaoTHSoccer/Source/Core/Cognition/Modules/Modeling/GoalLocator/ActiveGoalLocator/ActiveGoalLocator.cpp
@@ -205,7 +205,7 @@ void ActiveGoalLocator::execute() {
     //decide whether distError is feasable
     if (lastDistError < parameters.possibleGoalWidhtError) { //make param
 
-      if (ccSamples[id1].sampleSet.mean.angle() < ccSamples[id2].sampleSet.mean) {
+      if (ccSamples[id1].sampleSet.mean.angle() < ccSamples[id2].sampleSet.mean.angle()) {
         getLocalGoalModel().goal.leftPost  = ccSamples[id1].sampleSet.mean;
         getLocalGoalModel().goal.rightPost = ccSamples[id2].sampleSet.mean;
       } else {
