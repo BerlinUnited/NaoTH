cd960d8e7585a7c3caa1e2a5e70a7627813f4e5f
CNR <critter@informatik.hu-berlin.de>
2014-03-08 16:30:59 +0100

    added a debug request to simulate that a field is seen over the hole image (test scenarios)

--- NaoTHSoccer/Source/Core/Cognition/Modules/Perception/VisualCortex/Detectors/FieldDetector.cpp
+++ NaoTHSoccer/Source/Core/Cognition/Modules/Perception/VisualCortex/Detectors/FieldDetector.cpp
@@ -18,6 +18,7 @@ FieldDetector::FieldDetector()
   cameraID(CameraInfo::Bottom)
 {
   DEBUG_REQUEST_REGISTER("Vision:Detectors:FieldDetector:mark_field_polygon", "mark polygonal boundary of the detected field on the image", false);
+  DEBUG_REQUEST_REGISTER("Vision:Detectors:FieldDetector:setHoleImageAsField", "mark hole image as if field were detected", false);
 }
 
 
@@ -31,7 +32,45 @@ void FieldDetector::execute(CameraInfo::CameraID id)
   cameraID = id;
   CANVAS_PX(id);
 
-  if(getScanLineEdgelPercept().endPoints.size() > 0)
+  bool simulateField = false;
+
+  DEBUG_REQUEST("Vision:Detectors:FieldDetector:setHoleImageAsField",
+    simulateField = true;
+  );
+
+  if(simulateField)
+  {
+    vector<Vector2i > result;
+    result.push_back(Vector2i(1,1));
+    result.push_back(Vector2i(1,getImage().height()-2));
+    result.push_back(Vector2i(getImage().width()-2, getImage().height()-2));
+    result.push_back(Vector2i(getImage().width()-2,1));
+    result.push_back(Vector2i(1,1));
+
+    // create the polygon
+    FieldPercept::FieldPoly fieldPoly;
+    for(unsigned int i = 0; i < result.size(); i++)
+    {
+      fieldPoly.add(result[i]);
+    }
+
+    getFieldPercept().setField(fieldPoly, getArtificialHorizon());
+    if(fieldPoly.getArea() >= 5600)
+    {
+      getFieldPercept().valid = true;
+    }
+
+    DEBUG_REQUEST( "Vision:Detectors:FieldDetector:mark_field_polygon",
+      int idx = static_cast<int>(result.size())-1;
+      ColorClasses::Color color = getFieldPercept().valid ? ColorClasses::green : ColorClasses::red;
+      for(int i = 0; i < (int)result.size(); i++)
+      {
+        LINE_PX(color, result[idx].x, result[idx].y, result[i].x, result[i].y);
+        idx = i;
+      }
+    );    
+  }
+  else if(getScanLineEdgelPercept().endPoints.size() > 0)
   {
     vector<Vector2i > points(getScanLineEdgelPercept().endPoints.size());
 
