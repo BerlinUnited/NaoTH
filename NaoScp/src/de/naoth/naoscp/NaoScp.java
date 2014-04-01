35da1ac4d5af34620e51539da36d76a10a39819d
Thomas Krause <krauseto@hu-berlin.de>
2014-04-01 22:50:33 +0200

    allow player number 0-9

--- NaoScp/src/de/naoth/naoscp/NaoScp.java
+++ NaoScp/src/de/naoth/naoscp/NaoScp.java
@@ -1311,7 +1311,7 @@ public class NaoScp extends NaoScpMainFrame
             bodyIdToPlayerNumber.put(name, number);
             
             final MaskFormatter formatter = new MaskFormatter(name+": *");
-            formatter.setValidCharacters("12345");
+            formatter.setValidCharacters("123456790");
             formatter.setPlaceholderCharacter(Character.forDigit(number, 10));
             
             final JFormattedTextField input = new JFormattedTextField(formatter);
