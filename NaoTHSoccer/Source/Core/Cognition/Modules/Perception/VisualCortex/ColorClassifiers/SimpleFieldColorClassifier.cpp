101a312ab447577031b0296fba70321a34481ad7
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2014-04-04 16:43:55 +0200

    baah

--- NaoTHSoccer/Source/Core/Cognition/Modules/Perception/VisualCortex/ColorClassifiers/SimpleFieldColorClassifier.cpp
+++ NaoTHSoccer/Source/Core/Cognition/Modules/Perception/VisualCortex/ColorClassifiers/SimpleFieldColorClassifier.cpp
@@ -182,7 +182,7 @@ void SimpleFieldColorClassifier::execute(const CameraInfo::CameraID id)
     (int)maxWeightedIndexCr + (int)getParameters().fieldColorMax.v
     );
 
-  getFieldColorPercept().histogramField.y = (unsigned char)Math::clamp(maxWeightedIndexY,0,255);
+  getFieldColorPercept().histogramField.y = (unsigned char)Math::clamp(filteredHistogramY.median,0,255);
   getFieldColorPercept().histogramField.u = (unsigned char)Math::clamp(maxWeightedIndexCb,0,255);
   getFieldColorPercept().histogramField.v = (unsigned char)Math::clamp(maxWeightedIndexCr,0,255);
 
