a7004f1bb0b009bce3c059b5059966ddc78506aa
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2014-05-04 21:35:28 +0200

    cleanup: moved shared memory handlers to a separate file cleanup: removed the setCameraSettingsInternal(..) - V4LHandler is fully responsible for the parameter handling cleanup: removed the check for the correct size of the TeamMessageDataOut

--- Framework/Platforms/Source/DCM/NaoRobot/NaoController.h
+++ Framework/Platforms/Source/DCM/NaoRobot/NaoController.h
@@ -10,7 +10,6 @@
 #ifndef _NaoController_H_
 #define _NaoController_H_
 
-#include <glib.h>
 #include <string>
 
 //
@@ -38,107 +37,21 @@
 // local tools
 #include "Tools/IPCData.h"
 #include "Tools/NaoTime.h"
-#include "Tools/SharedMemory.h"
-
+#include "Tools/SharedMemoryIO.h"
 
 namespace naoth
 {
-
-// TODO: remove TEAMCOMM_MAX_MSG_SIZE
-static const unsigned int TEAMCOMM_MAX_MSG_SIZE = 4096;
-
-
-// MutexSharedMemoryReaderWithSwap
-template<class V>
-class SharedMemoryWriter
-{
-public:
-  SharedMemoryWriter() : mutex(NULL)
-  {
-    mutex = g_mutex_new();
-  }
-
-  ~SharedMemoryWriter()
-  {
-    g_mutex_free(mutex);
-    memory.close();
-  }
-
-  void open(std::string name)
-  {
-    // debug
-    std::cout << "Opening Shared Memory: " << name << std::endl;
-    memory.open(name);
-  }
-
-  template<class T>
-  void set(const T& data)
-  { 
-    g_mutex_lock(mutex);
-    // copy the data into the shared memory
-    memory.writing()->set(data);
-    memory.swapWriting();
-    g_mutex_unlock(mutex);
-  }
-
-private:
- GMutex* mutex;
- SharedMemory<V> memory;
-};//end class SharedMemoryWriter
-
-
-template<class V>
-class SharedMemoryReader
-{
-public:
-  SharedMemoryReader() : mutex(NULL)
-  {
-    mutex = g_mutex_new();
-  }
-
-  ~SharedMemoryReader()
-  {
-    g_mutex_free(mutex);
-    memory.close();
-  }
-
-  void open(std::string name)
-  {
-    // debug
-    std::cout << "Opening Shared Memory: " << name << std::endl;
-    memory.open(name);
-  }
-
-  template<class T>
-  void get(T& data) const 
-  { 
-    g_mutex_lock(mutex);
-    memory.reading()->get(data);
-    g_mutex_unlock(mutex);
-  }
-
-  // delegate
-  inline bool swapReading(){ return memory.swapReading(); }
-  inline const V& data() { return *memory.reading(); }
-
-private:
- GMutex* mutex;
- SharedMemory<V> memory;
-};//end class SharedMemoryReader
-
-
 
 class NaoController : public PlatformInterface
 {
 public:
   NaoController();
-  ~NaoController();
+  virtual ~NaoController();
 
   virtual string getBodyID() const { return theBodyID; }
   virtual string getBodyNickName() const { return theBodyNickName; }
   virtual string getHeadNickName() const { return theHeadNickName; }
   
-
   // camera stuff
   void get(Image& data){ theBottomCameraHandler.get(data); }
   void get(ImageTop& data){ theTopCameraHandler.get(data); }
@@ -156,14 +69,7 @@ public:
 
   // teamcomm stuff
   void get(TeamMessageDataIn& data) { theBroadCastListener->receive(data.data); }
-  void set(const TeamMessageDataOut& data)
-  {
-    // TODO: remove TEAMCOMM_MAX_MSG_SIZE
-    if ( data.data.size() <= TEAMCOMM_MAX_MSG_SIZE )
-      theBroadCaster->send(data.data);
-    else
-      cerr << "NaoController: TeamMessageDataOut is too big " << data.data.size() << endl;
-  }//end set TeamMessageDataOut
+  void set(const TeamMessageDataOut& data) { theBroadCaster->send(data.data); }
 
   // gamecontroller stuff
   void get(GameData& data){ theGameController->get(data, NaoTime::getNaoTimeInMilliSeconds()); }
@@ -208,11 +114,11 @@ public:
     // try to get some data from the DCM
     if ( !naoSensorData.swapReading() )
     {
-      std::cerr << "didn't get new sensor data" << std::endl;
+      std::cerr << "[NaoController] didn't get new sensor data" << std::endl;
     }
     PlatformInterface::getMotionInput();
     STOPWATCH_STOP("getMotionInput");
-  }//end getMotionInput
+  }
 
 
   virtual void setMotionOutput()
@@ -220,7 +126,7 @@ public:
     STOPWATCH_START("setMotionOutput");
     PlatformInterface::setMotionOutput();
     STOPWATCH_STOP("setMotionOutput");
-  }//end setMotionOutput
+  }
 
 
   virtual void getCognitionInput()
@@ -228,7 +134,7 @@ public:
     STOPWATCH_START("getCognitionInput");
     PlatformInterface::getCognitionInput();
     STOPWATCH_STOP("getCognitionInput");
-  }//end getCognitionInput
+  }
   
 
   void setCognitionOutput()
@@ -236,7 +142,7 @@ public:
     STOPWATCH_START("setCognitionOutput");
     PlatformInterface::setCognitionOutput();
     STOPWATCH_STOP("setCognitionOutput");
-  }//end setCognitionOutput
+  }
 
 
 protected:
@@ -245,14 +151,11 @@ protected:
     return new MessageQueue4Threads();
   }
 
-  void setCameraSettingsInternal(const CameraSettingsRequest& data,
-           CameraInfo::CameraID camID = CameraInfo::Bottom);
 
 protected:
   std::string theBodyID;
   std::string theBodyNickName;
   std::string theHeadNickName;
-
 
   // -- begin -- shared memory access --
   // DCM --> NaoController
