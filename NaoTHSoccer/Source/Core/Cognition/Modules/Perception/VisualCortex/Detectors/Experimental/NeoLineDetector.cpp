961be4b594ce0e8d5613652b6d31a0b42a2e5045
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2014-05-05 14:59:14 +0200

    unused

--- NaoTHSoccer/Source/Core/Cognition/Modules/Perception/VisualCortex/Detectors/Experimental/NeoLineDetector.cpp
+++ NaoTHSoccer/Source/Core/Cognition/Modules/Perception/VisualCortex/Detectors/Experimental/NeoLineDetector.cpp
@@ -599,7 +599,7 @@ void NeoLineDetector::calculatePairs(const std::vector<ScanLineEdgelPercept::Edg
 }
 
 
-void NeoLineDetector::calculateNeigbors(const std::vector<ScanLineEdgelPercept::EdgelPair>& edgels, std::vector<Neighbors>& neighbors, double threshold) const
+void NeoLineDetector::calculateNeigbors(const std::vector<ScanLineEdgelPercept::EdgelPair>& edgels, std::vector<Neighbors>& neighbors, double /*threshold*/) const
 {
   neighbors.resize(edgels.size());
   for(size_t i = 0; i < edgels.size(); i++) 
