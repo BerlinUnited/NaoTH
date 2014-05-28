c873d019509bacfc03264ecaa271051505b4442b
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2014-05-28 19:16:17 +0200

    feature: first version of a dialog for the jni log simulator

---
+++ RobotControl/RobotControlGUI/src/de/naoth/rc/LogSimulator.java
@@ -0,0 +1,23 @@
+package de.naoth.rc;
+
+/**
+ *
+ * @author Heinrich
+ */
+public class LogSimulator {
+    static {
+        // Load native library at runtime
+        System.loadLibrary("logsimulator");
+    }
+
+    public native void stepForward();
+    public native void stepBack();
+    public native void jumpToBegin();
+    public native void jumpToEnd();
+    public native void jumpTo(int position);
+    public native void openLogFile(String path);
+
+    public native int getCurrentFrame();
+    public native int getMinFrame();
+    public native int getMaxFrame();
+}
