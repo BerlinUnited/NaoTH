301f9919deb37ed7344e03a4459bad1e163c917a
woltersd <woltersd@informatik.hu-berlin.de>
2014-06-29 15:25:32 +0200

    synchro

--- RobotControl/RobotControlGUI/src/de/naoth/rc/LogSimulator.java
+++ RobotControl/RobotControlGUI/src/de/naoth/rc/LogSimulator.java
@@ -65,14 +65,18 @@ public class LogSimulator {
         private final List<LogSimulatorActionListener> listeners = new ArrayList<>();
         private final LogSimulator logSimulator = new LogSimulator();
         
-        private void notifyFrameChanged(int frame) {
+        private synchronized void notifyFrameChanged(int frame) {
+            try{
             BlackBoard b = logSimulator.new BlackBoard();
             for(LogSimulatorActionListener l: listeners) {
                 l.frameChanged(b, frame);
             }
+            } catch(NullPointerException ex) {
+                int x = 0;
+            }
         }
         
-        private void notifyFileOpenedChanged(String path) {
+        private synchronized void notifyFileOpenedChanged(String path) {
             BlackBoard b = logSimulator.new BlackBoard();
             for(LogSimulatorActionListener l: listeners) {
                 l.logfileOpened(b, path);
@@ -82,54 +86,54 @@ public class LogSimulator {
         // singleton
         private LogSimulatorManager() {}
         private static final LogSimulatorManager logSimulatorManager = new LogSimulatorManager();
-        public static LogSimulatorManager getInstance() {
+        public synchronized static LogSimulatorManager getInstance() {
             return logSimulatorManager;
         }
         
-        public void addListener(LogSimulatorActionListener l) {
+        public synchronized void addListener(LogSimulatorActionListener l) {
             listeners.add(l);
         }
         
-        public void removeListener(LogSimulatorActionListener l) {
+        public synchronized void removeListener(LogSimulatorActionListener l) {
             listeners.remove(l);
         }
         
         // open file
-        public void openLogFile(String path) {
+        public synchronized void openLogFile(String path) {
             this.logSimulator.openLogFile(path);
             notifyFileOpenedChanged(path);
         }
         
         // frame modifiers
-        public void stepForward() {
+        public synchronized void stepForward() {
             this.logSimulator.stepForward();
             notifyFrameChanged(this.logSimulator.getCurrentFrame());
         }
-        public void stepBack() {
+        public synchronized void stepBack() {
             this.logSimulator.stepBack();
             notifyFrameChanged(this.logSimulator.getCurrentFrame());
         }
-        public void jumpToBegin() {
+        public synchronized void jumpToBegin() {
             this.logSimulator.jumpToBegin();
             notifyFrameChanged(this.logSimulator.getCurrentFrame());
         }
-        public void jumpToEnd() {
+        public synchronized void jumpToEnd() {
             this.logSimulator.jumpToEnd();
             notifyFrameChanged(this.logSimulator.getCurrentFrame());
         }
-        public void jumpTo(int position) {
+        public synchronized void jumpTo(int position) {
             this.logSimulator.jumpTo(position);
             notifyFrameChanged(this.logSimulator.getCurrentFrame());
         }
         
         // getters
-        public int getCurrentFrame() {
+        public synchronized int getCurrentFrame() {
             return logSimulator.getCurrentFrame();
         }
-        public int getMinFrame() {
+        public synchronized int getMinFrame() {
             return logSimulator.getMinFrame();
         }
-        public int getMaxFrame() {
+        public synchronized int getMaxFrame() {
             return logSimulator.getMaxFrame();
         }
     }
