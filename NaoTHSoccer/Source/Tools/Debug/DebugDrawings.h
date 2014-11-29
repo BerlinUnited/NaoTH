0ba9c6d8b9aaa1d1556f918059478b98ad10fe60
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2014-11-29 17:08:25 +0100

    outsourced the Color class

--- NaoTHSoccer/Source/Tools/Debug/DebugDrawings.h
+++ NaoTHSoccer/Source/Tools/Debug/DebugDrawings.h
@@ -33,56 +33,6 @@ public:
   void update();
   std::stringstream& out();
 
-  /**
-   * class Color encodes a color by 4 double values.
-   * The values represent red, green, blue and the alpha chanels.
-   * every value has to be between 0.0 and 1.0
-   */
-  class Color: public Vector_n<double,4>
-  {
-  public:
-    enum Chanel
-    {
-      red,
-      green, 
-      blue,
-      alpha,
-      numberOfChanel
-    };
-
-    // default color is black
-    Color();
-
-    Color(const Vector_n<double,4>& colorVector);
-
-    Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a = 255);
-
-    Color(double r, double g, double b, double a = 1.0);
-
-    Color(const char* color);
-
-    /**
-     * Generate a color using an index number.
-     *
-     * The generation is done by more or less random parameters which showed that
-     * they generate differing colors for the first indexes.
-     */
-    Color(unsigned int colorIndex);
-
-    unsigned char getRed() const;
-    unsigned char getGreen() const;
-    unsigned char getBlue() const;
-    unsigned char getAlpha() const;
-
-    // html style hex string
-    std::string toString() const;
-
-
-    static std::string charToHexString(unsigned char c);
-    static int hexCharToInt(char c);
-    static char intToHexChar(int n);
-  };//end class Color
-
 private:
 
   // reference to the currentelly used buffer
@@ -92,14 +42,6 @@ private:
   std::stringstream bufferOne;
   std::stringstream bufferTwo;
 };
-
-/**
- * Streaming operator that writes a Color to a stream.
- * @param stream The stream to write on.
- * @param image The Color object.
- * @return The stream.
- */ 
-std::ostream& operator<<(std::ostream& stream, const DebugDrawings::Color& color);
 
 
 #ifdef DEBUG
