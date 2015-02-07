c4d3190c1cfbc3034d3b984456f64cd8bcd9613e
Thomas Krause <thomaskrause@posteo.de>
2015-02-07 12:12:26 +0100

    add RC BehaviorAnalyzer dialog

---
+++ RobotControl/RobotControlGUI/src/de/naoth/rc/logmanager/LogFileEventManagerImpl.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.naoth.rc.logmanager;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+
+/**
+ *
+ * @author thomas
+ */
+@PluginImplementation
+public class LogFileEventManagerImpl implements LogFileEventManager {
+    private final List<LogFrameListener> listeners = new ArrayList<>();
+    private final BlackBoardImpl blackBoard = new BlackBoardImpl();
+    
+    @Override
+    public void addListener(LogFrameListener l) {
+        listeners.add(l);
+    }
+    @Override
+    public void removeListener(LogFrameListener l) {
+        listeners.remove(l);
+    }
+    
+    @Override
+    public void fireLogFrameEvent(Collection<LogDataFrame> c, int number) {
+        
+        if(c != null) {
+            for(LogDataFrame f: c) {
+                blackBoard.add(f);
+            }
+        }
+        
+        for(LogFrameListener l: listeners) {
+            l.newFrame(blackBoard, number);
+        }
+    }
+}
