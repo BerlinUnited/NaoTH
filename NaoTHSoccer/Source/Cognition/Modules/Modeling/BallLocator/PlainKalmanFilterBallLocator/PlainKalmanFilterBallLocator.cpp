4cfc709550e5117fdd9b7cd9f9505b299c5eaf94
Steffen Kaden <kaden@informatik.hu-berlin.de>
2015-03-01 19:33:56 +0100

    begin naive and plain kalman filter implementation

--- NaoTHSoccer/Source/Cognition/Modules/Modeling/BallLocator/PlainKalmanFilterBallLocator/PlainKalmanFilterBallLocator.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/Modeling/BallLocator/PlainKalmanFilterBallLocator/PlainKalmanFilterBallLocator.cpp
@@ -10,3 +10,17 @@ PlainKalmanFilterBallLocator::~PlainKalmanFilterBallLocator()
 
 }
 
+void PlainKalmanFilterBallLocator::execute()
+{
+    // prediction
+    x_pre = F * x + B * u + w;
+
+    P_pre = F * P * F.transpose() + Q;
+
+    // fusion/update
+    K = P_pre * H.transpose() * (H * P_pre * H.transpose() + R).inverse();
+
+    x_corr = x_pre + K * (z - H * x_pre );
+
+    P_corr = P_pre - K*H*P_pre;
+}
--- NaoTHSoccer/Source/Cognition/Modules/Modeling/BallLocator/PlainKalmanFilterBallLocator/PlainKalmanFilterBallLocator.h
+++ NaoTHSoccer/Source/Cognition/Modules/Modeling/BallLocator/PlainKalmanFilterBallLocator/PlainKalmanFilterBallLocator.h
@@ -1,12 +1,69 @@
 #ifndef PLAINKALMANFILTERBALLLOCATOR_H
 #define PLAINKALMANFILTERBALLLOCATOR_H
 
+#include <ModuleFramework/Module.h>
 
-class PlainKalmanFilterBallLocator
+#include <eigen3/Eigen/Eigen>
+
+//////////////////// BEGIN MODULE INTERFACE DECLARATION ////////////////////
+
+BEGIN_DECLARE_MODULE(PlainKalmanFilterBallLocator)
+// debug
+
+//  PROVIDE(DebugDrawings)
+//  PROVIDE(DebugRequest)
+//  PROVIDE(DebugModify)
+//  PROVIDE(DebugPlot)
+//  PROVIDE(DebugParameterList)
+
+//  REQUIRE(BodyState)
+//  REQUIRE(FrameInfo)
+//  REQUIRE(FieldInfo)
+//  REQUIRE(OdometryData)
+//  REQUIRE(MotionStatus)
+//  REQUIRE(KinematicChain)
+//  REQUIRE(SituationStatus)
+
+//  REQUIRE(BallPercept)
+//  REQUIRE(BallPerceptTop)
+
+//  PROVIDE(BallModel)
+END_DECLARE_MODULE(PlainKalmanFilterBallLocator)
+
+
+//////////////////// END MODULE INTERFACE DECLARATION //////////////////////
+
+class PlainKalmanFilterBallLocator : private PlainKalmanFilterBallLocatorBase
 {
+
+
 public:
     PlainKalmanFilterBallLocator();
-    ~PlainKalmanFilterBallLocator();
+    virtual ~PlainKalmanFilterBallLocator();
+
+    virtual void execute();
+
+
+private:
+    Eigen::Matrix4d F; // state transition matrix
+    Eigen::Matrix4d B; // control matrix
+    Eigen::Vector4d x; // state
+    Eigen::Vector4d u; // control vector
+    Eigen::Vector4d x_pre; // predicted state
+    Eigen::Vector4d x_corr; // corrected state
+    Eigen::Vector4d w; // noise
+
+    Eigen::Matrix4d Q; // covariance matrix of process noise
+    Eigen::Matrix4d P; // covariance matrix of current state
+    Eigen::Matrix4d P_pre; // covariance matrix of predicted state
+    Eigen::Matrix4d P_corr; // covariance matrix of corrected state
+
+    Eigen::Vector4d z; // measurement
+    Eigen::Matrix4d H; // state to measurement transformation matrix
+    Eigen::Matrix4d R; // covariance matrix of measurement noise
+
+    Eigen::Matrix4d K; // kalman gain
+
 };
 
 #endif // PLAINKALMANFILTERBALLLOCATOR_H
