bb2a16f77ab94fe579ee10d51b91de7d92cc1305
Steffen Kaden <kaden@informatik.hu-berlin.de>
2015-03-05 20:46:03 +0100

    continued implementation of kalman filter

--- NaoTHSoccer/Source/Cognition/Modules/Modeling/BallLocator/PlainKalmanFilterBallLocator/PlainKalmanFilterBallLocator.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/Modeling/BallLocator/PlainKalmanFilterBallLocator/PlainKalmanFilterBallLocator.cpp
@@ -1,8 +1,48 @@
 #include "PlainKalmanFilterBallLocator.h"
 
-PlainKalmanFilterBallLocator::PlainKalmanFilterBallLocator()
-{
+#include <cmath>
 
+PlainKalmanFilterBallLocator::PlainKalmanFilterBallLocator():
+     dt(0.03),
+     epsilon(10e-6)
+{
+    // state transition matrix
+    F << 1, dt, 0, 0,
+         0,  1, 0, 0,
+         0,  0, 1, dt,
+         0,  0, 0, 1;
+
+    // control matrix
+    B << 0,  0, 0,  0,
+         1, dt, 0,  0,
+         0,  0, 0,  0,
+         0,  0, 1, dt;
+
+    // covariance matrix of process noise (values taken from old kalman filter)
+    double q = 3;
+    Q << q, 0, 0, 0,
+         0, 0, 0, 0,
+         0, 0, q, 0,
+         0, 0, 0, 0;
+
+    // inital covariance matrix of current state (values taken from old kalman filter)
+    double p = 250000;
+    P << p, p, 0, 0,
+         p, p, 0, 0,
+         0, 0, p, p,
+         0, 0, p, p;
+
+    // state to measurement transformation matrix
+    H << 1, 0, 0, 0,
+         0, 0, 0, 0,
+         0, 0, 1, 0,
+         0, 0, 0, 0;
+
+    // covariance matrix of measurement noise (values taken from old kalman filter)
+    R << 100, 0,   0, 0,
+           0, 0,   0, 0,
+           0, 0, 100, 0,
+           0, 0,   0, 0;
 }
 
 PlainKalmanFilterBallLocator::~PlainKalmanFilterBallLocator()
@@ -12,15 +52,75 @@ PlainKalmanFilterBallLocator::~PlainKalmanFilterBallLocator()
 
 void PlainKalmanFilterBallLocator::execute()
 {
+    //////////////////////////////////
+    // Odometry-update (translation): move the model into the current local coordinte system
+    //////////////////////////////////
+
+//    Pose2D odometryDelta = lastRobotOdometry - getOdometryData();
+//    Vector2<double> h = Vector2<double>(Sx[0], Sy[0]); // translation of the model
+//    h = odometryDelta * h;
+//    Sx[0] = h.x; Sy[0] = h.y;
+//    How to handle speed / velocity -> should be rotate
+
+    // handle friction as an input control for acceleration
+    double deceleration = 4;
+    if(fabs(x(1)) <= epsilon) {
+        u(1) = 0;
+    } else {
+        u(1) = ((x(1) < 0) ? 1 : -1) * deceleration;
+    }
+
+    if(fabs(x(3)) <= epsilon) {
+        u(3) = 0;
+    } else {
+        u(3) = ((x(3) < 0) ? 1 : -1) * deceleration;
+    }
+
     // prediction
-    x_pre = F * x + B * u + w;
+    x_pre = F * x + B * u; // + w;
 
     P_pre = F * P * F.transpose() + Q;
 
-    // fusion/update
-    K = P_pre * H.transpose() * (H * P_pre * H.transpose() + R).inverse();
+    // measurement
+    newPercept = getBallPercept();
+    if(newPercept.ballWasSeen)
+    {
+        z << newPercept.bearingBasedOffsetOnField.x,
+             0,
+             newPercept.bearingBasedOffsetOnField.y,
+             0;
 
-    x_corr = x_pre + K * (z - H * x_pre );
+        // fusion/update
+        K = P_pre * H.transpose() * (H * P_pre * H.transpose() + R).inverse();
 
-    P_corr = P_pre - K*H*P_pre;
+        x_corr = x_pre + K * (z - H * x_pre );
+
+        P_corr = P_pre - K*H*P_pre;
+
+        x = x_corr;
+        P = P_corr;
+    }
+    else
+    {
+        x = x_pre;
+        P = P_pre;
+    }
+
+    // set ball model representation
+    getBallModel().position.x = x(0);
+    getBallModel().position.y = x(2);
+    getBallModel().speed.x = x(1);
+    getBallModel().speed.y = x(3);
+
+    // TODO: decide whether model is valid depending on P or time
+    // e.g. if (max, min, std)
+    if(getFrameInfo().getTimeSince(getBallModel().frameInfoWhenBallWasSeen.getTime()) > 10000.0) // 10s
+    {
+      //reset(newPercept);
+      getBallModel().valid = false; // model is not valid enymore after 10s
+    }
+    else
+    {
+     getBallModel().valid = true;
+    }
 }
--- NaoTHSoccer/Source/Cognition/Modules/Modeling/BallLocator/PlainKalmanFilterBallLocator/PlainKalmanFilterBallLocator.h
+++ NaoTHSoccer/Source/Cognition/Modules/Modeling/BallLocator/PlainKalmanFilterBallLocator/PlainKalmanFilterBallLocator.h
@@ -3,6 +3,15 @@
 
 #include <ModuleFramework/Module.h>
 
+// representations
+#include <Representations/Perception/BallPercept.h>
+#include "Representations/Modeling/BallModel.h"
+
+#include "Representations/Modeling/OdometryData.h"
+#include "Representations/Modeling/KinematicChain.h"
+#include "Representations/Infrastructure/FrameInfo.h"
+
+
 #include <eigen3/Eigen/Eigen>
 
 //////////////////// BEGIN MODULE INTERFACE DECLARATION ////////////////////
@@ -17,17 +26,17 @@ BEGIN_DECLARE_MODULE(PlainKalmanFilterBallLocator)
 //  PROVIDE(DebugParameterList)
 
 //  REQUIRE(BodyState)
-//  REQUIRE(FrameInfo)
+  REQUIRE(FrameInfo)
 //  REQUIRE(FieldInfo)
-//  REQUIRE(OdometryData)
+  REQUIRE(OdometryData)
 //  REQUIRE(MotionStatus)
-//  REQUIRE(KinematicChain)
+  REQUIRE(KinematicChain)
 //  REQUIRE(SituationStatus)
 
-//  REQUIRE(BallPercept)
-//  REQUIRE(BallPerceptTop)
+  REQUIRE(BallPercept)
+  REQUIRE(BallPerceptTop)
 
-//  PROVIDE(BallModel)
+  PROVIDE(BallModel)
 END_DECLARE_MODULE(PlainKalmanFilterBallLocator)
 
 
@@ -43,19 +52,26 @@ public:
 
     virtual void execute();
 
+// from other kalman filter ball locator
+private:
+    OdometryData lastRobotOdometry; //just for prebuffered percepts
+    //OdometryData lastRobotOdometryAll;
+
+    BallPercept newPercept;
+
 
 private:
     Eigen::Matrix4d F; // state transition matrix
     Eigen::Matrix4d B; // control matrix
-    Eigen::Vector4d x; // state
-    Eigen::Vector4d u; // control vector
-    Eigen::Vector4d x_pre; // predicted state
+    Eigen::Vector4d x; // state x_location, x_velocity, y_location, y_velocity
+    Eigen::Vector4d u; // control vector x_velocity, x_acceleration, y_velocity, y_acceleration
+    Eigen::Vector4d x_pre;  // predicted state
     Eigen::Vector4d x_corr; // corrected state
     Eigen::Vector4d w; // noise
 
     Eigen::Matrix4d Q; // covariance matrix of process noise
     Eigen::Matrix4d P; // covariance matrix of current state
-    Eigen::Matrix4d P_pre; // covariance matrix of predicted state
+    Eigen::Matrix4d P_pre;  // covariance matrix of predicted state
     Eigen::Matrix4d P_corr; // covariance matrix of corrected state
 
     Eigen::Vector4d z; // measurement
@@ -64,6 +80,19 @@ private:
 
     Eigen::Matrix4d K; // kalman gain
 
+    const double dt; // 30 ms
+    const double epsilon; // 10e-6
+
+    const BallPercept& getBallPercept() const
+    {
+      if(!PlainKalmanFilterBallLocatorBase::getBallPercept().ballWasSeen &&
+          PlainKalmanFilterBallLocatorBase::getBallPerceptTop().ballWasSeen)
+      {
+        return PlainKalmanFilterBallLocatorBase::getBallPerceptTop();
+      } else {
+        return PlainKalmanFilterBallLocatorBase::getBallPercept();
+      }
+    }
 };
 
 #endif // PLAINKALMANFILTERBALLLOCATOR_H
