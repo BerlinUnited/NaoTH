66ce1275317f660b71eabe8c6b6309b5b69dc9fe
Benjamin Schlotter <benji@schlotterfam.de>
2015-07-06 16:42:45 +0200

    Added Teamball Locator Module from old repo

--- NaoTHSoccer/Source/Cognition/Cognition.cpp
+++ NaoTHSoccer/Source/Cognition/Cognition.cpp
@@ -71,7 +71,7 @@
 #include "Modules/Modeling/GoalModel/DummyActiveGoalLocator/DummyActiveGoalLocator.h"
 #include "Modules/Modeling/GoalModel/WholeGoalLocator/WholeGoalLocator.h"
 #include "Modules/Modeling/BallLocator/KalmanFilterBallLocator/KalmanFilterBallLocator.h"
-
+#include "Modules/Modeling/BallLocator/TeamBallLocator/TeamBallLocator.h"
 // behavior
 #include "Modules/Behavior/BasicTestBehavior/BasicTestBehavior.h"
 #include "Modules/Behavior/XABSLBehaviorControl/XABSLBehaviorControl.h"
@@ -159,7 +159,7 @@ void Cognition::init(naoth::ProcessInterface& platformInterface, const naoth::Pl
   REGISTER_MODULE(WholeGoalLocator);
   REGISTER_MODULE(DummyActiveGoalLocator);
   REGISTER_MODULE(KalmanFilterBallLocator);
-  
+  REGISTER_MODULE(TeamBallLocator);
   // behavior
   REGISTER_MODULE(BasicTestBehavior);
   REGISTER_MODULE(XABSLBehaviorControl);
