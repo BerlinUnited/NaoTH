45064ba2d94f562a4afff9dae1c37ddcbed4e3ca
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2016-03-27 23:01:19 +0200

    make the labeling tool really cool

--- Utils/py/ball_detector/ball_patch_label.py
+++ Utils/py/ball_detector/ball_patch_label.py
@@ -5,11 +5,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as ptc
 import matplotlib
+import json
 
 import patchReader
 
-lls = []
-idx = 0
+
+# todo make class
+patchdata = None
+labels = None
+window_idx = 0
+
+
+fig = plt.figure()
+image_canvas = None
+
 
 show_size = (10, 10) # width, height
 patch_size = (12, 12) # width, height
@@ -19,14 +28,22 @@ fullscreen = False
 def patch_pos(x,y):
   return (x*(patch_size[0]+1)-1,y*(patch_size[1]+1)-1)
 
-  
-selected = []
 patches = []
+selected = []
 for i in range(0,show_size[0]*show_size[1]):
   y = i // show_size[0]
   x = i % show_size[0]
   patches.append(ptc.Rectangle( patch_pos(x,y), width=patch_size[0]+1, height=patch_size[1]+1, alpha=0.3))
   selected.append(False)
+  
+def setMarker(i, v):
+  if v == 0 and selected[i]:
+    patches[i].remove()
+    selected[i] = False
+  elif v == 1 and not selected[i]:
+    ax = plt.gca()
+    ax.add_patch(patches[i])
+    selected[i] = True
   
   
 def on_click(event):
@@ -35,26 +52,31 @@ def on_click(event):
     y = int(event.ydata+0.5) / 13
     x = int(event.xdata+0.5) / 13
     i = show_size[0]*y + x
-    ax = plt.gca()
-    rect = patches[i]
     
-    if not selected[i]:
-      ax.add_patch(rect)
-      selected[i] = True
+    if labels[window_idx+i] == 0:
+      labels[window_idx+i] = 1
     else:
-      rect.remove()
-      selected[i] = False
+      labels[window_idx+i] = 0
       
+    setMarker(i, labels[window_idx+i])
     plt.draw()
+    
  
 def key_pressed(event):
   
   if event.key == 'enter':
-    plt.close()
+    for i in range(show_size[0]*show_size[1]):
+      setMarker(i, 0)
+      
+    save_labels(label_file)
+    global window_idx
+    window_idx += show_size[0]*show_size[1]
+    showPatches()
+    
   elif event.key == 'escape' or event.key == 'q':
     exit(0)
 
-def load_from_csv(file):
+def load_patches_from_csv(file):
   f = open(file, 'r')
   patches = []
   
@@ -63,45 +85,76 @@ def load_from_csv(file):
     a = np.array(s).astype(float)
     patches.append(a)
     
-if __name__ == "__main__":
-  #patches = load_from_csv("./patches.txt")
-  patchdata = patchReader.readAllPatchesFromLog('./ball-move-around-patches.log')
+def save_labels(file):
+  l = []
+  for i, val in enumerate(labels):
+    if val == 1:
+      l.append(i)
+    
+  with open(file, 'w') as outfile:
+        json.dump({"ball":l}, outfile)
+    
+    
+def load_labels(file):
+  tmp_labels = np.zeros((len(patchdata),))
   
+  if os.path.isfile(file):
+    with open(file, 'r') as data_file:
+      ball_labels = json.load(data_file)
+    tmp_labels[ball_labels["ball"]] = 1
+  return tmp_labels
+    
+    
+def showPatches():
   image = np.zeros(((patch_size[1]+1)*show_size[1], (patch_size[0]+1)*show_size[0]))
-
-  i = 0
-  for p in patchdata:
-    a = np.array(p).astype(float)
+    
+  for i in range(show_size[0]*show_size[1]):
+    if i+1 > len(patchdata):
+      break
+      
+    a = np.array(patchdata[window_idx+i]).astype(float)
     a = np.transpose(np.reshape(a, patch_size))
 
-    if i == show_size[0]*show_size[1]:
-      plt.connect('button_press_event', on_click)
-      plt.connect("key_press_event", key_pressed)
-      plt.imshow(image, cmap=plt.cm.gray, interpolation='nearest')
-      plt.xticks(())
-      plt.yticks(())
-      
-      if matplotlib.get_backend() == 'Qt4Agg':
-        f_manager = plt.get_current_fig_manager()
-        f_manager.window.move(0, 0)
-        if fullscreen:
-          f_manager.window.showMaximized()
-      
-      plt.show()
-      
-      for j in range(0,len(selected)):
-        if selected[j]:
-          lls.append(idx - show_size[0]*show_size[1] + j)
-        selected[j] = False
-        
-      print lls
-      i = 0
-    
     y = i // show_size[0]
     x = i % show_size[0]
     image[y*13:y*13+12,x*13:x*13+12] = a
-
-    i += 1
-    idx += 1
+    setMarker(i, labels[window_idx+i])
+    
+  global image_canvas
+  if image_canvas is None:
+    image_canvas = fig.gca().imshow(image, cmap=plt.cm.gray, interpolation='nearest')
+    fig.show()
+  else:
+    image_canvas.set_data(image)
+    fig.canvas.draw()
+    
+    
+if __name__ == "__main__":
+  #patches = load_patches_from_csv("./patches.txt")
+  
+  #file = 'ball-move-around-patches'
+  file = 'patches-approach-ball'
+  
+  patchdata = patchReader.readAllPatchesFromLog('./'+file+'.log')
+  label_file = './'+file+'.json'
+  labels = load_labels(label_file)
+  
+  
+  window_idx = 0
+  showPatches()
+  plt.connect('button_press_event', on_click)
+  plt.connect("key_press_event", key_pressed)
+  
+  #fig.gca().xticks(())
+  #fig.gca().yticks(())
+      
+  if matplotlib.get_backend() == 'Qt4Agg':
+    f_manager = plt.get_current_fig_manager()
+    f_manager.window.move(0, 0)
+    if fullscreen:
+      f_manager.window.showMaximized()
+  
+  plt.show()
+  
 
     
