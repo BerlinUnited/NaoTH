83c91ffc73468ab854ad1c97e977fb70ad3995c3
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2016-03-27 13:48:47 +0200

    print error if protoc was not successfull

--- Framework/BuildTools/protoc.lua
+++ Framework/BuildTools/protoc.lua
@@ -164,6 +164,8 @@ function invokeprotoc(inputFiles, cppOut, javaOut, ipaths)
           -- touch shadow file in order to remember this build date
           touchShadow(inputFiles[i], time)
       end -- end for each file to compile
+     else
+      print ("ERROR: protoc not sucessful")
      end
     end -- end if compile
 end
