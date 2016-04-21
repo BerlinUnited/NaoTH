2d6ef51caf9b429d94fc68ead70160b5fd4f96fe
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2016-04-21 14:59:01 +0200

    set default sound level before playing any sound

--- Utils/DeployStick/startBrainwashing.sh
+++ Utils/DeployStick/startBrainwashing.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# set volume to 88%
+sudo -u nao pactl set-sink-mute 0 false
+sudo -u nao pactl set-sink-volume 0 88%
+
+# play initial sound
 sudo -u nao /usr/bin/paplay /home/nao/naoqi/Media/usb_start.wav
 
 # stop naoqi and/or naoth if they are to replace
