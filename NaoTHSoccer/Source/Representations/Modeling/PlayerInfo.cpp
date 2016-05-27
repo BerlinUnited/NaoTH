8d42bc3f0b6d6a45c5df750a2cf1569fbadbbbd0
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2016-05-27 17:45:04 +0200

    - restructure PlayerInfo to reflect the internal game state of the robot

--- NaoTHSoccer/Source/Representations/Modeling/PlayerInfo.cpp
+++ NaoTHSoccer/Source/Representations/Modeling/PlayerInfo.cpp
@@ -1,8 +1,7 @@
 // 
 // File:   PlayerInfo.cpp
 // Author: thomas
-//
-// Created on 12. march 2008, 13:41
+// Author: Heinrich Mellmann
 //
 
 #include "PlayerInfo.h"
@@ -12,8 +11,11 @@ using namespace std;
 
 PlayerInfo::PlayerInfo() 
 :
-  timeSincePlayModeChanged(0),
-  timeSinceGameStateChanged(0),
+  playerNumber(0),
+  teamNumber(0),
+  teamColor(naoth::GameData::unknown_team_color),
+  kickoff(false),
+  robotState(initial),
   isPlayingStriker(false)
 {
 }
@@ -22,9 +24,32 @@ PlayerInfo::~PlayerInfo()
 {
 }
 
+
+#define RETURN_VALUE_TO_STR(v) case v: return #v
+std::string PlayerInfo::toString(RobotState value)
+{
+  switch (value)
+  {
+    RETURN_VALUE_TO_STR(initial);
+    RETURN_VALUE_TO_STR(ready);
+    RETURN_VALUE_TO_STR(set);
+    RETURN_VALUE_TO_STR(playing);
+    RETURN_VALUE_TO_STR(finished);
+    RETURN_VALUE_TO_STR(penalized);
+  }
+  
+  ASSERT(false);
+  return "invalide RobotState";
+}
+
 void PlayerInfo::print(ostream& stream) const
 {
-  stream << gameData;
+  stream << "playerNumber = " << playerNumber << endl;
+  stream << "teamNumber = " << teamNumber << endl;
+  stream << "teamName = " << teamName << endl;
+  stream << "teamColor = " << naoth::GameData::toString(teamColor) << endl;
+  stream << "kickoff = " << (kickoff?"yes":"no") << endl;
+  stream << "robotState = " << toString(robotState) << endl;
   stream << "isPlayingStriker = " << (isPlayingStriker?"yes":"no") << endl;
-}//end print
+}
 
