6e8af1685ccda023446fb1e8d05bd55dc4d865af
Thomas Krause <thomaskrause@posteo.de>
2016-06-18 19:31:12 +0200

    Deserialize the ball patches.

--- Evaluator/Source/BallDetectorEvaluator.cpp
+++ Evaluator/Source/BallDetectorEvaluator.cpp
@@ -1,5 +1,9 @@
 #include "BallDetectorEvaluator.h"
 
+#include <strstream>
+
+#include <Representations/Perception/BallCandidates.h>
+
 BallDetectorEvaluator::BallDetectorEvaluator(const std::string &dir)
   : file(dir), logFileScanner(dir)
 {
@@ -15,13 +19,31 @@ void BallDetectorEvaluator::execute()
 {
   if(ballDetector != NULL)
   {
+    // read in each frame
     for(LogFileScanner::FrameIterator it = logFileScanner.begin(); it != logFileScanner.end(); it++)
     {
 
       LogFileScanner::Frame frame;
-
-      std::cout << "Testing frame " << *it << std::endl;
       logFileScanner.readFrame(*it, frame);
+
+      // deserialize the ball candidates (bottom an top camera)
+      BallCandidates ballCandidatesBottom;
+      BallCandidates ballCandidatesTop;
+
+      auto frameBallCandidate = frame.find("BallCandidate");
+      auto frameBallCandidateTop = frame.find("BallCandidateTop");
+      if(frameBallCandidate!= frame.end())
+      {
+        std::istrstream stream(frameBallCandidate->second.data.data(), frameBallCandidate->second.data.size());
+        naoth::Serializer<BallCandidates>::deserialize(stream, ballCandidatesBottom);
+      }
+      if(frameBallCandidateTop != frame.end())
+      {
+        std::istrstream stream(frameBallCandidateTop->second.data.data(), frameBallCandidateTop->second.data.size());
+        naoth::Serializer<BallCandidatesTop>::deserialize(stream, ballCandidatesTop);
+      }
+
+//      std::cout << "Test frame " << *it << std::endl;
 
       ballDetector->execute();
     }
