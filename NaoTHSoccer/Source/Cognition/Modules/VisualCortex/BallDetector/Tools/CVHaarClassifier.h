84002552a4f1e7f86fb447113b0e5f12bc33b10b
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2016-06-20 18:33:47 +0200

    better interface for the haar classifier

--- NaoTHSoccer/Source/Cognition/Modules/VisualCortex/BallDetector/Tools/CVHaarClassifier.h
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/BallDetector/Tools/CVHaarClassifier.h
@@ -21,16 +21,17 @@ public:
   }
 
 
-  bool classify(BallCandidates::Patch& p, naoth::CameraInfo::CameraID cameraId)
+  int classify(BallCandidates::Patch& p)
   {
-    
+    ASSERT(p.data.size() == 24*24);
     // TODO: magic numbers
-    cv::Mat wrappedImg(12, 12, CV_8U, (void*) p.data.data());
+    cv::Mat wrappedImg(24, 24, CV_8U, (void*) p.data.data());
     //cv::transpose(wrappedImg, wrappedImg);
     std::vector<cv::Rect> out;
-    cascadeClasifier.detectMultiScale( wrappedImg, out, 1.01, 0, 0|CV_HAAR_SCALE_IMAGE, cv::Size(12,12) );
+    // TODO: more magic numbers
+    cascadeClasifier.detectMultiScale( wrappedImg, out, 1.1, 0, 0|CV_HAAR_SCALE_IMAGE, cv::Size(10,10), cv::Size(12,12) );
 
-    return !out.empty();
+    return out.size();
   }
 };
 
