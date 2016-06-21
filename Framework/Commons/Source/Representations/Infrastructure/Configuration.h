7f91c9400c4fc4fc06a109f1f3e7bec696b801a3
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2016-06-21 12:44:07 +0200

    now config are seected according to the robot name

--- Framework/Commons/Source/Representations/Infrastructure/Configuration.h
+++ Framework/Commons/Source/Representations/Infrastructure/Configuration.h
@@ -28,7 +28,8 @@ public:
                    const std::string &platform,
                    const std::string& scheme,
                    const std::string& bodyID,
-                   const std::string& headID);
+                   const std::string& headID,
+                   const std::string& robotName);
 
   // note this function only save private keys
   void save();
