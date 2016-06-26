ec9a0119365d4614ab122a0adca62a818af4017d
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2016-06-26 11:11:14 +0200

    moved the method out of the header

--- NaoTHSoccer/Source/Cognition/Modules/VisualCortex/IntegralImageProvider.h
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/IntegralImageProvider.h
@@ -81,60 +81,7 @@ private:
 private:
 
   template<class ImageType>
-  void makeGameColorIntegralBild(ImageType& integralImage) const
-  {
-    ASSERT(ImageType::MAX_COLOR >= 3);
-    const int32_t FACTOR = integralImage.FACTOR;
-    const uint32_t MAX_COLOR = ImageType::MAX_COLOR;
-
-    const uint imgWidth = getImage().width()/FACTOR;
-	  const uint imgHeight = getImage().height()/FACTOR;
-	  integralImage.setDimension(imgWidth, imgHeight);
-
-    uint32_t* dataPtr = integralImage.getDataPointer();
-
-    uint32_t* prevRowPtr = dataPtr;
-	  uint32_t* curRowPtr  = dataPtr + imgWidth*MAX_COLOR;
-
-    Pixel pixel;
-    Vector2i p;
-  
-    for(uint16_t y = 1; y < imgHeight; ++y) 
-    {
-      uint32_t akk[MAX_COLOR] = { 0 };
-
-      prevRowPtr += MAX_COLOR;
-      curRowPtr  += MAX_COLOR;
-
-      for(uint16_t x = 1; x < imgWidth; ++x) 
-      {
-  #ifndef WIN32
-        // preload the cache, so the next row(s) are loaded while we are busy
-			  // still working with them (https://gcc.gnu.org/onlinedocs/gcc-4.8.2/gcc/Other-Builtins.html)
-			  __builtin_prefetch(&prevRowPtr[64], 0);
-  #endif
-        p.x = x*FACTOR;
-        p.y = y*FACTOR;
-        getImage().get_direct(p.x, p.y, pixel);
-      
-        ColorClasses::Color c = getColorTable64().getColorClass(pixel.y,pixel.u,pixel.v);
-        if(c == ColorClasses::white) { 
-          ++akk[0];
-        } else if (c == ColorClasses::green || getBodyContour().isOccupied(p)){
-          ++akk[1];
-        } else if(c == ColorClasses::black) {
-          ++akk[2];
-        }
-
-        for(uint32_t i = 0; i < MAX_COLOR; ++i) {
-          curRowPtr[i] = akk[i] + prevRowPtr[i];
-        }
-
-        curRowPtr  += MAX_COLOR;
-        prevRowPtr += MAX_COLOR;
-      }
-    }
-  }
+  void makeGameColorIntegralBild(ImageType& integralImage) const;
 
 private:     
   
@@ -145,5 +92,62 @@ private:
   DOUBLE_CAM_REQUIRE(IntegralImageProvider, FieldColorPercept);
 
 };//end class IntegralImageProvider
+
+
+template<class ImageType>
+void IntegralImageProvider::makeGameColorIntegralBild(ImageType& integralImage) const
+{
+  ASSERT(ImageType::MAX_COLOR >= 3);
+  const int32_t FACTOR = integralImage.FACTOR;
+  const uint32_t MAX_COLOR = ImageType::MAX_COLOR;
+
+  const uint imgWidth = getImage().width()/FACTOR;
+	const uint imgHeight = getImage().height()/FACTOR;
+	integralImage.setDimension(imgWidth, imgHeight);
+
+  uint32_t* dataPtr = integralImage.getDataPointer();
+
+  uint32_t* prevRowPtr = dataPtr;
+	uint32_t* curRowPtr  = dataPtr + imgWidth*MAX_COLOR;
+
+  Pixel pixel;
+  Vector2i p;
+  
+  for(uint16_t y = 1; y < imgHeight; ++y) 
+  {
+    uint32_t akk[MAX_COLOR] = { 0 };
+
+    prevRowPtr += MAX_COLOR;
+    curRowPtr  += MAX_COLOR;
+
+    for(uint16_t x = 1; x < imgWidth; ++x) 
+    {
+#ifndef WIN32
+      // preload the cache, so the next row(s) are loaded while we are busy
+			// still working with them (https://gcc.gnu.org/onlinedocs/gcc-4.8.2/gcc/Other-Builtins.html)
+			__builtin_prefetch(&prevRowPtr[64], 0);
+#endif
+      p.x = x*FACTOR;
+      p.y = y*FACTOR;
+      getImage().get_direct(p.x, p.y, pixel);
+      
+      ColorClasses::Color c = getColorTable64().getColorClass(pixel.y,pixel.u,pixel.v);
+      if(c == ColorClasses::white) { 
+        ++akk[0];
+      } else if (c == ColorClasses::green || getBodyContour().isOccupied(p)){
+        ++akk[1];
+      } else if(c == ColorClasses::black) {
+        ++akk[2];
+      }
+
+      for(uint32_t i = 0; i < MAX_COLOR; ++i) {
+        curRowPtr[i] = akk[i] + prevRowPtr[i];
+      }
+
+      curRowPtr  += MAX_COLOR;
+      prevRowPtr += MAX_COLOR;
+    }
+  }
+}
 
 #endif // _IntegralImageProvider_H_
