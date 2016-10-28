a27fbbfdebc1a8db955a8769aac6a426cb2506e3
Steffen Kaden <kaden@informatik.hu-berlin.de>
2016-10-28 14:52:59 +0200

    replace "Eigen/Eigen" includes by a "eigen-include-header" which also applies required pragmas

---
+++ Framework/Commons/Source/Tools/naoth_eigen.h
@@ -0,0 +1,11 @@
+#ifndef NAOTH_EIGEN_H
+#define NAOTH_EIGEN_H
+
+#pragma GCC diagnostic ignored "-Wmisleading-indentation"
+
+#include <Eigen/Eigen>
+//#include <Eigen/Eigenvalues>
+
+#pragma GCC diagnostic pop
+
+#endif // NAOTH_EIGEN_H
