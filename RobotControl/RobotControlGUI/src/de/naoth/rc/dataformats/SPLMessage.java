e163d04d9d256688cd336dd38e7544a4748f715b
Philipp Strobel <strobel@informatik.hu-berlin.de>
2016-11-26 11:37:31 +0100

    added drawing to RC for the expected ball position

--- RobotControl/RobotControlGUI/src/de/naoth/rc/dataformats/SPLMessage.java
+++ RobotControl/RobotControlGUI/src/de/naoth/rc/dataformats/SPLMessage.java
@@ -3,17 +3,18 @@
 package de.naoth.rc.dataformats;
 
 import com.google.protobuf.InvalidProtocolBufferException;
+import de.naoth.rc.drawings.Arrow;
 import de.naoth.rc.drawings.Circle;
 import de.naoth.rc.drawings.DrawingCollection;
 import de.naoth.rc.drawings.FillOval;
 import de.naoth.rc.drawings.Line;
 import de.naoth.rc.drawings.Pen;
 import de.naoth.rc.drawings.Robot;
-import de.naoth.rc.drawings.Rotation;
 import de.naoth.rc.drawings.Text;
 import de.naoth.rc.math.Pose2D;
 import de.naoth.rc.math.Vector2D;
 import de.naoth.rc.messages.Representations;
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.nio.ByteBuffer;
 
@@ -233,6 +234,14 @@ public class SPLMessage {
                     (int) globalBall.x, (int) globalBall.y);
                 drawings.add(ballLine);
             }
+        }
+        // if it is our striker ...
+        if(user != null && intention == 3 && shootingTo_x != 0 && shootingTo_y != 0)
+        {
+            // ... draw the excpected ball position
+            drawings.add(new Pen(Color.gray, new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0)));
+            drawings.add(new Circle((int) shootingTo_x, (int) shootingTo_y, 65));
+            drawings.add(new Arrow((int) robotPose.translation.x, (int) robotPose.translation.y, (int) shootingTo_x, (int) shootingTo_y));
         }
     }
 
