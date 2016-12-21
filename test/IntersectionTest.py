87b9c3be18988961510eefd33dfc33103c0b172c
Benjamin Schlotter <benji@schlotterfam.de>
2016-12-21 14:09:47 +0100

    moved stuff

--- test/IntersectionTest.py
+++ test/IntersectionTest.py
@@ -1,25 +1,19 @@
 import math2d as m2d
 
-'''
+
 #Bad: checks intersection of lines not linesegments
 def line_intersection(line1, line2):
-    xdiff = (line1[0].x - line1[1].x, line2[0].x - line2[1].x)
-    ydiff = (line1[0].y - line1[1].y, line2[0].y - line2[1].y)
-
-    #xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
-    #ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
 
     def det(a, b):
         return a[0] * b[1] - a[1] * b[0]
-
-    def detv2(a,b):#takes vectors
-      return a.x * b.y - a.y * b.x
 
     div = det(xdiff, ydiff)
     if div == 0:
        raise Exception('lines do not intersect')
 
-    d = (detv2(line1[0],line1[1]), detv2(line2[0],line2[1]))
+    d = (det(line1[0],line1[1]), det(line2[0],line2[1]))
     x = det(d, xdiff) / div
     y = det(d, ydiff) / div
     return x, y
@@ -27,11 +21,15 @@ def line_intersection(line1, line2):
 test1 = m2d.Vector2(0,0)
 test2 = m2d.Vector2(10,10)
 test3 = m2d.Vector2(-10,10)
-test4 = m2d.Vector2(20,23)
+test4 = m2d.Vector2(10,10)
 
-#line_intersection(((0,0),(10,10)), ((-10,10),(10,-10)))
-print line_intersection((test1,test2), (test3,test4))
-'''
+lineTest1 = ((test1.x,test1.y),(test2.x,test2.y))
+lineTest2 = ((test3.x,test3.y),(test4.x,test4.y))
+lineTest3 = ((test3.x,test3.y),(test4.x,test4.y))
+
+print line_intersection(lineTest1, lineTest2)
+
+
 
 #Also bad: since parallel line make some problems
 def ccw(A,B,C):
@@ -45,4 +43,4 @@ test2 = m2d.Vector2(10,10)
 test3 = m2d.Vector2(-10,10)
 test4 = m2d.Vector2(10,-10)
 
-print intersect(test1,test2,test3,test4)
+#print intersect(test1,test2,test3,test4)
