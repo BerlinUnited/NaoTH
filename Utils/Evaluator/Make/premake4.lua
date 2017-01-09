d5d6de7d3c3436fb15f9d7549fe253663495b0b2
Benjamin Schlotter <benji@schlotterfam.de>
2017-01-09 18:35:11 +0100

    updated premake4

--- Utils/Evaluator/Make/premake4.lua
+++ Utils/Evaluator/Make/premake4.lua
@@ -27,20 +27,60 @@ solution "EvaluatorSolution"
   -- global include path for all projects and configurations
   includedirs (PATH["includes"])
 
-  if(_ACTION == "gmake") then
-      -- "position-independent code" needed to compile shared libraries.
-      -- In our case it's only the NaoSMAL. So, we probably don't need this one.
-      -- Premake4 automatically includes -fPIC if a project is declared as a SharedLib.
-      -- http://www.akkadia.org/drepper/dsohowto.pdf
-      --buildoptions {"-fPIC"}
-      -- may be needed for newer glib2 versions, remove if not needed
-      buildoptions {"-Wno-deprecated-declarations"}
-      buildoptions {"-Wno-deprecated"}
-      buildoptions {"-std=c++11"}
-      flags { "ExtraWarnings" }
-     -- links {"pthread"}
+  
+  -- additional defines for visual studio
+  configuration {"windows", "vs*"}
+    defines {"WIN32", "NOMINMAX", "EIGEN_DONT_ALIGN"}
+    buildoptions {"/wd4351"} -- disable warning: "...new behavior: elements of array..."
+    buildoptions {"/wd4996"} -- disable warning: "...deprecated..."
+    buildoptions {"/wd4290"} -- exception specification ignored (typed stecifications are ignored)
+    links {"ws2_32"}
+    debugdir "$(SolutionDir).."
+  
+  
+  configuration {"linux or windows", "Native", "gmake"}
+    -- "position-independent code" needed to compile shared libraries.
+    -- In our case it's only the NaoSMAL. So, we probably don't need this one.
+    -- Premake4 automatically includes -fPIC if a project is declared as a SharedLib.
+    -- http://www.akkadia.org/drepper/dsohowto.pdf
+    buildoptions {"-fPIC"}
     
-      -- Why? OpenCV is always dynamically linked and we can only garantuee that there is one version in Extern (Thomas)
-     linkoptions {"-Wl,-rpath \"" .. path.getabsolute(EXTERN_PATH .. "/lib/") .. "\""}
+    -- may be needed for newer glib2 versions, remove if not needed
+    buildoptions {"-Wno-deprecated-declarations"}
+    buildoptions {"-Wno-deprecated"}
+    buildoptions {"-std=c++11"}
+    --flags { "ExtraWarnings" }
+    links {"pthread"}
+  
+    if _OPTIONS["Wno-conversion"] == nil then
+      buildoptions {"-Wconversion"}
     end
+
+    if _OPTIONS["Wno-misleading-indentation"] ~= nil then
+      buildoptions {"-Wno-misleading-indentation"}
+    end
+
+    if _OPTIONS["Wno-ignored-attributes"] ~= nil then
+      buildoptions {"-Wno-ignored-attributes"}
+    end
+  
+    -- Why? OpenCV is always dynamically linked and we can only garantuee that there is one version in Extern (Thomas)
+    linkoptions {"-Wl,-rpath \"" .. path.getabsolute(EXTERN_PATH .. "/lib/") .. "\""}
+  
+  
+  configuration {"macosx"}
+    defines { "BOOST_SIGNALS_NO_DEPRECATION_WARNING" }
+    buildoptions {"-std=c++11"}
+    -- disable some warnings
+    buildoptions {"-Wno-deprecated-declarations"}
+    buildoptions {"-Wno-deprecated-register"}
+    buildoptions {"-Wno-logical-op-parentheses"}
+    -- use clang on macOS
+    -- NOTE: configuration doesn't affect these settings, they NEED to be in a if
+    if os.is("macosx") then
+      premake.gcc.cc = 'clang'
+      premake.gcc.cxx = 'clang++'
+    end
+  
+  
   dofile "Evaluator.lua"
