5c99b6a97b1d2f550dc280ad3d9f2d06e67bc72b
Yigit Akcay <yigit.akcay@icloud.com>
2017-01-11 18:11:59 +0100

    Added deserialization for GroundContactModel

--- NaoTHSoccer/Source/Representations/Modeling/GroundContactModel.cpp
+++ NaoTHSoccer/Source/Representations/Modeling/GroundContactModel.cpp
@@ -17,5 +17,27 @@ void Serializer<GroundContactModel>::serialize(const GroundContactModel& represe
 
   p.set_leftgroundcontact(representation.leftGroundContact);
   p.set_rightgroundcontact(representation.rightGroundContact);
-  p.set_leftgroundcontactaverage(representation.supportFoot);
-}
+  p.set_supportfoot(representation.supportFoot);
+} // end serialize
+
+void Serializer<GroundContactModel>::deserialize(std::istream& stream,GroundContactModel& representation)
+{
+  naothmessages::GroundContactModel p;
+  google::protobuf::io::IstreamInputStream buf(&stream);
+  p.ParseFromZeroCopyStream(&buf);
+
+  if (p.has_leftgroundcontact())
+  {
+    representation.leftGroundContact = p.leftgroundcontact();
+  }
+
+  if (p.has_rightgroundcontact())
+  {
+    representation.rightGroundContact = p.rightgroundcontact();
+  }
+
+  if (p.has_supportfoot())
+  {
+    representation.supportFoot = p.supportfoot();
+  }
+} // end deserialize
