d90b3cb8ba69f84675de7a5813975a1f516a0221
Yigit Akcay <yigit.akcay@icloud.com>
2017-01-19 17:01:01 +0100

    XABSL go_to_ball added.

--- NaoTHSoccer/Source/Cognition/Modules/Behavior/PathPlanner/PathPlanner.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/Behavior/PathPlanner/PathPlanner.cpp
@@ -7,15 +7,28 @@
  */
 
 #include "PathPlanner.h"
+//#include <iostream>
 
 PathPlanner::PathPlanner() 
 :
   isWalking(false)
 {
-  // walk
+  // Stand
+  DEBUG_REQUEST_REGISTER("PathPlanner:motion:stand",
+                         "Stand still", true);
+
+  // Walk
   DEBUG_REQUEST_REGISTER("PathPlanner:motion:walk_forward",
                          "Walk forward", false);
-
+  // Go to ball (XABSL: go_to_ball)
+  DEBUG_REQUEST_REGISTER("PathPlanner:motion:go_to_ball",
+                         "Go to the ball", false);
+  // Search for the ball
+  DEBUG_REQUEST_REGISTER("PathPlanner:head:Search",
+                         "Search for the ball", false);
+  // Look at the ball
+  DEBUG_REQUEST_REGISTER("PathPlanner:head:LookAtMultiBallPercept",
+                "Look at the ball", false);
 }
 
 void PathPlanner::execute() 
@@ -27,12 +40,60 @@ void PathPlanner::execute()
   getMotionRequest().walkRequest.target = Pose2D();
   getMotionRequest().forced = false;
 
-  // walk forward
-  DEBUG_REQUEST("PathPlanner:motion:walk_forward",
-
-                getMotionRequest().id = motion::walk;
-                getMotionRequest().walkRequest.target.translation.x = 500;
-
+  // Stand still
+  DEBUG_REQUEST("PathPlanner:motion:stand",
+                getMotionRequest().id = motion::stand;
                 );
 
+  // Walk forward
+  DEBUG_REQUEST("PathPlanner:motion:walk_forward",
+                getMotionRequest().id = motion::walk;
+                getMotionRequest().walkRequest.target.translation.x = 500;
+                );
+
+  // Walk to the ball (XABSL: go_to_ball)
+  DEBUG_REQUEST(
+    "PathPlanner:motion:go_to_ball",
+    if (getMultiBallPercept().wasSeen())
+    {
+      Vector2d preview = getBallModel().positionPreview;
+      Vector2d pos     = getBallModel().position;
+      double ballRad   = getMultiBallPercept().getPercepts().at(0).radiusInImage;
+      getMotionRequest().id                               = motion::walk;
+      getMotionRequest().walkRequest.coordinate           = WalkRequest::Hip;
+      getMotionRequest().walkRequest.character            = 0.5;
+      getMotionRequest().walkRequest.target.translation.x = std::max(0.7 * (pos.x - ballRad), 0.0);
+      getMotionRequest().walkRequest.target.translation.y = preview.y;
+      getMotionRequest().walkRequest.target.rotation      = preview.abs() > 250
+                                                          ? preview.angle()
+                                                          : 0;
+      /*std::cout << "preview.x: "   << preview.x                           << std::endl
+                << "pos.x: "       << pos.x                               << std::endl
+                << "ballRad: "     << ballRad                             << std::endl
+                << "translate x: " << 0.7 * (preview.x - pos.x - ballRad) << std::endl;*/
+    }
+  );
+
+  // Search for the ball
+  DEBUG_REQUEST("PathPlanner:head:Search",
+                getHeadMotionRequest().id = HeadMotionRequest::search;
+                getHeadMotionRequest().searchCenter = Vector3<double>(2000, 0, 0);
+                getHeadMotionRequest().searchSize = Vector3<double>(1500, 2000, 0);
+                );
+  // Look at the ball
+  DEBUG_REQUEST("PathPlanner:head:LookAtMultiBallPercept",
+                if (getMultiBallPercept().wasSeen())
+                {
+                  Vector2d pos = (*getMultiBallPercept().begin()).positionOnField;
+                  for(MultiBallPercept::ConstABPIterator iter = getMultiBallPercept().begin(); iter != getMultiBallPercept().end(); iter++) {
+                    if(pos.abs() > (*iter).positionOnField.abs()) {
+                      pos = (*iter).positionOnField;
+                    }
+                  }
+                  getHeadMotionRequest().id = HeadMotionRequest::look_at_world_point;
+                  getHeadMotionRequest().targetPointInTheWorld.x = pos.x;
+                  getHeadMotionRequest().targetPointInTheWorld.y = pos.y;
+                  getHeadMotionRequest().targetPointInTheWorld.z = getFieldInfo().ballRadius;
+                }
+                );
 }//end execute
--- NaoTHSoccer/Source/Cognition/Modules/Behavior/PathPlanner/PathPlanner.h
+++ NaoTHSoccer/Source/Cognition/Modules/Behavior/PathPlanner/PathPlanner.h
@@ -13,39 +13,25 @@
 // representations
 #include "Representations/Infrastructure/FrameInfo.h"
 #include "Representations/Infrastructure/FieldInfo.h"
-#include "Representations/Infrastructure/SoundData.h"
 #include "Representations/Modeling/OdometryData.h"
 #include "Representations/Perception/BallPercept.h"
 #include "Representations/Perception/MultiBallPercept.h"
-//#include "Representations/Perception/PlayersPercept.h"
 #include "Representations/Modeling/BallModel.h"
 
 #include "Representations/Motion/Request/HeadMotionRequest.h"
 #include "Representations/Motion/Request/MotionRequest.h"
 #include "Representations/Motion/MotionStatus.h"
 
-//#include "Representations/Modeling/AttentionModel.h"
-
-#include <Representations/Infrastructure/LEDRequest.h>
-#include "Representations/Infrastructure/JointData.h"
-#include "Representations/Modeling/BodyStatus.h"
-
-#include <Representations/Infrastructure/RemoteControlCommand.h>
-
-#include <Representations/Infrastructure/WhistlePercept.h>
-#include "Representations/Infrastructure/WhistleControl.h"
-
 // debug
 #include "Tools/Debug/DebugRequest.h"
 #include "Tools/Debug/DebugModify.h"
-//#include "Tools/Debug/DebugDrawings.h"
-
 
 BEGIN_DECLARE_MODULE(PathPlanner)
   PROVIDE(DebugRequest)
   PROVIDE(DebugModify)
 
   REQUIRE(FrameInfo)
+  REQUIRE(FieldInfo)
   REQUIRE(MotionStatus)
   REQUIRE(BallPercept)
   REQUIRE(MultiBallPercept)
