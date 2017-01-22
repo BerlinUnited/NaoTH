df9ea2ce13473696e62cb067c673a83b91dc028b
Benjamin Schlotter <benji@schlotterfam.de>
2017-01-22 18:20:37 +0100

    cleanup tool class

--- Utils/py/ActionSelection/tools.py
+++ Utils/py/ActionSelection/tools.py
@@ -3,49 +3,7 @@ from matplotlib.patches import Circle
 import math
 
 
-def line_intersection(line1, line2):  # Todo implement this algorithm for type line segments
-    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
-    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
-
-    def det(a, b):
-        return a[0] * b[1] - a[1] * b[0]
-
-    div = det(xdiff, ydiff)
-    if div == 0:
-        raise Exception('lines do not intersect')
-
-    d = (det(line1[0], line1[1]), det(line2[0], line2[1]))
-    x = det(d, xdiff) / div
-    y = det(d, ydiff) / div
-    return x, y
-
-
-def line_intersection2(line1, line2):  # Todo implement this algorithm for type line segments
-    xdiff = (line1.point1X - line1.point2X, line2.point1X - line2.point2X)
-    ydiff = (line1.point1Y - line1.point2Y, line2.point1Y - line2.point2Y)
-
-    def det(a, b):
-        return a[0] * b[1] - a[1] * b[0]
-
-    div = det(xdiff, ydiff)
-    if div == 0:
-        raise Exception('lines do not intersect')
-
-    d = (det(line1.point1X, line1.point2X, line1[1]), det(line2[0], line2[1]))
-    x = det(d, xdiff) / div
-    y = det(d, ydiff) / div
-    return x, y
-
-
-def ccw(A, B, C):
-    return (C.y-A.y)*(B.x-A.x) > (B.y-A.y)*(C.x-A.x)
-
-
-def intersect(A, B, C, D):
-    return ccw(A, C, D ) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
-
-
-def draw_field():
+def draw_field():  # Todo use field_info
 
     ax = plt.gca()
     ax.plot([0, 0], [-3000, 3000], 'white')  # Middle line
@@ -79,16 +37,7 @@ def draw_field():
     ax.plot([4500, 5000], [750, 750], 'white')  # own penalty
     ax.plot([5000, 5000], [-750, 750], 'white')  # own penalty
 
-    ax.set_xlim([-5200, 5200])  # Todo use FieldInfo Stuff
+    ax.set_xlim([-5200, 5200])
     ax.set_ylim([-3700, 3700])
     ax.set_axis_bgcolor('green')
     ax.set_aspect("equal")
-
-
-def distance(line):
-    y = line.point2Y - line.point1Y
-    x = line.point2X - line.point1X
-    # y = line[1][1]-line[0][1]
-    # x = line[1][0]-line[0][0]
-    ans = y*y+x*x
-    return math.sqrt(ans)
