e425aa76fc6603dda751b8207c1742fcfd59e5d0
Benjamin Schlotter <benji@schlotterfam.de>
2017-02-05 14:51:58 +0100

    better test for intersection

--- Utils/py/ActionSelection/test/IntersectionTest.py
+++ Utils/py/ActionSelection/test/IntersectionTest.py
@@ -1,5 +1,6 @@
 import math2d as m2d
 from matplotlib import pyplot as plt
+from matplotlib.patches import Circle
 
 plt.clf()
 ax = plt.gca()
@@ -9,7 +10,7 @@ point1 = m2d.Vector2(4400, -700)
 point2 = m2d.Vector2(4400, 700)
 
 point3 = m2d.Vector2(0, 0)
-point4 = m2d.Vector2(1000, 0)
+point4 = m2d.Vector2(5000, 0)
 
 lineSegment1 = m2d.LineSegment(point1, point2)
 lineSegment2 = m2d.LineSegment(point3, point4)
@@ -17,7 +18,10 @@ lineSegment2 = m2d.LineSegment(point3, point4)
 ax.plot([point1.x, point2.x], [point1.y, point2.y], 'white')  # lineSegment1
 ax.plot([point3.x, point4.x], [point3.y, point4.y], 'white')  # lineSegment2
 
-print lineSegment1.intersection(lineSegment2)
+t = lineSegment1.intersection2(lineSegment2)
+ax.add_artist(Circle(xy=(lineSegment1.point(t).x, lineSegment1.point(t).y), radius=75, fill=False, edgecolor='white'))
+
+print lineSegment1.intersection2(lineSegment2)
 
 
 ax.set_xlim([-5200, 5200])  # Todo use FieldInfo Stuff
