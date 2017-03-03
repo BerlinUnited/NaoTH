9e0a76dae82bbcd560ae8655ce6c10a349703610
Thomas Krause <thomaskrause@posteo.de>
2017-03-03 15:16:30 +0100

    Remove GLIB mutex from the SimSparkController.

--- Framework/Platforms/Source/SimSpark/SimSparkController.h
+++ Framework/Platforms/Source/SimSpark/SimSparkController.h
@@ -10,7 +10,6 @@
 #define _SIMSPARKCONTROLLER_H
 
 
-#include <glib.h>
 #include <map>
 
 #include <Representations/Infrastructure/JointData.h>
@@ -46,6 +45,8 @@
 #include <Extern/libb64/decode.h>
 #include <Extern/libb64/encode.h>
 #include <set>
+#include <mutex>
+#include <condition_variable>
 
 using namespace naoth;
 
@@ -227,9 +228,9 @@ public:
 
 private:
   // members for threads
-  GMutex*  theCognitionInputMutex;
-  GMutex*  theCognitionOutputMutex;
-  GCond* theCognitionInputCond;
+  std::mutex  theCognitionInputMutex;
+  std::mutex  theCognitionOutputMutex;
+  std::condition_variable theCognitionInputCond;
   double maxJointAbsSpeed;
   bool exiting;
 
@@ -238,14 +239,14 @@ private:
   unsigned int theLastSenseTime;
   unsigned int theNextActTime;
   void calculateNextActTime();
-  GCond* theTimeCond;
-  GMutex* theTimeMutex;
+  std::condition_variable theTimeCond;
+  std::mutex theTimeMutex;
 
   std::string theSensorData;
-  GMutex* theSensorDataMutex;
-  GCond* theSensorDataCond;
+  std::mutex theSensorDataMutex;
+  std::condition_variable theSensorDataCond;
 
-  GMutex*  theActDataMutex;
+  std::mutex  theActDataMutex;
   std::stringstream theActData;
   void act();
 
