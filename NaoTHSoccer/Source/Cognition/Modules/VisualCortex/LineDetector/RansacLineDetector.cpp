5f980c2bb43b2417345c419994b113b43d2eddb5
Robert <martinro@informatik.hu-berlin.de>
2017-05-02 10:10:32 +0200

    debug request draw edgels

--- NaoTHSoccer/Source/Cognition/Modules/VisualCortex/LineDetector/RansacLineDetector.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/LineDetector/RansacLineDetector.cpp
@@ -3,6 +3,7 @@
 RansacLineDetector::RansacLineDetector()
 {
   // initialize some stuff here
+  DEBUG_REQUEST_REGISTER("Vision:RansacLineDetector:edgel_pairs", "", false);
 }
 
 RansacLineDetector::~RansacLineDetector()
@@ -14,4 +15,22 @@ void RansacLineDetector::execute()
 {
   // do some stuff here
 
+  DEBUG_REQUEST("Vision:RansacLineDetector:edgel_pairs",
+    CANVAS("ImageTop");
+
+    for(size_t i = 0; i < getScanLineEdgelPerceptTop().pairs.size(); i++)
+    {
+      const ScanLineEdgelPercept::EdgelPair& pair = getScanLineEdgelPerceptTop().pairs[i];
+      const Edgel& edgelOne = getScanLineEdgelPerceptTop().edgels[pair.begin];
+      const Edgel& edgelTwo = getScanLineEdgelPerceptTop().edgels[pair.end];
+
+      PEN("000000",0.1);
+      //LINE_PX(ColorClasses::red, edgelOne.center.x, edgelOne.center.y, edgelTwo.center.x, edgelTwo.center.y);
+      LINE(edgelOne.point.x, edgelOne.point.y, edgelTwo.point.x, edgelTwo.point.y);
+      PEN("FF0000",0.1);
+      CIRCLE( edgelOne.point.x, edgelOne.point.y, 3);
+      PEN("0000FF",0.1);
+      CIRCLE( edgelTwo.point.x, edgelTwo.point.y, 2);
+    }
+  );
 }
--- NaoTHSoccer/Source/Cognition/Modules/VisualCortex/LineDetector/RansacLineDetector.h
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/LineDetector/RansacLineDetector.h
@@ -1,11 +1,33 @@
 #ifndef RANSACLINEDETECTOR_H
 #define RANSACLINEDETECTOR_H
 
+#include <iostream>
+
 #include <ModuleFramework/Module.h>
 #include <Representations/Infrastructure/FrameInfo.h>
 
-BEGIN_DECLARE_MODULE(RansacLineDetector)
+#include "Tools/Debug/DebugRequest.h"
+#include "Tools/Debug/DebugModify.h"
+#include "Tools/Debug/DebugDrawings.h"
+#include "Tools/Debug/DebugImageDrawings.h"
+#include "Tools/Debug/DebugParameterList.h"
 
+#include "Representations/Perception/CameraMatrix.h"
+#include "Representations/Perception/ScanLineEdgelPercept.h"
+
+BEGIN_DECLARE_MODULE(RansacLineDetector)
+  PROVIDE(DebugRequest)
+  PROVIDE(DebugModify)
+  PROVIDE(DebugDrawings)
+  PROVIDE(DebugImageDrawings)
+  PROVIDE(DebugImageDrawingsTop)
+  PROVIDE(DebugParameterList)
+
+  REQUIRE(ScanLineEdgelPercept)
+  REQUIRE(ScanLineEdgelPerceptTop)
+
+  REQUIRE(CameraMatrix)
+  REQUIRE(CameraMatrixTop)
 END_DECLARE_MODULE(RansacLineDetector)
 
 class RansacLineDetector: public RansacLineDetectorBase
