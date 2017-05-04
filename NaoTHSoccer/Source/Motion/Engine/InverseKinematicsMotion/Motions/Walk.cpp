6634e71c6a6d07f5aa5474fdc70cb7d8c4fa1751
y030 <yigit.akcay@icloud.com>
2017-05-04 17:34:11 +0200

    small fix. seems like bug was fixed, that has caused the kickfoot not being moved back (instead the support foot was moved forward) in certain unstable situations.

--- NaoTHSoccer/Source/Motion/Engine/InverseKinematicsMotion/Motions/Walk.cpp
+++ NaoTHSoccer/Source/Motion/Engine/InverseKinematicsMotion/Motions/Walk.cpp
@@ -197,9 +197,7 @@ void Walk::calculateNewStep(const Step& lastStep, Step& newStep, const WalkReque
 
   // STABILIZATION
   bool do_emergency_stop = com_errors.size() == com_errors.getMaxEntries() && com_errors.getAverage() > parameters().stabilization.emergencyStopError;
-  if ( (getMotionRequest().id != getId() || do_emergency_stop) /* walkRequest.stepControl.type != WalkRequest::StepControlRequest::StepType::KICKSTEP 
-                                                               nor && lastStep.walkRequest.stepControl.type != WalkRequest::StepControlRequest::StepType::KICKSTEP have helped
-                                                               could try ZEROSTEP instead of KICKSTEP (lastStep.walkRequest.....)*/
+  if ( (getMotionRequest().id != getId() || do_emergency_stop) && lastStep.walkRequest.stepControl.type == WalkRequest::StepControlRequest::WALKSTEP 
       //|| walkRequest.stepControl.stepID + 1 != stepBuffer.stepId())
       )
   {
