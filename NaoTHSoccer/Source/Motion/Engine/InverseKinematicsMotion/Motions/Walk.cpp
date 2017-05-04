340e7e299fc77844782527e67cea02c64f0a8ffd
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2017-05-04 21:35:01 +0200

    stop motion bug

--- NaoTHSoccer/Source/Motion/Engine/InverseKinematicsMotion/Motions/Walk.cpp
+++ NaoTHSoccer/Source/Motion/Engine/InverseKinematicsMotion/Motions/Walk.cpp
@@ -198,7 +198,8 @@ void Walk::calculateNewStep(const Step& lastStep, Step& newStep, const WalkReque
 
   // STABILIZATION
   bool do_emergency_stop = com_errors.size() == com_errors.getMaxEntries() && com_errors.getAverage() > parameters().stabilization.emergencyStopError;
-  if ( (getMotionRequest().id != getId() || do_emergency_stop) && lastStep.walkRequest.stepControl.type == WalkRequest::StepControlRequest::WALKSTEP 
+  if ( getMotionRequest().id != getId() || 
+      (do_emergency_stop && lastStep.walkRequest.stepControl.type == WalkRequest::StepControlRequest::WALKSTEP)
       //|| walkRequest.stepControl.stepID + 1 != stepBuffer.stepId())
       )
   {
