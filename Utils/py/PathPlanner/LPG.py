234e470e4736639b9edc40e8694ce68bb7b3a092
Yigit Akcay <yigit.akcay@icloud.com>
2017-05-24 18:37:32 +0200

    added obstacle costs.

--- Utils/py/PathPlanner/LPG.py
+++ Utils/py/PathPlanner/LPG.py
@@ -11,12 +11,18 @@ base           = 1.1789
 minimal_cell   = 100
 angular_part   = 16
 
-def distance(x, y):
+parameter_s = 1.25
+
+def distance(coords):
+    x = coords[0]
+    y = coords[1]
     return math.floor(math.log(((math.sqrt(x*x + y*y) * (base - 1)) / minimal_cell) + 1, base))
 def inv_distance(r):
     return (np.exp(np.log(base)*r) - 1) * minimal_cell / (base - 1)
 
-def cell_angle(x, y):
+def cell_angle(coords):
+    x = coords[0]
+    y = coords[1]
     return math.floor((angular_part / (2*math.pi)) * angle(x, y) + 0.5)
 def angle(x,y):
     if (x is 0):
@@ -24,43 +30,56 @@ def angle(x,y):
     else:
         return np.arctan2(y, x)
 
-def cell_mid(x, y):
-    if (x is not 0):
-        vorz_x = np.absolute(x) / x
-    else:
-        vorz_x = 1
-    if (y is not 0):
-        vorz_y = np.absolute(y) / y
-    else:
-        vorz_y = 1
+def cell_mid(coords): # returns cell mid from cartesian coordinates
+    x = coords[0]
+    y = coords[1]
     x = np.absolute(x)
     y = np.absolute(y)
 
     # distance and angle for the middle of the cell
-    dist = distance(x, y)
-    angl = cell_angle(x, y) * (2*math.pi/16)
-
+    dist = distance((x, y))
+    angl = cell_angle((x, y)) * (2*math.pi/16)
 
     x_new = np.cos(angl) * (((np.power(base, dist + 0.5) - 1) * minimal_cell) / (base-1))
     y_new = np.sin(angl) * (((np.power(base, dist + 0.5) - 1) * minimal_cell) / (base-1))
-    x_new *= vorz_x
-    y_new *= vorz_y
-
+    if np.sign(x) < 0:
+        x_new *= -1
+    if np.sign(y) < 0:
+        y_new *= -1
     return (x_new, y_new)
 
-def get_cell(x, y):
-    return [distance(x, y), cell_angle(x, y)]
-def get_xy_from_cell(r, a):
+def cell_mid_from_polar(r, a):   # returns cell mid from polar coordinates
     prd = (((np.power(base, r+0.5) - 1) * minimal_cell) / (base - 1))
     return (np.cos(a * (2*math.pi/16)) * prd, np.sin(a * (2*math.pi/16)) * prd)
 
+def get_cell(coords):
+    return (distance(coords), cell_angle(coords))
+
 def dist_between(a, b):
-    (x1, y1) = get_xy_from_cell(a[0], a[1])
-    (x2, y2) = get_xy_from_cell(b[0], b[1])
+    (x1, y1) = (a[0], a[1])
+    (x2, y2) = (b[0], b[1])
+    return np.sqrt(np.power(x1 - x2, 2) + np.power(y1 - y2, 2))
+def dist_between_cell(a, b):
+    (x1, y1) = cell_mid_from_polar(a[0], a[1])
+    (x2, y2) = cell_mid_from_polar(b[0], b[1])
     return np.sqrt(np.power(x1 - x2, 2) + np.power(y1 - y2, 2))
 
+def obst_func(cell, obst):   # obst is obstacle coordinates in x, y
+    r_f              = obst[2]
+    cell_mid         = cell_mid_from_polar(cell[0], cell[1])
+    dist_to_obst_mid = dist_between(cell_mid, obst)
+    obst_dist_to_mid = dist_between(obst, (0, 0))
+    r_d              = obst_dist_to_mid / 100
+
+    # parameters
+    a = r_f - r_d  # cost of constant part
+    r = r_f + r_d  # radius of constant part
+    s = parameter_s*r        # radius of linear decreasing part
+    
+    return np.maximum(np.minimum(1 - ((dist_to_obst_mid - r) / s), 1), 0) * a
+
 # A STAR IMPLEMENTATION
-def a_star_search(start, goal):
+def a_star_search(start, goal, obstacles):
     openlist = Q.PriorityQueue()
     closedlist = set()
     openlist.put((0, start))
@@ -71,45 +90,64 @@ def a_star_search(start, goal):
 
     while not openlist.empty():
         current = openlist.get()[1]
-        #print("current = " + str(current))
 
         if current == goal:
             break
 
         closedlist.add(current)
 
-        for the_next in [(current[0], current[1]+1), (current[0], current[1]-1), (current[0]-1, current[1]), (current[0]-1, current[1]+1), (current[0]-1, current[1]-1)]:
-            if the_next in closedlist:
-                continue
-            new_cost = cost_so_far[current] + dist_between(current, the_next)
-            if the_next not in cost_so_far or new_cost < cost_so_far[the_next]:
-                cost_so_far[the_next] = new_cost
-                priority = new_cost + dist_between(the_next, goal) #+ (np.absolute(goal[1] - the_next[1]))
+        for r in [0, -1]:
+            for a in [0, 1, -1]:
+                the_next = (current[0] + r, current[1] + a)
+                if the_next in closedlist:
+                    continue
+                # initialize cost_so_far
+                if math.isnan(cost_so_far[current]):
+                    cost_so_far[current] = 0
+
+                # cell cost without obstacles
+                new_cost = cost_so_far[current] + dist_between_cell(current, the_next)
+
+                # add obstacle costs to cell
+                #old_cost = new_cost
+                for obst in obstacles:
+                    new_cost += obst_func(the_next, obst)
                 #print(the_next)
-                #print(dist_between(the_next, goal))
-                #print("new_cost = " + str(new_cost))
-                #print("priority = " + str(priority))
-                openlist.put((priority, the_next))
-                came_from[the_next] = current
+                #print(new_cost - old_cost)
+                # add to or update openlist
+                if the_next not in cost_so_far or new_cost < cost_so_far[the_next]:
+                    cost_so_far[the_next] = new_cost
+                    priority = new_cost + dist_between_cell(the_next, goal)
+                    openlist.put((priority, the_next))
+                    came_from[the_next] = current
 
     return came_from, cost_so_far
 
-def compute_waypoints_LPG(tar):
-    start = (get_cell(tar[0], tar[1])[0], get_cell(tar[0], tar[1])[1])
+def compute_waypoints_LPG(tar, obstacles):
+    start = (get_cell(tar)[0], get_cell(tar)[1])  # target only occupies only cell
     target = (0, 0)
 
-    (a, b) = a_star_search(start, target)
+    # compute obstacle radius
+
+    (a, b) = a_star_search(start, target, obstacles)
     the_next = (0, 0)
     the_path = [the_next]
     while a[the_next] in a:
         the_next = a[the_next]
         the_path.append(the_next)
 
+    # compute gait vector for next step
+    x = 0
+    y = 0
+    k = 0
+    """while np.absolute(x) < 40 or np.absolute(y) < 40:
+        the_mid = cell_mid(the_path[k])
+        x += the_mid[0]
+        y += the_mid[1]
+        k += 1"""
     return the_path
 
-def compute_waypoints_LPGown(target):
-    return [[0, 0]]
-def compute_waypoints_own(target):
+def compute_waypoints_bisection(target):
     return [[0, 0]]
 
 def draw(obstacles, target, algorithm):
@@ -132,6 +170,15 @@ def draw(obstacles, target, algorithm):
     plt.axis("equal")
     ax = plt.gca()
 
+    # draw obstacles
+    if (obstacles is not []):
+        obstacle_set = set()
+        for k in obstacles:
+            obstacle_set.add(cell_mid(k))
+            ax.add_artist(Circle(xy=(k[0], k[1]), radius=(k[2]+(dist_between(k, (0, 0))/100)) * parameter_s, fill=True, color='green'))
+            ax.add_artist(Circle(xy=(k[0], k[1]), radius=k[2]+dist_between(k, (0, 0))/100, fill=True, color='red'))
+            ax.plot(k[0], k[1], '*', c='black')
+
     # draw rings
     for k in range(1, 17):
         rad = inv_distance(k)
@@ -141,29 +188,19 @@ def draw(obstacles, target, algorithm):
     for k in range(0, len(x)):
         ax.plot([0, x[k]], [0, y[k]], 'black')
 
-    # draw obstacles
-    if (obstacles is not []):
-        obstacle_set = set()
-        for k in obstacles:
-            obstacle_set.add(cell_mid(k[0], k[1]))
-        for k in obstacle_set:
-            ax.plot(k[0], k[1], '*', c='red')
     # draw target
-    ax.plot(cell_mid(target[0], target[1])[0], cell_mid(target[0], target[1])[1], 'x', c='green')
+    ax.plot(cell_mid(target)[0], cell_mid(target)[1], 'x', c='green')
 
     # draw path
     if algorithm is "LPG":
-        waypoints = compute_waypoints_LPG(target)
-    if algorithm is "LPGown":
-        waypoints = compute_waypoints_LPGown(target)
+        waypoints = compute_waypoints_LPG(target, obstacles)
     if algorithm is "own":
-        waypoints = compute_waypoints_own(target)
+        waypoints = compute_waypoints_bisection(target)
 
     # mark waypoint cells
     for k in waypoints:
-        (x, y) = get_xy_from_cell(k[0], k[1])
+        (x, y) = cell_mid_from_polar(k[0], k[1])
         ax.plot(x, y, ">", c='blue')
-        #print(get_xy_from_cell(k[0], k[1]))
 
     ax.set_xlim([-4500, 4500])
     ax.set_ylim([-3000, 3000])
--- Utils/py/PathPlanner/LPG_sim.py
+++ Utils/py/PathPlanner/LPG_sim.py
@@ -3,11 +3,12 @@ from random import randint
 
 #LPG.compute_waypoints_LPG([3000, 0])
 
-obstacles = [[1000, 2500], [-4500, -3000], [500, 2245], [-1600, -1000]]
-#target = [-1500, -2500]
-target = [randint(-45000, 45000)/10, randint(-30000, 30000)/10]
+#obstacles = [(1000, 0, 400)] #[(), ()] ansonsten fehler
+obstacles = [(1600, 0, 400), (2000, -500, 400)]
+target = [3500, 0]
+#target = [randint(-45000, 45000)/10, randint(-30000, 30000)/10]
 
 # plot
 while True:
-    target = [randint(-45000, 45000)/10, randint(-30000, 30000)/10]
+    #target = [randint(-45000, 45000)/10, randint(-30000, 30000)/10]
     LPG.draw(obstacles, target, "LPG")
