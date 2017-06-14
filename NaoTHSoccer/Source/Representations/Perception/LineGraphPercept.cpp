b8e2be402ece181f767f0f5f69982d9ddc19da33
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2017-06-14 17:22:06 +0200

    added helpers for serialization of std::vectors and edgels

--- NaoTHSoccer/Source/Representations/Perception/LineGraphPercept.cpp
+++ NaoTHSoccer/Source/Representations/Perception/LineGraphPercept.cpp
@@ -4,6 +4,7 @@
 #include "Messages/Representations.pb.h"
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 #include <Tools/DataConversion.h>
+#include <Tools/SerialzationHelpers.h>
 
 using namespace naoth;
 
@@ -11,13 +12,9 @@ void Serializer<LineGraphPercept>::serialize(const LineGraphPercept& representat
 {
   naothmessages::LineGraphPercept msg;
 
-  for (const Edgel& edgel : representation.edgels)
-  {
-    naothmessages::Edgel& edgel_msg = *(msg.add_edgelsonfield());
-
-    DataConversion::toMessage(edgel.point, *edgel_msg.mutable_point());
-    DataConversion::toMessage(edgel.direction, *edgel_msg.mutable_direction());
-  }
+  SerialzationHelpers::toMessage(representation.edgels, *msg.mutable_edgelsonfield());
+  SerialzationHelpers::toMessage(representation.edgelsInImage, *msg.mutable_edgelsinimage());
+  SerialzationHelpers::toMessage(representation.edgelsInImageTop, *msg.mutable_edgelsinimagetop());
 
   // serialize the message
   google::protobuf::io::OstreamOutputStream buf(&stream);
@@ -30,19 +27,12 @@ void Serializer<LineGraphPercept>::deserialize(std::istream& stream, LineGraphPe
   representation.reset();
 
   // deserialize the message
-  naothmessages::LineGraphPercept percept_msg;
+  naothmessages::LineGraphPercept msg;
   google::protobuf::io::IstreamInputStream buf(&stream);
-  percept_msg.ParseFromZeroCopyStream(&buf);
+  msg.ParseFromZeroCopyStream(&buf);
 
+  SerialzationHelpers::fromMessage(msg.edgelsonfield(), representation.edgels);
+  SerialzationHelpers::fromMessage(msg.edgelsinimage(), representation.edgelsInImage);
+  SerialzationHelpers::fromMessage(msg.edgelsinimagetop(), representation.edgelsInImageTop);
   
-  representation.edgels.resize(percept_msg.edgelsonfield_size());
-  for (int i = 0; i < percept_msg.edgelsonfield_size(); i++)
-  {
-    Edgel& edgel = representation.edgels[i];
-    const naothmessages::Edgel& edgel_msg = percept_msg.edgelsonfield(i);
-
-    DataConversion::fromMessage(edgel_msg.point(), edgel.point);
-    DataConversion::fromMessage(edgel_msg.direction(), edgel.direction);
-  }
-
 }//end deserialize
