7158c2e342dfda9b4b5e0a0beb0dd532300fdac8
Yigit Akcay <yigit.akcay@icloud.com>
2017-06-14 18:01:26 +0200

    second pathplanning algorithm simulation finished

--- Utils/py/PathPlanner/BISEC.py
+++ Utils/py/PathPlanner/BISEC.py
@@ -50,7 +50,7 @@ def draw_field(ax, x_off, y_off):
 
     ax.set_axis_bgcolor('green')
 
-def draw_obstacles(ax, x_off, y_off, obstacles):
+def draw_obstacles(ax, obstacles):
     # draw obstacles
     if obstacles:
         for k in obstacles:
@@ -58,41 +58,49 @@ def draw_obstacles(ax, x_off, y_off, obstacles):
             ax.add_artist(Circle(xy=(k[0], k[1]), radius=10, fill=True, color='black'))
 
 def draw_target(ax, target):
-    for k in target:
-        ax.plot(k[0], k[1], 'x', color='red')
+    ax.plot(target[0], target[1], 'x', color='red')
 
-def draw_steps(ax, x_off, y_off, steps):
-    x_dist = 0
-    y_dist = 0
+def draw_steps(ax, robot_pos, steps, col):
+    x_dist = robot_pos[0]
+    y_dist = robot_pos[1]
+    path   = []
     for k in steps:
-        ax.plot(k[0] + x_dist + x_off, k[1] + y_dist + y_off, 'x', color='black')
+        path.append((k[0] + x_dist, k[1] + y_dist))
         x_dist += k[0]
         y_dist += k[1]
-
-def draw_walked_path(ax, path):
     for k in range(0, len(path) - 1):
-        ax.plot([path[k][0], path[k+1][0]], [path[k][1], path[k+1][1]], c='red')
+        ax.plot([path[k][0], path[k+1][0]], [path[k][1], path[k+1][1]], c=col)
 
 def dist(start, target):
     return np.sqrt(np.power(start[0] - target[0], 2) + np.power(start[1] - target[1], 2))
 
-def hit_obstacle(obstacles, gait_pos):
-    for k in range(0, len(obstacles)):
-        if dist((obstacles[k][0], obstacles[k][1]), gait_pos) <= obstacles[k][2] + robot_radius:
-            return k
-    return -1
+def hit_obstacle(start, steps, obstacles):
+    (x_dist, y_dist) = (0, 0)
 
-def compute_steps(tar, obstacles, x_off, y_off):
-    target       = copy.copy(tar)
-    steps        = []
-    x_dist       = 0
-    y_dist       = 0
-    obstacle_hit = -1
+    for k in steps:
+        for l in obstacles:
+            if dist(l, (k[0] + x_dist + start[0], k[1] + y_dist + start[1])) <= l[2] + robot_radius:
+                return (True, (x_dist, y_dist))
+        x_dist += k[0]
+        y_dist += k[1]
 
-    while len(steps) < 20:
-        tar_dist       = dist((0, 0), target)
-        gait_unit_vec  = (target[0] / tar_dist, target[1] / tar_dist)
-        max_steplength = np.absolute(min(60, max(-60, min(tar_dist / gait_unit_vec[0], tar_dist / gait_unit_vec[1]))))
+    return (False, (0, 0))
+
+def has_collided(vec, obstacles):
+    for k in obstacles:
+        if dist(vec, k) <= k[2] + robot_radius:
+            return True
+    return False
+
+def compute_steps(start, target):
+    steps            = []
+    (x_dist, y_dist) = start
+    target           = (target[0] - start[0], target[1] - start[1])
+
+    while True:
+        target_dist    = dist((0, 0), target)
+        gait_unit_vec  = (target[0] / target_dist, target[1] / target_dist)
+        max_steplength = np.absolute(min(60, max(-60, min(target_dist / gait_unit_vec[0], target_dist / gait_unit_vec[1]))))
         gait           = (gait_unit_vec[0] * max_steplength, gait_unit_vec[1] * max_steplength)
 
         if math.isnan(gait[0]) or math.isnan(gait[1]):
@@ -102,36 +110,60 @@ def compute_steps(tar, obstacles, x_off, y_off):
         if gait[0] is -0 and gait[1] is -0:
             break
 
-        # check for obstacles
-        obstacle_hit = hit_obstacle(obstacles, (x_off + x_dist + gait[0], y_off + y_dist + gait[1]))
-
-        if obstacle_hit > -1:
-            break
-
         # check if target is closer than gait
         if np.sqrt(np.power(gait[0], 2) + np.power(gait[1], 2)) > np.sqrt(np.power(target[0], 2) + np.power(target[1], 2)):
             gait = target
+            steps.append(gait)
+            break
 
         steps.append(gait)
 
-        target = (target[0] - gait[0], target[1] - gait[1])
+        target  = (target[0] - gait[0], target[1] - gait[1])
         x_dist += gait[0]
         y_dist += gait[1]
 
-    return steps, obstacle_hit
+    return steps
 
-def compute_sub_target(obstacle, target, x_off, y_off):
-    tmp_target = (target[0] - x_off, target[1] - y_off)
-    unit_vec  = (tmp_target[0] / dist((0, 0), tmp_target), tmp_target[1] / dist((0, 0), tmp_target))
-    sub_target1 = (unit_vec[1] * -1 * (robot_radius * 1.5 + obstacle[2]) + obstacle[0], unit_vec[0] * (robot_radius * 1.5 + obstacle[2]) + obstacle[1])
-    sub_target2 = (unit_vec[1] * (robot_radius * 1.5 + obstacle[2]) + obstacle[0], unit_vec[0] * -1 * (robot_radius * 1.5 + obstacle[2]) + obstacle[1])
+def compute_sub_target(start, target, collision, obstacles, ax, sign):
+    robot_diameter_count = 1
 
-    dist1       = dist(target, sub_target1)
-    dist2       = dist(target, sub_target2)
+    target_vec  = (target[0] - start[0], target[1] - start[1])
+    target_dist = dist(start, target_vec)
+    unit_vec    = (target_vec[0] / target_dist, target_vec[1] / target_dist)
 
-    if dist1 > dist2:
-        sub_target = sub_target2
+    orth_vec1 = (unit_vec[1] * -1, unit_vec[0])
+    orth_vec2 = (unit_vec[1], unit_vec[0] * -1)
+
+    collision = (collision[0] + start[0], collision[1] + start[1])
+
+    if sign == 1:
+        sub_target = (orth_vec1[0] * (robot_radius * robot_diameter_count) + collision[0], orth_vec1[1] * (robot_radius * robot_diameter_count) + collision[1])
     else:
-        sub_target = sub_target1
+        sub_target = (orth_vec2[0] * (robot_radius * robot_diameter_count) + collision[0], orth_vec2[1] * (robot_radius * robot_diameter_count) + collision[1])
+
+    while has_collided(sub_target, obstacles):
+        robot_diameter_count += 1
+        if sign == 1:
+            sub_target = (orth_vec1[0] * (robot_radius * robot_diameter_count) + collision[0], orth_vec1[1] * (robot_radius * robot_diameter_count) + collision[1])
+        else:
+            sub_target = (orth_vec2[0] * (robot_radius * robot_diameter_count) + collision[0], orth_vec2[1] * (robot_radius * robot_diameter_count) + collision[1])
+
+    ax.plot(sub_target[0], sub_target[1], 'x', color='blue')
 
     return sub_target
+
+def compute_path(start, target, obst, depth, ax, sign):
+    collision = start
+    steps     = compute_steps(start, target)
+    obstacles = copy.copy(obst)
+
+    if depth > 20:
+        return steps
+
+    (has_collided, col_pos) = hit_obstacle(start, steps, obstacles)
+
+    if has_collided:
+        sub_target = compute_sub_target(start, target, col_pos, obstacles, ax, sign)
+        steps      = compute_path(start, sub_target, obstacles, depth+1, ax, sign) + compute_path(sub_target, target, obstacles, depth+1, ax, sign)
+
+    return steps
--- Utils/py/PathPlanner/run_sim_BISEC.py
+++ Utils/py/PathPlanner/run_sim_BISEC.py
@@ -11,33 +11,16 @@ import Queue as Q
 import copy
 import select
 
-def heard_pause():
-    i,o,e = select.select([sys.stdin],[],[],0.0001)
-    for s in i:
-        if s == sys.stdin:
-            input = sys.stdin.readline()
-            return True
-    return False
+obstacles      = [(1600, 1000, 300), (1600, 500, 300), (1600, 0, 300), (1600, -500, 300)] # [(), ()] ansonsten fehler
+target         = (3500, 0)#(3500, 0)]#, (-1000, -2750)]
 
-obstacles      = [(1600, 250, 300), (2000, -500, 300), (2500, -500, 300)] # [(), ()] ansonsten fehler
-target         = [(3500, 0)]#(3500, 0)]#, (-1000, -2750)]
-orig_target    = copy.copy(target)
+robot_pos = (0, 0)#(1600, -900)#(0, 0)
 
-x_off = 0
-y_off = 0
-
-obstacle_hit = -1
-
-walked_path = [(x_off, y_off)]
+steps       = []
 
 pause = False
 
 while True:
-    if heard_pause():
-        pause = not pause
-    while pause:
-        if heard_pause():
-            pause = not pause
     mpl.rcParams['lines.linewidth'] = 0.5
     plt.clf()
     plt.axis("equal")
@@ -48,37 +31,22 @@ while True:
     # draw the field
     B.draw_field(ax, 0, 0)
 
-    (steps, obstacle_hit) = B.compute_steps(target[0], obstacles, x_off, y_off)
-    if obstacle_hit > -1:
-        target[0]   = orig_target[0]
-        sub_target  = B.compute_sub_target(obstacles[obstacle_hit], target[0], x_off, y_off)
-        orig_target = [sub_target] + orig_target
-        sub_target  = (sub_target[0] - x_off, sub_target[1] - y_off)
-        target      = [sub_target] + target
+    steps_plus  = B.compute_path(robot_pos, target, obstacles, 0, ax, 1)
+    steps_minus = B.compute_path(robot_pos, target, obstacles, 0, ax, -1)
 
-        if len(target) > 2:
-            target.pop(1)
-            orig_target.pop(1)
+    if len(steps_plus) < len(steps_minus):
+        steps     = steps_plus
+        alt_steps = steps_minus
+    else:
+        steps     = steps_minus
+        alt_steps = steps_plus
 
-    B.draw_steps(ax, x_off, y_off, steps)
+    # draw the path
+    B.draw_steps(ax, robot_pos, alt_steps, 'black')
+    B.draw_steps(ax, robot_pos, steps, 'red')
 
     # draw obstacles and the target
-    B.draw_obstacles(ax, 0, 0, obstacles)
-    B.draw_target(ax, orig_target)
-
-    # draw the walked path
-    B.draw_walked_path(ax, walked_path)
-
-    # simulate gait
-    if (x_off != target[0][0] or y_off != target[0][1]) and len(steps) > 0 and obstacle_hit == -1:
-        x_off += math.floor(steps[0][0])
-        y_off += math.floor(steps[0][1])
-        target[0] = (target[0][0] - math.floor(steps[0][0]), target[0][1] - math.floor(steps[0][1]))
-        walked_path.append((x_off, y_off))
-    # change target
-    if np.absolute(x_off - orig_target[0][0]) < 1 and np.absolute(y_off - orig_target[0][1]) < 1 and len(target) > 1:
-        target.pop(0)
-        orig_target.pop(0)
-        target[0] = (target[0][0] - x_off, target[0][1] - y_off)
+    B.draw_obstacles(ax, obstacles)
+    B.draw_target(ax, target)
 
     plt.pause(0.000000000001)
