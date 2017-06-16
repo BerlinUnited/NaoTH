dbdf32ef8f507f5fcec6a11d78fdfaa575a10a55
Yigit Akcay <yigit.akcay@icloud.com>
2017-06-16 21:30:06 +0200

    small cleanup for LPG simulation

--- Utils/py/PathPlanner/LPG.py
+++ Utils/py/PathPlanner/LPG.py
@@ -15,66 +15,42 @@ angular_part   = 16
 
 parameter_s = 0.5
 
-def distance(coords):
-    x = coords[0]
-    y = coords[1]
-    return math.floor(math.log(((math.sqrt(x*x + y*y) * (base - 1)) / minimal_cell) + 1, base))
-def inv_distance(r):
+robot_radius = 0
+
+def get_r(coords):
+    return math.floor(math.log(((math.sqrt(np.power(coords[0], 2) + np.power(coords[1], 2)) * (base - 1)) / minimal_cell) + 1, base))
+def get_inv_r(r):
     return (np.exp(np.log(base)*r) - 1) * minimal_cell / (base - 1)
 
-def cell_angle(coords, rot):
-    x = coords[0]
-    y = coords[1]
-    return math.floor((angular_part / (2*math.pi)) * (angle(x, y) - rot) + 0.5)
-def angle(x,y):
-    if (x is 0):
-        return np.arctan2(y, 1)
+def get_a(coords, rot):
+    return math.floor((angular_part / (2*np.pi)) * (angle(coords) - rot) + 0.5)
+def angle(coords):
+    if (coords[0] == 0):
+        return np.arctan2(coords[1], 1)
     else:
-        return np.arctan2(y, x)
+        return np.arctan2(coords[1], coords[0])
 
-def cell_mid(coords, rot): # returns cell mid from cartesian coordinates
-    x = coords[0]
-    y = coords[1]
-    x = np.absolute(x)
-    y = np.absolute(y)
-    if np.sign(x) < 0:
-        x_sign = -1
-    else:
-        x_sign = 1
-    if np.sign(y) < 0:
-        y_sign = -1
-    else:
-        y_sign = 1
-
-    # distance and angle for the middle of the cell
-    dist = distance((x, y))
-    angl = cell_angle((x, y), rot) * (2*math.pi/16)
-
-    x_new = np.cos(angl) * (((np.power(base, dist + 0.5) - 1) * minimal_cell) / (base-1))
-    y_new = np.sin(angl) * (((np.power(base, dist + 0.5) - 1) * minimal_cell) / (base-1))
-    return (x_new, y_new)
-
-def cell_mid_from_polar(r, a, rot):   # returns cell mid from polar coordinates
+def get_cell_mid((r, a), rot):   # returns cell mid from polar coordinates
     prd = (((np.power(base, r+0.5) - 1) * minimal_cell) / (base - 1))
-    return (np.cos(a * (2*math.pi/16) + rot) * prd, np.sin(a * (2*math.pi/16) + rot) * prd)
+    return (np.cos(a * (2*np.pi/16) + rot) * prd, np.sin(a * (2*np.pi/16) + rot) * prd)
 
 def get_cell(coords, rot):
-    return (distance(coords), cell_angle(coords, rot))
+    return (get_r(coords), get_a(coords, rot))
 
-def dist_between(a, b):
+def dist(a, b):
     (x1, y1) = (a[0], a[1])
     (x2, y2) = (b[0], b[1])
     return np.sqrt(np.power(x1 - x2, 2) + np.power(y1 - y2, 2))
-def dist_between_cell(a, b, rot):
-    (x1, y1) = cell_mid_from_polar(a[0], a[1], rot)
-    (x2, y2) = cell_mid_from_polar(b[0], b[1], rot)
+def dist_cell(a, b, rot):
+    (x1, y1) = get_cell_mid(a, rot)
+    (x2, y2) = get_cell_mid(b, rot)
     return np.sqrt(np.power(x1 - x2, 2) + np.power(y1 - y2, 2))
 
 def obst_func(cell, obst, rot):   # obst is obstacle coordinates in x, y
     r_f              = obst[2]
-    cell_mid         = cell_mid_from_polar(cell[0], cell[1], rot)
-    dist_to_obst_mid = dist_between(cell_mid, obst)
-    obst_dist_to_mid = dist_between(obst, (0, 0))
+    cell_mid         = get_cell_mid(cell, rot)
+    dist_to_obst_mid = dist(cell_mid, obst)
+    obst_dist_to_mid = dist(obst, (0, 0))
     r_d              = obst_dist_to_mid / 10
 
     # parameters
@@ -113,7 +89,7 @@ def a_star_search(start, goal, obstacles, rot):
                     cost_so_far[current] = 0
 
                 # cell cost without obstacles
-                new_cost = cost_so_far[current] + dist_between_cell(current, the_next, rot)
+                new_cost = cost_so_far[current] + dist_cell(current, the_next, rot)
 
                 # add obstacle costs to cell
                 for obst in obstacles:
@@ -122,15 +98,15 @@ def a_star_search(start, goal, obstacles, rot):
                 # add to or update openlist
                 if the_next not in cost_so_far or new_cost < cost_so_far[the_next]:
                     cost_so_far[the_next] = new_cost
-                    priority = new_cost + dist_between_cell(the_next, goal, rot)
+                    priority = new_cost + dist_cell(the_next, goal, rot)
                     openlist.put((priority, the_next))
                     came_from[the_next] = current
 
     return came_from, cost_so_far
 
-def compute_waypoints_LPG(tar, obstacles, rot, rot_a):
-    start = get_cell(tar, rot)
-    target   = (0, rot_a)
+def compute_waypoints(tar, obstacles, rot, rot_a):
+    start  = get_cell(tar, rot)
+    target = (0, rot_a)
 
     (a, b) = a_star_search(start, target, obstacles, rot)
 
@@ -146,59 +122,59 @@ def compute_waypoints_LPG(tar, obstacles, rot, rot_a):
 def compute_gait(the_path, target, rot):
     (x, y) = (0, 0)
     for k in range(0, len(the_path)):
-        (x, y) = cell_mid_from_polar(the_path[k][0], the_path[k][1], rot)
+        (x, y) = get_cell_mid(the_path[k], rot)
         if (np.absolute(x) >= 60) or (np.absolute(y) >= 60):
             break
 
-    dist        = dist_between((x, y), (0, 0))
+    distance    = dist((x, y), (0, 0))
     step_length = 60
-    gait        = (x / dist * step_length, y / dist * step_length)
+    gait        = (x / distance * step_length, y / distance * step_length)
 
     if np.sqrt(np.power(gait[0], 2) + np.power(gait[1], 2)) > np.sqrt(np.power(target[0], 2) + np.power(target[1], 2)):
         gait = target
 
     return gait
 
-def draw_field(ax, x_off, y_off):
-    ax.plot([0 +x_off, 0+x_off], [-f.y_length * 0.5 + y_off, f.y_length * 0.5 + y_off], 'white')  # Middle line
+def draw_field(ax):
+    ax.plot([0 , 0], [-f.y_length * 0.5 , f.y_length * 0.5 ], 'white')  # Middle line
 
-    ax.plot([f.x_opponent_groundline+x_off, f.x_opponent_groundline+x_off], [f.y_left_sideline+y_off, f.y_right_sideline+y_off], 'white')  # opponent ground line
-    ax.plot([f.x_own_groundline+x_off, f.x_own_groundline+x_off], [f.y_right_sideline+y_off, f.y_left_sideline+y_off], 'white')  # own ground line
+    ax.plot([f.x_opponent_groundline, f.x_opponent_groundline], [f.y_left_sideline, f.y_right_sideline], 'white')  # opponent ground line
+    ax.plot([f.x_own_groundline, f.x_own_groundline], [f.y_right_sideline, f.y_left_sideline], 'white')  # own ground line
 
-    ax.plot([f.x_own_groundline+x_off, f.x_opponent_groundline+x_off], [f.y_left_sideline+y_off, f.y_left_sideline+y_off], 'white')
-    ax.plot([f.x_own_groundline+x_off, f.x_opponent_groundline+x_off], [f.y_right_sideline+y_off, f.y_right_sideline+y_off], 'white')
+    ax.plot([f.x_own_groundline, f.x_opponent_groundline], [f.y_left_sideline, f.y_left_sideline], 'white')
+    ax.plot([f.x_own_groundline, f.x_opponent_groundline], [f.y_right_sideline, f.y_right_sideline], 'white')
 
-    ax.plot([f.x_opponent_groundline-f.x_penalty_area_length+x_off, f.x_opponent_groundline-f.x_penalty_area_length+x_off], [-f.y_penalty_area_length*0.5+y_off, f.y_penalty_area_length*0.5+y_off], 'white')  # opp penalty
-    ax.plot([f.x_opponent_groundline+x_off, f.x_opponent_groundline-f.x_penalty_area_length+x_off], [f.y_penalty_area_length*0.5+y_off, f.y_penalty_area_length*0.5+y_off], 'white')  # opp penalty
-    ax.plot([f.x_opponent_groundline+x_off, f.x_opponent_groundline-f.x_penalty_area_length+x_off], [-f.y_penalty_area_length*0.5+y_off, -f.y_penalty_area_length*0.5+y_off], 'white')  # opp penalty
+    ax.plot([f.x_opponent_groundline-f.x_penalty_area_length, f.x_opponent_groundline-f.x_penalty_area_length], [-f.y_penalty_area_length*0.5, f.y_penalty_area_length*0.5], 'white')  # opp penalty
+    ax.plot([f.x_opponent_groundline, f.x_opponent_groundline-f.x_penalty_area_length], [f.y_penalty_area_length*0.5, f.y_penalty_area_length*0.5], 'white')  # opp penalty
+    ax.plot([f.x_opponent_groundline, f.x_opponent_groundline-f.x_penalty_area_length], [-f.y_penalty_area_length*0.5, -f.y_penalty_area_length*0.5], 'white')  # opp penalty
 
-    ax.plot([f.x_own_groundline+f.x_penalty_area_length+x_off, f.x_own_groundline+f.x_penalty_area_length+x_off], [-f.y_penalty_area_length*0.5+y_off, f.y_penalty_area_length*0.5+y_off], 'white')  # own penalty
-    ax.plot([f.x_own_groundline+x_off, f.x_own_groundline+f.x_penalty_area_length+x_off], [f.y_penalty_area_length*0.5+y_off, f.y_penalty_area_length*0.5+y_off], 'white')  # own penalty
-    ax.plot([f.x_own_groundline+x_off, f.x_own_groundline+f.x_penalty_area_length+x_off], [-f.y_penalty_area_length*0.5+y_off, -f.y_penalty_area_length*0.5+y_off], 'white')  # own penalty
+    ax.plot([f.x_own_groundline+f.x_penalty_area_length, f.x_own_groundline+f.x_penalty_area_length], [-f.y_penalty_area_length*0.5, f.y_penalty_area_length*0.5], 'white')  # own penalty
+    ax.plot([f.x_own_groundline, f.x_own_groundline+f.x_penalty_area_length], [f.y_penalty_area_length*0.5, f.y_penalty_area_length*0.5], 'white')  # own penalty
+    ax.plot([f.x_own_groundline, f.x_own_groundline+f.x_penalty_area_length], [-f.y_penalty_area_length*0.5, -f.y_penalty_area_length*0.5], 'white')  # own penalty
 
     # Middle Circle
-    ax.add_artist(Circle(xy=(0+x_off, 0+y_off), radius=f.center_circle_radius, fill=False, edgecolor='white'))
+    ax.add_artist(Circle(xy=(0, 0), radius=f.center_circle_radius, fill=False, edgecolor='white'))
     # Penalty Marks
-    ax.add_artist(Circle(xy=(f.x_opponent_groundline - f.x_penalty_mark_distance+x_off, 0+y_off), radius=f.penalty_cross_radius, color='white'))
-    ax.add_artist(Circle(xy=(f.x_own_groundline + f.x_penalty_mark_distance+x_off, 0+y_off), radius=f.penalty_cross_radius, color='white'))
+    ax.add_artist(Circle(xy=(f.x_opponent_groundline - f.x_penalty_mark_distance, 0), radius=f.penalty_cross_radius, color='white'))
+    ax.add_artist(Circle(xy=(f.x_own_groundline + f.x_penalty_mark_distance, 0), radius=f.penalty_cross_radius, color='white'))
 
     # Own goal box
-    ax.add_artist(Circle(xy=(f.own_goalpost_right.x+x_off, f.own_goalpost_right.y+y_off), radius=f.goalpost_radius, color='white'))  # GoalPostRight
-    ax.add_artist(Circle(xy=(f.own_goalpost_left.x+x_off, f.own_goalpost_left.y+y_off), radius=f.goalpost_radius, color='white'))  # GoalPostLeft
-    ax.plot([f.x_own_groundline+x_off, f.x_own_groundline - f.goal_depth+x_off], [-f.goal_width*0.5+y_off, -f.goal_width*0.5+y_off], 'white')  # own goal box
-    ax.plot([f.x_own_groundline+x_off, f.x_own_groundline - f.goal_depth+x_off], [f.goal_width*0.5+y_off, f.goal_width*0.5+y_off], 'white')  # own goal box
-    ax.plot([f.x_own_groundline - f.goal_depth+x_off, f.x_own_groundline - f.goal_depth+x_off], [-f.goal_width*0.5+y_off, f.goal_width*0.5+y_off], 'white')  # own goal box
+    ax.add_artist(Circle(xy=(f.own_goalpost_right.x, f.own_goalpost_right.y), radius=f.goalpost_radius, color='white'))  # GoalPostRight
+    ax.add_artist(Circle(xy=(f.own_goalpost_left.x, f.own_goalpost_left.y), radius=f.goalpost_radius, color='white'))  # GoalPostLeft
+    ax.plot([f.x_own_groundline, f.x_own_groundline - f.goal_depth], [-f.goal_width*0.5, -f.goal_width*0.5], 'white')  # own goal box
+    ax.plot([f.x_own_groundline, f.x_own_groundline - f.goal_depth], [f.goal_width*0.5, f.goal_width*0.5], 'white')  # own goal box
+    ax.plot([f.x_own_groundline - f.goal_depth, f.x_own_groundline - f.goal_depth], [-f.goal_width*0.5, f.goal_width*0.5], 'white')  # own goal box
 
     # Opp GoalBox
-    ax.add_artist(Circle(xy=(f.opponent_goalpost_right.x+x_off, f.opponent_goalpost_right.y+y_off), radius=f.goalpost_radius, color='white'))  # GoalPostRight
-    ax.add_artist(Circle(xy=(f.opponent_goalpost_left.x+x_off, f.opponent_goalpost_left.y+y_off), radius=f.goalpost_radius, color='white'))  # GoalPostLeft
-    ax.plot([f.x_opponent_groundline+x_off, f.x_opponent_groundline + f.goal_depth+x_off], [-f.goal_width*0.5+y_off, -f.goal_width*0.5+y_off], 'white')  # Opp goal box
-    ax.plot([f.x_opponent_groundline+x_off, f.x_opponent_groundline + f.goal_depth+x_off], [f.goal_width*0.5+y_off, f.goal_width*0.5+y_off], 'white')  # Opp goal box
-    ax.plot([f.x_opponent_groundline + f.goal_depth+x_off, f.x_opponent_groundline + f.goal_depth+x_off], [-f.goal_width*0.5+y_off, f.goal_width*0.5+y_off], 'white')  # Opp goal box
+    ax.add_artist(Circle(xy=(f.opponent_goalpost_right.x, f.opponent_goalpost_right.y), radius=f.goalpost_radius, color='white'))  # GoalPostRight
+    ax.add_artist(Circle(xy=(f.opponent_goalpost_left.x, f.opponent_goalpost_left.y), radius=f.goalpost_radius, color='white'))  # GoalPostLeft
+    ax.plot([f.x_opponent_groundline, f.x_opponent_groundline + f.goal_depth], [-f.goal_width*0.5, -f.goal_width*0.5], 'white')  # Opp goal box
+    ax.plot([f.x_opponent_groundline, f.x_opponent_groundline + f.goal_depth], [f.goal_width*0.5, f.goal_width*0.5], 'white')  # Opp goal box
+    ax.plot([f.x_opponent_groundline + f.goal_depth, f.x_opponent_groundline + f.goal_depth], [-f.goal_width*0.5, f.goal_width*0.5], 'white')  # Opp goal box
 
     ax.set_axis_bgcolor('green')
 
-def draw_LPG(ax, x_off, y_off, rot):
+def draw_LPG(ax, robot_pos, rot):
     a_length = 2*math.pi / angular_part
     radius = 60000
     a = (np.arange(0, angular_part) + 0.5) * a_length
@@ -206,24 +182,24 @@ def draw_LPG(ax, x_off, y_off, rot):
     y = np.sin(a + rot) * radius
 
     # draw rings
-    for k in range(0, 30):
-        rad = inv_distance(k)
-        ax.add_artist(Circle(xy=(0+x_off, 0+y_off), radius=rad, fill=False, color='black', alpha=.25))
+    for k in range(0, 17):
+        rad = get_inv_r(k)
+        ax.add_artist(Circle(xy=(robot_pos[0], robot_pos[1]), radius=rad, fill=False, color='black', alpha=.25))
 
     # draw angular partitions
     for k in range(0, len(x)):
-        ax.plot([0+x_off, x[k]+x_off], [0+y_off, y[k]+y_off], 'black', alpha=.25)
+        ax.plot([0 + robot_pos[0], x[k] + robot_pos[0]], [0 + robot_pos[1], y[k] + robot_pos[1]], 'black', alpha=.25)
 
-def draw_waypoints(ax, waypoints, x_off, y_off, rot):
+def draw_waypoints(ax, waypoints, robot_pos, rot):
     # draw waypoint cells
     for k in waypoints:
-        (way_x, way_y) = cell_mid_from_polar(k[0], k[1], rot)
-        ax.plot(way_x+x_off, way_y+y_off, ".", c='blue')
+        (way_x, way_y) = get_cell_mid(k, rot)
+        ax.plot(way_x + robot_pos[0], way_y + robot_pos[1], ".", c='blue')
 
-def draw_obstacles(ax, x_off, y_off, obstacles):
+def draw_obstacles(ax, robot_pos, obstacles):
     # draw obstacles
     if obstacles:
         for k in obstacles:
-            ax.add_artist(Circle(xy=(k[0], k[1]), radius=k[2]+(dist_between(k, (x_off, y_off))/10) + (k[2]+(dist_between(k, (x_off, y_off))/10) * parameter_s), fill=True, color='blue', alpha=.25))
-            ax.add_artist(Circle(xy=(k[0], k[1]), radius=k[2]+dist_between(k, (x_off, y_off))/10, fill=True, color='red', alpha=.25))
+            ax.add_artist(Circle(xy=(k[0], k[1]), radius=k[2]+(dist(k, robot_pos)/10) + (k[2]+(dist(k, robot_pos)/10) * parameter_s), fill=True, color='blue', alpha=.25))
+            ax.add_artist(Circle(xy=(k[0], k[1]), radius=k[2]+dist(k, robot_pos)/10, fill=True, color='red', alpha=.25))
             ax.add_artist(Circle(xy=(k[0], k[1]), radius=10, fill=True, color='black'))
--- Utils/py/PathPlanner/run_sim_LPG.py
+++ Utils/py/PathPlanner/run_sim_LPG.py
@@ -51,12 +51,11 @@ parameter_s = 1
 obstacles = []
 target    = [0, 0]
 
-x_off = 0
-y_off = 0
+robot_pos = (0, 0)
 rot   = np.arctan2(target[1], target[0])
-rot_a = LPG.cell_angle(target, rot)
+rot_a = LPG.get_a(target, rot)
 
-orig_waypoints = LPG.compute_waypoints_LPG(target, obstacles, rot, rot_a)
+orig_waypoints = LPG.compute_waypoints(target, obstacles, rot, rot_a)
 orig_obstacles = copy.copy(obstacles)
 orig_target    = copy.copy(target)
 
@@ -81,24 +80,23 @@ while loop_bool:
     ax = plt.gca()
 
     # plot field
-    LPG.draw_field(ax, 0, 0)
+    LPG.draw_field(ax)
 
-    LPG.draw_obstacles(ax, x_off, y_off, orig_obstacles)
+    LPG.draw_obstacles(ax, robot_pos, orig_obstacles)
 
-    waypoints = LPG.compute_waypoints_LPG(target, obstacles, rot, rot_a)
-    LPG.draw_waypoints(ax, waypoints, x_off, y_off, rot)
+    waypoints = LPG.compute_waypoints(target, obstacles, rot, rot_a)
+    LPG.draw_waypoints(ax, waypoints, robot_pos, rot)
 
-    LPG.draw_LPG(ax, x_off, y_off, rot)
+    LPG.draw_LPG(ax, robot_pos, rot)
 
     # compute next part of actual path
     gait = LPG.compute_gait(waypoints, target, rot)
     actual_path.append((actual_path[len(actual_path)-1][0] + gait[0], actual_path[len(actual_path)-1][1] + gait[1]))
 
     # simulate the gait
-    x_off += math.ceil(gait[0])
-    y_off += math.ceil(gait[1])
+    robot_pos = (robot_pos[0] + math.ceil(gait[0]), robot_pos[1] + math.ceil(gait[1]))
     rot    = np.arctan2(target[1], target[0])
-    rot_a  = LPG.cell_angle(target, rot)
+    rot_a  = LPG.get_a(target, rot)
     target[0] -= math.ceil(gait[0])
     target[1] -= math.ceil(gait[1])
     for k in range(0, len(obstacles)):
@@ -112,7 +110,7 @@ while loop_bool:
     ax.plot(orig_target[0], orig_target[1], 'x', c='red')
 
     # save experiment and start next
-    if x_off == orig_target[0] and y_off == orig_target[1]:
+    if robot_pos[0] == orig_target[0] and robot_pos[1] == orig_target[1]:
         if len(argv) == 3:
             loop_bool = eval('exp_count < max_exp')
             if not loop_bool:
@@ -121,35 +119,39 @@ while loop_bool:
         print("Experiment " + str(exp_count) + ".")
 
         if exp_count > 1 and len(argv) > 1:
-            file.write(str(actual_path) + ", " + str(obstacles) + ", " + str(target) + ", " + str((x_off, y_off)))
+            file.write(str(actual_path) + ", " + str(obstacles) + ", " + str(target) + ", " + str(robot_pos))
             file.write("\n")
 
-        obstacles      = []
-        (x_off, y_off) = (0, 0)
-        actual_path    = [(0, 0)]
-        waypoints      = []
-        target         = [math.ceil(rand(-f_inf[0], f_inf[0])), math.ceil(rand(-f_inf[1], f_inf[1]))]
+        obstacles   = []
+        robot_pos   = (0, 0)
+        actual_path = [(0, 0)]
+        waypoints   = []
+        target      = [math.ceil(rand(-f_inf[0], f_inf[0])), math.ceil(rand(-f_inf[1], f_inf[1]))]
         while len(obstacles) < 9:
             obst_r = 300
             obst_x = rand(-f_inf[0], f_inf[0])
             obst_y = rand(-f_inf[1], f_inf[1])
+            if obst_x <= (obst_r + LPG.robot_radius) and obst_x >= -1*(obst_r + LPG.robot_radius) and obst_y <= (obst_r + LPG.robot_radius) and obst_y >= -1*(obst_r + LPG.robot_radius):
+                continue
+            if LPG.dist((obst_x, obst_y), target) <= obst_r + LPG.robot_radius:
+                continue
             do_add = True
             if len(obstacles) > 0:
                 for k in obstacles:
-                    if LPG.dist_between((obst_x, obst_y), k) <= 2*k[2]:
+                    if LPG.dist((obst_x, obst_y), k) <= 2*k[2]:
                         do_add = False
                         break
             if do_add:
                 obstacles.append((obst_x, obst_y, obst_r))
         for k in obstacles:
-            while LPG.dist_between(k, target) <= 2*k[2]:
+            while LPG.dist(k, target) <= 2*k[2]:
                 target         = [math.ceil(rand(-f_inf[0], f_inf[0])), math.ceil(rand(-f_inf[1], f_inf[1]))]
 
         orig_target    = copy.copy(target)
         orig_obstacles = copy.copy(obstacles)
         rot            = np.arctan2(target[1], target[0])
-        rot_a          = LPG.cell_angle(target, rot)
-        orig_waypoints = LPG.compute_waypoints_LPG(target, obstacles, rot, rot_a)
+        rot_a          = LPG.get_a(target, rot)
+        orig_waypoints = LPG.compute_waypoints(target, obstacles, rot, rot_a)
 
     ax.set_xlim([-5500, 5500])
     ax.set_ylim([-3500, 3500])
