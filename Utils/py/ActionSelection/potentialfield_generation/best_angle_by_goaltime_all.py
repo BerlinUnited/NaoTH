b00e981cad93f455c03a49b78b09a01c0735a72e
Benjamin Schlotter <benji@schlotterfam.de>
2017-06-24 19:40:16 +0200

    renamed to more descriptive names

--- Utils/py/ActionSelection/potentialfield_generation/best_angle_by_goaltime_all.py
+++ Utils/py/ActionSelection/potentialfield_generation/best_angle_by_goaltime_all.py
@@ -3,7 +3,7 @@ import math
 from matplotlib import pyplot as plt
 from naoth import math2d as m2d
 
-from potentialfield_generation.simulate_best_angle import main as simulate_best_angle
+from potentialfield_generation.best_angle_by_goaltime import main as simulate_best_angle
 from tools import field_info as field
 from tools import tools
 
