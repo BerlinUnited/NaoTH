ccb9f130371d004d5a5737d016ef697127a9bfa2
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2017-06-29 23:08:03 +0200

    make more readable

--- Utils/MatlabDeepLearning/generatorFunctions/addConvolution2Dlayer.m
+++ Utils/MatlabDeepLearning/generatorFunctions/addConvolution2Dlayer.m
@@ -18,27 +18,52 @@ function [out_dim_x, out_dim_y] = addConvolution2Dlayer(fileID, step,layer,in_di
     
     fprintf(fileID, '// determine output for this convolution step\n');
     x_out = 0;
+    
     for x = -padding_x + 1 : stride_x : in_dim_x + padding_x - filter_dim_x + 1
         y_out = 0;
         for y = -padding_y + 1 : stride_y : in_dim_y + padding_y - filter_dim_y + 1
+            
+            fprintf(fileID, 'out_step%d[%d][%d] = ', step, x_out, y_out);
+            i = 0;
             % 1 > x,y cords > in_dim
             % mat to c cords :  -1
-            i = 1;
             for f_x = 1:filter_dim_x
                 for f_y = 1:filter_dim_y
                    if (x+f_x-1 < 1 || x+f_x-1 > in_dim_x) || (y+f_y-1 < 1 || y+f_y-1 > in_dim_y) 
                        continue;
                    else
-                       cstring(i)   = string(sprintf('%d * out_step%d[%d][%d]', weights(f_x,f_y), step-1, x+f_x-1-1, y+f_y-1-1));
-                       cstring(i+1) = " + ";
-                       i = i+2;
+                       % make the file more readable
+                        if mod(i,filter_dim_x) == 0
+                            fprintf(fileID, '\n  ');
+                        end
+
+                        if i > 0 && weights(f_x,f_y) >= 0
+                            fprintf(fileID, ' + ');
+                        else
+                            fprintf(fileID, ' - ');
+                        end
+
+                       fprintf(fileID, '%.8f * out_step%d[%2d][%2d]', abs(weights(f_x,f_y)), step-1, x+f_x-1-1, y+f_y-1-1);
+                       %cstring(i)   = string(sprintf('%d * out_step%d[%d][%d]', weights(f_x,f_y), step-1, x+f_x-1-1, y+f_y-1-1));
+                       %cstring(i+1) = " + ";
+                       %i = i+2;
+                       i = i+1;
                    end
                 end
             end
             
             % 
-            fprintf(fileID, 'out_step%d[%d][%d] = (%s) + %0.5e;\n', step, x_out, y_out, strjoin(cstring(1:numel(cstring)-1)), bias);
+            %fprintf(fileID, 'out_step%d[%d][%d] = (%s) + %0.5e;\n', step, x_out, y_out, strjoin(cstring(1:numel(cstring)-1)), bias);
 
+            fprintf(fileID, '\n  ');
+            if bias >= 0
+                fprintf(fileID, ' + ');
+            else
+                fprintf(fileID, ' - ');
+            end
+
+            fprintf(fileID, '%.8f;\n\n', abs(bias));
+            
             y_out = y_out + 1;
         end
         x_out = x_out + 1;
--- Utils/MatlabDeepLearning/generatorFunctions/addReLULayer.m
+++ Utils/MatlabDeepLearning/generatorFunctions/addReLULayer.m
@@ -7,9 +7,9 @@ fprintf(fileID, 'double out_step%d[%d][%d];\n\n', step, rows, cols);
 
 fprintf(fileID, '// determine value of activation function Rectified-Linear-Unit\n');
 for x = 1:rows
-        for y = 1:cols
-            fprintf(fileID, 'out_step%d[%d][%d] = (out_step%d[%d][%d] > 0) ? out_step%d[%d][%d] : 0;\n', step, x-1, y-1, step-1, x-1, y-1,  step-1, x-1, y-1);
-        end
+    for y = 1:cols
+        fprintf(fileID, 'out_step%d[%2d][%2d] = (out_step%d[%2d][%2d] > 0) ? out_step%d[%2d][%2d] : 0;\n', step, x-1, y-1, step-1, x-1, y-1,  step-1, x-1, y-1);
+    end
 end
 
 end
