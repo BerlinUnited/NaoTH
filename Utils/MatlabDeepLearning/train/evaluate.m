f22b914e5dc3f537da7320b5b575f0f33e65e7f0
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2017-07-02 20:19:15 +0200

    small cleanups

--- Utils/MatlabDeepLearning/train/evaluate.m
+++ Utils/MatlabDeepLearning/train/evaluate.m
@@ -24,8 +24,7 @@ function evaluate(convnet, image)
             case 'nnet.cnn.layer.SoftmaxLayer'
                 activation = calc_softmax(layer, activation);
             case 'nnet.cnn.layer.ClassificationOutputLayer'
-                fprintf('[%s] missing.\n',class(layer));
-                continue;
+                activation= calc_classificationOutput(layer, activation);
             otherwise
                 fprintf('[unknown layer] %s', class(layers(i)));
                 continue;
@@ -49,13 +48,12 @@ function output = calc_ImageInputLayer(layer, input)
 end
 
 function output = calc_convolution2D(layer, input)
-    in_dim_x = size(input,1);
-    in_dim_y = size(input,2);
-    in_dim_z = size(input,3);
-    
     weights = layer.Weights;
     bias = layer.Bias;
-
+    
+    in_dim_x = size(input,1);
+    in_dim_y = size(input,2);
+    
     stride_x  = layer.Stride(1);
     stride_y  = layer.Stride(2);
     padding_x = layer.Padding(1);
@@ -63,6 +61,7 @@ function output = calc_convolution2D(layer, input)
 
     filter_dim_x = layer.FilterSize(1);
     filter_dim_y = layer.FilterSize(2);
+    channels = layer.NumChannels;
 
     out_dim_x = floor((in_dim_x - filter_dim_x + 2*padding_x)/stride_x + 1);
     out_dim_y = floor((in_dim_y - filter_dim_y + 2*padding_y)/stride_y + 1); 
@@ -78,18 +77,18 @@ function output = calc_convolution2D(layer, input)
         for x = -padding_x + 1 : stride_x : in_dim_x + padding_x - filter_dim_x + 1
             y_out = 1;
             for y = -padding_y + 1 : stride_y : in_dim_y + padding_y - filter_dim_y + 1
-
-                for z = 1:in_dim_z
+                
+                for c = 1:channels
                     for f_x = 1:filter_dim_x
                         for f_y = 1:filter_dim_y
                            if (x+f_x-1 < 1 || x+f_x-1 > in_dim_x) || (y+f_y-1 < 1 || y+f_y-1 > in_dim_y) 
                                continue;
                            else
-                               res(x_out, y_out) = res(x_out, y_out) + weights(f_x,f_y,z,i)*input(x+f_x-1, y+f_y-1, z);
+                               res(x_out, y_out) = res(x_out, y_out) + weights(f_x,f_y,c,i)*input(x+f_x-1, y+f_y-1, c);
                            end
                         end
                     end
-                end % z
+                end % c
                 
                 res(x_out, y_out) = res(x_out, y_out) + b;
                 y_out = y_out + 1;
@@ -112,15 +111,14 @@ function output = calc_fullyConnected(layer, input)
     in_dim_y = size(input,2);
     in_dim_z = size(input,3);
     
-    out_rows = layer.OutputSize;
+    out_dim = layer.OutputSize;
     
     weights  = layer.Weights;
     bias     = layer.Bias;
     
-    output = zeros(1, out_rows);
+    output = zeros(1, out_dim);
     
-    for out = 1:out_rows
-
+    for out = 1:out_dim
         % iterate over input
         for z = 1 : in_dim_z
             for x = 1 : in_dim_x
@@ -135,8 +133,14 @@ function output = calc_fullyConnected(layer, input)
     
 end
 
-function output = calc_softmax(layer, input)
+function output = calc_softmax(~, input)
     exponents = input - max(input);
     expX = exp(exponents);
     output = expX./sum(expX);
 end
+
+function output = calc_classificationOutput(~,input)
+    output = input;
+end
+
+
