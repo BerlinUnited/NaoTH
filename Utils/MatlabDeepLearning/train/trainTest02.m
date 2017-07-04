043f639f3a492b35d80504fb8c24e383406623f7
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2017-07-04 16:14:07 +0200

    new net architecture

--- Utils/MatlabDeepLearning/train/trainTest02.m
+++ Utils/MatlabDeepLearning/train/trainTest02.m
@@ -4,6 +4,9 @@ digitDatasetPath = {...
     '../data/16x16_bw/rasen-ball-robot-move-5-best-24x24',...
     '../data/16x16_bw/2016-06-29-patches-play' };
 
+digitDatasetPath = {...
+    '../data/16x16_bw/small-set'};
+
 digitData = imageDatastore(digitDatasetPath, ...
         'IncludeSubfolders',true,...
         'LabelSource','foldernames');
@@ -17,26 +20,28 @@ disp(countLabel)
 numBallPatches = countLabel.Count(1)
 %%
 trainingNumFiles = round(numBallPatches*0.75);
-rng(1) % For reproducibility
+%rng(1) % For reproducibility
 [trainDigitData,testDigitData] = splitEachLabel(digitData, ...
 				trainingNumFiles,'randomize');
             
             
-layers = [imageInputLayer([16 16 1], 'DataAugmentation',{'randfliplr','randcrop'})
-          convolution2dLayer(3, 8, 'Stride', 2)
+layers = [imageInputLayer([16 16 1], 'DataAugmentation',{'randfliplr', 'randcrop'}) % ,'randcrop'
+          convolution2dLayer(3, 8,'Stride',2)
           reluLayer
-          convolution2dLayer(3, 10, 'Stride', 2)
+          convolution2dLayer(3, 10,'Stride',2)
+          reluLayer
+          fullyConnectedLayer(64)
           reluLayer
           fullyConnectedLayer(2)
           softmaxLayer
-          classificationLayer()];
+          classificationLayer];
       
 options = trainingOptions('sgdm',...
-    'MaxEpochs',150,...
-	'InitialLearnRate',0.001,...
-    'ExecutionEnvironment', 'parallel'); % 'cpu', 'parallel'
+    'MaxEpochs',200,...
+    'MiniBatchSize',30,...
+    'ExecutionEnvironment', 'cpu'); % 'cpu', 'parallel'
 
-convnet = trainNetwork(trainDigitData,layers,options);
+convnet = trainNetwork(digitData,layers,options);
 
-save cn4.mat convnet
+save cn10.mat convnet
 
