c34fb84ae5d20e2311c1f98aaf34d6eb979b6e82
Yigit Akcay <yigit.akcay@icloud.com>
2017-08-19 21:32:59 +0200

    started building bisection algorithm

---
+++ NaoTHSoccer/Source/Cognition/Modules/Behavior/PathPlanner/BPathPlanner.cpp
@@ -0,0 +1,9 @@
+//
+//  BPathPlanner.cpp
+//  NaoTHSoccer
+//
+//  Created by Yigit Akcay on 19.08.17.
+//
+//
+
+#include "BPathPlanner.hpp"
---
+++ NaoTHSoccer/Source/Cognition/Modules/Behavior/PathPlanner/BPathPlanner.h
@@ -0,0 +1,14 @@
+//
+//  BPathPlanner.hpp
+//  NaoTHSoccer
+//
+//  Created by Yigit Akcay on 19.08.17.
+//
+//
+
+#ifndef BPathPlanner_hpp
+#define BPathPlanner_hpp
+
+#include <stdio.h>
+
+#endif /* BPathPlanner_hpp */
