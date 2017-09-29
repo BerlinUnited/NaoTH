252ff1880221dcbc3775381bb54c4c0914336bc8
Benjamin Schlotter <benji@schlotterfam.de>
2017-09-29 13:42:43 +0200

    started cleanup of python MotionAnalysis deleted some old files

--- Utils/py/MotionAnalysis/average_current_consumption.py
+++ Utils/py/MotionAnalysis/average_current_consumption.py
@@ -3,103 +3,105 @@ import BehaviorParser
 from matplotlib import pyplot
 import numpy as np
 
-JointID = {}
-JointID["HeadPitch"] = 0
-JointID["HeadYaw"] = 1
-JointID["RShoulderRoll"] = 2
-JointID["LShoulderRoll"] = 3
-JointID["RShoulderPitch"] = 4
-JointID["LShoulderPitch"] = 5
-JointID["RElbowRoll"] = 6
-JointID["LElbowRoll"] = 7
-JointID["RElbowYaw"] = 8
-JointID["LElbowYaw"] = 9
-JointID["RHipYawPitch"] = 10
-JointID["LHipYawPitch"] = 11
-JointID["RHipPitch"] = 12
-JointID["LHipPitch"] = 13
-JointID["RHipRoll"] = 14
-JointID["LHipRoll"] = 15
-JointID["RKneePitch"] = 16
-JointID["LKneePitch"] = 17
-JointID["RAnklePitch"] = 18
-JointID["LAnklePitch"] = 19
-JointID["RAnkleRoll"] = 20
-JointID["LAnkleRoll"] = 21
+JointID = {
+  "HeadPitch": 0,
+  "HeadYaw": 1,
+  "RShoulderRoll": 2,
+  "LShoulderRoll": 3,
+  "RShoulderPitch": 4,
+  "LShoulderPitch": 5,
+  "RElbowRoll": 6,
+  "LElbowRoll": 7,
+  "RElbowYaw": 8,
+  "LElbowYaw": 9,
+  "RHipYawPitch": 10,
+  "LHipYawPitch": 11,
+  "RHipPitch": 12,
+  "LHipPitch": 13,
+  "RHipRoll": 14,
+  "LHipRoll": 15,
+  "RKneePitch": 16,
+  "LKneePitch": 17,
+  "RAnklePitch": 18,
+  "LAnklePitch": 19,
+  "RAnkleRoll": 20,
+  "LAnkleRoll": 21
+}
 
 
-def frameFilter(frame):
-  m = None
-  o = None
-  try:
-    
-    if "BehaviorStateComplete" in frame.messages:
-      m, o = frame["BehaviorStateComplete"]
-    else:
-      m, o = frame["BehaviorStateSparse"]
+def frame_filter(frame):
+    m = None
+    o = None
+    try:
+        if "BehaviorStateComplete" in frame.messages:
+            m, o = frame["BehaviorStateComplete"]
+        else:
+            m, o = frame["BehaviorStateSparse"]
 
-    return [frame["FrameInfo"].time/(1000.0), 
-            frame["BodyStatus"].currentSum[JointID["RHipYawPitch"]],
-            frame["BodyStatus"].currentSum[JointID["LHipYawPitch"]],
-            frame["BodyStatus"].currentSum[JointID["RHipPitch"]],
-            frame["BodyStatus"].currentSum[JointID["LHipPitch"]],
-            frame["BodyStatus"].currentSum[JointID["RHipRoll"]],
-            frame["BodyStatus"].currentSum[JointID["LHipRoll"]],
-            frame["BodyStatus"].currentSum[JointID["RKneePitch"]], 
-            frame["BodyStatus"].currentSum[JointID["LKneePitch"]],
-            frame["BodyStatus"].currentSum[JointID["RAnklePitch"]], 
-            frame["BodyStatus"].currentSum[JointID["LAnklePitch"]],
-            frame["BodyStatus"].currentSum[JointID["RAnkleRoll"]],
-            frame["BodyStatus"].currentSum[JointID["LAnkleRoll"]],
-            m["executed_motion.type"]
-           ]
-    
-  except KeyError:
-    raise StopIteration
+        return [frame["FrameInfo"].time/1000.0,
+                frame["BodyStatus"].currentSum[JointID["RHipYawPitch"]],
+                frame["BodyStatus"].currentSum[JointID["LHipYawPitch"]],
+                frame["BodyStatus"].currentSum[JointID["RHipPitch"]],
+                frame["BodyStatus"].currentSum[JointID["LHipPitch"]],
+                frame["BodyStatus"].currentSum[JointID["RHipRoll"]],
+                frame["BodyStatus"].currentSum[JointID["LHipRoll"]],
+                frame["BodyStatus"].currentSum[JointID["RKneePitch"]],
+                frame["BodyStatus"].currentSum[JointID["LKneePitch"]],
+                frame["BodyStatus"].currentSum[JointID["RAnklePitch"]],
+                frame["BodyStatus"].currentSum[JointID["LAnklePitch"]],
+                frame["BodyStatus"].currentSum[JointID["RAnkleRoll"]],
+                frame["BodyStatus"].currentSum[JointID["LAnkleRoll"]],
+                m["executed_motion.type"]
+                ]
 
-def doStatistics(time, data):
+    except KeyError:
+        raise StopIteration
+
+
+def do_statistics(time, data):
     time_total = sum(time)
 
-    d_data = map(lambda c,t: c/t, data, time)
+    d_data = map(lambda c, t: c/t, data, time)
 
-    weighted_mean = np.average(d_data, axis = 0, weights = time/time_total)
-    weighted_var  = np.average((d_data-weighted_mean)**2, axis = 0, weights = time/time_total)
-    weighted_std  = np.sqrt(weighted_var)
+    weighted_mean = np.average(d_data, axis=0, weights=time/time_total)
+    weighted_var = np.average((d_data - weighted_mean)**2, axis=0, weights=time/time_total)
+    weighted_std = np.sqrt(weighted_var)
     return [weighted_mean,
             weighted_var,
             weighted_std,
             np.mean(d_data),
             np.var(d_data),
             np.std(d_data)
-           ]
+            ]
 
-def printStatistics(title,stats):
+
+def print_statistics(title, stats):
     print "------------------"
     print title
     print "mean (time weighted): {}".format(stats[0])
-    #print "var  (time weighted): {}".format(stats[1])
+    # print "var  (time weighted): {}".format(stats[1])
     print "std  (time weighted): {}".format(stats[2])
-    #print "mean: {}".format(stats[3])
-    #print "var : {}".format(stats[4])
-    #print "std : {}".format(stats[5])
+    # print "mean: {}".format(stats[3])
+    # print "var : {}".format(stats[4])
+    # print "std : {}".format(stats[5])
     
 
 def init():
-    #fileName = "/home/steffen/NaoTH/Logs/MesseLeipzig/lm15-naodevils-2/"
-    #fileName = fileName + "091201-0819-Nao6022"
-    fileName = "/home/steffen/NaoTH/Logs/EuropeanOpen16/2016-03-30-test-naodevils/half1/"
-    fileName = fileName + "160330-1952-Nao6022"
-    fileName = fileName + "/game.log"
+    # file_name = "/home/steffen/NaoTH/Logs/MesseLeipzig/lm15-naodevils-2/"
+    # file_name = file_name + "091201-0819-Nao6022"
+    file_name = "/home/steffen/NaoTH/Logs/EuropeanOpen16/2016-03-30-test-naodevils/half1/"
+    file_name = file_name + "160330-1952-Nao6022"
+    file_name = file_name + "/game.log"
     parser = BehaviorParser.BehaviorParser()
-    log = BehaviorParser.LogReader(fileName, parser)
+    log = BehaviorParser.LogReader(file_name, parser)
     
     # enforce the whole log being parsed (this is necessary for older game logs)
     for frame in log:
-      if "BehaviorStateComplete" in frame.messages:
-        m = frame["BehaviorStateComplete"]
-      if "BehaviorStateSparse" in frame.messages:
-        m = frame["BehaviorStateSparse"]
-      frame["FrameInfo"]
+        if "BehaviorStateComplete" in frame.messages:
+            m = frame["BehaviorStateComplete"]
+        if "BehaviorStateSparse" in frame.messages:
+            m = frame["BehaviorStateSparse"]
+        frame["FrameInfo"]
       
     return log
     
@@ -113,33 +115,33 @@ def run(log):
     vlog = filter(lambda f: "BehaviorStateComplete" in f.messages or "BehaviorStateSparse" in f.messages, log)
    
     # apply the filter
-    a = map(frameFilter, vlog)
+    a = map(frame_filter, vlog)
    
     # make an numpy array
     data = np.array(a)
     
     size = data.shape
 
-    data2 = np.empty([0,14])
+    data2 = np.empty([0, 14])
 
     current = 0
     motiontypeIndex = 13
-    for x in xrange(1,size[0]) :
-        if data[current,motiontypeIndex] == data[x,motiontypeIndex] :
+    for x in xrange(1, size[0]):
+        if data[current, motiontypeIndex] == data[x, motiontypeIndex]:
             continue
-        elif (data[current,motiontypeIndex] == 4 or data[current,motiontypeIndex] == 5) :
-            temp = data[x,:]-data[current,:]
-            temp[motiontypeIndex] = data[current,motiontypeIndex]
+        elif data[current, motiontypeIndex] == 4 or data[current, motiontypeIndex] == 5:
+            temp = data[x, :]-data[current, :]
+            temp[motiontypeIndex] = data[current, motiontypeIndex]
             current = x
-            data2 = np.vstack((data2,temp))
-        else :
+            data2 = np.vstack((data2, temp))
+        else:
             current = x
     
     stand = [x for x in data2 if x[motiontypeIndex] == 4]
-    walk  = [x for x in data2 if x[motiontypeIndex] == 5]
+    walk = [x for x in data2 if x[motiontypeIndex] == 5]
 
     tr_stand = zip(*stand)
-    tr_walk  = zip(*walk)
+    tr_walk = zip(*walk)
     
     time_stand = sum(tr_stand[0])
     time_walk = sum(tr_walk[0])
@@ -149,89 +151,89 @@ def run(log):
     print "time walking : {}".format(time_walk)
     print "time total   : {}".format(time_stand + time_walk)
 
-    #RHipYawPitch
-    d_stand_RHipYawPitch_stats = doStatistics(tr_stand[0], tr_stand[1])
-    printStatistics("RHipYawPitch - stand", d_stand_RHipYawPitch_stats)
+    # RHipYawPitch
+    d_stand_RHipYawPitch_stats = do_statistics(tr_stand[0], tr_stand[1])
+    print_statistics("RHipYawPitch - stand", d_stand_RHipYawPitch_stats)
 
-    d_walk_RHipYawPitch_stats = doStatistics(tr_walk[0], tr_walk[1])
-    printStatistics("RHipYawPitch - walk", d_walk_RHipYawPitch_stats)
+    d_walk_RHipYawPitch_stats = do_statistics(tr_walk[0], tr_walk[1])
+    print_statistics("RHipYawPitch - walk", d_walk_RHipYawPitch_stats)
 
-    #LHipYawPitch
-    d_stand_LHipYawPitch_stats = doStatistics(tr_stand[0], tr_stand[2])
-    printStatistics("LHipYawPitch - stand", d_stand_LHipYawPitch_stats)
+    # LHipYawPitch
+    d_stand_LHipYawPitch_stats = do_statistics(tr_stand[0], tr_stand[2])
+    print_statistics("LHipYawPitch - stand", d_stand_LHipYawPitch_stats)
 
-    d_walk_LHipYawPitch_stats = doStatistics(tr_walk[0], tr_walk[2])
-    printStatistics("LHipYawPitch - walk", d_walk_LHipYawPitch_stats)
+    d_walk_LHipYawPitch_stats = do_statistics(tr_walk[0], tr_walk[2])
+    print_statistics("LHipYawPitch - walk", d_walk_LHipYawPitch_stats)
 
-    #RHipPitch
-    d_stand_RHipPitch_stats = doStatistics(tr_stand[0], tr_stand[3])
-    printStatistics("RHipPitch - stand", d_stand_RHipPitch_stats)
+    # RHipPitch
+    d_stand_RHipPitch_stats = do_statistics(tr_stand[0], tr_stand[3])
+    print_statistics("RHipPitch - stand", d_stand_RHipPitch_stats)
 
-    d_walk_RHipPitch_stats = doStatistics(tr_walk[0], tr_walk[3])
-    printStatistics("RHipPitch - walk", d_walk_RHipPitch_stats)
+    d_walk_RHipPitch_stats = do_statistics(tr_walk[0], tr_walk[3])
+    print_statistics("RHipPitch - walk", d_walk_RHipPitch_stats)
 
-    #LHipPitch
-    d_stand_LHipPitch_stats = doStatistics(tr_stand[0], tr_stand[4])
-    printStatistics("LHipPitch - stand", d_stand_LHipPitch_stats)
+    # LHipPitch
+    d_stand_LHipPitch_stats = do_statistics(tr_stand[0], tr_stand[4])
+    print_statistics("LHipPitch - stand", d_stand_LHipPitch_stats)
 
-    d_walk_LHipPitch_stats = doStatistics(tr_walk[0], tr_walk[4])
-    printStatistics("LHipPitch - walk", d_walk_LHipPitch_stats)
+    d_walk_LHipPitch_stats = do_statistics(tr_walk[0], tr_walk[4])
+    print_statistics("LHipPitch - walk", d_walk_LHipPitch_stats)
 
-    #RHipRoll
-    d_stand_RHipRoll_stats = doStatistics(tr_stand[0], tr_stand[5])
-    printStatistics("RHipRoll - stand", d_stand_RHipRoll_stats)
+    # RHipRoll
+    d_stand_RHipRoll_stats = do_statistics(tr_stand[0], tr_stand[5])
+    print_statistics("RHipRoll - stand", d_stand_RHipRoll_stats)
 
-    d_walk_RHipRoll_stats = doStatistics(tr_walk[0], tr_walk[5])
-    printStatistics("RHipRoll - walk", d_walk_RHipRoll_stats)
+    d_walk_RHipRoll_stats = do_statistics(tr_walk[0], tr_walk[5])
+    print_statistics("RHipRoll - walk", d_walk_RHipRoll_stats)
 
-    #LHipRoll
-    d_stand_LHipRoll_stats = doStatistics(tr_stand[0], tr_stand[6])
-    printStatistics("LHipRoll - stand", d_stand_LHipRoll_stats)
+    # LHipRoll
+    d_stand_LHipRoll_stats = do_statistics(tr_stand[0], tr_stand[6])
+    print_statistics("LHipRoll - stand", d_stand_LHipRoll_stats)
 
-    d_walk_LHipRoll_stats = doStatistics(tr_walk[0], tr_walk[6])
-    printStatistics("LHipRoll - walk", d_walk_LHipRoll_stats)
+    d_walk_LHipRoll_stats = do_statistics(tr_walk[0], tr_walk[6])
+    print_statistics("LHipRoll - walk", d_walk_LHipRoll_stats)
 
-    #RKneePitch
-    d_stand_RKneePitch_stats = doStatistics(tr_stand[0], tr_stand[7])
-    printStatistics("RKneePitch - stand", d_stand_RKneePitch_stats)
+    # RKneePitch
+    d_stand_RKneePitch_stats = do_statistics(tr_stand[0], tr_stand[7])
+    print_statistics("RKneePitch - stand", d_stand_RKneePitch_stats)
 
-    d_walk_RKneePitch_stats = doStatistics(tr_walk[0], tr_walk[7])
-    printStatistics("RKneePitch - walk", d_walk_RKneePitch_stats)
+    d_walk_RKneePitch_stats = do_statistics(tr_walk[0], tr_walk[7])
+    print_statistics("RKneePitch - walk", d_walk_RKneePitch_stats)
 
-    #LKneePitch
-    d_stand_LKneePitch_stats = doStatistics(tr_stand[0], tr_stand[8])
-    printStatistics("LKneePitch - stand", d_stand_LKneePitch_stats)
+    # LKneePitch
+    d_stand_LKneePitch_stats = do_statistics(tr_stand[0], tr_stand[8])
+    print_statistics("LKneePitch - stand", d_stand_LKneePitch_stats)
 
-    d_walk_LKneePitch_stats = doStatistics(tr_walk[0], tr_walk[8])
-    printStatistics("LKneePitch - walk", d_walk_LKneePitch_stats)
+    d_walk_LKneePitch_stats = do_statistics(tr_walk[0], tr_walk[8])
+    print_statistics("LKneePitch - walk", d_walk_LKneePitch_stats)
 
-    #RAnklePitch
-    d_stand_RAnklePitch_stats = doStatistics(tr_stand[0], tr_stand[9])
-    printStatistics("RAnklePitch - stand", d_stand_RAnklePitch_stats)
+    # RAnklePitch
+    d_stand_RAnklePitch_stats = do_statistics(tr_stand[0], tr_stand[9])
+    print_statistics("RAnklePitch - stand", d_stand_RAnklePitch_stats)
 
-    d_walk_RAnklePitch_stats = doStatistics(tr_walk[0], tr_walk[9])
-    printStatistics("RAnklePitch - walk", d_walk_RAnklePitch_stats)
+    d_walk_RAnklePitch_stats = do_statistics(tr_walk[0], tr_walk[9])
+    print_statistics("RAnklePitch - walk", d_walk_RAnklePitch_stats)
    
-    #LAnklePitch
-    d_stand_LAnklePitch_stats = doStatistics(tr_stand[0], tr_stand[10])
-    printStatistics("LAnklePitch - stand", d_stand_LAnklePitch_stats)
+    # LAnklePitch
+    d_stand_LAnklePitch_stats = do_statistics(tr_stand[0], tr_stand[10])
+    print_statistics("LAnklePitch - stand", d_stand_LAnklePitch_stats)
 
-    d_walk_LAnklePitch_stats = doStatistics(tr_walk[0], tr_walk[10])
-    printStatistics("LAnklePitch - walk", d_walk_LAnklePitch_stats)
+    d_walk_LAnklePitch_stats = do_statistics(tr_walk[0], tr_walk[10])
+    print_statistics("LAnklePitch - walk", d_walk_LAnklePitch_stats)
     
-    #RAnkleRoll
-    d_stand_RAnkleRoll_stats = doStatistics(tr_stand[0], tr_stand[11])
-    printStatistics("RAnkleRoll - stand", d_stand_RAnkleRoll_stats)
+    # RAnkleRoll
+    d_stand_RAnkleRoll_stats = do_statistics(tr_stand[0], tr_stand[11])
+    print_statistics("RAnkleRoll - stand", d_stand_RAnkleRoll_stats)
 
-    d_walk_RAnkleRoll_stats = doStatistics(tr_walk[0], tr_walk[11])
-    printStatistics("RAnkleRoll - walk", d_walk_RAnkleRoll_stats)
+    d_walk_RAnkleRoll_stats = do_statistics(tr_walk[0], tr_walk[11])
+    print_statistics("RAnkleRoll - walk", d_walk_RAnkleRoll_stats)
 
-    #LAnkleRoll
-    d_stand_LAnkleRoll_stats = doStatistics(tr_stand[0], tr_stand[12])
-    printStatistics("LAnkleRoll - stand", d_stand_LAnkleRoll_stats)
+    # LAnkleRoll
+    d_stand_LAnkleRoll_stats = do_statistics(tr_stand[0], tr_stand[12])
+    print_statistics("LAnkleRoll - stand", d_stand_LAnkleRoll_stats)
 
-    d_walk_LAnkleRoll_stats = doStatistics(tr_walk[0], tr_walk[12])
-    printStatistics("LAnkleRoll - walk", d_walk_LAnkleRoll_stats)
+    d_walk_LAnkleRoll_stats = do_statistics(tr_walk[0], tr_walk[12])
+    print_statistics("LAnkleRoll - walk", d_walk_LAnkleRoll_stats)
 
     stand2 = [x for x in a if x[13] == 4]
     walk2 = [x for x in a if x[13] == 5]
@@ -250,11 +252,12 @@ def run(log):
     
     pyplot.plot(d_stand[0], d_stand[10], ".", label="LAnklePitch-stand")
     pyplot.plot(d_walk[0], d_walk[10], ".", label="LAnklePitch-walk")
-    #bb = zip(*data)
-    #pyplot.plot(bb[0], bb[8], label="temperatur right")
-    #pyplot.plot(bb[0], bb[7], label="temperatur left")
-    #pyplot.plot(bb[0], bb[10], label="game")
-    #pyplot.plot(bb[0], bb[5], label="motion")
+
+    # bb = zip(*data)
+    # pyplot.plot(bb[0], bb[8], label="temperatur right")
+    # pyplot.plot(bb[0], bb[7], label="temperatur left")
+    # pyplot.plot(bb[0], bb[10], label="game")
+    # pyplot.plot(bb[0], bb[5], label="motion")
     
     pyplot.legend(loc='upper left')
     pyplot.grid()
--- Utils/py/MotionAnalysis/parse_power_cumsum.py
+++ Utils/py/MotionAnalysis/parse_power_cumsum.py
@@ -1,131 +1,125 @@
 import struct
-import sys
 
 # protobuf
-from CommonTypes_pb2 import *
-from Framework_Representations_pb2 import *
-from Messages_pb2 import *
-from Representations_pb2 import *
-from google.protobuf import text_format
+from naoth.CommonTypes_pb2 import *
+from naoth.Framework_Representations_pb2 import *
+from naoth.Messages_pb2 import *
+from naoth.Representations_pb2 import *
 
-import matplotlib
-#matplotlib.use('Qt4Agg')
-#matplotlib.use('TkAgg')
-#from matplotlib.backends import qt_compat
 from matplotlib import pyplot as plt
 from matplotlib import rcParams
 import numpy as np
 
 if __name__ == "__main__":
     
-  rcParams["font.family"] = "serif"
-  rcParams["xtick.labelsize"] = 10
-  rcParams["ytick.labelsize"] = 10
-  rcParams["axes.labelsize"] = 10
-  rcParams["axes.titlesize"] = 10
-  rcParams["legend.fontsize"] = 10
+    rcParams["font.family"] = "serif"
+    rcParams["xtick.labelsize"] = 10
+    rcParams["ytick.labelsize"] = 10
+    rcParams["axes.labelsize"] = 10
+    rcParams["axes.titlesize"] = 10
+    rcParams["legend.fontsize"] = 10
 
-  JointID = {}
-  JointID["HeadPitch"] = 0
-  JointID["HeadYaw"] = 1
-  JointID["RShoulderRoll"] = 2
-  JointID["LShoulderRoll"] = 3
-  JointID["RShoulderPitch"] = 4
-  JointID["LShoulderPitch"] = 5
-  JointID["RElbowRoll"] = 6
-  JointID["LElbowRoll"] = 7
-  JointID["RElbowYaw"] = 8
-  JointID["LElbowYaw"] = 9
-  JointID["RHipYawPitch"] = 10
-  JointID["LHipYawPitch"] = 11
-  JointID["RHipPitch"] = 12
-  JointID["LHipPitch"] = 13
-  JointID["RHipRoll"] = 14
-  JointID["LHipRoll"] = 15
-  JointID["RKneePitch"] = 16
-  JointID["LKneePitch"] = 17
-  JointID["RAnklePitch"] = 18
-  JointID["LAnklePitch"] = 19
-  JointID["RAnkleRoll"] = 20
-  JointID["LAnkleRoll"] = 21
-  
+    JointID = {
+      "HeadPitch": 0,
+      "HeadYaw": 1,
+      "RShoulderRoll": 2,
+      "LShoulderRoll": 3,
+      "RShoulderPitch": 4,
+      "LShoulderPitch": 5,
+      "RElbowRoll": 6,
+      "LElbowRoll": 7,
+      "RElbowYaw": 8,
+      "LElbowYaw": 9,
+      "RHipYawPitch": 10,
+      "LHipYawPitch": 11,
+      "RHipPitch": 12,
+      "LHipPitch": 13,
+      "RHipRoll": 14,
+      "LHipRoll": 15,
+      "RKneePitch": 16,
+      "LKneePitch": 17,
+      "RAnklePitch": 18,
+      "LAnklePitch": 19,
+      "RAnkleRoll": 20,
+      "LAnkleRoll": 21
+    }
 
-  slopes = []
+    slopes = []
 
-  for filename in sys.argv[1:]:
-    inFile = open(filename, "rb")
-    
-    mjd = []
-    sjd = []
-    mrd = []
+    for filename in sys.argv[1:]:
+      inFile = open(filename, "rb")
 
-    while True :
-      try:
-        currentFrameNumber = struct.unpack("=l", inFile.read(4))[0]
-       
-        # name is a '\0' terminated string
-        currentName = ""
-        c = struct.unpack("=c", inFile.read(1))[0]
-        while c != "\0":
-          currentName = currentName + c
+      mjd = []
+      sjd = []
+      mrd = []
+
+      while True :
+        try:
+          currentFrameNumber = struct.unpack("=l", inFile.read(4))[0]
+
+          # name is a '\0' terminated string
+          currentName = ""
           c = struct.unpack("=c", inFile.read(1))[0]
+          while c != "\0":
+            currentName = currentName + c
+            c = struct.unpack("=c", inFile.read(1))[0]
 
-        currentSize = struct.unpack("=l", inFile.read(4))[0]
-      
-        if currentName == "SensorJointData":
-          data = inFile.read(currentSize)
-          msg = globals()["SensorJointData"]()
-          msg.ParseFromString(data)
-          sjd.append([currentFrameNumber, msg])
-        elif currentName == "MotorJointData":
-          data = inFile.read(currentSize)
-          msg = globals()["JointData"]()
-          msg.ParseFromString(data)
-          mjd.append([currentFrameNumber, msg])
-        elif currentName == "MotionRequest":
-          data = inFile.read(currentSize)
-          msg = globals()["MotionRequest"]()
-          msg.ParseFromString(data)
-          mrd.append([currentFrameNumber, msg])
-          
-        else:
-          inFile.seek(currentSize,1)
-          continue
-         
-      except Exception as ex:
-        print ex
-        break
+          currentSize = struct.unpack("=l", inFile.read(4))[0]
 
-    
-    fm_s = np.array([jd[0]-sjd[0][0] for jd in sjd])
-  
-    # filter
-    filters = ["Knee", "Ankle", "Hip"]
+          if currentName == "SensorJointData":
+            data = inFile.read(currentSize)
+            msg = globals()["SensorJointData"]()
+            msg.ParseFromString(data)
+            sjd.append([currentFrameNumber, msg])
+          elif currentName == "MotorJointData":
+            data = inFile.read(currentSize)
+            msg = globals()["JointData"]()
+            msg.ParseFromString(data)
+            mjd.append([currentFrameNumber, msg])
+          elif currentName == "MotionRequest":
+            data = inFile.read(currentSize)
+            msg = globals()["MotionRequest"]()
+            msg.ParseFromString(data)
+            mrd.append([currentFrameNumber, msg])
 
-    currents = [[jd[1].electricCurrent[JointID[joint]] for jd in sjd] for joint in JointID.keys() if sum([1 for fil in filters if fil in joint]) > 0]
-    current = np.sum(np.vstack(currents), 0)
+          else:
+            inFile.seek(currentSize,1)
+            continue
 
-    # plot data
-    plt.plot(fm_s, np.cumsum(current), label=filename)
-   
-    # this is to discard the start and end
-    pad = int(0.1*len(fm_s))
-    # fit linear model for slopes
-    p = np.polyfit(fm_s[pad:-pad], np.cumsum(current)[pad:-pad], 1)
-    slopes.append([filename, p[0]])
-    # plot fit
-    x = np.linspace(fm_s[pad], fm_s[-pad], 100)
-    y = np.polyval(p, x)
-    plt.plot(x,y, "k:")
-  
-  slopes.sort(key=lambda x:x[1])
-  # norm to lowest
-  norm = slopes[0][1]
-  for i in range(len(slopes)):
-    slopes[i][1] = slopes[i][1] / norm
-  for filename, m in slopes:
-    print filename, m
+        except Exception as ex:
+          print ex
+          break
 
-  plt.grid()
-  plt.legend()
-  plt.show()
+
+      fm_s = np.array([jd[0]-sjd[0][0] for jd in sjd])
+
+      # filter
+      filters = ["Knee", "Ankle", "Hip"]
+
+      currents = [[jd[1].electricCurrent[JointID[joint]] for jd in sjd] for joint in JointID.keys() if sum([1 for fil in filters if fil in joint]) > 0]
+      current = np.sum(np.vstack(currents), 0)
+
+      # plot data
+      plt.plot(fm_s, np.cumsum(current), label=filename)
+
+      # this is to discard the start and end
+      pad = int(0.1*len(fm_s))
+      # fit linear model for slopes
+      p = np.polyfit(fm_s[pad:-pad], np.cumsum(current)[pad:-pad], 1)
+      slopes.append([filename, p[0]])
+      # plot fit
+      x = np.linspace(fm_s[pad], fm_s[-pad], 100)
+      y = np.polyval(p, x)
+      plt.plot(x,y, "k:")
+
+    slopes.sort(key=lambda x:x[1])
+    # norm to lowest
+    norm = slopes[0][1]
+    for i in range(len(slopes)):
+      slopes[i][1] = slopes[i][1] / norm
+    for filename, m in slopes:
+      print filename, m
+
+    plt.grid()
+    plt.legend()
+    plt.show()
