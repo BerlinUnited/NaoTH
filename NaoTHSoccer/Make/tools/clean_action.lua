b65c3a1805d9e61299aadfbd79a239c85bcc04c8
Benjamin Schlotter <benji@schlotterfam.de>
2017-10-02 08:31:31 +0200

    added warning message when build or dist folder can not be deleted

--- NaoTHSoccer/Make/tools/clean_action.lua
+++ NaoTHSoccer/Make/tools/clean_action.lua
@@ -16,9 +16,15 @@ newaction {
 	execute = function()
 		if os.isdir("../build") then
 			os.rmdir("../build")
+      if os.isdir("../build") then
+        premake.warn("Could not delete build folder. Please manually delete it.")
+      end
 		end
 		if os.isdir("../dist") then
 			os.rmdir("../dist")
+      if os.isdir("../dist") then
+        premake.warn("Could not delete dist folder. Please manually delete it.")
+      end
 		end
 		
 		-- force protobuf to recompile
