a8a5ad702d975e813e08d11a82b4fb6591637fda
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2017-10-12 10:42:26 +0200

    implement the simulator as a class for easier data collection new implementation for the choice of the direction for the current strategy

--- Utils/py/ActionSelection/compare_decision_schemes/play_striker.py
+++ Utils/py/ActionSelection/compare_decision_schemes/play_striker.py
@@ -6,6 +6,7 @@ if cmd_subfolder not in sys.path:
 
 import sys
 import math
+import copy
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.patches import Circle
@@ -21,16 +22,39 @@ from run_simulation_with_particleFilter import calculate_best_direction as heinr
 
 
 def direct_kick_strategy(state, action_list):
+  
+  turn_speed = math.radians(5.0)
   action_dir = 0
-  actions_consequences = []
-  # Simulate Consequences
-  for action in action_list:
-      single_consequence = a.ActionResults([])
-      actions_consequences.append(Sim.simulate_consequences(action, single_consequence, state, num_particles=30))
+  turn_direction = 0
+  
+  while True:
+  
+    # turn towards the direction of the action
+    state.pose.rotate(action_dir)
+  
+    actions_consequences = []
+    # Simulate Consequences
+    for action in action_list:
+        single_consequence = a.ActionResults([])
+        actions_consequences.append(Sim.simulate_consequences(action, single_consequence, state, num_particles=30))
 
-  # Decide best action
-  selected_action_idx = Sim.decide_smart(actions_consequences, state)
+    # Decide best action
+    selected_action_idx = Sim.decide_smart(actions_consequences, state)
 
+    # restore the previous oriantation
+    state.pose.rotate(-action_dir)
+    
+    if selected_action_idx != 0: 
+      break
+    else:
+      attack_direction = attack_dir.get_attack_direction(state)
+
+      if turn_direction == 0:
+        turn_direction = np.sign(attack_direction) # "> 0" => left, "< 0" => right
+    
+      # set motion request
+      action_dir += turn_direction*turn_speed
+  
   return selected_action_idx, action_dir
 
   
@@ -51,68 +75,71 @@ def optimal_kick_strategy(state, action_list):
 
   return selected_action_idx, action_dir
   
+
   
-def run(state, strategy, action_list):
+class Simulator:
+  def __init__(self, state, strategy, action_list):
+    self.state = state
+    self.strategy = strategy
+    self.action_list = action_list
+    
+    # 
+    self.goal_scored = None
+    self.inside_field = None
+    
+    # actions
+    self.selected_action_idx = None
+    self.turn_around_ball = 0
+    self.rotate = 0
+    self.walk_dist = 0
   
-  run_finished = False
   
-  turn_direction = 0
-  turn_speed = 1
+  def step(self):
   
-  history = [[state.pose.translation.x, state.pose.translation.y]]
-  
-  while not run_finished:
+    # reset stuff
+    self.turn_around_ball = 0
+    self.rotate = 0
+    self.walk_dist = 0
     
     # ---------------
     #  make a decision
     # ---------------
     
-    selected_action_idx, action_dir = strategy(state, action_list)
-    selected_action = action_list[selected_action_idx]
-    
-    print ("action: " + selected_action.name)
+    self.selected_action_idx, self.turn_around_ball = self.strategy(self.state, self.action_list)
+    selected_action = self.action_list[self.selected_action_idx]
     
     # ---------------
     #  execute the action
     # ---------------
     
     if selected_action.name == "none":
-      
-      attack_direction = attack_dir.get_attack_direction(state)
-
-      if turn_direction == 0:
-        turn_direction = np.sign(attack_direction) # "> 0" => left, "< 0" => right
-      
-      state.update_pos(state.pose.translation, math.degrees(state.pose.rotation) + turn_direction*turn_speed)
-
+      print("WARNING: action is NONE, what should we do here? " + str(self.selected_action_idx))
     else:
       # NOTE: reset turn direction
-      turn_direction = 0
+      self.turn_direction = 0
 
       # rotate around ball if necessary
-      state.pose.rotation += action_dir
+      self.state.pose.rotate(self.turn_around_ball)
       
       # create a new action which will actually be executed
       new_action = a.Action("new_action", selected_action.speed, selected_action.speed_std, selected_action.angle, 0)
             
       actions_consequences = []
       single_consequence = a.ActionResults([])
-      single_consequence = Sim.simulate_consequences(new_action, single_consequence, state, num_particles=30)
+      single_consequence = Sim.simulate_consequences(new_action, single_consequence, self.state, num_particles=30)
 
       # expected_ball_pos should be in local coordinates for rotation calculations
       new_ball_position = single_consequence.expected_ball_pos_mean
 
-      
       # walk to ball now
-      rotation_to_ball = new_ball_position.angle()
-      #distance_to_ball = new_ball_position.abs()
+      # set motion request
+      self.rotate = new_ball_position.angle()
+      self.walk_dist = new_ball_position.abs()
       
-      
-      state.update_pos(state.pose * new_ball_position, math.degrees(state.pose.rotation + rotation_to_ball))
+      #self.state.update_pos(self.state.pose * new_ball_position, math.degrees(self.state.pose.rotation + self.rotate))
+      self.state.pose.rotate(self.rotate)
+      self.state.pose.translate(self.walk_dist,0)    
 
-      history += [[state.pose.translation.x, state.pose.translation.y]]
-      
-      
     # ---------------
     #  evaluate the real situation
     # ---------------
@@ -121,15 +148,29 @@ def run(state, strategy, action_list):
     opp_goal_back_right = m2d.Vector2(field.opponent_goalpost_right.x + field.goal_depth, field.opponent_goalpost_right.y)
     opp_goal_box = m2d.Rect2d(opp_goal_back_right, field.opponent_goalpost_left)
 
-    ball_pos_global = state.pose * state.ball_position
+    ball_pos_global = self.state.pose * self.state.ball_position
     
-    goal_scored = opp_goal_box.inside(ball_pos_global)
-    inside_field = field.field_rect.inside(ball_pos_global)
+    # update the internal state
+    self.goal_scored = opp_goal_box.inside(ball_pos_global)
+    self.inside_field = field.field_rect.inside(ball_pos_global)
     
-    if goal_scored:
+    
+def run(state, strategy, action_list):
+  
+  simulator = Simulator(state, strategy, action_list)
+  
+  history = [[state.pose.translation.x, state.pose.translation.y]]
+  
+  while True:
+    
+    simulator.step()
+    
+    history += [[simulator.state.pose.translation.x, simulator.state.pose.translation.y]]
+    
+    if simulator.goal_scored:
       #print ("success")
       break
-    elif not inside_field:
+    elif not simulator.inside_field:
       #print ("failure")
       break
     else:
@@ -138,8 +179,9 @@ def run(state, strategy, action_list):
        
   return history
 
+  
 def select(actions, ids):
-  return [actions[x] for x in actions if x in ids]
+  return [actions[x] for x in ids]
 
 if __name__ == "__main__":
     
@@ -152,19 +194,26 @@ if __name__ == "__main__":
     }
 
     all_actions = select(actions, ["none", "kick_short", "sidekick_left", "sidekick_right"])
+    #print ([a.name for a in all_actions])
     
     # repeat it multiple times to see how reliable the result is
     for i in range(10):
-        state = State()
-        state.pose.rotation = np.radians(180)
+    
+        origin = State()
+        origin.pose.rotation = np.radians(70)
+        origin.pose.translation.x = 3000
+        origin.pose.translation.y = 2500
+        
+        state = copy.deepcopy(origin)
+        print("run optimal_kick_strategy")
         history1 = run(state, optimal_kick_strategy, select(actions, ["none", "kick_short"]))
         
-        state = State()
-        state.pose.rotation = np.radians(180)
+        state = copy.deepcopy(origin)
+        print("run optimal_kick_strategy")
         history2 = run(state, optimal_kick_strategy, all_actions)
         
-        state = State()
-        state.pose.rotation = np.radians(180)
+        state = copy.deepcopy(origin)
+        print("run direct_kick_strategy")
         history3 = run(state, direct_kick_strategy, all_actions)
         
         plt.clf()
