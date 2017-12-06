e81dd7b9e012d674f729529a5cedb0df4a0b554b
Steffen Kaden <kaden@informatik.hu-berlin.de>
2017-12-06 17:20:12 +0100

    fix performance test and add sse test

--- NaoTHSoccer/Test/Source/EigenPerformance/EigenPerformance.cpp
+++ NaoTHSoccer/Test/Source/EigenPerformance/EigenPerformance.cpp
@@ -2,15 +2,20 @@
 #include <iostream>
 #include <chrono>
 
-using Eigen::Matrix3d;
+using Eigen::Matrix3f;
 using Eigen::Matrix;
 
 typedef std::chrono::high_resolution_clock::time_point time_point;
 typedef std::chrono::high_resolution_clock::duration duration;
 
-int main(int /*argc*/, char** /*argv*/){
-    unsigned long long max_counter = 1e6;
+unsigned long long max_counter = 1e6;
 
+// the compiler is not allowed to optimize out global variables
+Matrix3f inverse3x3;
+Matrix<float,6,6> inverse6x6;
+Matrix<float,4,100> y;
+
+void test_invert(){
     time_point begin;
     time_point end;
 
@@ -18,14 +23,12 @@ int main(int /*argc*/, char** /*argv*/){
     unsigned long long counter = 0;
 
     while (counter < max_counter){
-        Matrix3d test;
-        Matrix3d inverse;
-
-        test << Matrix3d::Random();
+        Matrix3f test;
+        test << Matrix3f::Random();
 
         if(test.determinant()){
             begin = std::chrono::high_resolution_clock::now();
-            inverse = test.inverse();
+            inverse3x3 = test.inverse();
             end = std::chrono::high_resolution_clock::now();
 
             // we assume, that difference doesn't exeed the size of int
@@ -46,14 +49,13 @@ int main(int /*argc*/, char** /*argv*/){
     counter = 0;
 
     while (counter < max_counter ){
-        Matrix<double,6,6> test;
-        Matrix<double,6,6> inverse;
+        Matrix<float,6,6> test;
 
-        test << Matrix<double,6,6>::Random();
+        test << Matrix<float,6,6>::Random();
 
         if(test.determinant()){
             begin = std::chrono::high_resolution_clock::now();
-            inverse = test.inverse();
+            inverse6x6 = test.inverse();
             end = std::chrono::high_resolution_clock::now();
 
             // we assume, that difference doesn't exeed the size of int
@@ -69,6 +71,43 @@ int main(int /*argc*/, char** /*argv*/){
 
     std::cout << std::endl;
     std::cout << "average time 6x6: " << sum.count()/max_counter << " ns" << std::endl;
+}
+
+void test_sse(){
+    time_point begin;
+    time_point end;
+
+    duration sum = duration::zero();
+    unsigned long long counter = 0;
+
+    Matrix<float, 4,4> A;
+    Matrix<float, 4,100> x;
+
+    while (counter < max_counter ){
+        A << Matrix<float,4,4>::Random();
+        x << Matrix<float,4,100>::Random();
+
+        begin = std::chrono::high_resolution_clock::now();
+        y = A*x;
+        end = std::chrono::high_resolution_clock::now();
+
+        // we assume, that difference doesn't exeed the size of int
+        sum += end - begin;
+
+        counter++;
+
+        if(counter%(max_counter/10) == 0){
+            std::cout << "." << std::flush;
+        }
+    }
+
+    std::cout << std::endl;
+    std::cout << "average time 4x4*4x100 float: " << sum.count()/max_counter << " ns" << std::endl;
+}
+
+int main(int /*argc*/, char** /*argv*/){
+    test_invert();
+    test_sse();
 
     return 0;
 }
