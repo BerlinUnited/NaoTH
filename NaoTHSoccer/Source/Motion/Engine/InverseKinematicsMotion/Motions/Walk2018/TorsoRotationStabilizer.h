9735b20b4143e63884eb56f761dd37bad4c02e82
Steffen Kaden <kaden@informatik.hu-berlin.de>
2018-01-04 19:51:55 +0100

    encapsulate rotation stabilization in a module

---
+++ NaoTHSoccer/Source/Motion/Engine/InverseKinematicsMotion/Motions/Walk2018/TorsoRotationStabilizer.h
@@ -0,0 +1,121 @@
+/**
+ * @File:   FootTrajectoryGenerator.h
+ *
+ * @author <a href="mailto:xu@informatik.hu-berlin.de">Xu, Yuan</a>
+ * foot trajectory for walking
+ */
+
+#ifndef _TORSO_ROTATION_STABILIZER_H
+#define _TORSO_ROTATION_STABILIZER_H
+
+#include <ModuleFramework/Module.h>
+
+#include "Representations/Motion/Walk2018/StepBuffer.h"
+#include "Representations/Motion/Walk2018/TargetHipFeetPose.h"
+#include "Representations/Infrastructure/CalibrationData.h"
+#include "Representations/Modeling/InertialModel.h"
+#include "Representations/Infrastructure/GyrometerData.h"
+#include "Representations/Infrastructure/RobotInfo.h"
+
+#include "Motion/Engine/InverseKinematicsMotion/InverseKinematicsMotionEngine.h"
+
+#include "Tools/Debug/DebugPlot.h"
+#include "Tools/Debug/DebugRequest.h"
+
+BEGIN_DECLARE_MODULE(TorsoRotationStabilizer)
+    PROVIDE(DebugPlot)
+    PROVIDE(DebugRequest)
+
+    REQUIRE(FrameInfo)
+    REQUIRE(CalibrationData)
+    REQUIRE(InertialModel)
+    REQUIRE(GyrometerData)
+    REQUIRE(RobotInfo)
+    REQUIRE(StepBuffer)
+    REQUIRE(InverseKinematicsMotionEngineService)
+
+    PROVIDE(TargetHipFeetPose)
+END_DECLARE_MODULE(TorsoRotationStabilizer)
+
+class TorsoRotationStabilizer : private TorsoRotationStabilizerBase
+{
+  public:
+    TorsoRotationStabilizer(){}
+
+  virtual void execute(){
+    if(getCalibrationData().calibrated && getInverseKinematicsMotionEngineService().getEngine().getParameters().walk.stabilization.rotationStabilize) {
+      if(getStepBuffer().first().footStep.liftingFoot() == FootStep::LEFT) {
+        getTargetHipFeetPose().pose.localInRightFoot();
+      } else if(getStepBuffer().first().footStep.liftingFoot() == FootStep::RIGHT) {
+        getTargetHipFeetPose().pose.localInLeftFoot();
+      } else {
+        getTargetHipFeetPose().pose.localInHip();
+      }
+
+      rotationStabilize(
+        getInertialModel(),
+        getGyrometerData(),
+        getRobotInfo().getBasicTimeStepInSecond(),
+        getInverseKinematicsMotionEngineService().getEngine().getParameters().walk.stabilization.rotation.P,
+        getInverseKinematicsMotionEngineService().getEngine().getParameters().walk.stabilization.rotation.VelocityP,
+        getInverseKinematicsMotionEngineService().getEngine().getParameters().walk.stabilization.rotation.D,
+        getTargetHipFeetPose().pose
+      );
+    }
+  }
+
+  bool rotationStabilize(
+    const InertialModel& theInertialModel,
+    const GyrometerData& theGyrometerData,
+    const double timeDelta,
+    const Vector2d&  rotationP,
+    const Vector2d&  rotationVelocityP,
+    const Vector2d&  rotationD,
+    InverseKinematic::HipFeetPose& p)
+  {
+    const double alpha = 0.5;
+    Vector2d gyro = Vector2d(theGyrometerData.data.x, theGyrometerData.data.y);
+    static Vector2d filteredGyro = gyro;
+    filteredGyro = filteredGyro * (1.0f - alpha) + gyro * alpha;
+
+    const double observerMeasurementDelay = 40;
+    const int frameDelay = static_cast<int>(observerMeasurementDelay / (timeDelta*1000));
+
+    static RingBuffer<Vector2d, 10> buffer;
+    static Vector2d lastGyroError;
+    static RotationMatrix lastBodyRotationMatrix = p.hip.rotation;
+
+    const RotationMatrix relativeRotation = p.hip.rotation.invert() * lastBodyRotationMatrix;
+    lastBodyRotationMatrix = p.hip.rotation;
+
+    const double rotationY = atan2(relativeRotation.c[2].x, relativeRotation.c[2].z);
+    buffer.add(Vector2d(relativeRotation.getXAngle(), rotationY));
+
+    if(buffer.isFull() && frameDelay > 0 && frameDelay < buffer.size())
+    {
+      const Vector2d requestedVelocity = (buffer[frameDelay-1] - buffer[frameDelay]) / timeDelta;
+      const Vector2d error = requestedVelocity - filteredGyro;
+      const Vector2d errorDerivative = (error - lastGyroError) / timeDelta;
+
+      double correctionY = rotationVelocityP.y * error.y +
+                           rotationD.y * errorDerivative.y;
+
+      double correctionX =rotationVelocityP.x * error.x +
+                          rotationD.x * errorDerivative.x;
+
+      const Vector2d& inertial = theInertialModel.orientation;
+      correctionX += rotationP.x * inertial.x;
+      correctionY += rotationP.y * inertial.y;
+
+      p.localInHip();
+      p.hip.rotateX(correctionX);
+      p.hip.rotateY(correctionY);
+
+      lastGyroError = error;
+    }
+
+    return true;
+  }
+};
+
+#endif  /* _TORSO_ROTATION_STABILIZER_H */
