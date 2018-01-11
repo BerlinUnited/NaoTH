e8c856408d62460881cecd831ce8e2be707e483d
Steffen Kaden <kaden@informatik.hu-berlin.de>
2018-01-11 17:53:48 +0100

    own parameters for LiftingFootCompensator and small clean up and bugfix

--- NaoTHSoccer/Source/Motion/Engine/InverseKinematicsMotion/Motions/Walk2018/LiftingFootCompensator.h
+++ NaoTHSoccer/Source/Motion/Engine/InverseKinematicsMotion/Motions/Walk2018/LiftingFootCompensator.h
@@ -12,47 +12,65 @@
 
 #include "Representations/Motion/Walk2018/StepBuffer.h"
 #include "Representations/Motion/Walk2018/TargetCoMFeetPose.h"
-#include "Motion/Engine/InverseKinematicsMotion/InverseKinematicsMotionEngine.h"
 
 #include "Tools/Debug/DebugPlot.h"
 #include "Tools/Debug/DebugRequest.h"
+#include "Tools/Debug/DebugParameterList.h"
 
 BEGIN_DECLARE_MODULE(LiftingFootCompensator)
     PROVIDE(DebugPlot)
     PROVIDE(DebugRequest)
+    PROVIDE(DebugParameterList)
 
     REQUIRE(FrameInfo)
-    REQUIRE(InverseKinematicsMotionEngineService)
     REQUIRE(StepBuffer)
 
-    PROVIDE(TargetCoMFeetPose)  // modifying com heigth
+    PROVIDE(TargetCoMFeetPose)  // modifying com translation
 END_DECLARE_MODULE(LiftingFootCompensator)
 
 class LiftingFootCompensator : private LiftingFootCompensatorBase
 {
   public:
-    LiftingFootCompensator(){}
+    LiftingFootCompensator(){
+      getDebugParameterList().add(&parameters);
+    }
+
+    virtual ~LiftingFootCompensator(){
+      getDebugParameterList().remove(&parameters);
+    }
 
   virtual void execute(){
     InverseKinematic::CoMFeetPose tmp(getTargetCoMFeetPose().pose);
-    if(getStepBuffer().first().footStep.liftingFoot() == FootStep::LEFT) {
-      tmp.localInRightFoot();
-      getTargetCoMFeetPose().pose.com.translation.z += getInverseKinematicsMotionEngineService().getEngine().getParameters().walk.hip.comHeightOffset * tmp.feet.left.translation.z;
-      getTargetCoMFeetPose().pose.com.translation.y -= getInverseKinematicsMotionEngineService().getEngine().getParameters().walk.hip.comStepOffsetY  * tmp.feet.left.translation.z;
-      //getTargetCoMFeetPose().pose.com.rotateX( parameters().hip.comRotationOffsetX *tmp.feet.left.translation.z );
-
-      PLOT("Walk:theCoMFeetPose:total_rotationY",tmp.com.rotation.getYAngle());
+    switch(getStepBuffer().first().footStep.liftingFoot()) {
+      case FootStep::LEFT :
+        tmp.localInRightFoot();
+        getTargetCoMFeetPose().pose.com.translation.z += parameters.comHeightOffset * tmp.feet.left.translation.z;
+        getTargetCoMFeetPose().pose.com.translation.y -= parameters.comStepOffsetY  * tmp.feet.left.translation.z;
+        break;
+      case FootStep::RIGHT :
+        tmp.localInLeftFoot();
+        getTargetCoMFeetPose().pose.com.translation.z += parameters.comHeightOffset * tmp.feet.right.translation.z;
+        getTargetCoMFeetPose().pose.com.translation.y += parameters.comStepOffsetY  * tmp.feet.right.translation.z;
+        break;
     }
-    else if(getStepBuffer().first().footStep.liftingFoot() == FootStep::RIGHT)
-    {
-      tmp.localInLeftFoot();
-      getTargetCoMFeetPose().pose.com.translation.z += getInverseKinematicsMotionEngineService().getEngine().getParameters().walk.hip.comHeightOffset * tmp.feet.right.translation.z;
-      getTargetCoMFeetPose().pose.com.translation.y += getInverseKinematicsMotionEngineService().getEngine().getParameters().walk.hip.comStepOffsetY * tmp.feet.left.translation.z;
-      //getTargetCoMFeetPose().pose.com.rotateX( -parameters().hip.comRotationOffsetX*tmp.feet.right.translation.z );
 
-      PLOT("Walk:theCoMFeetPose:total_rotationY",tmp.com.rotation.getYAngle());
-    }
+    PLOT("Walk:theCoMFeetPose:total_rotationY",tmp.com.rotation.getYAngle());
   }
+
+  private:
+    class Parameters: public ParameterList{
+      public:
+        Parameters() : ParameterList("LiftingFootCompensator")
+        {
+          PARAMETER_REGISTER(comHeightOffset) = 0.18;
+          PARAMETER_REGISTER(comStepOffsetY)  = 0;
+
+          syncWithConfig();
+        }
+
+        double comHeightOffset;
+        double comStepOffsetY;
+    } parameters;
 };
 
 #endif  /* _LIFTING_FOOT_COMPENSATOR_H */
