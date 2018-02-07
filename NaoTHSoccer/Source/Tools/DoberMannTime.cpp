9ff9ffe09ade8d3518dff7f317333d7c3aec9cfa
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2018-02-07 17:33:13 +0100

    make it more compact :)

--- NaoTHSoccer/Source/Tools/DoberMannTime.cpp
+++ NaoTHSoccer/Source/Tools/DoberMannTime.cpp
@@ -13,8 +13,6 @@ std::uint32_t DoberMannTime::getSystemTimeMixedTeam()
   auto now = std::chrono::system_clock::now();
   auto time = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
 
-  // the time must be relative to the start of the current day
-  auto base = time - (time % (24 * 60 * 60 * 1000));
-
-  return static_cast<std::uint32_t>(time - base);
+  // the time relative to the start of the current day
+  return static_cast<std::uint32_t>(time % (24 * 60 * 60 * 1000));
 }
