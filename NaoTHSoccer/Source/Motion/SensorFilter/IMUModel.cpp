2a9fa70034eef18c287b545965e2845b92d8eadb
Steffen Kaden <kaden@informatik.hu-berlin.de>
2018-02-24 15:56:27 +0100

    add debug request wrapping all plot and 3d drawings

--- NaoTHSoccer/Source/Motion/SensorFilter/IMUModel.cpp
+++ NaoTHSoccer/Source/Motion/SensorFilter/IMUModel.cpp
@@ -13,6 +13,8 @@ IMUModel::IMUModel():
     DEBUG_REQUEST_REGISTER("IMUModel:reloadParameters", "", false);
     DEBUG_REQUEST_REGISTER("IMUModel:enableFilterWhileWalking", "enables filter update while walking", false);
 
+    DEBUG_REQUEST_REGISTER("IMUModel:enablePlotsAndDrawings", "enables plots and the drawings in 3d viewer", false);
+
     ukf_acc_global.P = Eigen::Matrix<double,3,3>::Identity(); // covariance matrix of current state
     ukf_rot.P        = Eigen::Matrix<double,6,6>::Identity(); // covariance matrix of current state
 
@@ -102,7 +104,9 @@ void IMUModel::execute(){
 
     writeIMUData();
 
-    plots();
+    DEBUG_REQUEST("IMUModel:enablePlotsAndDrawings",
+        plots();
+    );
 
     lastFrameInfo = getFrameInfo();
 }
