b689866f55cb885a2abac5e4851cf3da3607ce5d
CNR <critter@informatik.hu-berlin.de>
2018-03-02 16:27:25 +0100

    Merge remote-tracking branch 'origin/infrastrucure/whistledetector' into infrastructure/whistledetector
    # Conflicts:
    #	Framework/Commons/Source/Representations/Infrastructure/Configuration.cpp

---
+++ Framework/Platforms/Source/DCM/NaoRobot/WhistleDetector.h
@@ -0,0 +1,76 @@
+#ifndef _WhistleDetector_H
+#define _WhistleDetector_H
+
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <unistd.h>
+#include <limits>
+
+#include <fftw3/fftw3.h>
+#include <pulse/simple.h>
+
+#include "SoundConfig.h"
+
+class WhistleDetector
+{
+public:
+  WhistleDetector();
+  virtual ~WhistleDetector();
+
+protected:
+
+  //Parameter Substitute:
+  std::string whistleListFile = "whistles.lst";
+  std::string activeChannels = "1010";
+
+  double threshold = 0.25;
+  bool checkAllWhistles = true;
+  bool saveLog = false;
+  bool saveSpectrum = false;
+  bool saveRawAudio = false;
+
+  void initAudio();
+  void clearBuffers();
+  void loadReferenceWhistles();
+  void execute(); // This should be called in a loop -> create loop function like in CPUTemperatureReader
+  void deinitAudio();
+  bool detectWhistles(int channel);
+
+  pa_simple* paSimple;
+  //SharedMemoryWriter<Accessor<int> > shmWriter;
+  //SharedMemoryReader<int> shmReader;
+  //RingBufferWithSum<int, 10> detectedCountBuffer;
+  double signalBuffer[NUM_CHANNELS_RX][WHISTLE_BUFF_LEN];
+  double* signal2correlateBuffer;
+  fftw_complex* fftOut;
+  fftw_complex* fftIn;
+  double* correlatedBuffer;
+
+  fftw_plan planFFT;
+  fftw_plan planIFFT;
+
+  std::vector<short> test_samples;
+
+  //std::ifstream audioTestFile;
+  //bool testFileMode;
+
+  std::vector<fftw_complex*> referenceWhistleSpectra;
+  std::vector<float> referenceWhistleAutoCorrelationMaxima;
+  std::vector<std::string> referenceWhistleNames;
+
+
+  int overallWhistleEventCounter;
+  //unsigned long long aktFrameTime;
+  //unsigned long long lastFrameTime;
+  std::vector<short> audioReadBuffer;
+
+  volatile bool running;
+  bool recording;
+  std::ofstream outputFileStream;
+  int startStopCount;
+  int deinitCyclesCounter;
+
+};
+
+#endif // _WhistleDetector_H
