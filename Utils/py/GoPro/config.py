b7a356ca7102f6ac4b6350d6e469e1da94d6f38b
Benjamin Schlotter <benji@schlotterfam.de>
2018-03-13 13:12:53 +0100

    changed spelling in ssid

--- Utils/py/GoPro/config.py
+++ Utils/py/GoPro/config.py
@@ -1,8 +1,8 @@
-# NAOCAM / a1b0a1b0a1
+# NaoCam / a1b0a1b0a1
 # NAOCAM_2 / a1b0a1b0a1
 # GP26329941 / cycle9210
 # GP26297683 / epic0546
 
-ssid = 'GP26329941'
-passwd = 'cycle9210'
+ssid = 'NaoCam'
+passwd = 'a1b0a1b0a1'
 retries = -1
