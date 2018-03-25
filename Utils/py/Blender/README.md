747a9659b117e9363bdd64f72417323f3b6fddb6
Benjamin Schlotter <benji@schlotterfam.de>
2018-03-25 22:43:10 +0200

    added script for running the patch generation

---
+++ Utils/py/Blender/README.md
@@ -0,0 +1,3 @@
+# Blender Patch Generation
+
+Download the blend file from https://www2.informatik.hu-berlin.de/~naoth/ressources/BlenderPatchGeneration/ into this folder
---
+++ Utils/py/Blender/generate_patches.sh
@@ -0,0 +1,5 @@
+# Information on how to call blender python script with custom arguments: https://blender.stackexchange.com/questions/6817/how-to-pass-command-line-arguments-to-a-blender-python-script 
+
+#blender RoboCup_v_1_2_1_nonInteractive-benji.blend --background --python generate_patches_from_scene.py -- -c
+
+blender RoboCup_v_1_2_1_nonInteractive-benji.blend --python generate_patches_from_scene.py -- -c
