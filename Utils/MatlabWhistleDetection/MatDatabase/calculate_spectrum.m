83498fce4fb86963e57cade78b8a808eeadbc071
Benjamin Schlotter <benji@schlotterfam.de>
2018-03-26 16:12:43 +0200

    progress on spectrum calculation in matlab

--- Utils/MatlabWhistleDetection/MatDatabase/calculate_spectrum.m
+++ Utils/MatlabWhistleDetection/MatDatabase/calculate_spectrum.m
@@ -5,11 +5,15 @@ function [spectrum, auto_corr] = calculate_spectrum(raw)
 mat_calc_spectrum = fft(raw, length(raw)*2);
 mat_calc_spectrum = conj(mat_calc_spectrum); % since c++ version is conjugated
 
-% TODO normalize correctly?
-norm_mat_calc_spectrum = mat_calc_spectrum;
+% normalize like the cpp implementation
+short_max = 32767;
+norm_mat_calc_spectrum = mat_calc_spectrum / short_max;
 norm_mat_calc_spectrum = norm_mat_calc_spectrum(1:1025);
 
 % TODO calculate auto correlation
+[acor,lag] = xcorr(norm_mat_calc_spectrum, norm_mat_calc_spectrum);
+[~,I] = max(abs(acor))
+lagDiff = lag(I);
 
 spectrum = norm_mat_calc_spectrum;
 auto_corr = inputArg2;
--- Utils/MatlabWhistleDetection/compareSpectra.m
+++ Utils/MatlabWhistleDetection/compareSpectra.m
@@ -6,7 +6,7 @@ fileID = fopen ('data/andyDark_8kHz_1channels.dat', 'r');
 fclose(fileID);
 
 % get the correlation value and remove it from the array
-corr = cpp_calc_spectrum(end);
+cpp_auto_corr = cpp_calc_spectrum(end);
 cpp_calc_spectrum = cpp_calc_spectrum(1:end-1);
 
 % convert array of doubles to array of complex values
@@ -18,29 +18,47 @@ fileID = fopen ('data/andyDark_8kHz_1channels.raw', 'r');
     raw = fread(fileID, 'int16', 'ieee-le');
 fclose(fileID);
 
+% normalize the samples like the cpp implementation
+short_max = 32767;
+raw = raw / short_max;
+
 fftw('planner','estimate');
 mat_calc_spectrum = fft(raw, length(raw)*2);
 
-%% Test compare
-cpp_calc_spectrum = cpp_calc_spectrum(1:end);
-mat_calc_spectrum = conj(mat_calc_spectrum); % since c++ version is conjugated
-
-%normalize between 0 and 1
-norm_cpp_calc_spectrum = (cpp_calc_spectrum - min(cpp_calc_spectrum)) / ( max(cpp_calc_spectrum) - min(cpp_calc_spectrum) );
-norm_mat_calc_spectrum = (mat_calc_spectrum - min(mat_calc_spectrum)) / ( max(mat_calc_spectrum) - min(mat_calc_spectrum) );
-
-norm_mat_calc_spectrum = norm_mat_calc_spectrum(1:1025);
+%% Test IFFT
+fftw('planner','estimate');
+new_raw = ifft(mat_calc_spectrum, length(raw)*2);
 
 figure
-plot(abs(norm_mat_calc_spectrum), 'r')
-%hold on
-%plot(abs(norm_cpp_calc_spectrum), 'b')
+plot(abs(raw) , 'r')
+hold on
+plot(abs(new_raw), 'b')
+title('Raw Values and ...')
 
-%
-%diff = norm_mat_calc_spectrum - norm_cpp_calc_spectrum;
+%% Test xcorr vs correlation via fft/ifft
 
-%figure
-%plot(abs(diff), 'r')
-%title('Differenz')
+%% Test compare
+mat_calc_spectrum = conj(mat_calc_spectrum); % since c++ version is conjugated
+
+% only use half of the spectrum
+mat_calc_spectrum = mat_calc_spectrum(1:1025);
+%%
+% this cross correlation does not compute the same value as the c++
+% implementation
+[acor,lag] = xcorr(raw, raw);
+[value, index] = max(abs(acor));
+%lagDiff = lag(index);
+
+%%
+% figure
+% plot(abs(mat_calc_spectrum) , 'r')
+% hold on
+% plot(abs(cpp_calc_spectrum), 'b')
+% 
+% diff = mat_calc_spectrum - cpp_calc_spectrum;
+% 
+% figure
+% plot(abs(diff), 'r')
+% title('Differenz')
 
 
