1d252ebae74b8540eca8ae790049d8209cc96a94
Benjamin Schlotter <benji@schlotterfam.de>
2018-04-05 09:03:55 +0200

    added function for exporting the spectra from a database

---
+++ Utils/MatlabWhistleDetection/MatDatabase/exportSpectrum.m
@@ -0,0 +1,41 @@
+clear variables
+clc
+%% Check if reference_database exists
+try
+    load('reference_database.mat')
+    disp('INFO: using a previously created database')
+catch
+    disp('INFO: no reference_database was found')
+    return
+end
+% get categories
+names = string(fieldnames(reference_database));
+
+for i=1:length(names)
+    category = reference_database.(names(i));
+    
+    % iterate over every reference whistle in this category
+    [status, msg, msgID] = mkdir(names(i));
+    for j=1:length(category)
+        %TODO save spectrum in correct subfolders
+        output_name = strcat(category(j).name, '.dat');
+        output_name = fullfile(names(i), output_name);
+        fileID = fopen (output_name, 'w');
+            % unpack complex array again
+            real_part = real(category(j).spectralData);
+            imag_part = imag(category(j).spectralData);
+            % interleave the real and imaginary part for output
+            output = interleave(real_part, imag_part);
+            fwrite(fileID, category(j).spectralData, 'double', 'ieee-le');
+            % append max autocorrelation to the output
+            output = [output category(j).autocorrelation];
+        fclose(fileID);
+    end
+   
+end
+% input a category name
+% input a path where output should be stored
+
+% export audio files as wav
+
+% output nothing
--- Utils/MatlabWhistleDetection/MatDatabase/exportWhistles.m
+++
@@ -1,7 +0,0 @@
-% input path to database
-% input a category name
-% input a path where output should be stored
-
-% export audio files as wav
-
-% output nothing
