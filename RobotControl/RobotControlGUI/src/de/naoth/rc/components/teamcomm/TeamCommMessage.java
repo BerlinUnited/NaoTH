59c01ce9b3e364c19bc9d9f8d3fec862e63f6943
Philipp Strobel <philippstrobel@posteo.de>
2018-04-10 09:06:27 +0200

    TeamCommLogViewer can read GameController and converted logs moreover spl message errors are explicitly thrown and small ui change

--- RobotControl/RobotControlGUI/src/de/naoth/rc/components/teamcomm/TeamCommMessage.java
+++ RobotControl/RobotControlGUI/src/de/naoth/rc/components/teamcomm/TeamCommMessage.java
@@ -7,45 +7,67 @@ import de.naoth.rc.dataformats.SPLMessage;
  * @author Philipp Strobel <philippstrobel@posteo.de>
  */
 public class TeamCommMessage {
-        /**
-         * The unix timestamp, when the message was received.
-         */
-        public final long timestamp;
-        
-        /**
-         * The ip address from which "robot" the message was received.
-         */
-        public final String address;
-        
-        /**
-         * The data of the SPL message.
-         */
-        public final SPLMessage message;
-        
-        /**
-         * Indicates if this is a message of a teammate or opponent.
-         */
-        private final boolean isOpponent;
 
-        /**
-         * TeamCommMessage constructor.
-         * @param timestamp
-         * @param address
-         * @param message
-         * @param isOpponent 
-         */
-        public TeamCommMessage(long timestamp, String address, SPLMessage message, boolean isOpponent) {
-            this.timestamp = timestamp;
-            this.address = address;
-            this.message = message;
-            this.isOpponent = isOpponent;
-        }
+    /**
+     * The unix timestamp, when the message was received.
+     */
+    public final long timestamp;
 
-        /**
-         * Return true if this TeamCommMessage from a opponent.
-         * @return true|false
-         */
-        public boolean isOpponent() {
-            return isOpponent;
-        }
+    /**
+     * The ip address from which "robot" the message was received.
+     */
+    public final String address;
+
+    /**
+     * The data of the SPL message.
+     */
+    public final SPLMessage message;
+
+    /**
+     * Indicates if this is a message of a teammate or opponent.
+     */
+    private final boolean isOpponent;
+
+    /**
+     * Represents the game state, when this message was sent. '-1' is unknown.
+     */
+    public final int gameState;
+
+    /**
+     * Constructor, initializes this representation.
+     *
+     * @param timestamp
+     * @param address
+     * @param message
+     * @param isOpponent
+     */
+    public TeamCommMessage(long timestamp, String address, SPLMessage message, boolean isOpponent) {
+        this(timestamp, address, message, isOpponent, -1);
+    }
+
+    /**
+     * Constructor, initializes this representation.
+     *
+     * @param timestamp
+     * @param address
+     * @param message
+     * @param isOpponent
+     * @param gameState
+     */
+    public TeamCommMessage(long timestamp, String address, SPLMessage message, boolean isOpponent, int gameState) {
+        this.timestamp = timestamp;
+        this.address = address;
+        this.message = message;
+        this.isOpponent = isOpponent;
+        this.gameState = gameState;
+    }
+
+    /**
+     * Return true if this TeamCommMessage from a opponent.
+     *
+     * @return true|false
+     */
+    public boolean isOpponent() {
+        return isOpponent;
+    }
 }
