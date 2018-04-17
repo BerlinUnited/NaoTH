78454fab13b98ce9295b699de0f60fa76a618fbb
Steffen Kaden <kaden@informatik.hu-berlin.de>
2018-04-17 15:36:36 +0200

    add cpp part of symbols

---
+++ NaoTHSoccer/Source/Cognition/Modules/Behavior/XABSLBehaviorControl/Symbols/BDRSymbols.h
@@ -0,0 +1,43 @@
+/**
+* @file BDRSymbols.h
+*
+* @author <a href="mailto:mellmann@informatik.hu-berlin.de">Heinrich Mellmann</a>
+* Definition of class BDRSymbols
+*/
+
+#ifndef _BDRSymbols_H_
+#define _BDRSymbols_H_
+
+#include <ModuleFramework/Module.h>
+#include <XabslEngine/XabslEngine.h>
+
+// representations
+#include "Representations/Modeling/BDRPlayerState.h"
+
+BEGIN_DECLARE_MODULE(BDRSymbols)
+    PROVIDE(BDRPlayerState)
+END_DECLARE_MODULE(BDRSymbols)
+
+class BDRSymbols: public BDRSymbolsBase
+{
+
+public:
+  BDRSymbols() {
+    theInstance = this;
+  }
+  virtual ~BDRSymbols(){}
+  
+  /** registers the symbols at an engine */
+  void registerSymbols(xabsl::Engine& engine);
+
+  virtual void execute(){}
+
+private:
+  static BDRSymbols* theInstance;
+
+  static void setBDRActivity(int value);
+  static int  getBDRActivity();
+
+};//end class BDRSymbols
+
+#endif // _BDRSymbols_H_
