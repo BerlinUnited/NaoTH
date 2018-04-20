ecec0802bc1f9a1c59b04e00343e6c643e128873
Benjamin Schlotter <benji@schlotterfam.de>
2018-04-20 17:59:16 +0200

    added script for generating a mat database from audio tracks of game recordings

---
+++ Utils/MatlabWhistleDetection/db_creation/game_rec_db_create.m
@@ -0,0 +1,75 @@
+clear all
+clc
+% adds a raw file to the whistle capture database
+% TODO does not check if file is already inserted
+try
+    load('../data/game_rec_database.mat')
+    disp('INFO: using a previously created database')
+catch
+    disp('INFO: no previous mat file was found')
+    game_rec_database = struct;
+end
+
+%%
+data_path = '<insert_path_here>';
+
+% get information about the location of each raw file inside gamelog_path
+folderContents = dir(strcat(data_path, '/**/*.wav'));
+%% Extract the necessary infos from filepath
+% The following folder structure under gamelog_path is expected:
+% data_path
+% |-> game_name
+%     |-> wav files
+
+eventFolder = split(data_path, ["/","\"]);
+eventFolder = eventFolder{end}; % get last folder of gamelog_path
+
+for i=1:length(folderContents)
+    % use the path of the first raw file
+    testfolder = folderContents(i).folder;
+    extractedPath = extractAfter(testfolder,eventFolder);
+    
+    % remove slash in front
+    if strcmp(extractedPath(1), '\') || strcmp(extractedPath(1), '/')
+        extractedPath = extractedPath(2:end);
+    end
+    
+    extractedPath_split = split(extractedPath, ["/","\"]);
+    
+    game_name = string(extractedPath_split(1));
+    
+     % get wav file
+     % TODO: the wav files have two channels -> fix this in the ffmpeg
+     % script
+    filepath = fullfile(folderContents(i).folder, folderContents(i).name);
+    [y,Fs] = audioread(filepath);
+
+    % sometimes the exported wav file has two channels that are the same
+    if(size(y,2) == 2)
+        y = y(:,1);
+    end
+    
+    % subsample it so that it has the same frequency as the robot(8kHz)
+    target_samplerrate = 8000;
+    [P,Q] = rat(target_samplerrate / Fs);
+    y = resample(y,P,Q);
+
+    % fill the capture whistle fields
+    recording.game_name = game_name;
+    recording.samples = y;
+    recording.sample_rate = target_samplerrate;
+    
+    % create name for event that follows the struct field naming convention
+    eventname = ['e_',eventFolder];
+    if isfield(game_rec_database, eventname) 
+        game_rec_database.(eventname) = [game_rec_database.(eventname) recording];
+    else
+        game_rec_database.(eventname) = recording;
+    end
+
+end
+
+%% Save capture database
+disp('Saving Game Recording database');
+%TODO better name based on event folder
+save('game_rec_database.mat','game_rec_database')
