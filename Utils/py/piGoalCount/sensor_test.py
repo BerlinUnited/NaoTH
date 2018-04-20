c9b05b6e17932a7053360b69daf084efaafb73b7
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2018-04-20 15:23:36 +0200

    more sufisticated test

--- Utils/py/piGoalCount/sensor_test.py
+++ Utils/py/piGoalCount/sensor_test.py
@@ -8,24 +8,28 @@ GPIO_INPUT_GOAL1 = 23
 GPIO_INPUT_GOAL2 = 24
  
 #set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_INPUT_GOAL1, GPIO.IN)
-GPIO.setup(GPIO_INPUT_GOAL2, GPIO.IN)
- 
+GPIO.setup([GPIO_INPUT_GOAL1, GPIO_INPUT_GOAL2], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def handle_goal(pin):
+  if pin == GPIO_INPUT_GOAL1:
+    print ("Activated: Goal 1")
+  
+  if pin == GPIO_INPUT_GOAL2:
+    print ("Activated: Goal 2")
+  
+
 if __name__ == '__main__':
+
+  #GPIO.RISING
+  #GPIO.BOTH
+  GPIO.add_event_detect(BTN_G, GPIO.RISING, handle)
+
+  # dummy waiting, this will be done better
   try:
     while True:
-      goal1 = GPIO.input(GPIO_INPUT_GOAL1)
-      goal2 = GPIO.input(GPIO_INPUT_GOAL2)
-      
-      if goal1:
-        print ("Activated: Goal 1")
-      
-      if goal2:
-        print ("Activated: Goal 2")
-      
       time.sleep(0.01)
 
-    # Reset by pressing CTRL + C
+  # Reset by pressing CTRL + C
   except KeyboardInterrupt:
     print("Measurement stopped by User")
     GPIO.cleanup()
