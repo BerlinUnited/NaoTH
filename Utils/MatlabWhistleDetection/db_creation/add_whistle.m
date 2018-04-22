ceb52334eb60ac45c2ed045fc37aba7dccbd97c4
Benjamin Schlotter <benji@schlotterfam.de>
2018-04-22 19:51:51 +0200

    cleanup some old comments and files

--- Utils/MatlabWhistleDetection/db_creation/add_whistle.m
+++ Utils/MatlabWhistleDetection/db_creation/add_whistle.m
@@ -1,6 +1,6 @@
 function data = add_whistle(data, file, numChanels, sampleRate, category)
 
-    [path_to_file,filename,~] = fileparts(file);
+    [~,filename,~] = fileparts(file);
 
     % fill the referenceWhistle fields
     whistle.name = filename;
@@ -12,9 +12,6 @@ function data = add_whistle(data, file, numChanels, sampleRate, category)
         raw = fread(fileID, 'int16', 'ieee-le');
     fclose(fileID);
     whistle.rawData = raw;
-    
-    % TODO how to handle data from multiple channels, 
-    % should each channel be saved individually?
     
     % calculate spectrum from raw data
     [spectrum, max_auto_corr] = calculate_spectrum(raw);
