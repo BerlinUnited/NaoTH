1dd26b5e585fb41d7cb5942d4f7c65941e7cf1f8
Benjamin Schlotter <benji@schlotterfam.de>
2018-04-22 08:59:14 +0200

    better export functions

--- Utils/MatlabWhistleDetection/db_creation/capture_db_export.m
+++ Utils/MatlabWhistleDetection/db_creation/capture_db_export.m
@@ -8,11 +8,8 @@ load('../data/capture_database.mat')
 go17_recordings = capture_database.go17_recordings;
 rc17_recordings = capture_database.rc17_recordings;
 
-mkdir('../data/capture_data_raw/go17_recordings')
-mkdir('../data/capture_data_raw/rc17_recordings')
-
 for i=1:length(go17_recordings)
-    %TODO export the recordings as raw in the correct folder structure
+    % Export the recordings as raw in the correct folder structure
     game_name = go17_recordings(i).game_name;
     half = go17_recordings(i).half;
     robot_name = go17_recordings(i).robot_name;
@@ -21,7 +18,7 @@ for i=1:length(go17_recordings)
     path_to_outputfolder = fullfile('../data/capture_data_raw/go17_recordings', game_name, half, robot_name);
     mkdir(path_to_outputfolder)
     
-    % TODO database should have the original filename, if database is
+    % TODO raw files should have the original filename, if database creation is
     % changed fix this
     filename = fullfile(path_to_outputfolder, strcat(int2str(i), '.raw'));
     
@@ -30,6 +27,8 @@ for i=1:length(go17_recordings)
     fclose(fileID);
     
     % Export samples as wav
+    % TODO wav files should have the original filename, if database creation is
+    % changed fix this
     filename2 = fullfile(path_to_outputfolder, strcat(int2str(i), '.wav'));
     Fs=8000;
     y = rawData ./ max( abs(rawData(:)) );
@@ -37,7 +36,7 @@ for i=1:length(go17_recordings)
 end
 
 for i=1:length(rc17_recordings)
-    %TODO export the recordings as raw in the correct folder structure
+    % Export the recordings as raw in the correct folder structure
     game_name = rc17_recordings(i).game_name;
     half = rc17_recordings(i).half;
     robot_name = rc17_recordings(i).robot_name;
@@ -46,7 +45,7 @@ for i=1:length(rc17_recordings)
     path_to_outputfolder = fullfile('../data/capture_data_raw/rc17_recordings', game_name, half, robot_name);
     mkdir(path_to_outputfolder)
     
-    % TODO database should have the original filename, if database is
+    % TODO raw files should have the original filename, if database creation is
     % changed fix this
     filename = fullfile(path_to_outputfolder, strcat(int2str(i), '.raw'));
     
@@ -55,6 +54,8 @@ for i=1:length(rc17_recordings)
     fclose(fileID);
     
     % Export samples as wav
+    % TODO wav files should have the original filename, if database creation is
+    % changed fix this
     filename2 = fullfile(path_to_outputfolder, strcat(int2str(i), '.wav'));
     Fs=8000;
     y = rawData ./ max( abs(rawData(:)) );
--- Utils/MatlabWhistleDetection/db_creation/reference_db_export.m
+++ Utils/MatlabWhistleDetection/db_creation/reference_db_export.m
@@ -8,23 +8,65 @@ ref_bhuman = reference_database.bhuman;
 ref_kln17= reference_database.kln17;
 ref_rc17= reference_database.rc17;
 
-for i=1:length(ref_bhuman)
-    %TODO export the recordings as wav in the correct folder structure
-    %TODO export the spectrum as dat in the correct folder structure
+for i=1:length(ref_bhuman)    
+    % Export the recordings as raw in the correct folder structure
+    name = ref_bhuman(i).name;
+    rawData = ref_bhuman(i).rawData;
     
+    path_to_outputfolder = fullfile('../data/reference_data_raw/bhuman');
+    mkdir(path_to_outputfolder)
+    
+    filename = fullfile(path_to_outputfolder, strcat(name, '.raw'));
+    
+    % Export raw data
+    fileID = fopen(filename, 'w');
+        fwrite(fileID, rawData, 'int16', 'ieee-le');
+    fclose(fileID);
+    
+    % TODO Export wav
+    
+    %TODO export the spectrum as dat in the correct folder structure 
     %for spectrum export the exportSpectrum.m script can be used
 end
 
 for i=1:length(ref_kln17)
-    %TODO export the recordings as wav in the correct folder structure
-    %TODO export the spectrum as dat in the correct folder structure
+    % Export the recordings as raw in the correct folder structure
+    name = ref_kln17(i).name;
+    rawData = ref_kln17(i).rawData;
     
+    path_to_outputfolder = fullfile('../data/reference_data_raw/kln17');
+    mkdir(path_to_outputfolder)
+    
+    filename = fullfile(path_to_outputfolder, strcat(name, '.raw'));
+    
+    % Export raw data
+    fileID = fopen(filename, 'w');
+        fwrite(fileID, rawData, 'int16', 'ieee-le');
+    fclose(fileID);
+    
+    % TODO Export wav
+    
+    %TODO export the spectrum as dat in the correct folder structure 
     %for spectrum export the exportSpectrum.m script can be used
 end
 
 for i=1:length(ref_rc17)
-    %TODO export the recordings as wav in the correct folder structure
-    %TODO export the spectrum as dat in the correct folder structure
+    % Export the recordings as raw in the correct folder structure
+    name = ref_rc17(i).name;
+    rawData = ref_rc17(i).rawData;
     
+    path_to_outputfolder = fullfile('../data/reference_data_raw/rc17');
+    mkdir(path_to_outputfolder)
+    
+    filename = fullfile(path_to_outputfolder, strcat(name, '.raw'));
+    
+    % Export raw data
+    fileID = fopen(filename, 'w');
+        fwrite(fileID, rawData, 'int16', 'ieee-le');
+    fclose(fileID);
+    
+    % TODO Export wav
+    
+    %TODO export the spectrum as dat in the correct folder structure 
     %for spectrum export the exportSpectrum.m script can be used
 end
