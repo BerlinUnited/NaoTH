ef25918ee8a574c6dbbc3ae88890dcb9d9cefb78
Benjamin Schlotter <benji@schlotterfam.de>
2018-04-22 09:39:24 +0200

    even better capture export

--- Utils/MatlabWhistleDetection/db_creation/capture_db_export.m
+++ Utils/MatlabWhistleDetection/db_creation/capture_db_export.m
@@ -3,7 +3,6 @@ clc
 %% Example of how to get capture data and apply a detector
 load('../data/capture_database.mat')
 
-
 %% Get the capture categories
 go17_recordings = capture_database.go17_recordings;
 rc17_recordings = capture_database.rc17_recordings;
@@ -14,23 +13,19 @@ for i=1:length(go17_recordings)
     half = go17_recordings(i).half;
     robot_name = go17_recordings(i).robot_name;
     rawData = go17_recordings(i).rawData;
+    filename = go17_recordings(i).filename;
     
     path_to_outputfolder = fullfile('../data/capture_data_raw/go17_recordings', game_name, half, robot_name);
     mkdir(path_to_outputfolder)
     
-    % TODO raw files should have the original filename, if database creation is
-    % changed fix this
-    filename = fullfile(path_to_outputfolder, strcat(int2str(i), '.raw'));
-    
-    fileID = fopen(filename, 'w');
+    fileID = fopen(fullfile(path_to_outputfolder, filename), 'w');
         fwrite(fileID, rawData, 'int16', 'ieee-le');
     fclose(fileID);
     
     % Export samples as wav
-    % TODO wav files should have the original filename, if database creation is
-    % changed fix this
-    filename2 = fullfile(path_to_outputfolder, strcat(int2str(i), '.wav'));
-    Fs=8000;
+    [~,filepartname,~] = fileparts(filename);
+    filename2 = fullfile(path_to_outputfolder, strcat(filepartname, '.wav'));
+    Fs = go17_recordings(i).samplerate;
     y = rawData ./ max( abs(rawData(:)) );
     audiowrite(char(filename2),y,Fs);
 end
@@ -41,23 +36,19 @@ for i=1:length(rc17_recordings)
     half = rc17_recordings(i).half;
     robot_name = rc17_recordings(i).robot_name;
     rawData = rc17_recordings(i).rawData;
+    filename = rc17_recordings(i).filename;
     
     path_to_outputfolder = fullfile('../data/capture_data_raw/rc17_recordings', game_name, half, robot_name);
     mkdir(path_to_outputfolder)
     
-    % TODO raw files should have the original filename, if database creation is
-    % changed fix this
-    filename = fullfile(path_to_outputfolder, strcat(int2str(i), '.raw'));
-    
-    fileID = fopen (filename, 'w');
+    fileID = fopen (fullfile(path_to_outputfolder, filename), 'w');
         fwrite(fileID, rawData, 'int16', 'ieee-le');
     fclose(fileID);
     
     % Export samples as wav
-    % TODO wav files should have the original filename, if database creation is
-    % changed fix this
-    filename2 = fullfile(path_to_outputfolder, strcat(int2str(i), '.wav'));
-    Fs=8000;
+    [~,filepartname,~] = fileparts(filename);
+    filename2 = fullfile(path_to_outputfolder, strcat(filepartname, '.wav'));
+    Fs = rc17_recordings(i).samplerate;
     y = rawData ./ max( abs(rawData(:)) );
     audiowrite(char(filename2),y,Fs);
 end
