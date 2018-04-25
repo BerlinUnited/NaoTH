6ad626a2fc5b01bdc4caebf108b15f6cc086ed7b
Robert <martinro@informatik.hu-berlin.de>
2018-04-25 18:24:45 +0200

    added green scores for grid cells

--- NaoTHSoccer/Source/Cognition/Modules/VisualCortex/FieldDetector/FieldAreaDetector.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/FieldDetector/FieldAreaDetector.cpp
@@ -29,27 +29,54 @@ void FieldAreaDetector::execute(CameraInfo::CameraID id)
 
   const int32_t factor = getBallDetectorIntegralImage().FACTOR;
 
+  // TODO Parameter
   int32_t grid_size = 32/factor;
-  const double green_pixels = static_cast<double>(grid_size*grid_size)*params.proportion_of_green;
+  double green_weight = .5;
+
+  const double pixels_per_grid = static_cast<double>(grid_size*grid_size);
+  const double green_pixels = pixels_per_grid*params.proportion_of_green;
 
   int32_t minX, maxX, minY, maxY;
+
+  double score = 0, best_score = -1;
+  int32_t best_grid_x=0, best_grid_y=0;
+
+  // TODO start after own body
+
   for (minX = 0; minX < (int)getBallDetectorIntegralImage().getWidth(); minX += grid_size) {
     maxX = std::min(minX + grid_size, (int)getBallDetectorIntegralImage().getWidth()-1);
 
     for(minY = getBallDetectorIntegralImage().getHeight()-grid_size; minY >= 0; minY -= grid_size) {
       maxY = std::min(minY + grid_size, (int)getBallDetectorIntegralImage().getHeight()-1);
 
-      double density = (int)getBallDetectorIntegralImage().getSumForRect(minX, minY, maxX, maxY, 1);
-      if(density >= green_pixels) {
+      double sum_of_green = (int)getBallDetectorIntegralImage().getSumForRect(minX, minY, maxX, maxY, 1);
+
+      if(sum_of_green >= green_pixels) {
         RECT_PX(ColorClasses::green, minX*factor+1, minY*factor+1, maxX*factor-1, maxY*factor-1);
       } else {
         RECT_PX(ColorClasses::red, minX*factor+1, minY*factor+1, maxX*factor-1, maxY*factor-1);
       }
-
-      double t = density/static_cast<double>(grid_size*grid_size);
+      double t = sum_of_green/static_cast<double>(grid_size*grid_size);
       Color color = green*t + white*(1-t);
       PEN(color, 0.1);
       FILLBOX(minX*factor+1, minY*factor+1, maxX*factor-1, maxY*factor-1);
+
+      score += (green_weight*sum_of_green) - ((1-green_weight)*(pixels_per_grid - sum_of_green));
+      std::cout << "score " << score << std::endl;
+      if(score >= best_score) {
+        best_score = score;
+        best_grid_x = minX;
+        best_grid_y = minY;
+      }
     }
+    std::cout << "best_score: " << best_score << std::endl;
+    if(best_score > 0) {
+      RECT_PX(ColorClasses::blue, best_grid_x*factor+1, best_grid_y*factor+1,
+        (best_grid_x+grid_size)*factor-1, (best_grid_y-grid_size)*factor-1);
+    }
+    score = 0;
+    best_score = -1;
   }
+
+
 }
