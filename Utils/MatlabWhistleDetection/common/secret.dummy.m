c23471fd58ab6ee604c46df623ebf4df43399f97
CNR <critter@informatik.hu-berlin.de>
2018-04-28 16:12:51 +0200

    modified secrets.m dummy

--- Utils/MatlabWhistleDetection/common/secret.dummy.m
+++ Utils/MatlabWhistleDetection/common/secret.dummy.m
@@ -1,9 +1,16 @@
 function secret()
+    databases_path = "<path_to_databases>/";
+    assignin('base','databases_path', databases_path)
 
-assignin('base','capture_database_path', '<path_to_database>')
-assignin('base','capture_export_path', '<path_to_export_folder>')
+    assignin('base','tests_database_path', char(strcat(databases_path, "<test_captures_database>.mat")))
+    assignin('base','capture_database_path', char(strcat(databases_path, "<capture_database>.mat")))
+    assignin('base','reference_database_path', char(strcat(databases_path, "<reference_database>.mat")))
 
-%TODO create more variables for other things than capture databases
+    assignin('base','capture_import_path', char(strcat(databases_path, "<subdir_to_import_captures>/")))
 
+    assignin('base','capture_export_path', '<path_to_export_folder_for_captures>')
+    assignin('base','figure_export_path', '<path_to_export_folder_for_figures>')
+
+    %TODO create more variables for other things than capture databases
 end
 
