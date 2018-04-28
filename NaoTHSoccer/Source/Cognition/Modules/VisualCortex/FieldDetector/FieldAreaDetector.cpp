aebdf11543c65f2f303f6194deee84f0a86b98cf
Robert <martinro@informatik.hu-berlin.de>
2018-04-28 11:48:22 +0200

    refine cell, find endpoint of field

--- NaoTHSoccer/Source/Cognition/Modules/VisualCortex/FieldDetector/FieldAreaDetector.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/FieldDetector/FieldAreaDetector.cpp
@@ -7,6 +7,7 @@ FieldAreaDetector::FieldAreaDetector()
 {
   DEBUG_REQUEST_REGISTER("Vision:FieldAreaDetector:draw_grid", "", false);
   DEBUG_REQUEST_REGISTER("Vision:FieldAreaDetector:draw_best_squares", "", false);
+  DEBUG_REQUEST_REGISTER("Vision:FieldAreaDetector:draw_split", "", false);
   getDebugParameterList().add(&params);
 }
 
@@ -20,62 +21,136 @@ FieldAreaDetector::~FieldAreaDetector()
 void FieldAreaDetector::execute(CameraInfo::CameraID id)
 {
   cameraID = id;
-  CANVAS(((cameraID == CameraInfo::Top)?"ImageTop":"ImageBottom"));
-  Color white(1.0,1.0,1.0,0.0); // transparent
-  Color green(0.0,1.0,0.0,1.0);
 
   if(!getBallDetectorIntegralImage().isValid()) {
     return;
   }
 
-  const int32_t factor = getBallDetectorIntegralImage().FACTOR;
-
-  int32_t grid_size = params.grid_size/factor;
-
-  const double pixels_per_grid = static_cast<double>(grid_size*grid_size);
-  const double green_pixels = pixels_per_grid*params.proportion_of_green;
+  factor = getBallDetectorIntegralImage().FACTOR;
+  grid_size = params.grid_size/factor;
+  int n_cells_x = getBallDetectorIntegralImage().getWidth()/grid_size;
+  int offset = (getBallDetectorIntegralImage().getWidth()-(n_cells_x*grid_size))/n_cells_x;
+  pixels_per_cell = ((grid_size+1)*(grid_size+1));
+  min_green = (int)(pixels_per_cell*params.proportion_of_green);
 
   int32_t minX, maxX, minY, maxY;
-
-  double score = 0, best_score = -1;
-  int32_t best_grid_y = 0;
-
-  // TODO start after own body
-  for (minX = 0; minX < (int)getBallDetectorIntegralImage().getWidth(); minX += grid_size) {
+  int32_t last_green_grid_maxY = 0;
+  int32_t last_green_grid_minY = 0;
+  for (minX = offset; minX < (int)getBallDetectorIntegralImage().getWidth(); minX += grid_size+offset) {
     maxX = std::min(minX + grid_size, (int)getBallDetectorIntegralImage().getWidth()-1);
-
+    // Hack
+    Vector2i start_left(minX*factor, getImage().height()-1);
+    Vector2i start_right(maxX*factor, getImage().height()-1);
+    int start_y = std::min(getBodyContour().getFirstFreeCell(start_left).y/factor,
+                           getBodyContour().getFirstFreeCell(start_right).y/factor);
+    bool green_found = false;
     for(minY = getBallDetectorIntegralImage().getHeight()-grid_size; minY >= 0; minY -= grid_size) {
-      maxY = std::min(minY + grid_size, (int)getBallDetectorIntegralImage().getHeight()-1);
+      maxY = std::min((int)minY + grid_size, (int)getBallDetectorIntegralImage().getHeight()-1);
+      if(maxY > start_y) {
+        continue;
+      }
 
-      double sum_of_green = (int)getBallDetectorIntegralImage().getSumForRect(minX, minY, maxX, maxY, 1);
-
-      score += (params.green_weight*sum_of_green) - ((1-params.green_weight)*(pixels_per_grid - sum_of_green));
-      if(score >= best_score) {
-        best_score = score;
-        best_grid_y = minY;
+      int sum_of_green = getBallDetectorIntegralImage().getSumForRect(minX, minY, maxX, maxY, 1);
+      if(sum_of_green >= min_green) {
+        last_green_grid_minY = minY;
+        last_green_grid_maxY = maxY;
+        green_found = true;
+      } else {
+        break;
       }
 
       DEBUG_REQUEST("Vision:FieldAreaDetector:draw_grid",
-        if(sum_of_green >= green_pixels) {
+        if(sum_of_green >= min_green) {
           RECT_PX(ColorClasses::green, minX*factor+1, minY*factor+1, maxX*factor-1, maxY*factor-1);
         } else {
           RECT_PX(ColorClasses::red, minX*factor+1, minY*factor+1, maxX*factor-1, maxY*factor-1);
         }
+        CANVAS(((cameraID == CameraInfo::Top)?"ImageTop":"ImageBottom"));
+        Color white(1.0,1.0,1.0,0.0); // transparent
+        Color green(0.0,1.0,0.0,1.0);
         double t = sum_of_green/static_cast<double>(grid_size*grid_size);
         Color color = green*t + white*(1-t);
         PEN(color, 0.1);
         FILLBOX(minX*factor+1, minY*factor+1, maxX*factor-1, maxY*factor-1);
       );
     }
-    if(best_score > 0) {
-      DEBUG_REQUEST("Vision:FieldAreaDetector:draw_grid",
-        RECT_PX(ColorClasses::blue, minX*factor+1, best_grid_y*factor+1,
-          maxX*factor-1, (best_grid_y-grid_size)*factor-1);
-      );
+
+    if(green_found) {
+      Cell green_cell(minX, last_green_grid_minY, maxX, last_green_grid_maxY);
+      refine_cell(green_cell);
+
+      Cell upper, lower;
+      int half_grid_size = split_cell(green_cell, upper, lower);
+
+      Vector2i endpoint;
+      if (upper.sum_of_green >= params.proportion_of_green * half_grid_size * half_grid_size) {
+        find_endpoint(upper, endpoint);
+      } else {
+        find_endpoint(lower, endpoint);
+      }
     }
-    score = 0;
-    best_score = -1;
   }
+}
 
+void FieldAreaDetector::find_endpoint(const Cell& cell, Vector2i& endpoint) {
+  int y_grid_size = cell.maxY - cell.minY;
+  int pixels_per_cell = y_grid_size * y_grid_size;
 
+  double cut = cell.sum_of_green / (double)pixels_per_cell;
+  endpoint.x = ((cell.minX+cell.maxX)/2) * factor;
+  endpoint.y = (cell.maxY-((int)(cut*y_grid_size))) * factor;
+
+  DEBUG_REQUEST("Vision:FieldAreaDetector:draw_best_squares",
+    RECT_PX(ColorClasses::blue, cell.minX*factor+2, cell.minY*factor+2,
+      cell.maxX*factor-2, cell.maxY*factor-2);
+    RECT_PX(ColorClasses::orange, endpoint.x-1, endpoint.y-1, endpoint.x+1, endpoint.y+1);
+  );
+}
+
+int32_t FieldAreaDetector::split_cell(const Cell& cell, Cell& upper, Cell& lower) {
+  int32_t half_grid_size = grid_size/2;
+
+  upper.minX = lower.minX = cell.minX + (half_grid_size/2);
+  upper.maxX = lower.maxX = lower.minX + half_grid_size;
+
+  lower.minY = cell.maxY - half_grid_size;
+  lower.maxY = cell.maxY;
+  lower.sum_of_green = getBallDetectorIntegralImage().getSumForRect(lower.minX, lower.minY, lower.maxX, lower.maxY, 1);
+
+  upper.maxY = lower.minY;
+  upper.minY = lower.maxY - grid_size;
+  upper.sum_of_green = getBallDetectorIntegralImage().getSumForRect(upper.minX, upper.minY, upper.maxX, upper.maxY, 1);
+
+  DEBUG_REQUEST("Vision:FieldAreaDetector:draw_split",
+    ColorClasses::Color color;
+    if(upper.sum_of_green >= (half_grid_size*half_grid_size*params.proportion_of_green)) {
+      color = ColorClasses::green;
+    } else {
+      color = ColorClasses::red;
+    }
+    RECT_PX(color, upper.minX*factor+1, upper.minY*factor+1, upper.maxX*factor-1, upper.maxY*factor-1);
+    if(lower.sum_of_green >= (half_grid_size*half_grid_size*params.proportion_of_green)) {
+      color = ColorClasses::green;
+    } else {
+      color = ColorClasses::red;
+    }
+    RECT_PX(color, lower.minX*factor+1, lower.minY*factor+1, lower.maxX*factor-1, lower.maxY*factor-1);
+  );
+
+  return half_grid_size;
+}
+
+void FieldAreaDetector::refine_cell(Cell& cell) {
+  int sum_of_green = 0;
+  // move cell up
+  for(int offset = 0; offset < grid_size && cell.minY-offset>=0; ++offset) {
+    sum_of_green = getBallDetectorIntegralImage().getSumForRect(cell.minX, cell.minY-offset, cell.maxX, cell.maxY-offset, 1);
+    if(sum_of_green >= min_green) {
+      cell.minY = cell.minY-offset;
+      cell.maxY = cell.maxY-offset;
+      cell.sum_of_green = sum_of_green;
+    } else {
+      break;
+    }
+  }
 }
--- NaoTHSoccer/Source/Cognition/Modules/VisualCortex/FieldDetector/FieldAreaDetector.h
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/FieldDetector/FieldAreaDetector.h
@@ -8,6 +8,7 @@
 #include "Representations/Perception/ArtificialHorizon.h"
 #include "Representations/Perception/BodyContour.h"
 #include "Representations/Perception/MultiChannelIntegralImage.h"
+#include "Representations/Perception/FieldColorPercept.h"
 
 #include "Tools/Debug/DebugRequest.h"
 #include <Tools/Debug/DebugImageDrawings.h>
@@ -33,6 +34,8 @@ BEGIN_DECLARE_MODULE(FieldAreaDetector)
   REQUIRE(ArtificialHorizonTop)
   REQUIRE(BodyContour)
   REQUIRE(BodyContourTop)
+  REQUIRE(FieldColorPercept)
+  REQUIRE(FieldColorPerceptTop)
 
   REQUIRE(BallDetectorIntegralImage)
   REQUIRE(BallDetectorIntegralImageTop)
@@ -61,19 +64,50 @@ public:
 
     Parameters() : ParameterList("FieldAreaDetector")
     {
-      PARAMETER_REGISTER(proportion_of_green) = 0.7;
+      PARAMETER_REGISTER(proportion_of_green) = .5;
       PARAMETER_REGISTER(grid_size) = 32;
-      PARAMETER_REGISTER(green_weight) = .5;
       syncWithConfig();
     }
     double proportion_of_green;
     int32_t grid_size;
-    double green_weight;
   } params;
 
 
 private:
+  struct Cell
+  {
+    int32_t minX;
+    int32_t minY;
+    int32_t maxX;
+    int32_t maxY;
+    int sum_of_green;
+
+    Cell() {}
+    Cell(int32_t minX, int32_t minY, int32_t maxX, int32_t maxY){
+      this->minX = minX;
+      this->minY = minY;
+      this->maxX = maxX;
+      this->maxY = maxY;
+      sum_of_green = -1;
+    }
+    Cell(int32_t minX, int32_t minY, int32_t maxX, int32_t maxY, int sum_of_green){
+      Cell(minX, minY, maxX, maxY);
+      this->sum_of_green = sum_of_green;
+    }
+  };
+
+  void find_endpoint_in_cell(int32_t minX, int32_t minY, int32_t maxX, int32_t maxY, int best_green_sum, Vector2i& endpoint);
+  void optimize_cell_and_find_endpoint(int32_t minX, int32_t minY, int32_t maxX, int32_t maxY, Vector2i& endpoint);
+
+  void find_endpoint(const Cell& cell, Vector2i& endpoint);
+  void refine_cell(Cell& cell);
+  int32_t split_cell(const Cell& cell, Cell& upper, Cell& lower);
+
   CameraInfo::CameraID cameraID;
+  int pixels_per_cell;
+  int min_green;
+  int factor;
+  int32_t grid_size;
 
   DOUBLE_CAM_PROVIDE(FieldAreaDetector, DebugImageDrawings);
 
@@ -82,6 +116,7 @@ private:
   DOUBLE_CAM_REQUIRE(FieldAreaDetector, Image);
   DOUBLE_CAM_REQUIRE(FieldAreaDetector, ArtificialHorizon);
   DOUBLE_CAM_REQUIRE(FieldAreaDetector, BodyContour);
+  DOUBLE_CAM_REQUIRE(FieldAreaDetector, FieldColorPercept);
 
   DOUBLE_CAM_REQUIRE(FieldAreaDetector, BallDetectorIntegralImage);
 };
