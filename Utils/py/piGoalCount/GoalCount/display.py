a8fe22108c4ae7445e9187e7361c1fcb0bd53090
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2018-05-24 17:27:43 +0200

    added a test for the display added functionality to show images (bdr and naoth logo)

--- Utils/py/piGoalCount/GoalCount/display.py
+++ Utils/py/piGoalCount/GoalCount/display.py
@@ -1,6 +1,7 @@
 import pygame
 from datetime import timedelta
 from datetime import datetime
+from time import sleep
 
 class Display:
   def __init__(self, a = 0, b = 0):
@@ -21,22 +22,25 @@ class Display:
     self.goalsLeft = a
     self.goalsRight = b
     self.gameTime = 0
-    self.update()
+    self.updateText()
 
   def setValues(self, a, b, t):
     self.goalsLeft = a
     self.goalsRight = b
     self.gameTime = t
-    self.update()  
+    self.updateText()  
     
-  def update(self):
+  def clear(self):
     color_bg = (255,255,255)
+    self.screen.fill(color_bg)
+    
+  def updateText(self):
+    self.clear()
+    
     color_text = (0,0,0)
     
     offset_goals_y = 70
     offset_time_y = -30
-    
-    self.screen.fill(color_bg)
     
     textGoals = self.fontScore.render("{}:{}".format(self.goalsLeft,self.goalsRight), True, color_text)
     self.screen.blit(textGoals, (self.width // 2 - textGoals.get_width() // 2, self.height // 2 - textGoals.get_height() // 2 + offset_goals_y))
@@ -50,5 +54,35 @@ class Display:
     
     pygame.display.flip()
     
+    
+  def updateImage(self, i):
+    self.clear()
+    
+    imgs = [
+      pygame.image.load("./img/bdr-500.png").convert(),
+      pygame.image.load("./img/bu-500.png").convert(),
+    ]
+    
+    im = imgs[i]
+    self.screen.blit(im, (self.width // 2 - im.get_width() // 2, self.height // 2 - im.get_height() // 2))
+    pygame.display.flip()
+    
+    
   def end(self):
     pygame.quit()
+    
+    
+if __name__ == '__main__':
+  print ("test display")
+  display = Display()
+  
+  i = 0
+  while True:
+    #display.setValues(2,7,142)
+    display.updateImage(i)
+    i += 1
+    if i > 1:
+      i = 0
+    sleep(2)
+
+  display.end()
