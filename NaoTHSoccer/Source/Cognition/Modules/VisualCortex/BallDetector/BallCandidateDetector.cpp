b0c374b4235d5ae66f8654d268ba9d844688bfd4
Benjamin Schlotter <benji@schlotterfam.de>
2018-06-03 13:16:57 +0200

    added second ball detection net from nao devils

--- NaoTHSoccer/Source/Cognition/Modules/VisualCortex/BallDetector/BallCandidateDetector.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/BallDetector/BallCandidateDetector.cpp
@@ -13,6 +13,7 @@
 
 #include "Classifier/DortmundCNN/CNN_dortmund.h"
 #include "Classifier/DortmundCNN/CNN_dortmund2018.h"
+#include "Classifier/DortmundCNN/CNN_dortmund2018_keras.h"
 
 using namespace std;
 
@@ -113,7 +114,8 @@ std::map<string, std::shared_ptr<AbstractCNNClassifier> > BallCandidateDetector:
   std::map<string, std::shared_ptr<AbstractCNNClassifier> > result;
 
   result.insert({"dortmund", std::make_shared<CNN_dortmund>()});
-  result.insert({ "dortmund2018", std::make_shared<CNN_dortmund2018>() });
+  //result.insert({ "dortmund2018", std::make_shared<CNN_dortmund2018>() });
+  result.insert({ "dortmund2018_keras", std::make_shared<CNN_dortmund2018_keras>() });
 
   return std::move(result);
 }
