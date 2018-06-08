0833d39214c368e58f0eec5e17047469140d084c
Thomas Krause <thomaskrause@posteo.de>
2018-06-08 14:45:08 +0200

    Introduce the double click button event

--- NaoTHSoccer/Source/Cognition/Modules/Infrastructure/ButtonEventMonitor/ButtonEventMonitor.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/Infrastructure/ButtonEventMonitor/ButtonEventMonitor.cpp
@@ -20,6 +20,9 @@ void ButtonEventMonitor::execute()
 
 void ButtonEventMonitor::update(ButtonEvent& buttonEvent, bool pressed)
 {
+  const unsigned int doubleClickWaitTime = 300;
+
+
   buttonEvent = ButtonEvent::NONE;
 
   // detect change in the status of the button
@@ -31,10 +34,24 @@ void ButtonEventMonitor::update(ButtonEvent& buttonEvent, bool pressed)
 
   // button is pressed not longer than 1s
   if(buttonEvent == ButtonEvent::RELEASED && getFrameInfo().getTime() < buttonEvent.timeOfLastEvent + 1000) {
-    buttonEvent = ButtonEvent::CLICKED;
+    if(buttonEvent.lastEventWasClick && getFrameInfo().getTime() < buttonEvent.timeOfLastEvent + doubleClickWaitTime) {
+      // double click occured
+      buttonEvent.lastEventWasClick = false;
+      buttonEvent = ButtonEvent::DOUBLE_CLICKED;
+    } else {
+      // remember that there was a click in this frame and make sure this event is process later
+      buttonEvent.lastEventWasClick = true;
+    }
   }
 
-  if(!(buttonEvent == ButtonEvent::NONE)) {
+
+  if(buttonEvent == ButtonEvent::NONE) {
+    // check if we have to process an old click event after waiting long enough for a potential double click
+    if(buttonEvent.lastEventWasClick && getFrameInfo().getTime() < buttonEvent.timeOfLastEvent + doubleClickWaitTime) {
+      buttonEvent.lastEventWasClick = false;
+      buttonEvent = ButtonEvent::CLICKED;
+    }
+  } else {
     buttonEvent.timeOfLastEvent = getFrameInfo().getTime();
   }
 }
