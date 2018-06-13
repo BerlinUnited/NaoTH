5db0449848bbae0c7bfd914c1f37f71b8d9b914a
Philipp Strobel <philippstrobel@posteo.de>
2018-06-13 19:37:26 +0200

    started to implement a select agent dialog

--- RobotControl/RobotControlGUI/src/de/naoth/rc/dialogs/multiagentconfiguration/MultiAgentConfigurationFx.java
+++ RobotControl/RobotControlGUI/src/de/naoth/rc/dialogs/multiagentconfiguration/MultiAgentConfigurationFx.java
@@ -7,40 +7,50 @@ import de.naoth.rc.dialogs.multiagentconfiguration.ui.AgentTab;
 import de.naoth.rc.dialogs.multiagentconfiguration.ui.AgentTabGlobal;
 import java.awt.SplashScreen;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Properties;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
-import javafx.event.EventType;
+import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.CheckBoxTreeItem;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.cell.CheckBoxListCell;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
 
 /**
@@ -58,21 +68,15 @@ public class MultiAgentConfigurationFx extends AbstractJFXDialog
     private ToggleButton btn_connect;
     
     @FXML
-    private TextField field_ip;
-    
-    @FXML
-    private TextField field_ip_end;
-    
-    @FXML
-    private TextField field_port_start;
-    
-    @FXML
-    private TextField field_port_end;
-    
-    @FXML
     private TabPane tabpane;
     
     private final AgentTabGlobal allTab = new AgentTabGlobal();
+    
+    private Properties config = new Properties();
+    
+    private final ExecutorService executor = Executors.newCachedThreadPool();
+    
+    private final ObservableList<String> hostList = FXCollections.observableArrayList(new TreeSet());
     
     @Override
     protected boolean isSelfController() {
@@ -87,18 +91,71 @@ public class MultiAgentConfigurationFx extends AbstractJFXDialog
     @Override
     public void afterInit() {
         // TODO
-        field_ip.disableProperty().bind(btn_connect.selectedProperty());
-        field_ip_end.disableProperty().bind(btn_connect.selectedProperty());
-        field_port_start.disableProperty().bind(btn_connect.selectedProperty());
-        field_port_end.disableProperty().bind(btn_connect.selectedProperty());
         btn_connect.selectedProperty().addListener((ov, t, t1) -> {
+            /*
             if(t1) {
-                connecting();
-                allTab.setDisable(false);
+            connecting();
+            allTab.setDisable(false);
             } else {
-                disconnecting();
-                allTab.setDisable(true);
+            disconnecting();
+            allTab.setDisable(true);
             }
+             */
+            hostList.clear();
+            String[] ips = config.getProperty("iplist","").split(",");
+
+            for(final String ip: ips) {
+                executor.submit(() -> {
+                    try {
+                        InetAddress address = InetAddress.getByName(ip);
+                        if(address.isReachable(500)) {
+                            Platform.runLater(() -> hostList.add(ip));
+                        }
+                    } catch (Exception e) { /* ignore exception */ }
+                });
+            }
+            for (int p = 5401; p < 5411; p++) {
+                final int port = p;
+                executor.submit(() -> {
+                    try {
+                        (new Socket("localhost", port)).close();
+                        Platform.runLater(() -> hostList.add("localhost:"+port));
+                    } catch (IOException ex) {
+                    }
+                });
+                
+            }
+            VBox vb = new VBox();
+            
+            ListView l = new ListView(hostList);
+            l.setCellFactory(CheckBoxListCell.forListView((param) -> {
+                System.out.println(param);
+                BooleanProperty observable = new SimpleBooleanProperty(true);
+                return observable;
+            }));
+            vb.getChildren().add(l);
+            vb.getChildren().add(new CheckBox("All Agents"));
+//            l.setCellFactory(CheckBoxListCell.forListView((param) -> {}));
+
+//            l.getItems().
+            Dialog<Set<String>> dialog = new Dialog<>();
+            dialog.setTitle("Agent Selection");
+            dialog.initStyle(StageStyle.UTILITY);
+//            dialog.setHeaderText("Select the agents you want to connect to.");
+            dialog.getDialogPane().setContent(vb);
+            dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+            
+            dialog.setResultConverter(dialogButton -> {
+                if (dialogButton == ButtonType.OK) {
+                    System.out.println(this);
+                    return new TreeSet<>();
+                }
+                return null;
+            });
+
+            Optional<Set<String>> result = dialog.showAndWait();
+            
+            System.out.println(result);
         });
         
         allTab.setDisable(true);
@@ -154,6 +211,9 @@ public class MultiAgentConfigurationFx extends AbstractJFXDialog
     System.out.println(n);
     if (n instanceof Parent) for (Node c : ((Parent) n).getChildrenUnmodifiable()) dump(c, depth + 1);
   }
+    public void setConfig(Properties c) {
+        config = c;
+    }
 
     @Override
     public Map<KeyCombination, Runnable> getGlobalShortcuts() {
@@ -186,29 +246,30 @@ public class MultiAgentConfigurationFx extends AbstractJFXDialog
         System.out.println(((StackPane)tabpane.lookup(".tab-header-area")).getChildren());
         System.out.println(((StackPane)tabpane.lookup(".headers-region")).getInsets());
         
-        List<String> ip_parts = Arrays.asList(field_ip.getText().trim().split("\\.", -1));
-        String ip_range = field_ip_end.getText().trim().equals("0") ? ip_parts.get(3) : field_ip_end.getText().trim();
-        int port_start = Integer.parseInt(field_port_start.getText().trim());
-        int port_end = Integer.parseInt(field_port_end.getText().trim());
-
-        if(ip_range.compareTo(ip_parts.get(3)) < 0 || port_end < port_start) {
-            // TODO: indicate invalid values!?
-            btn_connect.setSelected(false);
-            return;
-        }
-
-        for (int i = Integer.parseInt(ip_parts.get(3)); i <= Integer.parseInt(ip_range); i++) {
-            String ip = ip_parts.get(0) + "." + ip_parts.get(1) + "." + ip_parts.get(2) + "." + i;
-            for (int port = port_start; port <= port_end; port++) {
-                System.out.println(ip + ":" + port);
-//                System.out.println(tabpane.getTabs());
-                AgentTab tab = new AgentTab(ip, port);
-                tab.connectDivider(allTab);
-                tab.connectAgentList(allTab);
-                tab.connectButtons(allTab);
-                tabpane.getTabs().add(tab);
-            }
-        }
+        // TODO: ! 
+//        List<String> ip_parts = Arrays.asList(field_ip.getText().trim().split("\\.", -1));
+//        String ip_range = field_ip_end.getText().trim().equals("0") ? ip_parts.get(3) : field_ip_end.getText().trim();
+//        int port_start = Integer.parseInt(field_port_start.getText().trim());
+//        int port_end = Integer.parseInt(field_port_end.getText().trim());
+//
+//        if(ip_range.compareTo(ip_parts.get(3)) < 0 || port_end < port_start) {
+//            // TODO: indicate invalid values!?
+//            btn_connect.setSelected(false);
+//            return;
+//        }
+//
+//        for (int i = Integer.parseInt(ip_parts.get(3)); i <= Integer.parseInt(ip_range); i++) {
+//            String ip = ip_parts.get(0) + "." + ip_parts.get(1) + "." + ip_parts.get(2) + "." + i;
+//            for (int port = port_start; port <= port_end; port++) {
+//                System.out.println(ip + ":" + port);
+////                System.out.println(tabpane.getTabs());
+//                AgentTab tab = new AgentTab(ip, port);
+//                tab.connectDivider(allTab);
+//                tab.connectAgentList(allTab);
+//                tab.connectButtons(allTab);
+//                tabpane.getTabs().add(tab);
+//            }
+//        }
     }
     
     public void disconnect() {
@@ -249,6 +310,7 @@ public class MultiAgentConfigurationFx extends AbstractJFXDialog
                 loader.setController(controller);
                 
                 Parent root = loader.load();
+                controller.setConfig(readConfigFromFile());
                 controller.afterInit();
 
                 Scene scene = new Scene(root);
@@ -267,6 +329,19 @@ public class MultiAgentConfigurationFx extends AbstractJFXDialog
             } catch (IOException ex) {
                 Logger.getLogger(MultiAgentConfigurationFxMain.class.getName()).log(Level.SEVERE, null, ex);
             }
+        }
+
+        private Properties readConfigFromFile() {
+            Properties config = new Properties();
+//          splashScreenMessage("Loading config file ...");
+            try {
+                // load main config
+                Properties mainConfig = new Properties();
+                mainConfig.load(MultiAgentConfigurationFxMain.class.getResourceAsStream("/de/naoth/rc/config"));
+                config.putAll(mainConfig);
+
+            } catch (IOException ex) {}
+            return config;
         }
     }
 }
