a46c17cd65464f457b53481ecad8b580ad9e8d29
Philipp Strobel <philippstrobel@posteo.de>
2018-06-15 21:29:55 +0200

    to be fully comply with the teamcomm protocol, the generator must receive teamcomm messages in order to reply correctly

--- Utils/py/TeamCommGenerator/main.py
+++ Utils/py/TeamCommGenerator/main.py
@@ -11,7 +11,7 @@ def parseArguments():
     parser = argparse.ArgumentParser(description='Starts a TeamCommMessage generator', epilog="Example: {} -n 10.0.4.255 -p 10004".format(sys.argv[0]))
     parser.add_argument('-n', '--net', action='store', default='localhost', help='the host / net, where the messages are send to; e.g. 10.0.4.255; default: localhost')
     parser.add_argument('-p', '--port', action='store', default=10000, type=int, help='the port number where the messages are send to; default: 10000')
-    parser.add_argument('-d', '--delay', action='store', default=0.4, type=float, help='sets the time between two messages (in seconds)), default: 0.4')
+    parser.add_argument('-d', '--delay', action='store', default=0.8, type=float, help='sets the time between two messages (in seconds)), default: 0.4')
 
     return parser.parse_args()
 
