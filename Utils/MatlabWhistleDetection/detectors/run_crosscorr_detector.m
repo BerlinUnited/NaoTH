79a1c84eb92b15875e471bfb08033e0864cb7e03
CNR <critter@informatik.hu-berlin.de>
2018-06-15 15:34:59 +0200

    - generate comparison figures per reference whistle set and threshold - clear and close figures after saving for better memory efficiency

--- Utils/MatlabWhistleDetection/detectors/run_crosscorr_detector.m
+++ Utils/MatlabWhistleDetection/detectors/run_crosscorr_detector.m
@@ -22,6 +22,10 @@ end
 %% config
 windows_size = 1024;
 window_offset = 786;
+threshold = 0.25;
+%threshold = 0.4;
+% flip = false;
+flip = true;
 
 fftw('planner','estimate');
 
@@ -31,14 +35,16 @@ create_detection_plots = true;
 create_non_detection_plots = false;
 
 %% set references whistle set to use
-% ref = reference_database.bhuman;
-% ref = reference_database.kln17;
-% ref = reference_database.rc17;
-% ref = reference_database.go18;
-%ref = reference_database.go17;
-ref = reference_database.gen_from_go18_recordings;
+% ref_set_name = 'bhuman';
+% ref_set_name = 'kln17';
+% ref_set_name = 'rc17';
+% ref_set_name = 'go18';
+ref_set_name = 'go17';
+%ref_set_name = 'gen_from_go18_recordings';
 
-ref_stat = struct;
+ref = reference_database.(ref_set_name);
+
+ref_stat_tpl = struct;
 for r = 1:length(ref)
     % replace points and minus signs with underscore
     ref(r).name = strrep(ref(r).name, '-', '_');
@@ -47,25 +53,30 @@ for r = 1:length(ref)
     ev  = struct;
     ev.tp = 0;
     ev.fp = 0;
-    ref_stat.(char(ref(r).name)) = ev;
-end
-  
+    ev.valid = false;
+    if ref(r).autocorrelation ~= 0
+        ev.valid = true; 
+    end
+
+    ref_stat_tpl.(char(ref(r).name)) = ev;
+end  
 %%
 recordings_names = fieldnames(database);
 for t = 1:length(recordings_names)
+    ref_stat = ref_stat_tpl;
+    has_any_annotation = false;
     recording_name = recordings_names(t);
     recording = database.(recording_name{1});
-    
-    sprintf("Use captures from %s", cell2mat(recording_name))
+    disp(sprintf("Use captures from %s", cell2mat(recording_name)))
     for c = 1:length(recording)
-        sprintf("Check file %d of %d",c, length(recording))
+        disp(sprintf("Check file %d of %d", c, length(recording)))
         if ~isempty(recording(c).annotations)
+            has_any_annotation = true;
             % get raw capture data
             capture_data = recording(c).rawData;
             record_name = strcat(recording(c).game_name, " ", recording(c).half, " ", recording(c).robot_name, " ", recording(c).filename);
             %% Max Cross Correlation Detector       
-            threshold = 0.25;
-            results = maxcrosscorr_detector(capture_data, windows_size, window_offset, threshold, ref, true);
+            results = maxcrosscorr_detector(capture_data, windows_size, window_offset, threshold, ref, flip);
             means = [];
             for i=1:window_offset:length(capture_data)
                 if length(capture_data) - i >= windows_size
@@ -89,25 +100,31 @@ for t = 1:length(recordings_names)
                     plot(results(r).positions, results(r).responses, 'mo')
                     plot(results(r).indices, means, 'm:')
 
-                    plot([1 length(capture_data)], [0.25 0.25], 'y')
+                    plot([1 length(capture_data)], [threshold threshold], 'y')
                     title(strrep(record_name, '_', '\_') )
                     hold off
                     subdir = "no_detections/";
                     if results(r).whistle_detected
                         subdir = "with_detections/";
                     end
-
-                    if exist(strcat(figure_export_path, recording_name, "/", subdir, "/", results(r).name, "/threshold_", num2str(threshold)), 'dir') ~= 7
-                        mkdir(char(strcat(figure_export_path, recording_name, "/", subdir, "/", results(r).name, "/threshold_", num2str(threshold))))
+                    if flip
+                        subdir = strcat("flipped_", subdir);
+                    end                    
+                    
+                    if exist(strcat(figure_export_path, recording_name, "/ref_", ref_set_name, "/", subdir, "/", results(r).name, "/th_", num2str(threshold)), 'dir') ~= 7
+                        mkdir(char(strcat(figure_export_path, recording_name, "/ref_", ref_set_name, "/", subdir, "/", results(r).name, "/th_", num2str(threshold))))
                     end
-                    filepath = strcat(figure_export_path, recording_name, "/", subdir, results(r).name, "/threshold_", num2str(threshold), "/", record_name);
+                    filepath = strcat(figure_export_path, recording_name, "/ref_", ref_set_name, "/", subdir, results(r).name, "/th_", num2str(threshold), "/", record_name);
                     filepath = strrep(filepath,' ', '_');
-                    filepath = strrep(filepath, 'raw', 'png');
+                    filepath = strrep(filepath, '.raw', '_raw');
                     try
                         saveas(fig, char(filepath), 'png');
                     catch
                         disp(strcat("could not save figure for ", filepath))
                     end
+                    clf(fig);
+                    close(fig);
+                    clear fig filepath
                 end
                 
                 if results(r).whistle_detected
@@ -120,49 +137,102 @@ for t = 1:length(recordings_names)
                     end
                 end                   
             end
+            clear means results
         end %if ~isempty(recording(c).annotations)
     end %c = 1:length(recording)
-    %%
-    % Generate the TP/FP plots
-    tps = [];
-    fps = [];
-    for r = 1:length(ref)
-        % TODO should generate a bar even if there were no detections
-        if ref_stat.(char(ref(r).name)).tp >= 0 || ref_stat.(char(ref(r).name)).fp >= 0
-            tps(end + 1) = ref_stat.(char(ref(r).name)).tp;
-            fps(end + 1) = ref_stat.(char(ref(r).name)).fp;
+    if has_any_annotation
+        %%
+        % Generate the TP/FP plots
+        tps = [];
+        fps = [];
+        tps1 = [];
+        fps1 = [];
+        tps2 = [];
+        fps2 = [];
+        tick_labels = strings(0);
+        tick_labels1 = strings(0);
+        tick_labels2 = strings(0);
+        for r = 1:length(ref)
+            if ~ref_stat.(char(ref(r).name)).valid
+                continue
+            end
+            % TODO should generate a bar even if there were no detections
+            if ref_stat.(char(ref(r).name)).tp > 0 || ref_stat.(char(ref(r).name)).fp > 0
+                tps(end + 1) = ref_stat.(char(ref(r).name)).tp;
+                fps(end + 1) = ref_stat.(char(ref(r).name)).fp;
+                tick_labels(end + 1) = strrep(ref(r).name, '_', '\_');
+            end
+            if ref_stat.(char(ref(r).name)).tp > 0 && ref_stat.(char(ref(r).name)).fp == 0
+                tps1(end + 1) = ref_stat.(char(ref(r).name)).tp;
+                fps1(end + 1) = ref_stat.(char(ref(r).name)).fp;
+                tick_labels1(end + 1) = strrep(ref(r).name, '_', '\_');
+            end
+            if ref_stat.(char(ref(r).name)).fp > 0
+                tps2(end + 1) = ref_stat.(char(ref(r).name)).tp;
+                fps2(end + 1) = ref_stat.(char(ref(r).name)).fp;
+                tick_labels2(end + 1) = strrep(ref(r).name, '_', '\_');
+            end
         end
-    end
-    tick_labels = strings(length(tps));
-    tick = 1;
-    for r = 1:length(ref)
-        if ref_stat.(char(ref(r).name)).tp >= 0 || ref_stat.(char(ref(r).name)).fp >= 0
-            tick_labels(tick) = strrep(ref(r).name, '_', '\_');
-            tick = tick + 1;
+        fig = figure('name', strcat("comparison TP and FP ", recording_name, " threshold: ", num2str(threshold)), 'Position', get(0,'ScreenSize'), 'Visible', 'off');
+        max_count = max([max(tps) max(fps)]);
+        if ~isempty(tps)
+            subplot(2,2,1)
+            bar(tps)
+            title('TP all')
+            xtickangle(45)
+            xticks(1:length(tick_labels))
+            xticklabels(tick_labels)
+            ylabel('total event count')
+            ylim([0 max_count + 1])
+    %         ylim([0 100])
         end
+        if ~isempty(fps)
+            subplot(2,2,2)
+            bar(fps)
+            title('FP all')
+            xtickangle(45)
+            xticks(1:length(tick_labels))
+            xticklabels(tick_labels)
+            ylabel('total event count')
+            ylim([0 max_count + 1])  
+        end  
+        max_count = max([max(tps1) max(fps1)]);
+        if ~isempty(tps1)
+            subplot(2,2,3)
+            bar(tps1)
+            title('has TP only')
+            xtickangle(45)
+            xticks(1:length(tick_labels1))
+            xticklabels(tick_labels1)
+            ylabel('total event count')
+            ylim([0 max_count + 1])
+    %         ylim([0 100])
+        end
+        max_count = max([max(tps2) max(fps2)]);
+        if ~isempty(fps2)
+            subplot(2,2,4)
+            bar(fps2)
+            title('has FP')
+            xtickangle(45)
+            xticks(1:length(tick_labels2))
+            xticklabels(tick_labels2)
+            ylabel('total event count')
+            ylim([0 max_count + 1])  
+        end
+        figtitle = "comparison_TP_FP_";
+        if flip
+            figtitle = strcat(figtitle, "flipped_");
+        end                    
+        filepath = strcat(figure_export_path, recording_name, "/ref_", ref_set_name, "/", figtitle, num2str(threshold), ".png");
+        try
+            saveas(fig, char(filepath), 'png');
+        catch
+            disp(strcat("could not save figure for ", filepath))
+        end
+        clf(fig);
+        close(fig);
+        clear fig filepath tps fps tps1 fps1 tps2 fps2 tick_labels tick_labels1 tick_labels2
     end
-    max_count = max([max(tps) max(fps)]);
-    figure('name', strcat("comparison TP and FP ", recording_name, " threshold: ", num2str(threshold) ))
-    if ~isempty(tps)
-        subplot(1,2,1)
-        bar(tps)
-        title('TP')
-        xtickangle(45)
-        xticks(1:length(tick_labels))
-        xticklabels(tick_labels)
-        ylabel('total event count')
-        %ylim([0 max_count + 1])
-        ylim([0 100])
-    end
-    if ~isempty(fps)
-        subplot(1,2,2)
-        bar(fps)
-        title('FP')
-        xtickangle(45)
-        xticks(1:length(tick_labels))
-        xticklabels(tick_labels)
-        ylabel('total event count')
-        ylim([0 max_count + 1])  
-    end
-    return
+    clear ref_stat
 end %for t = 1:length(capture_database)
+ 
