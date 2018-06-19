01677a9bfe9a27ce8b0559fa2e21f80f37ea4c99
Yigit Akcay <yigitakcay@outlook.com>
2018-06-18 23:22:24 -0400

    parameterize character for move around ball walksteps.

--- NaoTHSoccer/Source/Cognition/Modules/Behavior/PathPlanner/PathPlanner2018.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/Behavior/PathPlanner/PathPlanner2018.cpp
@@ -139,7 +139,7 @@ void PathPlanner2018::moveAroundBall(const double direction, const double radius
     StepBufferElement new_step;
     new_step.setPose(pose);
     new_step.setStepType(StepType::WALKSTEP);
-    new_step.setCharacter(0.7);
+    new_step.setCharacter(params.moveAroundBallCharacter);
     new_step.setScale(1.0);
     new_step.setCoordinate(Coordinate::Hip);
     new_step.setFoot(Foot::NONE);
