4399ec54649d06baa98f9d96c65d85600e2dc651
Etienne Couque-Castelnovo <etienne.couque@gmail.com>
2018-07-30 15:26:31 +0200

    Added reference hull file and added some more debug requests

--- NaoTHSoccer/Source/Motion/SensorFilter/ArmCollisionDetector2018.cpp
+++ NaoTHSoccer/Source/Motion/SensorFilter/ArmCollisionDetector2018.cpp
@@ -12,7 +12,7 @@ ArmCollisionDetector2018::ArmCollisionDetector2018()
 	DEBUG_REQUEST_REGISTER("Motion:SensorFilter:BufferL", "Print left point buffer", false);
 	DEBUG_REQUEST_REGISTER("Motion:SensorFilter:BufferR", "Print right point buffer", false);
 	DEBUG_REQUEST_REGISTER("Motion:SensorFilter:BufferL_hull", "Print calculated convex hull for left joint", false);
-	DEBUG_REQUEST_REGISTER("Motion:SensorFilter:BufferR_hull", "Print calculated convex hull for left joint", false);
+	DEBUG_REQUEST_REGISTER("Motion:SensorFilter:BufferR_hull", "Print calculated convex hull for right joint", false);
 
 
 	getDebugParameterList().add(&params);
@@ -39,13 +39,20 @@ ArmCollisionDetector2018::~ArmCollisionDetector2018()
 
 void ArmCollisionDetector2018::execute()
 {
+	//Reference Hull DEBUG-REQUEST must be here at the beginning because execute() returns quiet early when armmode isn't OK
+	DEBUG_REQUEST("Motion:SensorFilter:ReferenceHull",
+		for (std::vector<Point>::iterator it = P.begin(); it != P.end(); ++it) {
+			std::cout << "x= " << it->getX() << " y= " << it->getY() << std::endl;
+		}
+	);
 	//Check if robot is in a suitable situation to recognize collisions
 	const bool armModeOK =
 		getMotionRequest().armMotionRequest.id == ArmMotionRequest::arms_synchronised_with_walk ||
 		getMotionRequest().armMotionRequest.id == ArmMotionRequest::arms_down;
 
 	const bool motionModeOK = getMotionStatus().currentMotion == motion::walk || getMotionStatus().currentMotion == motion::stand;
-	DEBUG_REQUEST("Motion:SensorFilter:Arm_and:Motion_Ok", 
+	
+	DEBUG_REQUEST("Motion:SensorFilter:Arm_and_Motion_ok", 
 		std::cout << "Arm Mode: " << armModeOK << std::endl << "Motion Mode: " << motionModeOK << std::endl;
 	);
 
@@ -85,6 +92,11 @@ void ArmCollisionDetector2018::execute()
 		//Next compare result to reference points
 		PointBufferLeft.insert(PointBufferLeft.end(), P.begin(), P.end());
 		vBuff = ConvexHull::convexHull(PointBufferLeft);
+		DEBUG_REQUEST("Motion:SensorFilter:BufferL_hull",
+			for (std::vector<Point>::iterator it = vBuff.begin(); it != vBuff.end(); ++it) {
+				std::cout << "x= " << it->getX() << " y= " << it->getY() << std::endl;
+			}
+		);
 		if (vBuff == P)
 		{
 			//No Collision
@@ -105,6 +117,11 @@ void ArmCollisionDetector2018::execute()
 		//Next compare result to reference points
 		PointBufferRight.insert(PointBufferRight.end(), P.begin(), P.end());
 		vBuff = ConvexHull::convexHull(PointBufferRight);
+		DEBUG_REQUEST("Motion:SensorFilter:BufferR_hull",
+			for (std::vector<Point>::iterator it = vBuff.begin(); it != vBuff.end(); ++it) {
+				std::cout << "x= " << it->getX() << " y= " << it->getY() << std::endl;
+			}
+		);
 		if (vBuff == P)
 		{
 			//No Collision
@@ -119,5 +136,17 @@ void ArmCollisionDetector2018::execute()
 		}
 	}
 
+	//Some DEBUG-REQUEST prints
+
+	DEBUG_REQUEST("Motion:SensorFilter:BufferL",
+		for (std::vector<Point>::iterator it = PointBufferLeft.begin(); it != PointBufferLeft.end(); ++it) {
+			std::cout << "x= " << it->getX() << " y= " << it->getY() << std::endl;
+		}
+	);
+	DEBUG_REQUEST("Motion:SensorFilter:BufferR",
+		for (std::vector<Point>::iterator it = PointBufferRight.begin(); it != PointBufferRight.end(); ++it) {
+			std::cout << "x= " << it->getX() << " y= " << it->getY() << std::endl;
+		}
+	);
 
 }
