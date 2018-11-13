5fd10ca0ecda00aa2286900cbdfbfab2ab5266e4
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2018-11-13 17:00:04 +0100

    freshen up the basic Vector2 and Vector3 implementations: - unified formatting - pass all scalars by value - pass all class types by reference - added operators for adding a subtracting a scalar to/from a vector

--- Framework/Commons/Source/Tools/Math/Vector3.h
+++ Framework/Commons/Source/Tools/Math/Vector3.h
@@ -20,29 +20,25 @@ template <class V> class Vector3
   public:
 
   /** The vector values */
-  V x,y,z;
+  V x, y, z;
 
-  /** Default constructor 4 gcc. */
+  /** Default constructor*/
   Vector3<V>():x(0),y(0),z(0)
-  {
-  }
+  {}
 
   /** Default constructor. */
-  Vector3<V>(V x, V y, V z):x(x),y(y),z(z)
-  {
-  }
+  Vector3<V>(V x, V y, V z) : x(x), y(y), z(z)
+  {}
 
   /** Copy constructor
   *\param other The other vector that is copied to this one
   */
   template<class W>
-  Vector3<V>(const Vector3<W>& other)
-    :
+  Vector3<V>(const Vector3<W>& other) :
     x(static_cast<V>(other.x)),
     y(static_cast<V>(other.y)),
     z(static_cast<V>(other.z))
-  {
-  }
+  {}
 
   /** Addition of another vector to this one. 
   *\param other The other vector that will be added to this one
@@ -56,6 +52,18 @@ template <class V> class Vector3
     return *this;
   }
 
+  /** Addition of a scalar to this vector.
+  *\param other The v scalar that will be added to each component of this vector.
+  *\return A reference to this object after the calculation.
+  */
+  Vector3<V>& operator+=(const V v)
+  {
+    x += v;
+    y += v;
+    z += v;
+    return *this;
+  }
+
   /** Substraction of this vector from another one.
   *\param other The other vector this one will be substracted from 
   *\return A reference to this object after the calculation.
@@ -65,6 +73,18 @@ template <class V> class Vector3
     x -= other.x;
     y -= other.y;
     z -= other.z;
+    return *this;
+  }
+
+  /** Substraction of a scalar from this vector.
+  *\param v The scalar that will be substracted from each component of this vector.
+  *\return A reference to this object after the calculation.
+  */
+  Vector3<V>& operator-=(const V v)
+  {
+    x -= v;
+    y -= v;
+    z -= v;
     return *this;
   }
 
@@ -94,28 +114,46 @@ template <class V> class Vector3
   *\param other The other vector that will be added to this one
   *\return A new object that contains the result of the calculation.
   */
-  Vector3<V> operator+(const Vector3<V>& other) const
-    {return Vector3<V>(*this) += other;}
+  Vector3<V> operator+(const Vector3<V>& other) const {
+    return Vector3<V>(*this) += other;
+  }
+
+  /** Addition of a scalar.
+  *\param other The v scalar that will be added to x and y
+  *\return A new object that contains the result of the calculation.
+  */
+  Vector3<V> operator+(const V v) const {
+    return Vector3<V>(*this) += v;
+  }
 
   /** Subtraction of another vector to this one.
   *\param other The other vector that will be added to this one
   *\return A new object that contains the result of the calculation.
   */
-  Vector3<V> operator-(const Vector3<V>& other) const
-    {return Vector3<V>(*this) -= other;}
+  Vector3<V> operator-(const Vector3<V>& other) const {
+    return Vector3<V>(*this) -= other;
+  }
+
+  /** Subtraction of a scalar.
+  *\param other The v scalar that will be subtracted from x and y
+  *\return A new object that contains the result of the calculation.
+  */
+  Vector3<V> operator-(const V v) const {
+    return Vector3<V>(*this) -= v;
+  }
 
   /** Negation of this vector.
   *\return A new object that contains the result of the calculation.
   */
-  Vector3<V> operator-() const
-    {return Vector3<V>() -= *this;}
+  Vector3<V> operator-() const {
+    return Vector3<V>() -= *this;
+  }
 
   /** Inner product of this vector and another one.
   *\param other The other vector this one will be multiplied by 
   *\return The inner product.
   */
-  V operator*(const Vector3<V>& other) const
-  {
+  V operator*(const Vector3<V>& other) const {
     return (x*other.x + y*other.y + z*other.z);
   }
 
@@ -123,23 +161,43 @@ template <class V> class Vector3
   *\param factor The factor this vector is multiplied by 
   *\return A new object that contains the result of the calculation.
   */
-  Vector3<V> operator*(const V factor) const
-    {return Vector3<V>(*this) *= factor;}
+  Vector3<V> operator*(const V factor) const {
+    return Vector3<V>(*this) *= factor;
+  }
 
   /** Division of this vector by a factor.
   *
   *\param factor The factor this vector is divided by 
   *\return A new object that contains the result of the calculation.
   */
-  Vector3<V> operator/(const V factor) const
-    {return Vector3<V>(*this) /= factor;}
+  Vector3<V> operator/(const V factor) const {
+    return Vector3<V>(*this) /= factor;
+  }
+
+  /** Crossproduct of this vector and another vector.
+  *\param other The factor this vector is multiplied with.
+  *\return A new object that contains the result of the calculation.
+  */
+  Vector3<V> operator^(const Vector3<V>& other) const {
+    return Vector3<V>(y * other.z - z * other.y, 
+                      z * other.x - x * other.z, 
+                      x * other.y - y * other.x);
+  }
+
+  /** Crossproduct of this vector and another vector.
+  *\param other The factor this vector is multiplied with.
+  *\return A reference to this object after the calculation.
+  */
+  Vector3<V>& operator^=(const Vector3<V>& other) {
+    *this = *this ^ other; 
+    return *this;
+  }
 
   /** Comparison of another vector with this one.
   *\param other The other vector that will be compared to this one
   *\return Whether the two vectors are equal.
   */
-  bool operator==(const Vector3<V>& other) const
-  {
+  bool operator==(const Vector3<V>& other) const {
     return (x==other.x && y==other.y && z==other.z);
   }
 
@@ -147,51 +205,35 @@ template <class V> class Vector3
   *\param other The other vector that will be compared to this one
   *\return Whether the two vectors are unequal.
   */
-  bool operator!=(const Vector3<V>& other) const
-    {return !(*this == other);}
+  bool operator!=(const Vector3<V>& other) const {
+    return !(*this == other);
+  }
 
-  
   /**
   * array-like member access.
   * \param i index of coordinate
   * \return reference to x, y or z
   */
-  V& operator[](int i)
-  {
+  V& operator[](int i) {
     return  (&x)[i];
   }
 
-  V operator[](int i) const
-  {
-      return (&x)[i];
+  V operator[](int i) const {
+    return (&x)[i];
   }
   
   /** Calculation of the length of this vector.
   *\return The length.
   */
-  V abs() const 
-  {return (V) sqrt(double((*this) * (*this)));}
+  V abs() const {
+    return static_cast<V>(std::sqrt(static_cast<double>(x*x + y*y + z*z)));
+  }
 
   /** calculate the sqr of length
    */
-  V abs2() const
-  { return (*this)*(*this); }
-
-  /** Crossproduct of this vector and another vector.
-  *\param other The factor this vector is multiplied with.
-  *\return A new object that contains the result of the calculation.
-  */
-  Vector3<V> operator^(const Vector3<V>& other) const
-  {return Vector3<V>(y * other.z - z * other.y, 
-                    z * other.x - x * other.z, 
-                    x * other.y - y * other.x);}
-
-  /** Crossproduct of this vector and another vector.
-  *\param other The factor this vector is multiplied with.
-  *\return A reference to this object after the calculation.
-  */
-  Vector3<V>& operator^=(const Vector3<V>& other)
-  {*this = *this ^ other; return *this;}
+  V abs2() const { 
+    return x*x + y*y + z*z; 
+  }
 
   /** normalize this vector.
   *\param len The length, the vector should be normalized to, default=1.
@@ -207,8 +249,7 @@ template <class V> class Vector3
  /** normalize this vector.
   *\return the normalized vector.
   */
-  Vector3<V> normalize()
-  {
+  Vector3<V> normalize() {
     return normalize(static_cast<V>(1));
   }
 
