b6e1b81dbae54ef486cafc2955782e95391694e5
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2018-11-21 15:58:30 +0100

    correct inidices for multiple chanels

--- NaoTHSoccer/Source/Cognition/Modules/Infrastructure/WhistleDetector/STFT.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/Infrastructure/WhistleDetector/STFT.cpp
@@ -8,6 +8,7 @@
 #include <complex>
 #include <iostream>
 
+//                     0, params.nWindowSize, params.nWindowSkipping, params.nWindowSizePadded, spectrumHandler
 STFT::STFT(const int channelOffset, const int windowTime, const int windowTimeStep, const int windowFrequency, SpectrumHandler *handler)
     : offset(channelOffset),
       windowTime(windowTime), 
@@ -25,7 +26,7 @@ STFT::STFT(const int channelOffset, const int windowTime, const int windowTimeSt
     //WARN(windowFrequency >= windowTime, "Frequency window must be greater than Time Window.");
 
     for(int i = 0; i < windowFrequency; ++i) {
-		input[i] = 0.0;
+      input[i] = 0.0;
     }
 
     plan = fftw_plan_dft_r2c_1d(windowFrequency, input, output, FFTW_MEASURE);
@@ -68,12 +69,15 @@ void STFT::newData(const int16_t *data, int length, short channels)
     }
 
     iBegin = 0;
-    while(true) {
-        iEnd = iBegin + windowTimeStep;
+    while(true) 
+    {
+        iEnd = iBegin * channels + windowTime*channels;
         if(iEnd > length) {
             break;
         }
 
+        // copy the data fro the window
+        
         iDataChannel = iBegin * channels + offset;
         while(iBuffer < windowTime) {
             intToFloat(data[iDataChannel], input[iBuffer]);
@@ -88,23 +92,22 @@ void STFT::newData(const int16_t *data, int length, short channels)
         for(int i = 0; i < windowFrequencyHalf; ++i) {
             outputMag[i] = std::sqrt(output[i][0]*output[i][0] + output[i][1]*output[i][1]);
         }
-        if(handler != NULL)
-        {
+
+        if(handler != NULL) {
           handler->handle(outputMag, windowFrequencyHalf);
         }
 
         /* next cycle */
-        iBuffer = 0;
         iBegin  += windowTimeStep;
+        iBuffer = 0;
     }
 
     nOverflow = 0;
     iDataChannel = iBegin * channels + offset;
-    while(iBegin < length) {
+    while(iDataChannel < length) {
         /* copy to overflow buffer */
         overflownData[nOverflow] = data[iDataChannel];
         ++nOverflow;
-        ++iBegin;
         iDataChannel += channels;
     }
 }
