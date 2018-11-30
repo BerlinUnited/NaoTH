8fb2b01348b6db01a8386756ceb1618a128ff08a
Benjamin Schlotter <benji@schlotterfam.de>
2018-11-30 21:15:43 +0100

    copied collision detector from ancient times

---
+++ NaoTHSoccer/Source/Cognition/Modules/Modeling/CollisionDetectorOld/CollisionDetectorOld.h
@@ -0,0 +1,32 @@
+#ifndef COLLISIONDETECTOR_OLD_H
+#define COLLISIONDETECTOR_OLD_H
+
+#include <ModuleFramework/Module.h>
+
+#include <Representations/Modeling/CollisionModel.h>
+#include <Representations/Infrastructure/UltraSoundData.h>
+#include <Representations/Infrastructure/ButtonData.h>
+#include <Representations/Infrastructure/FrameInfo.h>
+#include <Tools/Debug/DebugModify.h>
+
+BEGIN_DECLARE_MODULE(CollisionDetectorOld)
+REQUIRE(FrameInfo)
+REQUIRE(UltraSoundReceiveData)
+REQUIRE(ButtonData)
+PROVIDE(DebugModify)
+PROVIDE(CollisionModel)
+
+END_DECLARE_MODULE(CollisionDetectorOld)
+
+class CollisionDetectorOld : public CollisionDetectorOldBase
+{
+public:
+  CollisionDetectorOld();
+  virtual ~CollisionDetectorOld();
+
+  virtual void execute();
+private:
+  FrameInfo timeSinceFree;
+};
+
+#endif // COLLISIONDETECTOR_OLD_H
