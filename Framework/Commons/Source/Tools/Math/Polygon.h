660ed11a804ff2b4a8654cd384d1bfb92fc10b0f
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2018-12-22 14:40:22 +0100

    removed unnecessary code more compact formatting added comments

--- Framework/Commons/Source/Tools/Math/Polygon.h
+++ Framework/Commons/Source/Tools/Math/Polygon.h
@@ -2,6 +2,7 @@
 * @file Polygon.h
 *
 * @author <a href="mailto:rittercn@student.hu-berlin.de">Claas-Norman Ritter</a>
+* @author <a href="mailto:mellmann@informatik.hu-berlin.de">Heinrich Mellmann</a>
 * Implementation of class Polygon
 */
 
@@ -49,8 +50,8 @@ class Polygon
       makeFromPointSet(point_set);
     }
 
-    //returns the Area surounded by the Polygon defined by the points in the list
-    //in their respective order
+    // returns the Area surounded by the Polygon defined by the points in the list
+    // in their respective order
     double getArea() const {
       return getArea(points);
     }
@@ -59,29 +60,23 @@ class Polygon
       ConvexHull::convexHull(point_set, points);
     }
 
+    // NOTE: the returned value is 0 if points.size() <= 2.
     template <typename P>
     static double getArea(const std::vector<Vector2<P> >& points)
     {
-      if(points.size() > 2)
-      {
-        double area = 0;
-
-        //    [ points[n].x                  points[n].y                ]
-        // A =[                                                         ]
-        //    [ points[(n + 1) % length].x   points[(n + 1) % length].y ]
-        // area += det(A)
-        for(size_t n = 0; n < points.size(); n++) {
-          area += (points[n].y + points[(n + 1) % points.size()].y) * (points[n].x - points[(n + 1) % points.size()].x);
-        }
-
-        return fabs(area) / 2.0;
+      //    [ points[n].x                  points[n].y                ]
+      // A =[                                                         ]
+      //    [ points[(n + 1) % length].x   points[(n + 1) % length].y ]
+      // area += det(A)
+      double area = 0;
+      for(size_t n = 0; n < points.size(); n++) {
+        area += (points[n].y + points[(n + 1) % points.size()].y) * (points[n].x - points[(n + 1) % points.size()].x);
       }
-      else
-        return -1;
+
+      return fabs(area) / 2.0;
     }
 
     inline bool isInside(const Vector2<T>& point) const {
-      //return testCrossingNumber(point) == 1;
       return testWindingNumber(points, point) != 0;
     }
 
@@ -91,97 +86,52 @@ class Polygon
     }
 
   private:
-    // TODO: the code below is a simple hessial form for a line
-    //       it's public domain and no copyright for it can be claimed
+    // Calculate the signed distance of the point 'p' to the line defined by the points 'a' and 'b'.
     //
-    // Copyright 2001, softSurfer (www.softsurfer.com)
-    // This code may be freely used and modified for any purpose
-    // providing that this copyright notice is included with it.
-    // SoftSurfer makes no warranty for this code, and cannot be held
-    // liable for any real or imagined damage resulting from its use.
-    // Users of this code must verify correctness for their application.
-
-    //    a Point is defined by its coordinates {int x, y;}
-    //===================================================================
-
-    // tests if a point is Left|On|Right of an infinite line.
-    //    Input:  three points P0, P1, and P2
-    //    Return: >0 for P2 left of the line through P0 and P1
-    //            =0 for P2 on the line
-    //            <0 for P2 right of the line
-    //    See: the January 2001 Algorithm "Area of 2D and 3D Triangles and Polygons"
-    inline static T getRelPos(const Vector2<T>& p0, const Vector2<T>& p1, const Vector2<T>& p2 ) {
-        return (p1.x - p0.x) * (p2.y - p0.y) - (p2.x - p0.x) * (p1.y - p0.y);
+    // Input:  three points a, b, and p
+    // Return: >0 for p left of the line through a and b
+    //         =0 for p on the line
+    //         <0 for p right of the line
+    inline static T getRelPos(const Vector2<T>& a, const Vector2<T>& b, const Vector2<T>& p ) {
+        return (b.x - a.x) * (p.y - a.y) - (p.x - a.x) * (b.y - a.y);
     }
-    //===================================================================
 
-    /**
-    * crossing number test for a point in a polygon
-     * This code is patterned after [Franklin, 2000]
-    */
-    // NOTE: this code is not used, do we need it?
-    /*
-    template <typename P>
-    static int testCrossingNumber(const std::vector<Vector2<P> >& points, const Vector2<T>& p)
-    {
-      int crossingNr = 0;    // the crossing number counter
-
-      int j = size() - 1;
-//      bool oddNodes = false;
-
-      for (size_t i = 0; i < points.size(); i++)
-      {
-        if (points[i].y < p.y && points[j].y >= p.y || points[j].y < p.y && points[i].y >= p.y)
-        {
-          if (points[i].x + (p.y - points[i].y) / (points[j].y - points[i].y) * (points[j].x - points[i].x) < p.x)
-          {
-//            oddNodes = !oddNodes;
-            ++crossingNr;
-          }
-        }
-        j = i;
-      }
-      return (crossingNr & 1);    // 0 if even (out), and 1 if odd (in)
-    }
-    */
-    //===================================================================
-
-    // winding number test for a point in a polygon
+    // 
+    // calculate the winding number test for a point in a polygon
+    //
     template <typename P>
     static int testWindingNumber(const std::vector<Vector2<P> >& points, const Vector2<T>& p)
     {
-        int windingNr = 0;    // the winding number counter
+        // the counter for the winding number 
+        int windingNr = 0;    
 
+        // NOTE: j points to the the last element. 
+        //       If points.size() == 0 the loop below doesnt do anything
+        //       and j is not used.
         size_t j = points.size() - 1;
 
         // loop through all edges of the polygon
         for (size_t i = 0; i < points.size(); i++)
-        {   // edge from V[i] to V[i+1]
-            if (points[j].y <= p.y)
-            {         // start y <= P.y
-                if (points[i].y > p.y)      // an upward crossing
-                {
-                    if (getRelPos( points[j], points[i], p) > 0)  // P left of edge
-                    {
-                        ++windingNr;            // have a valid up intersect
+        {   
+            // consider edge from V[i] to V[i+1]
+            if (points[j].y <= p.y) {                               // start y <= P.y 
+                if (points[i].y > p.y) {                            // an upward crossing
+                    if (getRelPos( points[j], points[i], p) > 0) {  // P left of the edge
+                        ++windingNr;                                // have a valid up intersect
                     }
                 }
-            }
-            else
-            {                       // start y > P.y (no test needed)
-                if (points[i].y <= p.y)     // a downward crossing
-                {
-                    if (getRelPos( points[j], points[i], p) < 0)  // P right of edge
-                    {
-                        --windingNr;            // have a valid down intersect
+            } else {                                                // start y >= P.y (no test needed)
+                if (points[i].y <= p.y) {                           // a downward crossing
+                    if (getRelPos( points[j], points[i], p) < 0) {  // P right of the edge
+                        --windingNr;                                // have a valid down intersect
                     }
                 }
             }
             j = i;
         }
+
         return windingNr;
     }
-    //===================================================================
 
 };//end class Polygon
 
