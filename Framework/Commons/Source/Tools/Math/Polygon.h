217fb3308ab734d5aebf962ad54dfb7c41edee86
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2018-12-22 14:57:33 +0100

    added a rudimentary test

--- Framework/Commons/Source/Tools/Math/Polygon.h
+++ Framework/Commons/Source/Tools/Math/Polygon.h
@@ -85,6 +85,32 @@ class Polygon
       return testWindingNumber(points, point) != 0;
     }
 
+    inline static void test() 
+    {
+      // square
+      std::vector<Vector2d> points = {
+        {-1, 1},
+        { 1, 1},
+        { 1,-1},
+        {-1,-1}
+      };
+
+      Polygon<double> square(points);
+      std::cout << "Area: " << square.getArea() << std::endl;
+      std::cout << "Inside: " << square.isInside({-0.5,0.5}) << std::endl;
+      std::cout << "Not inside: " << square.isInside({-5,5}) << std::endl;
+
+      Polygon<double> empty;
+      std::cout << "Area: " << empty.getArea() << std::endl;
+      std::cout << "Not Inside: " << empty.isInside({-0.5,0.5}) << std::endl;
+      std::cout << "Not inside: " << empty.isInside({-5,5}) << std::endl;
+
+      Polygon<double> two({{-1,-1}, {1, 1}});
+      std::cout << "Area: " << two.getArea() << std::endl;
+      std::cout << "Not Inside: " << two.isInside({-0.5,0.5}) << std::endl;
+      std::cout << "Not inside: " << two.isInside({-5,5}) << std::endl;
+    }
+
   private:
     // Calculate the signed distance of the point 'p' to the line defined by the points 'a' and 'b'.
     //
