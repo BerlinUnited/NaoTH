734a269d52fc8e66c0ccfc9c98832c79a2a49ff2
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2019-01-22 19:50:19 +0100

    added infrastructure to sort out the "augmented" parts of the old line perception

---
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/LineDetector/LineAugmenter.cpp
@@ -0,0 +1,18 @@
+
+#include "LineAugmenter.h"
+
+LineAugmenter::LineAugmenter()
+{
+  
+}
+
+LineAugmenter::~LineAugmenter()
+{
+}
+
+void LineAugmenter::execute()
+{
+  getLinePerceptAugmented().reset();
+
+
+}
---
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/LineDetector/LineAugmenter.h
@@ -0,0 +1,44 @@
+#ifndef _LineAugmenter_H
+#define _LineAugmenter_H
+
+#include <ModuleFramework/Module.h>
+
+#include <Representations/Infrastructure/FrameInfo.h>
+#include "Representations/Infrastructure/FieldInfo.h"
+#include "Representations/Perception/LinePercept2018.h"
+#include "Representations/Perception/LinePerceptAugmented.h"
+
+#include "Tools/Debug/DebugRequest.h"
+#include "Tools/Debug/DebugModify.h"
+#include "Tools/Debug/DebugDrawings.h"
+#include "Tools/Debug/DebugImageDrawings.h"
+#include "Tools/Debug/DebugParameterList.h"
+
+BEGIN_DECLARE_MODULE(LineAugmenter)
+  PROVIDE(DebugRequest)
+  PROVIDE(DebugModify)
+  PROVIDE(DebugDrawings)
+  PROVIDE(DebugImageDrawings)
+  PROVIDE(DebugImageDrawingsTop)
+  PROVIDE(DebugParameterList)
+
+  REQUIRE(FieldInfo)
+  REQUIRE(RansacCirclePercept2018)
+  
+  PROVIDE(LinePerceptAugmented)
+  
+END_DECLARE_MODULE(LineAugmenter)
+
+class LineAugmenter: public LineAugmenterBase
+{
+public:
+ LineAugmenter();
+ ~LineAugmenter();
+
+ virtual void execute();
+
+private:
+  //
+};
+
+#endif // _LineAugmenter_H
