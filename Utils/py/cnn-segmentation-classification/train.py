dfdb42d1c15bb780b41a5180afcf305744d8f829
Thomas Krause <thomaskrause@posteo.de>
2019-04-28 12:22:28 +0200

    Move models into extra file

--- Utils/py/cnn-segmentation-classification/train.py
+++ Utils/py/cnn-segmentation-classification/train.py
@@ -7,6 +7,7 @@ from keras.models import *
 from keras.layers import *
 from datetime import datetime
 import numpy as np
+import model_zoo;
 
 class AccHistory(keras.callbacks.Callback):
     def on_train_begin(self, logs={}):
@@ -31,50 +32,8 @@ class AccHistory(keras.callbacks.Callback):
         if max_idx >= 0 and max_idx < len(self.acc):
             print("Maximum is {:.4f}".format(self.acc[max_idx]) )
 
-def naodevils():
-    model = Sequential()
-    model.add(Convolution2D(8, (5, 5), input_shape=(x.shape[1], x.shape[2], 1),
-                            activation='relu', strides=(2, 2), padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Convolution2D(12, (3, 3), activation='relu'))
-    model.add(Convolution2D(2, (2, 2), activation='softmax'))
-    model.add(Flatten())
 
-    # radius, x, y
-    model.add(Dense(3))
-
-    return model
-
-
-def fy_max():
-    input_shape = (16, 16, 1)
-
-    model = Sequential()
-    model.add(Convolution2D(12, (3, 3), input_shape=input_shape, padding='same'))
-    model.add(LeakyReLU(alpha=0.0))  # alpha unknown, so default
-
-    model.add(Convolution2D(16, (3, 3), padding='same'))
-    model.add(LeakyReLU())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(24, (3, 3), padding='same'))
-    model.add(LeakyReLU())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(32, (3, 3), padding='same'))
-    model.add(LeakyReLU(alpha=0.0))
-
-    model.add(Convolution2D(32, (1, 1), padding='same'))
-   
-    # classifier
-    model.add(Flatten())
-    #    model.add(Dense(32))
-    # radius, x, y
-    model.add(Dense(3, activation="relu"))
-
-    return model
-
-def fy_1300():
+def tmp_model():
     input_shape = (16, 16, 1)
 
     model = Sequential()
@@ -102,155 +61,7 @@ def fy_1300():
 
     return model
 
-def fy_stride():
-    input_shape = (16, 16, 1)
 
-    model = Sequential()
-    model.add(Convolution2D(4, (3, 3), input_shape=input_shape, padding='same'))
-    model.add(LeakyReLU(alpha=0.0))  # alpha unknown, so default
-
-    model.add(Convolution2D(5, (3, 3), padding='same'))
-    model.add(LeakyReLU())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(6, (3, 3), strides=(2, 2), padding='same'))
-    model.add(LeakyReLU())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(7, (3, 3), padding='same'))
-    model.add(LeakyReLU(alpha=0.0))
-
-    model.add(Convolution2D(7, (1, 1), padding='same'))
-   
-    # classifier
-    model.add(Flatten())
-    #    model.add(Dense(32))
-    # radius, x, y
-    model.add(Dense(3, activation="relu"))
-
-    return model
-
-def fy_1500():
-    input_shape = (16, 16, 1)
-
-    model = Sequential()
-    model.add(Convolution2D(4, (3, 3), input_shape=input_shape, padding='same'))
-    model.add(LeakyReLU(alpha=0.0))  # alpha unknown, so default
-
-    model.add(Convolution2D(4, (3, 3), padding='same'))
-    model.add(LeakyReLU())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(8, (3, 3), padding='same'))
-    model.add(LeakyReLU())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(8, (3, 3), padding='same'))
-    model.add(LeakyReLU(alpha=0.0))
-
-    model.add(Convolution2D(8, (1, 1), padding='same'))
-   
-    # classifier
-    model.add(Flatten())
-    #    model.add(Dense(32))
-    # radius, x, y
-    model.add(Dense(3, activation="relu"))
-
-    return model
-
-def fy_half():
-    input_shape = (16, 16, 1)
-
-    model = Sequential()
-    model.add(Convolution2D(6, (3, 3), input_shape=input_shape, padding='same'))
-    model.add(LeakyReLU(alpha=0.0))  # alpha unknown, so default
-    # model.add(BatchNormalization())
-
-    model.add(Convolution2D(8, (3, 3), padding='same'))
-    model.add(LeakyReLU())
-    # model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(16, (3, 3), padding='same'))
-    model.add(LeakyReLU())
-    # model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(32, (3, 3), padding='same'))
-    model.add(LeakyReLU(alpha=0.0))
-    # model.add(BatchNormalization())
-#    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(16, (1, 1), padding='same'))
-   
-    # classifier
-    model.add(Flatten())
-    #    model.add(Dense(32))
-    # radius, x, y
-    model.add(Dense(3, activation="relu"))
-
-    return model
-
-def fy_step1():
-    input_shape = (16, 16, 1)
-
-    model = Sequential()
-    model.add(Convolution2D(12, (3, 3), input_shape=input_shape, padding='same'))
-    model.add(LeakyReLU(alpha=0.0))  # alpha unknown, so default
- 
-    model.add(Convolution2D(13, (3, 3), padding='same'))
-    model.add(LeakyReLU())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(14, (3, 3), padding='same'))
-    model.add(LeakyReLU())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(15, (3, 3), padding='same'))
-    model.add(LeakyReLU(alpha=0.0))
- 
-    model.add(Convolution2D(15, (1, 1), padding='same'))
-   
-    # classifier
-    model.add(Flatten())
-    # radius, x, y
-    model.add(Dense(3, activation="relu"))
-
-    return model
-
-def fy_lessfeat():
-    input_shape = (16, 16, 1)
-
-    model = Sequential()
-    model.add(Convolution2D(4, (3, 3), input_shape=input_shape, padding='same'))
-    model.add(LeakyReLU(alpha=0.0))  # alpha unknown, so default
-    # model.add(BatchNormalization())
-
-    model.add(Convolution2D(10, (3, 3), padding='same'))
-    model.add(LeakyReLU())
-    # model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(20, (3, 3), padding='same'))
-    model.add(LeakyReLU())
-    # model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(22, (3, 3), padding='same'))
-    model.add(LeakyReLU(alpha=0.0))
-    # model.add(BatchNormalization())
-#    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Convolution2D(22, (1, 1), padding='same'))
-   
-    # classifier
-    model.add(Flatten())
-    #    model.add(Dense(32))
-    # radius, x, y
-    model.add(Dense(3, activation="relu"))
-
-    return model
-    
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
@@ -297,15 +108,8 @@ with open(imgdb_path, "rb") as f:
 if args.proceed is None or args.proceed == False:
     print("Creating new model")
 
-    model = fy_stride()
-#    model = fy_max()
-#    model = fy_half()
-#    model = fy_lessfeat()
-#    model = fy_step1()
-#    model = fy_1300()
-#    model = fy_1000()
-#    model = fy_1500()
-#    model = naodevils()
+    model = model_zoo.fy_1300()
+    #model = tmp_model()
 else:
     print("Loading model " + model_path)
     model = load_model(model_path)
