28c31e601e882c4c916b7a53d49488752916cf2e
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2019-05-28 13:14:46 +0200

    export the recorded AudioData into a separate logfile

---
+++ Utils/py/WhistleDetector/export_audiodata.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import sys
+import getopt
+from naoth.LogReader import LogReader
+
+def write(f, name, out):
+  position, size, _ = f.messages[name]
+  f.reader.log.seek(position)
+  data = f.reader.readBytes(size)
+
+  out.write(f.number.to_bytes(4, byteorder='little', signed=False))
+  out.write(name.encode('ascii'))
+  out.write(b'\0')
+  out.write(size.to_bytes(4, byteorder='little', signed=False))
+  out.write(data)
+
+if __name__ == "__main__":
+    logFilePath = "D:/RoboCup/log/2019-famff/game.log"
+
+    out = open("audio_data.log", "wb")
+    
+    for f in LogReader(logFilePath):
+      if "AudioData" in f.messages or "WhistlePercept" in f.messages:
+        write(f, "FrameInfo", out)
+      if "AudioData" in f.messages:
+        write(f, "AudioData", out)
+      if "WhistlePercept" in f.messages:
+        write(f, "WhistlePercept", out)
+        
+    out.close()
+        
