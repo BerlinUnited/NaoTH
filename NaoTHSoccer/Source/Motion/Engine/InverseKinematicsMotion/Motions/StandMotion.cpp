78ffe8ed7ddec089032c25f160927bedd9551742
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2019-07-04 09:17:26 +1000

    removed debug print

--- NaoTHSoccer/Source/Motion/Engine/InverseKinematicsMotion/Motions/StandMotion.cpp
+++ NaoTHSoccer/Source/Motion/Engine/InverseKinematicsMotion/Motions/StandMotion.cpp
@@ -357,7 +357,8 @@ void StandMotion::tuneJointOffsets()
       }
     }
 
-    std::cout << "max: " << maxId << " : " << jointMonitors[maxId].filteredCurrent() << std::endl; 
+    // DEBUG: print max joint
+    //std::cout << "max: " << maxId << " : " << jointMonitors[maxId].filteredCurrent() << std::endl; 
 
     // if greater than Threshold then try to tune the offsets
     if(jointMonitors[maxId].filteredCurrent() > getEngine().getParameters().stand.relax.jointOffsetTuning.currentThreshold) {
