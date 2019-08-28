a2c2bf10648debdf5a33cc07a713e99e149fef61
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2019-08-28 18:12:07 +0200

    use *.cc, *.h and *.py target files to check if a recompile is necessary only rebuild files that need to be rebuilt removed unused legacy code

--- Framework/BuildTools/protoc.lua
+++ Framework/BuildTools/protoc.lua
@@ -1,54 +1,31 @@
--- as found on http://lua-users.org/wiki/SplitJoin
-local function split(str, delim, maxNb)
-    -- Eliminate bad cases...
-    if string.find(str, delim) == nil then
-        return { str }
-    end
-    if maxNb == nil or maxNb < 1 then
-        maxNb = 0    -- No limit
-    end
-    local result = {}
-    local pat = "(.-)" .. delim .. "()"
-    local nb = 0
-    local lastPos
-    for part, pos in string.gfind(str, pat) do
-        nb = nb + 1
-        result[nb] = part
-        lastPos = pos
-        if nb == maxNb then break end
-    end
-    -- Handle the last field
-    if nb ~= maxNb then
-        result[nb + 1] = string.sub(str, lastPos)
-    end
-    return result
-end
 
-local function getShadowFile(file)
-  name = path.getname(file)
-  dir = path.getdirectory(file)
-  return dir .. "/." .. name .. ".lastCompile~"
-end
+local function checkRecompileNeeded(file, cppOut, javaOut, pythonOut)
 
-local function checkRecompileNeeded(file)
-
-  file_info = os.stat(file)
-  shadow_info = os.stat(getShadowFile(file))
-  if(shadow_info == nil or (file_info ~= nil and file_info.mtime > shadow_info.mtime)) then
-    print ("INFO: " .. file ..  " was changed")
-    return true
+  local file_info = os.stat(file)
+  local name = path.getbasename(file)
+  
+  if file_info == nil then
+    print ("ERROR: conld not get the stats from file " .. file)
+    return false
   end
-  return false
+  
+  local cpp_info = os.stat(path.join(cppOut, name..".pb.cc"))
+  local hpp_info = os.stat(path.join(cppOut, name..".pb.h"))
+  --local java_info = os.stat(path.join(javaOut, name..".java"))
+  local python_info = os.stat(path.join(pythonOut, name:gsub("-", "_").."_pb2.py"))
+  
+  if python_info == nil then
+    print(path.join(pythonOut, name.."_pb2.py"))
+  end
+
+  return (cpp_info == nil    or file_info.mtime > cpp_info.mtime) or
+         (hpp_info == nil    or file_info.mtime > cpp_info.mtime) or
+         --(java_info == nil   or file_info.mtime > java_info.mtime) or
+         (python_info == nil or file_info.mtime > python_info.mtime)
 end
 
-local function touchShadow(file, time)
-  local shadowFile = getShadowFile(file)
-  io.output(shadowFile)
-  io.write(time)
-  io.close()  
-end
 
-local function protocCompile(inputFiles, cppOut, javaOut, pythonOut, ipaths)
+local function protocCompileAll(inputFiles, cppOut, javaOut, pythonOut, ipaths)
   -- get the protobuf compiler from the EXTERN_PATH
   compiler = os.ishost("windows") and "protoc.exe" or "protoc"
   
@@ -61,25 +38,30 @@ local function protocCompile(inputFiles, cppOut, javaOut, pythonOut, ipaths)
   end
   
   local args = ""
-  -- hack
-  args = args .. "--python_out=\"" ..  pythonOut .. "\" " -- always mind the space at the end
+
+  if(pythonOut ~= nil) then
+    args = args .. "--python_out=\"" ..  pythonOut .. "\" " -- always mind the space at the end
+    os.mkdir(pythonOut) -- create the output directory if needed
+  end 
   
   if(cppOut ~= nil) then
     args = args .. "--cpp_out=\"" ..  cppOut .. "\" " -- always mind the space at the end
+    os.mkdir(cppOut) -- create the output directory if needed
   end
+  
   if(javaOut ~= nil) then
     args = args .. "--java_out=\"" ..  javaOut .. "\" " -- always mind the space at the end
+    os.mkdir(javaOut) -- create the output directory if needed
   end
+  
   if(ipaths ~= nil) then
     for i=1,#ipaths do
       args = args .. "--proto_path=\"" ..  ipaths[i] .. "\" " -- always mind the space at the end
     end
   end 
 
-  --args = args .. table.concat(inputFiles, " ")
-  for i,v in pairs(inputFiles) do
-	args = args .. " \"" .. v .. "\""
-  end
+  -- add files to compile
+  args = args .. table.concat(inputFiles, " ")
   
   local cmd = "\"" .. compilerPath .. "/" .. compiler .. "\" " .. args
   
@@ -87,22 +69,16 @@ local function protocCompile(inputFiles, cppOut, javaOut, pythonOut, ipaths)
     cmd = "\"" .. cmd .. "\""
   end
   
-  -- HACK: create the output directories if needed
-  os.mkdir(cppOut)
-  os.mkdir(javaOut)
-  
-  -- delete all generated message files
+  -- TODO. why is this necessary?
+  -- delete the generated cpp files
   print("Try to remove the pb.cc and .pb.h files ")
-  -- print(os.matchfiles(path.join(cppOut,"**.pb.h")))
-  for i,file in ipairs(os.matchfiles(path.join(cppOut,"**.pb.h"))) do
-    ok, err = os.remove (file)
-  end
-  for i,file in ipairs(os.matchfiles(path.join(cppOut,"**.pb.cc"))) do
-    ok, err = os.remove (file)
+  for i,file in ipairs(inputFiles) do
+    local name = path.getbasename(file)
+    ok, err = os.remove (path.join(cppOut, name..".pb.cc"))
+    ok, err = os.remove (path.join(cppOut, name..".pb.h"))
   end
   print("Removed all generated message files")
-
-
+  
   -- generate the message files
   print("INFO: (Protbuf) executing " .. cmd)
   local succ, status, returnCode = os.execute(cmd)
@@ -117,6 +93,9 @@ local function protocCompile(inputFiles, cppOut, javaOut, pythonOut, ipaths)
   return returnCode == 0
 end
 
+-- adds some content at the beginning and and the end of the file.
+-- the new content looks like this: <prefix><original content><suffix>
+-- TODO: it was used to add some warning pragmas. check if it still necessarey
 function file_add_prefix_sufix(file_name, prefix, suffix)
   local f = io.open(file_name, "r")
   if (f == nil) then
@@ -174,32 +153,54 @@ function makeprotoc(arg)
   invokeprotoc(arg.inputFiles, arg.cppOut, arg.javaOut, arg.pythonOut, arg.includeDirs)
 end
 
-function invokeprotoc(inputFiles, cppOut, javaOut, pythonOut, includeDirs)
+function invokeprotocAll(inputFiles, cppOut, javaOut, pythonOut, includeDirs)
 	-- check if protobuf compile is explicitely requested
-    local compile = (_OPTIONS["protoc"] ~= nil)
+  local compile = (_OPTIONS["protoc"] ~= nil)
 
 	-- iterate over all files to check if any of them was changed
-    for i = 1, #inputFiles do
-      if(checkRecompileNeeded(inputFiles[i])) then
-        compile = true
-        break
-      end
+  for i = 1, #inputFiles do
+    if(checkRecompileNeeded(inputFiles[i], cppOut, javaOut, pythonOut)) then
+      compile = true
+      break
     end
+  end
 
   if(compile) then
     -- execute compile process for each file
     local time = os.time()
     -- do the recompilation
-   if( protocCompile(inputFiles, cppOut, javaOut, pythonOut, includeDirs)) then
-     -- if successfull touch the shadow files
-     for i = 1, #inputFiles do
+    if( protocCompile(inputFiles, cppOut, javaOut, pythonOut, includeDirs)) then
+      -- if successfull touch the shadow files
+      for i = 1, #inputFiles do
         -- touch shadow file in order to remember this build date
         touchShadow(inputFiles[i], time)
-    end -- end for each file to compile
-   else
-    print ("ERROR: protoc not successful")
-   end
+      end -- end for each file to compile
+    else
+      print ("ERROR: protoc not successful")
+    end
   end -- end if compile
+end
+
+function invokeprotoc(inputFiles, cppOut, javaOut, pythonOut, includeDirs)
+	-- check if protobuf compile is explicitely requested
+  local compileAll = (_OPTIONS["protoc"] ~= nil)
+
+  -- remember the time in case it is needed for the shadow file
+  local time = os.time()
+  local filesToCompile = {}
+
+  -- collect the files that need to be compiled
+  for i = 1, #inputFiles do
+    if checkRecompileNeeded(inputFiles[i], cppOut, javaOut, pythonOut) then
+      table.insert(filesToCompile, inputFiles[i])
+    end
+  end
+
+  -- filesToCompile is not empty
+  if next(filesToCompile) ~= nil then
+    protocCompileAll(filesToCompile, cppOut, javaOut, pythonOut, includeDirs)
+  end
+  
 end
 
 newoption {
@@ -230,18 +231,3 @@ newoption {
   value = "OUT-DIR",
   description = "Output directory for the Python classes generated by protoc"
 }
-
-
--- the action protoc takes the files to compile as argument
--- see also the "protoc-cpp" and "protoc-java" arguments for specifying what kind of file
--- to generate and where to generate them
-
---[[
-newaction {
-  trigger = "protoc",
-  description = "Compile premake5 messages given as argument",
-  execute = function ()
-    invokeprotoc(_ARGS, _OPTIONS["protoc-cpp"], _OPTIONS["protoc-java"], _OPTIONS["protoc-python"], split(_OPTIONS["protoc-ipath"], ":") )    
-  end
-}
-]]--
