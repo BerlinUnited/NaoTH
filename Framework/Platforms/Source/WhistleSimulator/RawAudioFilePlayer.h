fab1798d5653541771484222f1ae18a9688b60ce
Steffen Kaden <kaden@informatik.hu-berlin.de>
2019-10-08 13:07:43 +0200

    fix style

--- Framework/Platforms/Source/WhistleSimulator/RawAudioFilePlayer.h
+++ Framework/Platforms/Source/WhistleSimulator/RawAudioFilePlayer.h
@@ -9,34 +9,33 @@
 
 #include "myconio.h"
 
-namespace naoth
-{
-    class RawAudioFilePlayer
-    {
+namespace naoth {
+    class RawAudioFilePlayer {
         public:
-        RawAudioFilePlayer();
-        virtual ~RawAudioFilePlayer();
+            RawAudioFilePlayer();
+            virtual ~RawAudioFilePlayer();
 
-        void setParams(int numChannels, int sampleRate, int samples, int overlap);
-        void open(const std::string& filePath);
+            void setParams(int numChannels, int sampleRate, int samples, int overlap);
+            void open(const std::string& filePath);
 
-        void get(naoth::AudioData& data);
+            void get(naoth::AudioData& data);
 
-        unsigned int readingTimestamp;
+            unsigned int readingTimestamp;
 
         protected:
-        std::ifstream audioFile;
+            std::ifstream audioFile;
 
-        int numChannels_;
-        int sampleRate_;
-        int samples_;
-        int overlap_;
+            int numChannels_;
+            int sampleRate_;
+            int samples_;
+            int overlap_;
 
-        bool ready;
+            bool ready;
 
-        void readAupFile(std::ifstream& aupFile);
-        size_t extractField(const std::string& line, const std::string name, std::string& value, const size_t start);
-  };
+            void readAupFile(std::ifstream& aupFile);
+            size_t extractField(const std::string& line, const std::string name,
+                                std::string& value, const size_t start);
+    };
 }
 
 #endif // _RawAudioFilePlayer_H
