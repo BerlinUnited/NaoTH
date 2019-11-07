f693a4563b8fff866cdecf21ffd15d7f0bfd49d6
Steffen Kaden <kaden@informatik.hu-berlin.de>
2019-11-07 10:45:02 +0100

    add simple percept for the ultrasonic sensors

--- NaoTHSoccer/Source/Representations/Perception/ObstaclePercept.h
+++ NaoTHSoccer/Source/Representations/Perception/ObstaclePercept.h
@@ -1,6 +1,15 @@
 #ifndef OBSTACLE_H
 #define OBSTACLE_H
 
+#include <vector>
+#include <Tools/Math/Vector2.h>
+
+class UltrasonicObstaclePercept {
+public:
+    // location of the distance measurement projected on the ground
+    std::vector<Vector2d> location_on_ground;
+};
+
 class VisionObstacle {
 public:
   VisionObstacle() : valid(false) {}
