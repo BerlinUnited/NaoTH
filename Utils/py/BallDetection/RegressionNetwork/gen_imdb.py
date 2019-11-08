51ff6d9c5a171b7a13be44d09be26a0d02f23005
Benjamin Schlotter <benji@schlotterfam.de>
2019-11-08 14:50:33 +0100

    cleanup imdb creation script

--- Utils/py/BallDetection/RegressionNetwork/gen_imdb.py
+++ Utils/py/BallDetection/RegressionNetwork/gen_imdb.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import argparse
 from utility_functions.csv_loader import loadImages
 import pickle
@@ -14,40 +12,39 @@ def str2bool(v):
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
-parser = argparse.ArgumentParser(description='Generate the image database for training etc. '
-                                             'using a folder with 0, 1 etc. subfolders with png images.')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate the image database for training etc. '
+                                                 'using a folder with 0, 1 etc. subfolders with'
+                                                 ' png images.')
+    parser.add_argument('-b', '--database-path', dest='imgdb_path',
+                        help='Path to the image database to write. '
+                             'Default is img.db in current folder.')
+    parser.add_argument('-i', '--image-folder', dest='img_path', nargs='+',
+                        help='Path to the CSV file(s) with region annotation.')
+    parser.add_argument('-r', '--resolution', dest='res',
+                        help='Images will be resized to this resolution. Default is 16x16')
+    parser.add_argument("-l", "--limit-noball", type=str2bool, nargs='?', dest="limit_noball",
+                        const=True,
+                        help="Randomly select at most |balls| from no balls class")
 
-parser.add_argument('-b', '--database-path', dest='imgdb_path',
-                    help='Path to the image database to write. '
-                         'Default is img.db in current folder.')
-parser.add_argument('-i', '--image-folder', dest='img_path', nargs='+',
-                    help='Path to the CSV file(s) with region annotation.')
-parser.add_argument('-r', '--resolution', dest='res',
-                    help='Images will be resized to this resolution. Default is 16x16')
+    args = parser.parse_args()
 
-parser.add_argument("-l", "--limit-noball", type=str2bool, nargs='?', dest="limit_noball",
-                    const=True,
-                    help="Randomly select at most |balls| from no balls class")
+    # set default values for output path, input path and resolution
+    imgdb_path = "img.db"
+    img_path = "."
+    res = {"x": 16, "y": 16}
 
-args = parser.parse_args()
+    if args.imgdb_path is not None:
+        imgdb_path = args.imgdb_path
+    if args.img_path is not None:
+        img_path = args.img_path
+    if args.res is not None:
+        res = {"x": int(args.res), "y": int(args.res)}
 
-imgdb_path = "img.db"
-img_path = "."
-res = {"x": 16, "y": 16}
+    with open(imgdb_path, "wb") as f:
+        x, y, mean, p = loadImages(img_path, res, args.limit_noball)
 
-if args.imgdb_path is not None:
-    imgdb_path = args.imgdb_path
-
-if args.img_path is not None:
-    img_path = args.img_path
-
-if args.res is not None:
-    res = {"x": int(args.res), "y": int(args.res)}
-
-with open(imgdb_path, "wb") as f:
-    x, y, mean, p = loadImages(img_path, res, args.limit_noball)
-
-    pickle.dump(mean, f)
-    pickle.dump(x, f)
-    pickle.dump(y, f)
-    pickle.dump(p, f)
+        pickle.dump(mean, f)
+        pickle.dump(x, f)
+        pickle.dump(y, f)
+        pickle.dump(p, f)
