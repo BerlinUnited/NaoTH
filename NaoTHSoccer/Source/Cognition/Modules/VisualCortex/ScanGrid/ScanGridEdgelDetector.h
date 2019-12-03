67d5378b3ce26403f1f2a6c2f4b0a418079b9f9d
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2019-12-03 13:21:38 +0100

    bugfix: removed a c++17 feature (using)

--- NaoTHSoccer/Source/Cognition/Modules/VisualCortex/ScanGrid/ScanGridEdgelDetector.h
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/ScanGrid/ScanGridEdgelDetector.h
@@ -161,8 +161,9 @@ private:
 
   class MinPeakScan: public MaxPeakScan
   {
-    using MaxPeakScan::MaxPeakScan;
   public:
+    explicit MinPeakScan(int threshold) : MaxPeakScan(threshold) {}
+
     inline bool check(int point, int value) override
     {
       return MaxPeakScan::check(point, -value);
