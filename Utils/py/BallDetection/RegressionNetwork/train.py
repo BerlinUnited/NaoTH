005cfdb4a52a4abc30153a4060df81bccbbd68a5
Benjamin Schlotter <benji@schlotterfam.de>
2019-12-29 20:00:47 +0100

    set the number of epochs via arparse as well add possibility to define the training model from external python module

--- Utils/py/BallDetection/RegressionNetwork/train.py
+++ Utils/py/BallDetection/RegressionNetwork/train.py
@@ -4,11 +4,11 @@ import argparse
 import pickle
 from datetime import datetime
 from pathlib import Path
+from sys import exit
 
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras as keras
-
 import model_zoo
 
 
@@ -45,23 +45,26 @@ def str2bool(v):
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
-def main(raw_args=None):
+def main(raw_args=None, model=None):
     parser = argparse.ArgumentParser(description='Train the network given ')
 
     parser.add_argument('-b', '--database-path', dest='imgdb_path', default="img.db",
-                        help='Path to the image database to use for training. '
-                             'Default is img.db in current folder.')
+                        help='Path to the image database to use for training. Default is img.db in current folder.')
     parser.add_argument('-m', '--model-path', dest='model_path', default="model.h5",
                         help='Store the trained model using this path. Default is model.h5.')
+    parser.add_argument("--log", dest="log", default="./logs/", help="Tensorboard log location.")
+
     parser.add_argument("--proceed", type=str2bool, nargs='?', dest="proceed",
-                        const=True,
-                        help="Use the stored and pre-trained model base.")
-    parser.add_argument("--log", dest="log", default="./logs/",
-                        help="Tensorboard log location.")
+                        const=True, help="Use the stored and pre-trained model base.")
     parser.add_argument("--batch-size", dest="batch_size", default=256,
                         help="Batch size. Default is 256")
+    parser.add_argument("--epochs", dest="epochs", default=200,
+                        help="Number of epochs")
 
     args = parser.parse_args(raw_args)
+
+    if model is not None:
+        model.summary()
 
     if not Path(args.log).exists():
         Path.mkdir(Path(args.log))
@@ -72,15 +75,15 @@ def main(raw_args=None):
         y = pickle.load(f)
 
     # define the Keras network
-    if args.proceed is None or args.proceed is False:
-        print("Creating new model")
-
-        # FIXME make it possible to select a diffenrent model when calling train
-        model = model_zoo.fy_1500()
-    else:
+    if args.proceed is not None and args.proceed is True:
         print("Loading model " + args.model_path)
         model = tf.keras.models.load_model(args.model_path)
 
+    elif model is not None:
+        print("Creating new model")
+    else:
+        print("ERROR: No model specified")
+        exit(1)
     # Define precision and recall for 0.5, 0.8 and 0.9 threshold
     # class_id=3 means use the third element of the output vector
     precision_class_05 = tf.keras.metrics.Precision(name="precision_classifcation_0.5", thresholds=0.5, class_id=3)
@@ -106,10 +109,11 @@ def main(raw_args=None):
     log_callback = keras.callbacks.TensorBoard(log_dir=log_path, profile_batch=0)
     callbacks.append(log_callback)
 
-    history = model.fit(x, y, batch_size=args.batch_size, epochs=200, verbose=1, validation_split=0.1,
+    history = model.fit(x, y, batch_size=args.batch_size, epochs=args.epochs, verbose=1, validation_split=0.1,
                         callbacks=callbacks)
     model.save(args.model_path)
 
 
 if __name__ == '__main__':
-    main()
+    test_model = model_zoo.fy_1500()
+    main(model=test_model)
