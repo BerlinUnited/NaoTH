a1c884211d173909a58753a2808c383a7e57da62
Benjamin Schlotter <benji@schlotterfam.de>
2020-01-22 19:10:29 +0100

    implemented argparse for input and automatic download for test logfile

--- Utils/py/log_parser_examples/image-log_example.py
+++ Utils/py/log_parser_examples/image-log_example.py
@@ -1,19 +1,31 @@
-#!/usr/bin/python
-
-# this script exports images from the plain image log to png files
-
+"""
+    this script exports images from the plain image log to png files
+"""
+# for fast processing
+import multiprocessing as mp
 import os
 import sys
-import getopt
-import math
+from argparse import ArgumentParser
+from pathlib import Path
 
 import numpy
 from PIL import Image
-from PIL import PngImagePlugin
-import matplotlib.pyplot as plt
+from pywget import wget
 
-# for fast processing
-import multiprocessing as mp
+
+def get_demo_logfiles():
+    base_url = "https://www2.informatik.hu-berlin.de/~naoth/ressources/log/demo_image/"
+
+    logfile_list = ["images.log"]
+    # taken from /vol/repl261-vol4/naoth/logs/2019-11-21_Koeln/
+    # 2019-11-21_16-20-00_Berlin United_vs_Nao_Devils_Dortmund_half1/game_logs/1_96_Nao0377_191122-0148
+
+    target_dir = Path("logs")
+    Path.mkdir(target_dir, exist_ok=True)
+
+    for logfile in logfile_list:
+        if not Path(target_dir / logfile).is_file():
+            wget.download(base_url + logfile, target_dir)
 
 
 def image_from_data(data, width, height):
@@ -48,16 +60,20 @@ def save_data_to_png(data, width, height, img_path):
 if __name__ == "__main__":
     # TODO add argparse here
     # TODO figure out how to read the other values
-    if len(sys.argv) > 1:
-        path = sys.argv[1]
-    else:
-        sys.exit("[ERROR] need a path to a log file as argument.")
+    # TODO save metadata to images
+    get_demo_logfiles()
 
-    if not os.path.exists(path):
-        sys.exit("[ERROR] path doesn't exist: {0}".format(path))
+    parser = ArgumentParser(
+        description='script to display or export images from images.log files')
+    parser.add_argument("-i", "--input", help='logfile, containing the images', default="logs/images.log")
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.input):
+        sys.exit("[ERROR] path doesn't exist: {0}".format(args.input))
 
     # create out directory
-    base_file, file_extension = os.path.splitext(path)
+    base_file, file_extension = os.path.splitext(args.input)
     out_dir = base_file
 
     if os.path.isdir(out_dir):
@@ -73,7 +89,7 @@ if __name__ == "__main__":
 
     # read all data
     image_data = []
-    with open(path, 'rb') as f:
+    with open(args.input, 'rb') as f:
         j = 0
         while True:
             frame = f.read(4)
@@ -88,7 +104,6 @@ if __name__ == "__main__":
                 print("save " + img_path)
                 save_data_to_png(data, width, height, img_path)
             else:
-                # TODO fix image data
                 image_data += [(data, width, height, img_path)]
             j += 1
 
