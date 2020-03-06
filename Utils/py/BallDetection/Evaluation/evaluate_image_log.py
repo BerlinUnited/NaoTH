d0afbffa1746947a71754eeea245fd034ad0236d
Thomas Krause <krauseto@hu-berlin.de>
2020-03-06 19:23:40 +0100

    Load image files and move them into representation

--- Utils/py/BallDetection/Evaluation/evaluate_image_log.py
+++ Utils/py/BallDetection/Evaluation/evaluate_image_log.py
@@ -1,22 +1,55 @@
 import os
 import argparse
 import cppyy
+import PIL.Image
+from typing import NamedTuple
+import numpy as np
+import ctypes
 
 def get_naoth_dir():
     script_path = os.path.abspath(__file__)
     return os.path.abspath(os.path.join(script_path, "../../../../../"))
 
+
 def get_toolchain_dir():
     return os.path.abspath(os.environ["NAOTH_TOOLCHAIN_PATH"])
 
-def get_files_of_dir(d):
-    imageFiles = [os.path.join(d,f) for f in os.listdir(d) if os.path.isfile(os.path.join(d,f)) and f.endswith(".png")]
+class Frame(NamedTuple):
+    file: str
 
-    # TODO: add camera matrix to result
-    # TODO: parse XML and add ground truth to result
 
-    return imageFiles
+def get_frames_for_dir(d):
+    file_names = os.listdir(d)
+    file_names.sort()
+    imageFiles = [os.path.join(d, f) for f in file_names if os.path.isfile(
+        os.path.join(d, f)) and f.endswith(".png")]
 
+    result = list()
+
+    for file in imageFiles:
+        frame = Frame(file)        
+        # TODO: parse camera matrix using PIL to result
+        # TODO: parse XML and add ground truth to result
+        result.append(frame)
+
+    return result
+
+def load_image(f: Frame):
+    # don't import cv globally, because the dummy simulator shared library might need to load a non-system library
+    # and we need to make sure loading the dummy simulator shared library happens first
+    import cv2 as cv
+
+    cv_img = cv.imread(f)
+
+    # convert image for bottom to yuv422
+    cv_img = cv.cvtColor(cv_img, cv.COLOR_BGR2YUV).tobytes()
+    yuv422 = np.ndarray(480*640*2, np.uint8)
+    for i in range(0, 480 * 640, 2):
+            yuv422[i*2] = cv_img[i*3]
+            yuv422[i*2 + 1] = (cv_img[i*3 + 1] + cv_img[i*3 + 4]) / 2.0
+            yuv422[i*2 + 2] = cv_img[i*3 + 3]
+            yuv422[i*2 + 3] = (cv_img[i*3 + 2] + cv_img[i*3 + 5]) / 2.0
+    return yuv422
 
 class Evaluator:
 
@@ -29,10 +62,12 @@ class Evaluator:
             naoth_dir, "NaoTHSoccer/dist/Native/libdummysimulator.so"))
 
         # add relevant include paths to allow mapping our code
-        cppyy.add_include_path(os.path.join(naoth_dir, "Framework/Commons/Source"))
+        cppyy.add_include_path(os.path.join(
+            naoth_dir, "Framework/Commons/Source"))
         cppyy.add_include_path(os.path.join(naoth_dir, "NaoTHSoccer/Source"))
-        cppyy.add_include_path(os.path.join(toolchain_dir, "toolchain_native/extern/include"))
-        
+        cppyy.add_include_path(os.path.join(
+            toolchain_dir, "toolchain_native/extern/include"))
+
         # include platform
         cppyy.include(os.path.join(
             naoth_dir, "Framework/Commons/Source/PlatformInterface/Platform.h"))
@@ -60,38 +95,45 @@ class Evaluator:
         self.sim.registerMotion(callable_mo)
 
         # get access to the module manager and return it to the calling function
-        cppyy.include(os.path.join(naoth_dir, "Framework/Commons/Source/ModuleFramework/ModuleManager.h"))
-        cppyy.include(os.path.join(naoth_dir, "NaoTHSoccer/Source/Cognition/Cognition.h"))
-        
+        cppyy.include(os.path.join(
+            naoth_dir, "Framework/Commons/Source/ModuleFramework/ModuleManager.h"))
+        cppyy.include(os.path.join(
+            naoth_dir, "NaoTHSoccer/Source/Cognition/Cognition.h"))
+
         self.moduleManager = cppyy.gbl.getModuleManager(cog)
 
         # get the ball detector module
-        self.ball_detector = self.moduleManager.getModule("CNNBallDetector").getModule()
+        self.ball_detector = self.moduleManager.getModule(
+            "CNNBallDetector").getModule()
+
+    def set_current_frame(self, frame, bottom=True):
+        # get reference to the image input representation
+        if bottom:
+            imageRepresentation = self.ball_detector.getRequire().at("Image")
+        else:
+            imageRepresentation = self.ball_detector.getRequire().at("ImageTop")
+
+        print(frame.file)
+
+        # load image in YUV422 format
+        yuv422 = load_image(frame.file)
+        p_data = yuv422.ctypes.data_as(ctypes.POINTER(ctypes.c_uint8))
+        # move image into representation
+        imageRepresentation.copyImageDataYUV422(p_data, yuv422.size)
+
 
     def execute(self, directories):
         for d in directories:
-            imageFiles = get_files_of_dir(d)
-            for f in imageFiles:
-                self.set_images_from_file(f)
-                self.sim.executeFrame()        
-            
-
-    def set_images_from_file(self, file, bottom=True):
-        # get reference to the image input representation
-        if bottom:
-            image = self.ball_detector.getRequire().at("Image")
-        else:
-            image = self.ball_detector.getRequire().at("ImageTop")
-
-        print(file)
-        # TODO: read  image from file with opencv
-        # TODO: convert image
-        # TODO: move image into representation
+            frames = get_frames_for_dir(d)
+            for f in frames:
+                self.set_current_frame(f)
+                self.sim.executeFrame()
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Evaluate ball detection on logfile images annotated with CVAT.')
+    parser = argparse.ArgumentParser(
+        description='Evaluate ball detection on logfile images annotated with CVAT.')
     parser.add_argument('directory', nargs='+',
                         help='A list of directories containing the images files (as png with the integrated camera matrix)')
 
