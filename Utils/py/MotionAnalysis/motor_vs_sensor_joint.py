0b3e967a92b4f5251a684ef406d86ba4db6feb42
Benjamin Schlotter <benji@schlotterfam.de>
2020-04-21 09:19:26 +0200

    made minimal example of motor vs sensor joint data work

--- Utils/py/MotionAnalysis/motor_vs_sensor_joint.py
+++ Utils/py/MotionAnalysis/motor_vs_sensor_joint.py
@@ -1,9 +1,7 @@
-import math
 from argparse import ArgumentParser
 
-import numpy as np
-from matplotlib import pyplot
-from naoth.log import BehaviorParser
+from matplotlib import pyplot as plt
+from naoth.log import Parser
 from naoth.log import Reader as LogReader
 
 JointID = {
@@ -58,157 +56,45 @@ JointNames = {
 
 
 def frame_filter(frame):
-    try:
-        if "BehaviorStateComplete" in frame.messages:
-            (m, o, s) = frame["BehaviorStateComplete"]
-        else:
-            (m, o, s) = frame["BehaviorStateSparse"]
-
-        return [frame["FrameInfo"].time / 1000.0,
-                frame["MotionRequest"],
-                frame["MotorJointData"],
-                frame["SensorJointData"]
-                ]
-
-    except KeyError:
-        raise StopIteration
-
-
-def run(log):
-    print(log.names)
-
-    # we want only the frames which contain BehaviorState*
-    # vlog = filter(lambda f: "BehaviorStateComplete" in f.messages or "BehaviorStateSparse" in f.messages, log)
-
-    # apply the filter
-    b = map(frame_filter, log)
-
-    for x in range(10, 21, 2):
-        pyplot.close('all')
-
-        a = [[
-            m[0],  # time
-            m[2].position[x],  # motor  position R
-            m[3].jointData.position[x],  # sensor position R
-            m[2].position[x + 1],  # motor  position L
-            m[3].jointData.position[x + 1],  # sensor position L
-            m[3].temperature[x],  # temp R
-            m[3].temperature[x + 1],  # temp L
-            m[3].electricCurrent[x],  # current R
-            m[3].electricCurrent[x + 1]  # current L
-        ] for m in b]
-
-        # make an numpy array
-        data = np.array(a)
-
-        size = data.shape
-
-        data2 = np.array(zip(*data))
-
-        f, axarr = pyplot.subplots(3, sharex=True)
-
-        d1 = np.append([0, 0, 0, 0], data2[1])
-        d2 = np.append(data2[2], [0, 0, 0, 0])
-        diff = abs(d2 - d1)
-        print(JointNames[x] + ": {}".format(np.amax(diff[4:-4]) * 180 / math.pi))
-
-        # pyplot.plot(d1)
-        # pyplot.plot(d2)
-        axarr[0].set_title('cum_error')
-        axarr[0].plot(np.cumsum(diff), label=JointNames[x])
-
-        d1 = np.append([0, 0, 0, 0], data2[3])
-        d2 = np.append(data2[4], [0, 0, 0, 0])
-        diff = abs(d2 - d1)
-        print(JointNames[x + 1] + ": {}".format(np.amax(diff[4:-4]) * 180 / math.pi))
-
-        # pyplot.plot(d1)
-        # pyplot.plot(d2)
-        axarr[0].plot(np.cumsum(diff), label=JointNames[x + 1])
-        axarr[0].legend(loc='upper left')
-
-        # d1 = np.append(data2[5], [0,0,0,0])
-        # d2 = np.append(data2[6], [0,0,0,0])
-        d1 = data2[5]
-        d2 = data2[6]
-        axarr[2].set_title('temperature')
-        axarr[2].plot(d1, label=JointNames[x])
-        axarr[2].plot(d2, label=JointNames[x + 1])
-        axarr[2].legend(loc='upper left')
-
-        axarr[1].set_title('cum_current')
-        axarr[1].plot(np.cumsum(data2[7]), label=JointNames[x])
-        axarr[1].plot(np.cumsum(data2[8]), label=JointNames[x + 1])
-        axarr[1].legend(loc='upper left')
-
-        '''
-        tr_walk  = zip(*walk)
-        
-        time_stand = sum(tr_stand[0])
-        time_walk  = sum(tr_walk[0])
-        
-        print "------------------"
-        print "time standing: {}".format(time_stand)
-        print "time walking : {}".format(time_walk)
-        print "time total   : {}".format(time_stand + time_walk)
-    
-        # LLeg
-        d_stand_LLeg_stats = do_statistics(tr_stand[0], tr_stand[1])
-        print_statistics("left leg - stand", d_stand_LLeg_stats)
-    
-        d_walk_LLeg_stats = do_statistics(tr_walk[0], tr_walk[1])
-        print_statistics("left leg - walk", d_walk_LLeg_stats)
-    
-        # RLeg
-        d_stand_RLeg_stats = do_statistics(tr_stand[0], tr_stand[2])
-        print_statistics("right leg - stand", d_stand_RLeg_stats)
-    
-        d_walk_RLeg_stats = do_statistics(tr_walk[0], tr_walk[2])
-        print_statistics("right leg - walk", d_walk_RLeg_stats)
-    
-        bb = zip(*data)
-        pyplot.plot(bb[0], bb[2], label="temperatur right")
-        pyplot.plot(bb[0], bb[1], label="temperatur left")
-        pyplot.plot(bb[0], bb[10], label="game")
-        pyplot.plot(bb[0], bb[5], label="motion")
-        
-        pyplot.legend(loc='upper left')
-        pyplot.grid()
-        '''
-        pyplot.show()
+    return [frame["FrameInfo"].time / 1000.0,
+            frame["MotionRequest"],
+            frame["MotorJointData"],
+            frame["SensorJointData"]
+            ]
 
 
 def analyze_log(logfile):
-    behavior_parser = BehaviorParser()
-    behavior_parser.parser.register("MotorJointData", "JointData")
+    behavior_parser = Parser()
+    behavior_parser.register("MotorJointData", "JointData")
 
     log = LogReader(logfile, behavior_parser)
 
     frame_list = list()
     for frame in log:
-        print(frame.messages)
-        quit()
         a = frame_filter(frame)
         frame_list.append(a)
-    pass
+
+    sensor_joint_pos_r_knee_pitch = list()
+    motor_joint_pos_r_knee_pitch = list()
+
+    for frame in frame_list:
+        motor_joint_pos_r_knee_pitch.append(frame[2].position[16])
+        sensor_joint_pos_r_knee_pitch.append(frame[3].jointData.position[16])
+
+    fig, axs = plt.subplots(2)
+    fig.suptitle('RKneePitch Position over time')
+    axs[0].plot(sensor_joint_pos_r_knee_pitch)
+    axs[0].set_title("Sensorjoint Data")
+    axs[1].plot(motor_joint_pos_r_knee_pitch)
+    axs[1].set_title("Motorjoint Data")
+    plt.show()
 
 
 if __name__ == '__main__':
     parser = ArgumentParser(
-        description='script to generate some energy statistics from logfile')
-    parser.add_argument("-i", "--input", help='logfile, containing the images', default="logs/game_eu.log")
+        description='script for plotting the sensor and motor positions of a joint')
+    parser.add_argument("-i", "--input", help='logfile, containing the images', default="logs/walk_on_floor_motion.log")
 
     args = parser.parse_args()
 
     analyze_log(args.input)
-
-    quit()
-    # enforce the whole log being parsed (this is necessary for older game logs)
-    for frame in log:
-        if "BehaviorStateComplete" in frame.messages:
-            m = frame["BehaviorStateComplete"]
-        if "BehaviorStateSparse" in frame.messages:
-            m = frame["BehaviorStateSparse"]
-        frame["FrameInfo"]
-
-    run(log)
