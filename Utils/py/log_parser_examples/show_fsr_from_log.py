3289dd6820801bbdb5a21763f1ccd5a4a434768c
Robert Martin <martinro@informatik.hu-berlin.de>
2020-05-09 22:44:11 +0200

    ported show_fsr_from_log.py to experimental parser

--- Utils/py/log_parser_examples/show_fsr_from_log.py
+++ Utils/py/log_parser_examples/show_fsr_from_log.py
@@ -33,36 +33,37 @@ if __name__ == "__main__":
     myParser.register("FSRData", "FSRData")
 
     # get all of the fsr data from the logfile
-    data = map(get_fsr_data, LogReader(log[0], myParser))
+    with LogReader(log[0], parser=myParser) as reader:
+        data = map(get_fsr_data, reader.read())
 
-    frame_numbers = []
-    left_data = defaultdict(list)
-    right_data = defaultdict(list)
+        frame_numbers = []
+        left_data = defaultdict(list)
+        right_data = defaultdict(list)
 
-    for frame_num, fsr_left, fsr_right in data:
-        frame_numbers.append(frame_num)
-        left_data["FrontLeft"].append(fsr_left[0])
-        left_data["FrontRight"].append(fsr_left[1])
-        left_data["RearLeft"].append(fsr_left[2])
-        left_data["RearRight"].append(fsr_left[3])
+        for frame_num, fsr_left, fsr_right in data:
+            frame_numbers.append(frame_num)
+            left_data["FrontLeft"].append(fsr_left[0])
+            left_data["FrontRight"].append(fsr_left[1])
+            left_data["RearLeft"].append(fsr_left[2])
+            left_data["RearRight"].append(fsr_left[3])
 
-        right_data["FrontLeft"].append(fsr_right[0])
-        right_data["FrontRight"].append(fsr_right[1])
-        right_data["RearLeft"].append(fsr_right[2])
-        right_data["RearRight"].append(fsr_right[3])
+            right_data["FrontLeft"].append(fsr_right[0])
+            right_data["FrontRight"].append(fsr_right[1])
+            right_data["RearLeft"].append(fsr_right[2])
+            right_data["RearRight"].append(fsr_right[3])
 
-    figure_left, axes_left = plt.subplots(4)
-    figure_left.suptitle('FSR Left Foot')
-    axes_left[0].plot(frame_numbers, left_data["FrontLeft"])
-    axes_left[1].plot(frame_numbers, left_data["FrontRight"])
-    axes_left[2].plot(frame_numbers, left_data["RearLeft"])
-    axes_left[3].plot(frame_numbers, left_data["RearRight"])
+        figure_left, axes_left = plt.subplots(4, constrained_layout=True)
+        figure_left.suptitle('FSR Left Foot')
+        axes_left[0].plot(frame_numbers, left_data["FrontLeft"])
+        axes_left[1].plot(frame_numbers, left_data["FrontRight"])
+        axes_left[2].plot(frame_numbers, left_data["RearLeft"])
+        axes_left[3].plot(frame_numbers, left_data["RearRight"])
 
-    figure_right, axes_right = plt.subplots(4)
-    figure_right.suptitle('FSR Right Foot')
-    axes_right[0].plot(frame_numbers, right_data["FrontLeft"])
-    axes_right[1].plot(frame_numbers, right_data["FrontRight"])
-    axes_right[2].plot(frame_numbers, right_data["RearLeft"])
-    axes_right[3].plot(frame_numbers, right_data["RearRight"])
+        figure_right, axes_right = plt.subplots(4, constrained_layout=True)
+        figure_right.suptitle('FSR Right Foot')
+        axes_right[0].plot(frame_numbers, right_data["FrontLeft"])
+        axes_right[1].plot(frame_numbers, right_data["FrontRight"])
+        axes_right[2].plot(frame_numbers, right_data["RearLeft"])
+        axes_right[3].plot(frame_numbers, right_data["RearRight"])
 
-    plt.show()
+        plt.show()
