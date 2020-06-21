1bf1106085285d05c9a9791a2f71423e32114778
Robert Martin <martinro@informatik.hu-berlin.de>
2020-06-21 16:37:45 +0200

    added run_tests.py script; log choosing; properly handle incorrect test runs

--- Utils/py/naoth/tests/test_log.py
+++ Utils/py/naoth/tests/test_log.py
@@ -1,38 +1,29 @@
+import os
 import unittest
 
-from naoth.datasets import edgels
-from naoth.log import Reader
+from naoth.log import Reader, IncompleteFrameException
 
 
-# TODO: Select a smaller log file
-# Note: Test requires that the log file is complete and frames are not broken.
-TEST_LOG = edgels.load_data()
-TEST_LOG_FRAME_COUNT = 21623
-
-
-def _frame_count_more_than(log_path, number):
+def _skip_unless_env(*variables):
     """
-    Checks of a given log has more than a given number of frames
-    @param log_path: path to log file to check
-    @param number: number of frames
-    @returns True if condition was met
+    Skip test if any of the given environment variables are not defined
+    @param vars: Any number of environment variables
     """
-    n_frames = 0
-    with Reader(log_path) as reader:
-        for frame in reader.read():
-            n_frames += 1
-            if n_frames > number:
-                return True
-    return False
+    for var in variables:
+        try:
+            value = os.environ[var]
+        except KeyError:
+            return unittest.skip(f'Environment variable {var} is not defined.')
+    return lambda func: func
 
 
 class TestLogReader(unittest.TestCase):
-    @unittest.skipIf(not _frame_count_more_than(TEST_LOG, 2), 'Test log has not enough frames to complete the test.')
+    @_skip_unless_env('TEST_LOG_FILE')
     def test_multiple_iterators(self):
         """
         Test of nested log file reads. Checks if two nested iterators return the same number of frames.
         """
-        with Reader(TEST_LOG) as reader:
+        with Reader(os.environ['TEST_LOG_FILE']) as reader:
             n_frames_a = n_frames_b = 0
 
             for i, a in enumerate(reader.read()):
@@ -42,48 +33,62 @@ class TestLogReader(unittest.TestCase):
                     for b in reader.read():
                         n_frames_b += 1
 
-            # read a and read b should generate the same number of frames
-            self.assertEqual(TEST_LOG_FRAME_COUNT, len(reader.frames))
-            self.assertEqual(TEST_LOG_FRAME_COUNT, n_frames_a)
-            self.assertEqual(TEST_LOG_FRAME_COUNT, n_frames_b)
+            if n_frames_a <= 2:
+                raise unittest.SkipTest('Test log has not enough frames to complete the test.')
 
+            # read a and read b should generate the same number of frames
+            self.assertEqual(len(reader.frames), n_frames_a)
+            self.assertEqual(len(reader.frames), n_frames_b)
+
+    @_skip_unless_env('TEST_LOG_FILE')
     def test_read_size(self):
         """
         Test if the whole log file was read correctly.
         Note: Requires a complete test log file without broken frames.
         """
         parsed_size = 0
-        with Reader(TEST_LOG) as reader:
-            for frame in reader.read():
-                self.assertIsNotNone(frame)
+        with Reader(os.environ['TEST_LOG_FILE'], skip_incomplete=False) as reader:
+            try:
+                for frame in reader.read():
+                    self.assertIsNotNone(frame)
 
-                parsed_size += len(bytes(frame))
+                    parsed_size += len(bytes(frame))
+            except IncompleteFrameException as exc:
+                raise unittest.SkipTest(f'Could not complete Test: {exc}')
 
             self.assertEqual(reader.log_file_size, parsed_size)
 
+    @_skip_unless_env('TEST_LOG_FILE')
     def test_diet_read_size(self):
         """
         Test if the whole log file was read correctly.
         Note: Requires a complete test log file without broken frames.
         """
         parsed_size = 0
-        with Reader(TEST_LOG) as reader:
-            for frame in reader.diet_read():
-                self.assertIsNotNone(frame)
-                parsed_size += len(bytes(frame))
+        with Reader(os.environ['TEST_LOG_FILE'], skip_incomplete=False) as reader:
+            try:
+                for frame in reader.diet_read():
+                    self.assertIsNotNone(frame)
+                    parsed_size += len(bytes(frame))
+            except IncompleteFrameException as exc:
+                raise unittest.SkipTest(f'Could not complete Test: {exc}')
 
             self.assertEqual(reader.log_file_size, parsed_size)
 
+    @_skip_unless_env('TEST_LOG_FILE')
     def test_get_item(self):
         """
         Test if the __getitem__ function returns all frames correctly.
         Note: Requires a complete test log file without broken frames.
         """
         parsed_size = 0
-        with Reader(TEST_LOG) as reader:
-            # iterating directly over the reader calls the __getitem__ function
-            for frame in reader:
-                self.assertIsNotNone(frame)
-                parsed_size += len(bytes(frame))
+        with Reader(os.environ['TEST_LOG_FILE'], skip_incomplete=False) as reader:
+            try:
+                # iterating directly over the reader calls the __getitem__ function
+                for frame in reader:
+                    self.assertIsNotNone(frame)
+                    parsed_size += len(bytes(frame))
+            except IncompleteFrameException as exc:
+                raise unittest.SkipTest(f'Could not complete Test: {exc}')
 
             self.assertEqual(reader.log_file_size, parsed_size)
