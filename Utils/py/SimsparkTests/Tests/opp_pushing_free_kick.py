097d8045101dbb88dfce3c727d5f62c2bfb2f959
Philipp Strobel <philippstrobel@posteo.de>
2020-07-07 17:50:19 +0200

    fixed the opponent pushing free kick test, BUT it doesn't work as expected with the role decisions module :/

--- Utils/py/SimsparkTests/Tests/opp_pushing_free_kick.py
+++ Utils/py/SimsparkTests/Tests/opp_pushing_free_kick.py
@@ -1,6 +1,6 @@
 import math
 
-from naoth.log import BehaviorParser
+from naoth.log._parser import BehaviorParser
 
 from SimsparkController import SimsparkController
 from Utils import *
@@ -19,26 +19,18 @@ def position_and_execute(s, a, parser, pos, ball):
     a.debugrequest('gamecontroller:game_state:play', True)
 
     start = time.monotonic()
-    pending_cmds = {}
     # max. 90sec for this test (NOTE: walking of the robot in the simulation is not smooth, therefore we wait longer)
     while time.monotonic() <= (start + 90):
-        pending_cmds[a.behavior()] = 'Behavior'
-        time.sleep(0.3)
-
-        for p in pending_cmds:
-            data = a.command_result(p)
-            if data:
-                if pending_cmds[p] == 'Behavior':
-                    parser.parse(data)
-                    if parser.isActiveOption('free_kick_opp'):
-                        #print(parser.getActiveOptionState('free_kick_opp'))
-                        if parser.getActiveOptionState('free_kick_opp') == 'done' and time.monotonic() > (start + 10):
-                            #print(parser.symbols.decimal('ball.distance'))
-                            start = 0
-                            break
+        parser.parse(a.behavior().result())
+        if parser.isActiveOption('free_kick_opp'):
+            #print(parser.getActiveOptionState('free_kick_opp'))
+            if parser.getActiveOptionState('free_kick_opp') == 'done' and time.monotonic() > (start + 10):
+                #print(parser.symbols.decimal('ball.distance'))
+                start = 0
+                break
 
     # its a simulation of a free kick - the ball should be touched only once
-    wait_for_cmd(a, a.debugrequest('gamecontroller:game_state:play', False))
+    a.debugrequest('gamecontroller:game_state:play', False).result()
     # wait until the robot sits (hip is below 0.2)!
     wait_for(lambda: s.get_robot(a.number)['z'] <= 0.2, 0.3, max_time=5.0)
 
@@ -75,12 +67,13 @@ def opp_pushing_free_kick(args):
 
     a = AgentController(args.agent, args.config, number=3, start_instance=not args.no_agent)
     a.start()
-    a.started.wait() # wait for the agent to be fully started
+    logging.info('Wait for agent connection')
+    a.wait_connected()  # wait for the agent to be fully started
 
     # create parser for the agents behavior
     parser = BehaviorParser()
     # in order to use the parser, we have to retrieve the complete behavior first
-    parser.init(wait_for_cmd(a, a.behavior(True)))
+    parser.init(a.behavior(True).result())
 
     # it takes sometimes a while until simspark got the correct player number
     wait_for(lambda: s.get_robot(a.number) is not None, 0.3)
@@ -90,7 +83,7 @@ def opp_pushing_free_kick(args):
     logging.info('Some Simspark commands')
 
     # eg. for en-/disabling a module
-    a.agent('path_spl_soccer')
+    a.agent('soccer_agent')
 
     # prepare the test
     s.cmd_dropball()  # put the ball into the game
@@ -109,10 +102,9 @@ def opp_pushing_free_kick(args):
                 return_code += 1
             time.sleep(0.5)
 
-    a.cancel()
+    a.stop()
     s.cancel()
 
-    a.join()
     s.join()
 
     return return_code == 0
