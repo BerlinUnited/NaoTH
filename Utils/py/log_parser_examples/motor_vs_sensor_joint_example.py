0cccb3af3886330bb45cbbf20d142e536dfd5ee6
Benjamin Schlotter <benji@schlotterfam.de>
2020-07-11 21:21:49 +0200

    moved joint definitions in extra python file to make usage in scripts more clear

--- Utils/py/log_parser_examples/motor_vs_sensor_joint_example.py
+++ Utils/py/log_parser_examples/motor_vs_sensor_joint_example.py
@@ -12,56 +12,6 @@ from naoth.log import Parser
 from naoth.log import Reader as LogReader
 from naoth.datasets import motion
 
-JointID = {
-    "HeadPitch": 0,
-    "HeadYaw": 1,
-    "RShoulderRoll": 2,
-    "LShoulderRoll": 3,
-    "RShoulderPitch": 4,
-    "LShoulderPitch": 5,
-    "RElbowRoll": 6,
-    "LElbowRoll": 7,
-    "RElbowYaw": 8,
-    "LElbowYaw": 9,
-    "RHipYawPitch": 10,
-    "LHipYawPitch": 11,
-    "RHipPitch": 12,
-    "LHipPitch": 13,
-    "RHipRoll": 14,
-    "LHipRoll": 15,
-    "RKneePitch": 16,
-    "LKneePitch": 17,
-    "RAnklePitch": 18,
-    "LAnklePitch": 19,
-    "RAnkleRoll": 20,
-    "LAnkleRoll": 21
-}
-
-JointNames = {
-    0: "HeadPitch",
-    1: "HeadYaw",
-    2: "RShoulderRoll",
-    3: "LShoulderRoll",
-    4: "RShoulderPitch",
-    5: "LShoulderPitch",
-    6: "RElbowRoll",
-    7: "LElbowRoll",
-    8: "RElbowYaw",
-    9: "LElbowYaw",
-    10: "RHipYawPitch",
-    11: "LHipYawPitch",
-    12: "RHipPitch",
-    13: "LHipPitch",
-    14: "RHipRoll",
-    15: "LHipRoll",
-    16: "RKneePitch",
-    17: "LKneePitch",
-    18: "RAnklePitch",
-    19: "LAnklePitch",
-    20: "RAnkleRoll",
-    21: "LAnkleRoll",
-}
-
 
 def frame_filter(frame):
     return [frame["FrameInfo"].time / 1000.0,
