7f97dcb2c5170322d90e5674f922bbca36b64c9e
Philipp Strobel <philippstrobel@posteo.de>
2020-08-21 17:14:27 +0200

    added convenience class to show alert dialogs with one line of code

---
+++ RobotControl/src/de/naoth/rc/componentsFx/AlertDialog.java
@@ -0,0 +1,78 @@
+package de.naoth.rc.componentsFx;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+
+/**
+ * A convenience class to get an alert dialog with one line of code.
+ * 
+ * @author Philipp Strobel <philippstrobel@posteo.de>
+ */
+public class AlertDialog
+{
+    private static final Alert alert = new Alert(AlertType.NONE);
+   
+    /**
+     * Returns the pressed Button of the alert.
+     * That can be used to in a condition, eg.:
+     * showAlert(AlertType.CONFIRMATION, "OK?", "Is it ok?") == ButtonType.OK
+     * 
+     * @param type  the alert type
+     * @param title the title of the alert
+     * @param text  the text of the alert
+     * @return      the button type (depends on the alert type)
+     */
+    public static ButtonType showAlert(AlertType type, String title, String text) {
+        alert.setAlertType(type);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(text);
+
+        return alert.showAndWait().get();
+    }
+    
+    /**
+     * Shorthand method to show an error alert.
+     * 
+     * @param title title of the alert
+     * @param text  the text of the alert
+     * @return      the pressed button type
+     */
+    public static ButtonType showError(String title, String text) {
+        return showAlert(AlertType.ERROR, title, text);
+    }
+    
+    /**
+     * Shorthand method to show a warning alert.
+     * 
+     * @param title title of the alert
+     * @param text  the text of the alert
+     * @return      the pressed button type
+     */
+    public static ButtonType showWarning(String title, String text) {
+        return showAlert(AlertType.WARNING, title, text);
+    }
+    
+    /**
+     * Shorthand method to show an info alert.
+     * 
+     * @param title title of the alert
+     * @param text  the text of the alert
+     * @return      the pressed button type
+     */
+    public static ButtonType showInfo(String title, String text) {
+        return showAlert(AlertType.INFORMATION, title, text);
+    }
+    
+    /**
+     * Shorthand method to show a confirmation alert.
+     * 
+     * @param title title of the alert
+     * @param text  the text of the alert
+     * @return      the pressed button type
+     */
+    public static ButtonType showConfirmation(String title, String text) {
+        return showAlert(AlertType.CONFIRMATION, title, text);
+    }
+}
