7a3b603c5a03aeddcbe5b14140e3cdc73c9a6cf4
Philipp Strobel <philippstrobel@posteo.de>
2020-08-22 18:30:16 +0200

    cont. implementing the module configuration dialog

--- RobotControl/src/de/naoth/rc/dialogsFx/moduleconfiguration/ModuleConfigurationController.java
+++ RobotControl/src/de/naoth/rc/dialogsFx/moduleconfiguration/ModuleConfigurationController.java
@@ -5,10 +5,22 @@ import de.naoth.rc.RobotControl;
 import de.naoth.rc.core.messages.Messages;
 import de.naoth.rc.core.server.Command;
 import de.naoth.rc.core.server.ResponseListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.MapProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleMapProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TreeView;
 
 /**
@@ -25,7 +37,15 @@ public class ModuleConfigurationController implements ResponseListener
     private final Command cmd_modules_motion_store = new Command("Motion:modules:store");
     
     /** The ui tree of the available debug requests */
-    @FXML protected TreeView<String> moduleTree;
+    @FXML private Button btnSave;
+    @FXML private Button btnExport;
+    @FXML private TreeView<String> moduleTree;
+    @FXML private ListView<String> infoList;
+    @FXML private ComboBox<Module> module;
+    @FXML private ComboBox<Representation> representation;
+    
+    private final ListProperty<Module> moduleList = new SimpleListProperty<>(FXCollections.observableArrayList());
+    private final ListProperty<Representation> representationList = new SimpleListProperty<>(FXCollections.observableArrayList());
     
     /**
      * Default constructor for the FXML loader.
@@ -52,6 +72,14 @@ public class ModuleConfigurationController implements ResponseListener
         moduleTree.setRoot(new TreeNodeItem<>());
         moduleTree.getRoot().setExpanded(true);
         */
+        
+        btnSave.disableProperty().bind(moduleList.emptyProperty());
+        btnExport.disableProperty().bind(moduleList.emptyProperty());
+        
+        module.setItems(moduleList);
+        module.disableProperty().bind(moduleList.emptyProperty());
+        representation.setItems(representationList);
+        representation.disableProperty().bind(representationList.emptyProperty());
     }
     
     /**
@@ -61,13 +89,16 @@ public class ModuleConfigurationController implements ResponseListener
     public void setRobotControl(RobotControl control) {
         this.control = control;
     }
-    
+
     /**
      * Handles the update button click.
      */
     @FXML
     private void updateModules() {
         if (control != null && control.checkConnected()) {
+            moduleList.clear();
+            representationList.clear();
+            
             control.getMessageServer().executeCommand(this, cmd_modules_cognition);
             control.getMessageServer().executeCommand(this, cmd_modules_motion);
         }
@@ -85,9 +116,9 @@ public class ModuleConfigurationController implements ResponseListener
     public void handleResponse(byte[] result, Command command) {
         Platform.runLater(() -> {
             if(command.equals(cmd_modules_cognition)) {
-                handleCognitionModulesResponse(result);
+                handleModuleResponse("Cognition", result);
             } else if(command.equals(cmd_modules_motion)) {
-                handleMotionModulesResponse(result);
+                handleModuleResponse("Motion", result);
             } else {
                 Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Unknown command response: {0}", command);
             }
@@ -98,24 +129,110 @@ public class ModuleConfigurationController implements ResponseListener
     public void handleError(int code) {
     }
     
-    private void handleCognitionModulesResponse(byte[] result) {
+    private void handleModuleResponse(String type, byte[] result) {
         try {
+            // temp. reference LUT
+            final Map<String, Module> ml = new HashMap<>();
+            final Map<String, Representation> rl = new HashMap<>();
+            
             Messages.ModuleList list = Messages.ModuleList.parseFrom(result);
+            list.getModulesList().forEach((m) -> {
+                String[] str = m.getName().split("\\|");
+                if (str.length == 2) {
+                    Module mod = new Module(type, str[0], str[1], m.getActive());
+                    // process the provided representations
+                    m.getProvidedRepresentationsList().forEach((r) -> {
+                        // add representation to LUT
+                        if (!rl.containsKey(r)) {
+                            rl.put(r, new Representation(r));
+                        }
+                        // set the providing info to the module and representation
+                        Representation rep = rl.get(r);
+                        mod.addProvide(rep);
+                        rep.addProvider(mod);
+                    });
+                    // process the required representations
+                    m.getUsedRepresentationsList().forEach((r) -> {
+                        // add representation to LUT
+                        if (!rl.containsKey(r)) {
+                            rl.put(r, new Representation(r));
+                        }
+                        // set the requiring info to the module and representation
+                        Representation rep = rl.get(r);
+                        mod.addRequire(rep);
+                        rep.addRequirer(mod);
+                    });
+                    ml.put(mod.name, mod);
+                } else {
+                    System.err.println("Invalid module string: " + m.getName() + " [" + type + "]");
+                }
+            });
+            
+            // finally update the lists
+            moduleList.addAll(ml.values());
+            representationList.addAll(rl.values());
+            
             // TODO:
             //moduleTree.getRoot().getChildren().add(Utils.createModulesCognition(list, cognitionModuleRequest));
         } catch (InvalidProtocolBufferException ex) {
             Logger.getLogger(ModuleConfigurationController.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
     
-    private void handleMotionModulesResponse(byte[] result) {
-        try {
-            Messages.ModuleList list = Messages.ModuleList.parseFrom(result);
-            // TODO:
-            //moduleTree.getRoot().getChildren().add(0, Utils.createModulesMotion(list, motionModuleRequest));
-        } catch (InvalidProtocolBufferException ex) {
-            Logger.getLogger(ModuleConfigurationController.class.getName()).log(Level.SEVERE, null, ex);
+    class Module
+    {
+        String type;
+        String name;
+        String path;
+        
+        boolean active = false;
+        
+        List<Representation> require = new ArrayList<>();
+        List<Representation> provide = new ArrayList<>();
+
+        public Module(String type, String name, String path, boolean active) {
+            this.type = type;
+            this.name = name;
+            this.path = path;
+            this.active = active;
+        }
+
+        @Override
+        public String toString() {
+            return name + " [" + type + "]";
+        }
+        
+        public void addRequire(Representation r) {
+            require.add(r);
+        }
+        
+        public void addProvide(Representation r) {
+            provide.add(r);
+        }
+    }
+    
+    class Representation
+    {
+        String name;
+
+        List<Module> required = new ArrayList<>();
+        List<Module> provided = new ArrayList<>();
+        
+        public Representation(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+        
+        public void addProvider(Module m) {
+            provided.add(m);
+        }
+        
+        public void addRequirer(Module m) {
+            required.add(m);
         }
     }
 }
--- RobotControl/src/de/naoth/rc/dialogsFx/moduleconfiguration/ModuleConfigurationDialog.java
+++ RobotControl/src/de/naoth/rc/dialogsFx/moduleconfiguration/ModuleConfigurationDialog.java
@@ -4,8 +4,6 @@ import de.naoth.rc.RobotControl;
 import de.naoth.rc.core.dialog.AbstractJFXDialog;
 import de.naoth.rc.core.dialog.DialogPlugin;
 import de.naoth.rc.core.dialog.RCDialog;
-import de.naoth.rc.logmanager.LogFileEventManager;
-import de.naoth.rc.manager.GenericManagerFactory;
 import java.net.URL;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
 import net.xeoh.plugins.base.annotations.injections.InjectPlugin;
@@ -21,10 +19,6 @@ public class ModuleConfigurationDialog extends AbstractJFXDialog
     public static class Plugin extends DialogPlugin<ModuleConfigurationDialog> {
         @InjectPlugin
         public static RobotControl parent;
-        @InjectPlugin
-        public static GenericManagerFactory genericManagerFactory;
-        @InjectPlugin
-        public static LogFileEventManager logFileEventManager;
         
         @Override
         public String getDisplayName() { return "ModuleConfiguration (FX)"; }
@@ -45,6 +39,7 @@ public class ModuleConfigurationDialog extends AbstractJFXDialog
     @Override
     public void afterInit() {
         // set the global robot control instance to the view controller
-        ((ModuleConfigurationController)getController()).setRobotControl(Plugin.parent);
+        ModuleConfigurationController controller = ((ModuleConfigurationController)getController());
+        controller.setRobotControl(Plugin.parent);
     }
 }
