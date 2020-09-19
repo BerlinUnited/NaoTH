3d8a6575ebf24a60168d830c71691abe3a6d8d07
Benjamin Schlotter <benji@schlotterfam.de>
2020-09-19 13:40:58 +0200

    add new version of fy1500 model where activations are not extra layers. This is important for the new compiler

--- Utils/py/BallDetection/RegressionNetwork/utility_functions/model_zoo.py
+++ Utils/py/BallDetection/RegressionNetwork/utility_functions/model_zoo.py
@@ -1,3 +1,8 @@
+"""
+    use activation argument in layers instead of defining activation functions as extra layers. The result is the same
+    (https://stackoverflow.com/questions/59476564/relu-as-a-parameter-in-dense-or-any-other-layer-vs-relu-as-a-layer-in-keras)
+    but the code generators support it better
+"""
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Convolution2D, LeakyReLU, MaxPooling2D, Flatten, Dense, ReLU, \
     PReLU, Softmax
@@ -9,7 +14,7 @@ def fy_1500():
     input_shape = (16, 16, 1)
 
     model = Sequential()
-    model._name = "fy_1500"
+    model._name = "fy_1500_old"
 
     model.add(Convolution2D(4, (3, 3), input_shape=input_shape, padding='same', name="Conv2D_1"))
     model.add(LeakyReLU(alpha=0.0, name="activation_1"))  # alpha unknown, so default
@@ -328,5 +333,35 @@ def fy_lessfeat():
     model.add(Flatten(name="flatten_1"))
     # output is radius, x, y, confidence
     model.add(Dense(4, activation="relu", name="dense_1"))
+
+    return model
+
+
+def fy_1500_new():
+    input_shape = (16, 16, 1)
+
+    model = Sequential()
+    model._name = "fy_1500_new"
+
+    model.add(Convolution2D(4, (3, 3), input_shape=input_shape, padding='same', name="Conv2D_1", activation='relu'))
+    # model.add(LeakyReLU(alpha=0.0, name="activation_1"))  # alpha unknown, so default
+
+    model.add(Convolution2D(4, (3, 3), padding='same', name="Conv2D_2", activation='relu'))
+    # model.add(LeakyReLU(name="activation_2"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Convolution2D(8, (3, 3), padding='same', name="Conv2D_3", activation="relu"))
+    # model.add(LeakyReLU(name="activation_3"))
+    model.add(MaxPooling2D(pool_size=(2, 2), name="pooling_1"))
+
+    model.add(Convolution2D(8, (3, 3), padding='same', name="Conv2D_4", activation='relu'))
+    # model.add(LeakyReLU(alpha=0.0, name="activation_4"))
+
+    model.add(Convolution2D(8, (1, 1), padding='same', name="Conv2D_5"))
+
+    # classifier
+    model.add(Flatten(name="flatten_1"))
+    # output is radius, x, y, confidence
+    model.add(Dense(4, activation='relu', name="dense_1"))
 
     return model
