7ffbf45739d96ceb2ef9bcf7b1704ff466902029
Benjamin Schlotter <benji@schlotterfam.de>
2020-09-25 18:51:40 +0200

    wip fixes for import stuff

--- Utils/py/BallDetection/RegressionNetwork/train.py
+++ Utils/py/BallDetection/RegressionNetwork/train.py
@@ -2,6 +2,7 @@
 
 import argparse
 import pickle
+import sys
 from datetime import datetime
 from pathlib import Path
 from sys import exit
@@ -9,6 +10,7 @@ from sys import exit
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras as keras
+
 import utility_functions.model_zoo as model_zoo
 
 DATA_DIR = Path(Path(__file__).parent.absolute() / "data").resolve()
@@ -125,8 +127,10 @@ def main(raw_args=None, model=None):
 
 if __name__ == '__main__':
     test_model = model_zoo.fy_1500()
-    train_history = main(['--output', "models"], model=test_model)
+    # forward commandline arguments to the argparser in the main function
+    train_history = main(sys.argv[1:] + ['--output', "models"], model=test_model)
 
+    # TODO save in same folder as model
     with open("history_" + test_model.name + ".pkl", "wb") as f:
         pickle.dump(train_history.history, f)
 
