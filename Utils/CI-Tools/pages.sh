f501634adf0cf7d4e7e5ff0a99a9d909b670bd54
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2021-03-10 18:23:59 +0100

    add stuff for pages deploy in extra script

---
+++ Utils/CI-Tools/pages.sh
@@ -0,0 +1,19 @@
+function validate_url(){
+  if [[ `wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then echo "true"; fi
+}
+
+url="https://pages.cms.hu-berlin.de/berlinunited/naoth-2020/content.tar.gz"
+echo $(validate_url $url)
+
+if [ -n $(validate_url $url >/dev/null) ]; then
+        wget --no-check-certificate $url -O prev_content.tar.gz;
+        if [ -f prev_content.tar.gz ]; then 
+        	tar -xzf prev_content.tar.gz public/; 
+        fi
+else
+        echo "remote url does not exist";
+fi
+
+mkdir -p public/$CI_COMMIT_REF_NAME/cppcheck
+cp -r $branch_name/* public/$CI_COMMIT_REF_NAME/cppcheck/
+tar -zcf public/content.tar.gz public/
