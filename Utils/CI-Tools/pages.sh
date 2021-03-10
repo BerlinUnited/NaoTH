336d49a209826e6906822bb63a6bac0c06a9be89
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2021-03-10 19:43:49 +0100

    maybe fix?

--- Utils/CI-Tools/pages.sh
+++ Utils/CI-Tools/pages.sh
@@ -19,7 +19,7 @@ new_branch_name="$(sed 's/\//_/g' <<< "${CI_COMMIT_REF_NAME}")"
 # rename folders like feature/my_cool_feature to feature_my_cool_feature
 if [ "$CI_COMMIT_REF_NAME" != "$new_branch_name" ]; then mv $CI_COMMIT_REF_NAME $new_branch_name; else new_branch_name=$CI_COMMIT_REF_NAME; fi
 mkdir -p public/$new_branch_name
-cp -r $branch_name/* public/$new_branch_name/
+cp -r $CI_COMMIT_REF_NAME/* public/$new_branch_name/
 tar -zcf content.tar.gz public/
 
 # copy the content.tar.gz inside public folder so its part of the pages deployment
