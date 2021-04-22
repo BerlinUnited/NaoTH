087876c4107c9ef04f1b67f9485e90449346359b
Philipp Strobel <philippstrobel@posteo.de>
2021-04-22 21:49:57 +0200

    consistent indentation

--- Framework/Platforms/Source/DCM/LolaAdaptor/LolaAdaptor.cpp
+++ Framework/Platforms/Source/DCM/LolaAdaptor/LolaAdaptor.cpp
@@ -62,53 +62,53 @@ void LolaAdaptor::writeNaoInfo() const
 
 void LolaAdaptor::start()
 {
-  exiting = false;
-  lolaThread = std::thread([this] {this->run();});
-  //ThreadUtil::setPriority(lolaThread, ThreadUtil::Priority::highest);
-  ThreadUtil::setName(lolaThread, "LOLA");
+    exiting = false;
+    lolaThread = std::thread([this] {this->run();});
+    //ThreadUtil::setPriority(lolaThread, ThreadUtil::Priority::highest);
+    ThreadUtil::setName(lolaThread, "LOLA");
 
-  // NOTE: SCHED_FIFO is a real time sheduler
-  //       max priority for a FIFO thread is 99
-  sched_param param;
-  param.sched_priority = 50;
-  if(pthread_setschedparam(lolaThread.native_handle(), SCHED_FIFO, &param)) {
-    std::cerr << "[LolaAdaptor] error setting thread priority" << std::endl;
-    assert(false);
-  }
+    // NOTE: SCHED_FIFO is a real time sheduler
+    //       max priority for a FIFO thread is 99
+    sched_param param;
+    param.sched_priority = 50;
+    if(pthread_setschedparam(lolaThread.native_handle(), SCHED_FIFO, &param)) {
+        std::cerr << "[LolaAdaptor] error setting thread priority" << std::endl;
+        assert(false);
+    }
 
-  // int tryCount = 0;
-  while(waitingForLola && ! exiting)
-  {
-    fprintf(stderr, "[LolaAdaptor] Waiting for LoLA socket.\n");
-    std::this_thread::sleep_for(std::chrono::milliseconds(125));
-    // if(tryCount > 40 )
-    // {
-    //   fprintf(stderr, "[LolaAdaptor] Waiting for LoLA socket failed after %d ms.\n", tryCount * 125);
-    //   waitingForLola = false;
-    //   assert(false);
-    // }
-    // tryCount++;
-  }
-  fprintf(stderr, "[LolaAdaptor] LoLA socket connection established.\n");
-  //HACK: pulseaudio is not initialized correctly, but after playing a sound as no it works?!?
+    // int tryCount = 0;
+    while(waitingForLola && ! exiting)
+    {
+        fprintf(stderr, "[LolaAdaptor] Waiting for LoLA socket.\n");
+        std::this_thread::sleep_for(std::chrono::milliseconds(125));
+        // if(tryCount > 40 )
+        // {
+        //   fprintf(stderr, "[LolaAdaptor] Waiting for LoLA socket failed after %d ms.\n", tryCount * 125);
+        //   waitingForLola = false;
+        //   assert(false);
+        // }
+        // tryCount++;
+    }
+    fprintf(stderr, "[LolaAdaptor] LoLA socket connection established.\n");
+    //HACK: pulseaudio is not initialized correctly, but after playing a sound as no it works?!?
 }
 
 void LolaAdaptor::stop()
 {
-  std::cout << "[LolaAdaptor] stop wait" << std::endl;
+    std::cout << "[LolaAdaptor] stop wait" << std::endl;
 
-  // request the thread to stop
-  exiting = true;
+    // request the thread to stop
+    exiting = true;
 
-  if(lolaThread.joinable()) {
-    lolaThread.join();
-  }
-  std::cout << "[LolaAdaptor] stop done" << std::endl;
+    if(lolaThread.joinable()) {
+        lolaThread.join();
+    }
+    std::cout << "[LolaAdaptor] stop done" << std::endl;
 }
 
 bool LolaAdaptor::isRunning() const
 {
-  return running;
+    return running;
 }
 
 void LolaAdaptor::run()
@@ -228,126 +228,125 @@ void LolaAdaptor::setMotorJoints(ActuatorData &actuators)
 
 void LolaAdaptor::setWarningLED(ActuatorData& actuators, bool red)
 {
-  static naoth::LEDData theLEDData;
-  static int count = 0;
+    static naoth::LEDData theLEDData;
+    static int count = 0;
 
-  int begin = ((++count)/10)%10;
-  theLEDData.theMonoLED[LEDData::EarRight0 + begin] = 0;
-  theLEDData.theMonoLED[LEDData::EarLeft0 + begin] = 0;
-  int end = (begin+2)%10;
-  theLEDData.theMonoLED[LEDData::EarRight0 + end] = 1;
-  theLEDData.theMonoLED[LEDData::EarLeft0 + end] = 1;
+    int begin = ((++count)/10)%10;
+    theLEDData.theMonoLED[LEDData::EarRight0 + begin] = 0;
+    theLEDData.theMonoLED[LEDData::EarLeft0 + begin] = 0;
+    int end = (begin+2)%10;
+    theLEDData.theMonoLED[LEDData::EarRight0 + end] = 1;
+    theLEDData.theMonoLED[LEDData::EarLeft0 + end] = 1;
 
-  for(int i=0; i<LEDData::numOfMultiLED; i++)
-  {
-    theLEDData.theMultiLED[i][LEDData::RED] = red ? 1 : 0;
-    theLEDData.theMultiLED[i][LEDData::GREEN] = 0;
-    theLEDData.theMultiLED[i][LEDData::BLUE] = red ? 0 : 1;
-  }
-  LolaDataConverter::set(actuators, theLEDData);
+    for(int i=0; i<LEDData::numOfMultiLED; i++)
+    {
+        theLEDData.theMultiLED[i][LEDData::RED] = red ? 1 : 0;
+        theLEDData.theMultiLED[i][LEDData::GREEN] = 0;
+        theLEDData.theMultiLED[i][LEDData::BLUE] = red ? 0 : 1;
+    }
+    LolaDataConverter::set(actuators, theLEDData);
 }//end setWarningLED
 
 bool LolaAdaptor::runEmergencyMotion(ActuatorData& actuators)
 {
-  if(state == DISCONNECTED)
-  {
-    if(initialMotion == NULL && command_data_available && sensor_data_available)
+    if(state == DISCONNECTED)
     {
-       std::cout << "emerg: start init motion" << std::endl;
-       // take the last command data
-      const Accessor<MotorJointData>* commandData = naoCommandMotorJointData.reading();
-      initialMotion = new BasicMotion(theMotorJointData, commandData->get(), theInertialSensorData);
-    }
+        if(initialMotion == NULL && command_data_available && sensor_data_available)
+        {
+            std::cout << "emerg: start init motion" << std::endl;
+            // take the last command data
+            const Accessor<MotorJointData>* commandData = naoCommandMotorJointData.reading();
+            initialMotion = new BasicMotion(theMotorJointData, commandData->get(), theInertialSensorData);
+        }
 
-    setWarningLED(actuators, state == DISCONNECTED);
-  }//end if
+        setWarningLED(actuators, state == DISCONNECTED);
+    }//end if
 
-  // after reconnect: wait until the init motion is finished
-  if(initialMotion != NULL)
-  {
-    if(state == CONNECTED && initialMotion->isFinish())
+    // after reconnect: wait until the init motion is finished
+    if(initialMotion != NULL)
     {
-      std::cout << "emerg: stop init motion" << std::endl;
-      delete initialMotion;
-      initialMotion = NULL;
-    }
-    else
-    {
-      // execute the emergency motion
-      // (the resulting joint commands are written to theMotorJointData)
-      initialMotion->execute();
+        if(state == CONNECTED && initialMotion->isFinish())
+        {
+            std::cout << "emerg: stop init motion" << std::endl;
+            delete initialMotion;
+            initialMotion = NULL;
+        }
+        else
+        {
+            // execute the emergency motion
+            // (the resulting joint commands are written to theMotorJointData)
+            initialMotion->execute();
 
-      // write MotorJointData to LolaActuatorData
-      LolaDataConverter::set(actuators, theMotorJointData);
-    }
-  }//end if
+            // write MotorJointData to LolaActuatorData
+            LolaDataConverter::set(actuators, theMotorJointData);
+        }
+    }//end if
 
-  return initialMotion != NULL;
+    return initialMotion != NULL;
 }//end runEmergencyMotion
 
 void LolaAdaptor::notify()
 {
-  static int drop_count = 10;
+    static int drop_count = 10;
 
-  // raise the semaphore: triggers core
-  if(sem != SEM_FAILED)
-  {
-    int sval;
-    if(sem_getvalue(sem, &sval) == 0)
+    // raise the semaphore: triggers core
+    if(sem != SEM_FAILED)
     {
-      if(sval < 1)
-      {
-        sem_post(sem);
+        int sval;
+        if(sem_getvalue(sem, &sval) == 0)
+        {
+            if(sval < 1)
+            {
+                sem_post(sem);
 
-        // until now we were disconnected, but now we're alive and connected ...
-        if(state == DISCONNECTED) {
-          fprintf(stderr, "[LolaAdaptor] I think the core is alive.\n");
+                // until now we were disconnected, but now we're alive and connected ...
+                if(state == DISCONNECTED) {
+                    fprintf(stderr, "[LolaAdaptor] I think the core is alive.\n");
+                }
+
+                drop_count = 0;
+                state = CONNECTED;
+            }
+            else
+            {
+                if(drop_count == 0) {
+                    fprintf(stderr, "[LolaAdaptor] dropped sensor data.\n");
+                } else if(drop_count == 10) {
+                    fprintf(stderr, "[LolaAdaptor] I think the core is dead.\n");
+                    state = DISCONNECTED;
+                }
+
+                // don't count more than 11
+                drop_count += (drop_count < 11);
+            }//end if
         }
-
-        drop_count = 0;
-        state = CONNECTED;
-      }
-      else
-      {
-        if(drop_count == 0) {
-          fprintf(stderr, "[LolaAdaptor] dropped sensor data.\n");
-        } else if(drop_count == 10) {
-          fprintf(stderr, "[LolaAdaptor] I think the core is dead.\n");
-          state = DISCONNECTED;
+        else
+        {
+            fprintf(stderr, "[LolaAdaptor] I couldn't get value by sem_getvalue.\n");
         }
-
-        // don't count more than 11
-        drop_count += (drop_count < 11);
-      }//end if
-    }
-    else
-    {
-      fprintf(stderr, "[LolaAdaptor] I couldn't get value by sem_getvalue.\n");
-    }
-  }//end if SEM_FAILED
+    }//end if SEM_FAILED
 }
 
 void LolaAdaptor::shutdownCallback()
 {
-  // play a sound that the user knows we recognized his shutdown request
-  system("paplay /opt/aldebaran/share/naoqi/wav/bip_power_off.wav");
+    // play a sound that the user knows we recognized his shutdown request
+    system("paplay /opt/aldebaran/share/naoqi/wav/bip_power_off.wav");
 
-  // stop the user program
-  std::cout << "[LolaAdaptor] stopping naoth" << std::endl;
-  // stop naoth to trigger emergency motion
-  system("killall naoth");
-  // in NaoSMAL it's like this
-  //system("naoth stop");
+    // stop the user program
+    std::cout << "[LolaAdaptor] stopping naoth" << std::endl;
+    // stop naoth to trigger emergency motion
+    system("killall naoth");
+    // in NaoSMAL it's like this
+    //system("naoth stop");
 
-  sleep(5);
+    sleep(5);
 
-  // we are the child process, do a blocking call to shutdown
-  std::cout << "[LolaAdaptor] System shutdown requested" << std::endl;
-  system("/sbin/shutdown -h now");
+    // we are the child process, do a blocking call to shutdown
+    std::cout << "[LolaAdaptor] System shutdown requested" << std::endl;
+    system("/sbin/shutdown -h now");
 
-  // await termination
-  while(true) {
-    sleep(100);
-  }
-
+    // await termination
+    while(true) {
+        sleep(100);
+    }
 }//end shutdownCallback
