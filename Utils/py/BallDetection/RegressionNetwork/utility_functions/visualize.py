585368367d02dd7c09b90112d7bf7de349984bd9
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2021-05-25 23:04:49 +0200

    improve bhuman dataset visualizer

--- Utils/py/BallDetection/RegressionNetwork/utility_functions/visualize.py
+++ Utils/py/BallDetection/RegressionNetwork/utility_functions/visualize.py
@@ -4,6 +4,7 @@ TODO show images from dataset with annotations for different datasets (naoth, b-
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
 import pickle
 from pathlib import Path
 from bhuman_helper import download_bhuman2019
@@ -12,33 +13,67 @@ DATA_DIR = Path(Path(__file__).parent.parent.absolute() / "data").resolve()
 MODEL_DIR = Path(Path(__file__).parent.parent.absolute() / "data/best_models").resolve()
 
 
-def visualize_bhuman_dataset():
-    download_bhuman2019("https://sibylle.informatik.uni-bremen.de/public/datasets/b-alls-2019/b-alls-2019.hdf5",
-                        "data/bhuman/b-alls-2019.hdf5")
-    download_bhuman2019("https://sibylle.informatik.uni-bremen.de/public/datasets/b-alls-2019/readme.txt",
-                        "data/bhuman/readme.txt")
+class BhumanVisualizer(object):
+    def __init__(self, index_start):
+        self.ind = index_start + 1
 
-    f = h5py.File('../data/bhuman/b-alls-2019.hdf5', 'r')
+        self.get_b_human_data()
+        self.init_plot()
 
-    # TODO combine the data and
-    negative_data = np.array(f.get('negatives/data'))
-    positive_data = np.array(f.get('positives/data'))
-    negative_labels = np.array(f.get('negatives/labels'))
-    positive_labels = np.array(f.get('positives/labels'))
+    def get_b_human_data(self):
+        download_bhuman2019("https://sibylle.informatik.uni-bremen.de/public/datasets/b-alls-2019/b-alls-2019.hdf5",
+                            str(DATA_DIR) + "/bhuman/b-alls-2019.hdf5")
+        download_bhuman2019("https://sibylle.informatik.uni-bremen.de/public/datasets/b-alls-2019/readme.txt",
+                            str(DATA_DIR) + "/bhuman/readme.txt")
 
-    fig = plt.figure(figsize=(8, 8))
-    columns = 4
-    rows = 5
-    for i in range(1, columns * rows + 1):
-        img = positive_data[i]
-        fig.add_subplot(rows, columns, i)
-        current_label = positive_labels[i]
-        print("current_label:", current_label)
-        circle1 = plt.Circle((current_label[1], current_label[2]), current_label[3], color='r', alpha=0.7)
-        ax = fig.gca()
-        ax.add_patch(circle1)
-        plt.imshow(img, cmap='gray')
-    plt.show()
+        f = h5py.File('../data/bhuman/b-alls-2019.hdf5', 'r')
+
+        # TODO combine the data and
+        self.negative_data = np.array(f.get('negatives/data'))
+        self.positive_data = np.array(f.get('positives/data'))
+        self.negative_labels = np.array(f.get('negatives/labels'))
+        self.positive_labels = np.array(f.get('positives/labels'))
+
+    def init_plot(self):
+        # Adjust bottom to make room for Buttons
+        fig = plt.figure(figsize=(8, 8))
+        columns = 4
+        rows = 5
+        start_value = 0
+
+        self.subplot_list = list()
+
+        for plot_idx, i in enumerate(range(start_value, start_value + columns * rows)):
+            img = self.positive_data[i]
+            a = fig.add_subplot(rows, columns, plot_idx + 1)
+            self.subplot_list.append(a)
+            current_label = self.positive_labels[i]
+
+            circle1 = plt.Circle((current_label[1], current_label[2]), current_label[3], color='r', alpha=0.7)
+            ax = fig.gca()
+            ax.add_patch(circle1)
+            plt.imshow(img, cmap='gray')
+
+        # Connect to a "switch" Button, setting its left, top, width, and height
+        axswitch = plt.axes([0.40, 0.07, 0.2, 0.05])
+        bswitch = Button(axswitch, 'Next')
+        bswitch.on_clicked(self.next)
+        plt.show()
+
+    # This function is called when bswitch is clicked
+    def next(self, event):
+        for idx, subplot in enumerate(self.subplot_list):
+            subplot.cla()
+            # get new image data
+            img = self.positive_data[self.ind]
+            current_label = self.positive_labels[self.ind]
+            circle1 = plt.Circle((current_label[1], current_label[2]), current_label[3], color='r', alpha=0.7)
+
+            subplot.add_patch(circle1)
+            subplot.imshow(img, cmap='gray')
+
+            self.ind += 1
+            plt.draw()
 
 
 """
@@ -152,4 +187,4 @@ def visualize_tk3_segmentation():
 
 
 if __name__ == '__main__':
-    visualize_tk3_detection3()
+    BhumanVisualizer(0)
