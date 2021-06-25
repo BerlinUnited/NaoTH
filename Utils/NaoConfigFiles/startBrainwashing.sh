18c9b4056bde85d2273cc1b79f16aa172bc19f64
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2021-06-25 16:46:41 +0200

    no nohup

--- Utils/NaoConfigFiles/startBrainwashing.sh
+++ Utils/NaoConfigFiles/startBrainwashing.sh
@@ -23,12 +23,12 @@ if [ -f "/opt/aldebaran/bin/lola" ] | [ -f "/usr/bin/lola" ]; then
 	echo "Nao V6"
 	
 	chmod +x brainwash_v6.sh
-	nohup ./brainwash_v6.sh & 
+	. ./brainwash_v6.sh
 
 else
 	echo "Nao V5 or older"	
 
 	chmod +x brainwash_v3v4v5.sh
-	nohup ./brainwash_v3v4v5.sh &
+	. ./brainwash_v3v4v5.sh
 
 fi
