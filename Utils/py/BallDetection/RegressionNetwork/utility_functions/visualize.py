6edfe9a8859cfffbb43944183f59096ae83d6ec2
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2021-06-29 21:45:49 +0200

    more cleanup

--- Utils/py/BallDetection/RegressionNetwork/utility_functions/visualize.py
+++ Utils/py/BallDetection/RegressionNetwork/utility_functions/visualize.py
@@ -1,5 +1,5 @@
 """
-TODO show images from dataset with annotations for different datasets (naoth, b-human, etc)
+ show images from dataset with annotations for different datasets (naoth, b-human, etc)
 """
 import numpy as np
 import h5py
@@ -8,7 +8,6 @@ from matplotlib.widgets import Button
 import pickle
 from pathlib import Path
 from bhuman_helper import download_bhuman2019
-import tensorflow.keras as keras
 
 DATA_DIR = Path(Path(__file__).parent.parent.absolute() / "data").resolve()
 MODEL_DIR = Path(Path(__file__).parent.parent.absolute() / "data/best_models").resolve()
@@ -77,11 +76,6 @@ class BhumanVisualizer(object):
             plt.draw()
 
 
-"""
-
-"""
-
-
 class TK3Visualizer(object):
     def __init__(self, index_start):
         self.ind = index_start + 1
@@ -109,11 +103,13 @@ class TK3Visualizer(object):
             self.subplot_list.append(a)
             current_label = self.y[i]
 
-            #circle1 = plt.Circle((current_label[1], current_label[2]), current_label[3], color='r', alpha=0.7)
-            circle1 = plt.Circle((current_label[1] * 16, current_label[2] * 16), current_label[0] * 16, color='r',
-                                 alpha=0.5)
-            ax = fig.gca()
-            ax.add_patch(circle1)
+            # only draw circle if there is actually a circle annotation in the groundtruth
+            if current_label[3] == 1.0:
+                # format for plotting is (xy), r
+                circle1 = plt.Circle((current_label[1] * 16, current_label[2] * 16), current_label[0] * 16, color='r',
+                                     alpha=0.5)
+                ax = fig.gca()
+                ax.add_patch(circle1)
             plt.imshow(img, cmap='gray')
 
         # Connect to a "switch" Button, setting its left, top, width, and height
@@ -129,120 +125,19 @@ class TK3Visualizer(object):
             # get new image data
             img = self.x[self.ind]
             current_label = self.y[self.ind]
-            circle1 = plt.Circle((current_label[1], current_label[2]), current_label[3], color='r', alpha=0.7)
 
-            subplot.add_patch(circle1)
+            # only draw circle if there is actually a circle annotation in the groundtruth
+            if current_label[3] == 1.0:
+                circle1 = plt.Circle((current_label[1] * 16, current_label[2] * 16), current_label[0] * 16, color='r',
+                                     alpha=0.5)
+                subplot.add_patch(circle1)
+
             subplot.imshow(img, cmap='gray')
 
             self.ind += 1
             plt.draw()
 
 
-def visualize_tk3_detection():
-    imgdb_path = str(DATA_DIR / 'tk03_natural_detection.pkl')
-    with open(imgdb_path, "rb") as f:
-        mean = pickle.load(f)
-        x = pickle.load(f)
-        y = pickle.load(f)
-
-    fig = plt.figure(figsize=(8, 8))
-    columns = 4
-    rows = 5
-    for i in range(1, columns * rows + 1):
-        img = x[i]
-        fig.add_subplot(rows, columns, i)
-        current_label = y[i]
-        print("current_label:", current_label)
-        # format for plotting is (xy), r
-        circle1 = plt.Circle((current_label[1] * 16, current_label[2] * 16), current_label[0] * 16, color='r',
-                             alpha=0.5)
-        ax = fig.gca()
-        ax.add_patch(circle1)
-        plt.imshow(img, cmap='gray')
-    plt.show()
-
-def visualize_tk3_classification():
-    pass
-
-
-
-
-
-def visualize_tk3_detection2():
-    """
-    this is for steffen
-    :return:
-    """
-    imgdb_path = str(DATA_DIR / 'tk03_natural_detection.pkl')
-    with open(imgdb_path, "rb") as f:
-        mean = pickle.load(f)
-        x = pickle.load(f)
-        y = pickle.load(f)
-
-    counter = 0
-    for i in range(100):
-        img = x[i]
-        img = img - mean
-        target = y[i]
-        print(target)
-        # if its a ball
-        if target[-1] == 1:
-            counter = counter + 1
-            fig = plt.figure()
-            current_label = y[i]
-            circle1 = plt.Circle((current_label[1] * 16, current_label[2] * 16), current_label[0] * 16, color='b',
-                                 alpha=0.35)
-            ax = fig.gca()
-            ax.add_patch(circle1)
-            plt.imshow(img, cmap='gray')
-            plt.show()
-            # plt.imsave("test.png", img)
-            if counter > 10:
-                break
-
-
-def visualize_tk3_detection3():
-    """
-    this is for steffen
-    :return:
-    """
-    imgdb_path = str(DATA_DIR / 'tk03_natural_detection.pkl')
-    with open(imgdb_path, "rb") as f:
-        mean = pickle.load(f)
-        x = pickle.load(f)
-        y = pickle.load(f)
-
-    model = keras.models.load_model(str(MODEL_DIR / 'fy1500_conf.h5'))
-    model.summary()
-
-    counter = 0
-    for i in range(100):
-        img = x[i] - mean
-        target = y[i]
-        if target[-1] == 1:
-            continue
-
-        input = x[0].reshape(1, *x[0].shape)
-        output = model.predict(input)[0]
-
-        counter = counter + 1
-        fig = plt.figure()
-        print(output)
-        circle1 = plt.Circle((output[1] * 16, output[2] * 16), output[0] * 16, color='b',
-                             alpha=0.35)
-        ax = fig.gca()
-        ax.add_patch(circle1)
-        plt.imshow(img, cmap='gray')
-        plt.show()
-        # plt.imsave("test.png", img)
-        if counter > 10:
-            break
-
-
-def visualize_tk3_segmentation():
-    pass
-
-
 if __name__ == '__main__':
-    BhumanVisualizer(0)
-    #TK3Visualizer(0)
+    # BhumanVisualizer(0)
+    TK3Visualizer(0)
