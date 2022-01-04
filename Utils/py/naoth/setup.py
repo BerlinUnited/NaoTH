10e3a26ebcb73f3bd6dbc75122c22dd6ae1dc823
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2022-01-04 19:52:32 +0100

    improve setup.py

--- Utils/py/naoth/setup.py
+++ Utils/py/naoth/setup.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
-import os
 from setuptools import setup, find_packages
+from pathlib import Path
 
-version_file = open(os.path.join(".", 'VERSION'))
+# The directory containing this file
+HERE = Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+VERSION_STRING = (HERE / "VERSION").read_text()
 
 setup(name='naoth',
-      version=version_file.read().strip(),
+      version=VERSION_STRING,
       author='NaoTH Berlin United',
       author_email='nao-team@informatik.hu-berlin.de',
       description='Python utils for the NaoTH toolchain',
+      long_description=README,
+      long_description_content_type="text/markdown",
       packages=find_packages(exclude=["tests"]),
+      license='Apache License 2.0',
       zip_safe=False,
       setup_requires=['wheel'],
       install_requires=[
