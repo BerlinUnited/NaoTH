82c8b0bf0cee892de8674c20a4020f07aa0efa28
Stella Alice Schlotter <stella.schlotter@deepl.com>
2023-07-05 03:46:06 +0200

    add dummy module for attention engine - no implementation yet

---
+++ NaoTHSoccer/Source/Cognition/Modules/Modeling/AttentionEngine/AttentionEngine.cpp
@@ -0,0 +1,19 @@
+/**
+* @file AttentionEngine.cpp
+*/
+
+#include "AttentionEngine.h"
+
+AttentionEngine::AttentionEngine()
+{
+
+}
+
+AttentionEngine::~AttentionEngine()
+{
+}
+
+void AttentionEngine::execute()
+{
+  
+}
