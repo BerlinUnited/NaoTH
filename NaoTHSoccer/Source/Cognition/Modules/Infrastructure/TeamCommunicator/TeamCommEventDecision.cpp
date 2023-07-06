2490743cc661d85a75245808bb9348142f8c6900
Philipp Strobel <philippstrobel@posteo.de>
2023-07-06 14:16:03 +0200

    bugfix: use the safte timestamp also in the initial phase for time syncing

--- NaoTHSoccer/Source/Cognition/Modules/Infrastructure/TeamCommunicator/TeamCommEventDecision.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/Infrastructure/TeamCommunicator/TeamCommEventDecision.cpp
@@ -42,11 +42,6 @@ void TeamCommEventDecision::byInterval()
 
 void TeamCommEventDecision::byDistance()
 {
-    // there is no limit/budget in the initial phase; use this phase for syncing
-    if (getGameData().gameState == GameData::initial) {
-        getTeamMessageDecision().send_ntpRequests.set();
-    }
-
     // if the robot is not playing (eg. penalized), do not send any message
     if (getPlayerInfo().robotState != PlayerInfo::playing) {
         return;
@@ -55,6 +50,14 @@ void TeamCommEventDecision::byDistance()
     // as kind of safety set a lower bound how often we can send messages
     if ((unsigned int)getFrameInfo().getTimeSince(params.byDistance_lastSentTimestamp) < params.byDistance_minInterval) {
         return;
+    }
+
+    // use the initial phase phase for syncing
+    if (getGameData().gameState == GameData::initial)
+    {
+        getTeamMessageDecision().send_ntpRequests.set();
+        // update timestamp for the safety condition
+        params.byDistance_lastSentTimestamp = getFrameInfo().getTime();
     }
 
     const auto& role = getRoleDecisionModel().getRole(getPlayerInfo().playerNumber);
