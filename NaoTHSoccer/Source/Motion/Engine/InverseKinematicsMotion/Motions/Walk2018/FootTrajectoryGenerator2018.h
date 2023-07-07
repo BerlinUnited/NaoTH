8515967a8bba2ddceb431b857049beaae673ee44
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2023-07-07 10:13:45 +0200

    cleanup new spline trajectory for the kick (no sidekicks for now)

--- NaoTHSoccer/Source/Motion/Engine/InverseKinematicsMotion/Motions/Walk2018/FootTrajectoryGenerator2018.h
+++ NaoTHSoccer/Source/Motion/Engine/InverseKinematicsMotion/Motions/Walk2018/FootTrajectoryGenerator2018.h
@@ -22,7 +22,10 @@
 #include "Tools/Debug/DebugPlot.h"
 #include "Tools/Debug/DebugRequest.h"
 
+// TODO: which one is better?
 #include "Tools/DataStructures/Spline.h"
+#include "Tools/Math/CubicSpline.h"
+
 
 BEGIN_DECLARE_MODULE(FootTrajectoryGenerator2018)
     PROVIDE(DebugPlot)
@@ -39,61 +42,79 @@ END_DECLARE_MODULE(FootTrajectoryGenerator2018)
 class FootTrajectoryGenerator2018 : private FootTrajectoryGenerator2018Base
 {
   public:
-    FootTrajectoryGenerator2018() : parameters(getWalk2018Parameters().footTrajectoryGenerator2018Params) {
+    FootTrajectoryGenerator2018() 
+      : parameters(getWalk2018Parameters().footTrajectoryGenerator2018Params) 
+    {
+      // Generate reference splines for step trajectories. 
+      // Those splines are adjusted to the different steps.
 
-    // reference points for splines in xy
-    const std::vector<double> xA{ 0.0, 0.125, 0.25, 0.5, 0.75, 0.875, 1.0 };
-    const std::vector<double> yA{
-      0.0,
-      (1 - cos(0.125*Math::pi))*0.5,
-      (1 - cos(0.25 *Math::pi))*0.5,
-      (1 - cos(0.5  *Math::pi))*0.5,
-      (1 - cos(0.75 *Math::pi))*0.5,
-      (1 - cos(0.875*Math::pi))*0.5,
-      1.0
-    };
+      // reference points for splines in xy
+      const std::vector<double> t_XY { 0.0, 0.125, 0.25, 0.5, 0.75, 0.875, 1.0 };
+      const std::vector<double> f_XY {
+        0.0,
+        (1 - cos(0.125*Math::pi))*0.5,
+        (1 - cos(0.25 *Math::pi))*0.5,
+        (1 - cos(0.5  *Math::pi))*0.5,
+        (1 - cos(0.75 *Math::pi))*0.5,
+        (1 - cos(0.875*Math::pi))*0.5,
+        1.0
+      };
 
-    // reference points for splines in z
-    const std::vector<double> xB{ 0.0, 0.125, 0.25, 0.5, 0.75, 0.875, 1.0 };
-    const std::vector<double> yB{
-      0.0,
-      (1 - cos(0.125*Math::pi2))*0.5,
-      (1 - cos(0.25 *Math::pi2))*0.5,
-      1.0,
-      (1 - cos(0.75 *Math::pi2))*0.5,
-      (1 - cos(0.875*Math::pi2))*0.5,
-      0.0
-    };
+      theCubicSplineXY.set_boundary(tk::spline::first_deriv, 0.0, tk::spline::first_deriv,0.0, false);
+      theCubicSplineXY.set_points(t_XY, f_XY);
 
-        theCubicSplineXY.set_boundary(tk::spline::first_deriv,0.0, tk::spline::first_deriv,0.0, false);
-        theCubicSplineXY.set_points(xA,yA);
+      
+      // reference points for splines in z
+      const std::vector<double> t_Z { 0.0, 0.125, 0.25, 0.5, 0.75, 0.875, 1.0 };
+      const std::vector<double> f_Z {
+        0.0,
+        (1 - cos(0.125*Math::pi2))*0.5,
+        (1 - cos(0.25 *Math::pi2))*0.5,
+        1.0,
+        (1 - cos(0.75 *Math::pi2))*0.5,
+        (1 - cos(0.875*Math::pi2))*0.5,
+        0.0
+      };
 
-        theCubicSplineZ.set_boundary(tk::spline::first_deriv,0.0, tk::spline::first_deriv,0.0, false);
-        theCubicSplineZ.set_points(xB,yB);
+      theCubicSplineZ.set_boundary(tk::spline::first_deriv ,0.0, tk::spline::first_deriv,0.0, false);
+      theCubicSplineZ.set_points(t_Z, f_Z);
     }
 
     virtual void execute();
 
   private:
+
+    // use the following functions to generate the trajectory and calculate the nex pos for the current step
+    Pose3D calculateLiftingFootPos(const Step& step) const;
+
+
+    // LEGACY (uses sin and cos for trajectory generation)
+    // genera trajectory for STEP_CONTROL using sin and cos
+    Pose3D stepControl(const Pose3D& oldFoot, const Pose3D& targetFoot,
+                        double cycle, double samplesDoubleSupport, double samplesSingleSupport,
+                        double stepHeight, double footPitchOffset, double footRollOffset,
+                        double speedDirection, double scale) const;
+
+
+    // adjusted for forward kicks
+    // TODO: check if the sideicks still work
+    Pose3D stepControlNew(const FootStep& step,
+                          double cycle, double samplesDoubleSupport, double samplesSingleSupport,
+                          double stepHeight, double footPitchOffset, double footRollOffset,
+                          double speedDirection, double scale, double sidekick_width) const;
+
+
+    // LEGACY (uses sin and cos for trajectory generation)
+    // generte a trajectory for WALK steps
     Pose3D genTrajectory(const Pose3D& oldFoot, const Pose3D& targetFoot,
                          double cycle, double samplesDoubleSupport, double samplesSingleSupport,
                          double stepHeight, double footPitchOffset, double footRollOffset ) const;
 
-    Pose3D stepControl(const Pose3D& oldFoot, const Pose3D& targetFoot,
-                       double cycle, double samplesDoubleSupport, double samplesSingleSupport,
-                       double stepHeight, double footPitchOffset, double footRollOffset,
-					   double speedDirection, double scale) const;
-
-	Pose3D stepControlNew(const FootStep& step,
-						  double cycle, double samplesDoubleSupport, double samplesSingleSupport,
-						  double stepHeight, double footPitchOffset, double footRollOffset,
-		                  double speedDirection, double scale, double sidekick_width) const;
 
     Pose3D genTrajectoryWithSplines(const Pose3D& oldFoot, const Pose3D& targetFoot,
                                     double cycle, double duration,
                                     double stepHeight, double footPitchOffset, double footRollOffset ) const;
 
-    Pose3D calculateLiftingFootPos(const Step& step) const;
 
   private:
     tk::spline theCubicSplineXY;
