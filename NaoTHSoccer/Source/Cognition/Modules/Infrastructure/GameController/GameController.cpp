cd753a8173ae72d6322f765ec725683f5f24ce59
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2024-04-18 20:59:41 +0200

    added a debug request for "unstiff"

--- NaoTHSoccer/Source/Cognition/Modules/Infrastructure/GameController/GameController.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/Infrastructure/GameController/GameController.cpp
@@ -14,6 +14,8 @@ GameController::GameController()
   DEBUG_REQUEST_REGISTER("gamecontroller:game_state:ready", "force the ready state", false);
   DEBUG_REQUEST_REGISTER("gamecontroller:game_state:set", "force the set state", false);
   DEBUG_REQUEST_REGISTER("gamecontroller:game_state:finished", "force the finished state", false);
+  DEBUG_REQUEST_REGISTER("gamecontroller:game_state:unstiff", "force the unstiff state", false);
+
   DEBUG_REQUEST_REGISTER("gamecontroller:set_play:none", "force the setPlay state to none", false);
   DEBUG_REQUEST_REGISTER("gamecontroller:set_play:goal_free_kick", "force the setPlay state to goal free kick", false);
   DEBUG_REQUEST_REGISTER("gamecontroller:set_play:pushing_free_kick", "force the setPlay state to pushing free kick", false);
@@ -192,6 +194,9 @@ void GameController::handleDebugRequest()
   );
   DEBUG_REQUEST("gamecontroller:game_state:finished",
     debugState = PlayerInfo::finished;
+  );
+  DEBUG_REQUEST("gamecontroller:game_state:unstiff",
+    debugState = PlayerInfo::unstiff;
   );
 
   // DebugRequests for the set play state (free kicks)
