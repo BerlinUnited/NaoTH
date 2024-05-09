2889a4f5e703ad5972663febc353b43b30736d29
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2024-05-09 17:17:34 +0200

    formatting

--- Framework/Commons/Source/Tools/Communication/Network/UDPReceiver.cpp
+++ Framework/Commons/Source/Tools/Communication/Network/UDPReceiver.cpp
@@ -67,12 +67,14 @@ GError* UDPReceiver::bindAndListen(unsigned int port)
 {
   GError* err = NULL;
   socket = g_socket_new(G_SOCKET_FAMILY_IPV4, G_SOCKET_TYPE_DATAGRAM, G_SOCKET_PROTOCOL_UDP, &err);
-  if (err) return err;
+  if (err) {
+    return err;
+  }
 
   g_socket_set_blocking(socket, true);
 
   GInetAddress* inetAddress = g_inet_address_new_any(G_SOCKET_FAMILY_IPV4);
-  GSocketAddress* socketAddress = g_inet_socket_address_new(inetAddress, static_cast<unsigned short>(port));
+  GSocketAddress* socketAddress = g_inet_socket_address_new(inetAddress, static_cast<guint16>(port));
 
   g_socket_bind(socket, socketAddress, true, &err);
 
