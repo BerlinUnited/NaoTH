e339cf88042feace64a812e286638770a4904573
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2024-05-13 14:49:51 +0200

    better colors for the selected rows in the treeview

--- RobotControl/src/de/naoth/rc/CustomNimbusLookAndFeel.java
+++ RobotControl/src/de/naoth/rc/CustomNimbusLookAndFeel.java
@@ -5,6 +5,7 @@
  */
 package de.naoth.rc;
 
+import java.awt.Color;
 import java.awt.Font;
 import javax.swing.UIDefaults;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
@@ -21,6 +22,8 @@ public class CustomNimbusLookAndFeel extends NimbusLookAndFeel
         this.parent = parent;
     }
     
+    // reference
+    // https://docs.oracle.com/javase%2Ftutorial%2Fuiswing%2F%2F/lookandfeel/_nimbusDefaults.html
     @Override
     public UIDefaults getDefaults()
     {
@@ -28,6 +31,18 @@ public class CustomNimbusLookAndFeel extends NimbusLookAndFeel
       if(parent != null)
       {
         defaults.put("defaultFont", new Font(Font.SANS_SERIF, Font.PLAIN, parent.getFontSize()));
+        
+        defaults.put("nimbusSelectionBackground", new Color(240, 250, 255));
+        
+        // don't change color of the expansion icon
+        defaults.put("Tree[Enabled+Selected].collapsedIconPainter", defaults.get("Tree[Enabled].collapsedIconPainter"));
+        defaults.put("Tree[Enabled+Selected].expandedIconPainter",  defaults.get("Tree[Enabled].expandedIconPainter"));
+
+        // don't change the color of the text on selection
+        defaults.put("Tree.selectionForeground", defaults.get("Tree.foreground"));
+        defaults.put("Tree:TreeCell[Focused+Selected].textForeground", defaults.get("Tree.foreground"));
+        defaults.put("Tree:TreeCell[Enabled+Selected].textForeground", defaults.get("Tree.foreground"));
+        
       }
       return defaults;
     }
