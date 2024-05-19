a6bd17161bebd638e82cc37dcaa2f615bf23d1b4
Max Paul Patzelt <max.paul.patzelt@student.hu-berlin.de>
2024-05-19 20:35:55 +0200

    Cleanup unused classifiers

--- Utils/py/BallDetection/RegressionNetwork/devil_code_generator1/onbcg.py
+++ Utils/py/BallDetection/RegressionNetwork/devil_code_generator1/onbcg.py
@@ -9,10 +9,12 @@ __email__ = "oliver.urbann@tu-dortmund.de"
 
 import os
 import platform
+
 import numpy as np
 from tensorflow.keras import backend as K
-from tensorflow.keras.layers import Convolution2D, MaxPooling2D, Flatten, Dropout, BatchNormalization, \
-    LeakyReLU, Dense, ReLU
+from tensorflow.keras.layers import (BatchNormalization, Convolution2D, Dense,
+                                     Dropout, Flatten, LeakyReLU, MaxPooling2D,
+                                     ReLU)
 
 sse_conv = '''
 {indent}w = _mm_set_ps({w3}f, {w2}f, {w1}f, {w0}f);
@@ -207,8 +209,6 @@ class NaoTHCompiler:
             print("#ifndef {}_H".format(class_name.upper()), file=fp)
             print("#define {}_H".format(class_name.upper()), file=fp)
             print("", file=fp)
-            print("#include <emmintrin.h>", file=fp)
-            print("", file=fp)
             print("#include \"AbstractCNNClassifier.h\"", file=fp)
             print("", file=fp)
             print("class {} : public AbstractCNNFinder {{".format(class_name), file=fp)
@@ -264,11 +264,15 @@ class NaoTHCompiler:
         self.c_inf["f"].write('#include \"{}.h\"\n\n'.format(class_name))
         self.c_inf["f"].write("""#if WIN32\n\t#define alignas(x) __declspec(align(x))\n#endif\n\n""")
         
-        self.c_inf["f"].write('// disable on macos and aarch64, i.e apple silicon\n')
+        self.c_inf["f"].write('// disable on MacOS ARM, i.e, apple silicon\n')
         self.c_inf["f"].write('// where emmintrin.h is not available\n')
-        self.c_inf["f"].write('#if !defined(__APPLE__) && !defined(__aarch64__)\n')
+        self.c_inf["f"].write('#if defined(__APPLE__) && defined(__aarch64__)\n')
+        self.c_inf["f"].write('    void {}::cnn(float x0[16][16][1]){{}}\n'.format(class_name))
+        self.c_inf["f"].write('#else\n')
         self.c_inf["f"].write('\n')
         
+        # TODO: is this check necessary?
+        # the destinction was probably necessary for older NAO versions <5
         if arch == 'sse3':
             self.c_inf["f"].write('#include <emmintrin.h>\n\n')
 
@@ -329,15 +333,18 @@ void {}::predict(const BallCandidates::PatchYUVClassified& patch, double meanBri
     def write_footer(self, _x, class_name):
         if self.c_inf["f"] is not None:
         
+            # todo: why is this here?
+            # close cnn()
             self.c_inf["f"].write('}\n')
+            
+            # close the macro for "...defined(__APPLE__)..."
+            self.c_inf["f"].write('#endif\n')
             
             self.write_predict_function(class_name)
             self.write_get_radius_function(class_name)
             self.write_get_center_function(class_name)
             self.write_get_confidence_function(class_name)
             
-            self.c_inf["f"].write('\n')
-            self.c_inf["f"].write('#endif')
             self.c_inf["f"].write('\n')
             
             self.c_inf["f"].close()
