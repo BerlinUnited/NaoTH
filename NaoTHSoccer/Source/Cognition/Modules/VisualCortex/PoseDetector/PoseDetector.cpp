dddc90ceb92d019671d9acb416e925b61ad7252a
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-07-15 16:15:37 +0200

    try to create input for movenet

--- NaoTHSoccer/Source/Cognition/Modules/VisualCortex/PoseDetector/PoseDetector.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/PoseDetector/PoseDetector.cpp
@@ -3,8 +3,87 @@
 PoseDetector::PoseDetector()
 {
   getDebugParameterList().add(&params);
+  TfLiteModel* model = TfLiteModelCreateFromFileWithErrorReporter(("Config/" + params.tflite_model_file).c_str(), error_reporter, nullptr);
+  MY_ASSERT_NE(model, nullptr);
+
+  #ifndef WIN32
+  TfLiteXNNPackDelegateOptions xnnPackDelegateOption = TfLiteXNNPackDelegateOptionsDefault();
+  xnnPackDelegateOption.num_threads = numThreads;
+  delegate = TfLiteXNNPackDelegateCreate(&xnnPackDelegateOption);
+  MY_ASSERT_NE(delegate, nullptr);
+  #endif // undef WIN32
+
+  TfLiteInterpreterOptions* options = TfLiteInterpreterOptionsCreate();
+  MY_ASSERT_NE(options, nullptr);
+  TfLiteInterpreterOptionsSetNumThreads(options, numThreads);
+  
+  #ifndef WIN32
+  TfLiteInterpreterOptionsAddDelegate(options, delegate);
+  #endif // undef WIN32
+
+  interpreter = TfLiteInterpreterCreate(model, options);
+  MY_ASSERT_NE(interpreter, nullptr);
+
+  inputTensor = TfLiteInterpreterGetInputTensor(interpreter, 0);
+  MY_ASSERT_NE(inputTensor, nullptr);
+  MY_ASSERT_EQ(TfLiteTensorType(inputTensor), kTfLiteFloat32);
+
+  // Free options and model now that the interpreter is created
+  TfLiteInterpreterOptionsDelete(options);
+  TfLiteModelDelete(model);
+
+  // Resize input tensor and allocate tensors
+  MY_ASSERT_EQ(TfLiteInterpreterResizeInputTensor(interpreter, 0, input_dims.data(), input_dims.size()), kTfLiteOk);
+  MY_ASSERT_EQ(TfLiteInterpreterAllocateTensors(interpreter), kTfLiteOk);
+
+  MY_ASSERT_NE(inputTensor, nullptr);
+  MY_ASSERT_EQ(TfLiteTensorType(inputTensor), kTfLiteFloat32);
+
+  const TfLiteTensor* outputTensor = TfLiteInterpreterGetOutputTensor(interpreter, 0);
+  MY_ASSERT_NE(outputTensor, nullptr);
+  MY_ASSERT_EQ(TfLiteTensorType(outputTensor), kTfLiteFloat32);
 }
 
 PoseDetector::~PoseDetector() {
   getDebugParameterList().remove(&params);
+
+  if (interpreter) {
+      TfLiteInterpreterDelete(interpreter);
+  }
+  #ifndef WIN32
+  if (delegate) {
+      TfLiteXNNPackDelegateDelete(delegate);
+  }
+  #endif
+}
+
+void PoseDetector::execute(){
+  Pixel p;
+  //TODO expect input as 1,353,257,3 rgb for posenet or 1x192x192x3 rgb for movenet
+  //int y_value = ((int)getImageTop().getY_direct(0,0));
+  //cv::Mat img = cv::Mat::eye(192,192,CV_32FC3);
+  cv::Mat image(192, 192, CV_32FC3, cv::Scalar(0.0f, 0.0f, 0.0f)); // Create a 192x192 image with 3 channels (RGB) initialized to black
+  for (unsigned int y = 0; y < 192; y+=1) {
+      for (unsigned int x = 0; x < 192; x+=1) {
+          getImageTop().get(x,y,p); 
+
+          naoth::ColorModelConversions::fromYCbCrToRGB(
+          p.y, p.u, p.v,
+          p.y, p.u, p.v); 
+          //inputTensor->data.f[320 * (y/2) + (x/2)] = y_value / 255.0f;
+          //std::cout << 240 * (y/2) + (x/2) << std::endl;
+          image.at<cv::Vec3f>(y, x) = cv::Vec3f(p.v / 255.0, p.u / 255.0, p.y / 255.0);     
+      }
+  }
+  cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
+  float* inputImg_ptr = image.ptr<float>(0);
+  memcpy(inputTensor->data.f, image.ptr<float>(0), 192 * 192 * 3 * sizeof(float));
+   
+  std::cout << image.at<cv::Vec3f>(0, 0) << std::endl;
+  std::cout << inputTensor->data.f[0] << std::endl;
+
+  STOPWATCH_START("PoseDetector:predict");
+  MY_ASSERT_EQ(TfLiteInterpreterInvoke(interpreter), kTfLiteOk);
+  STOPWATCH_STOP("PoseDetector:predict");
+
 }
--- NaoTHSoccer/Source/Cognition/Modules/VisualCortex/PoseDetector/PoseDetector.h
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/PoseDetector/PoseDetector.h
@@ -3,56 +3,94 @@
 
 #include <iostream>
 
+
 #include <ModuleFramework/Module.h>
 
 #include "Tools/Debug/DebugRequest.h"
 #include "Tools/Debug/DebugModify.h"
 #include "Tools/Debug/DebugDrawings.h"
 #include "Tools/Debug/DebugParameterList.h"
-#include "Tools/DoubleCamHelpers.h"
+#include <Representations/Debug/Stopwatch.h>
 
 // Representations
-//#include "Representations/Infrastructure/Image.h"
-#include "Representations/Infrastructure/CameraInfo.h"
-#include "Representations/Perception/CameraMatrix.h"
-#include "Representations/Perception/MultiChannelIntegralImage.h"
-#include "Representations/Perception/ObstaclePercept.h"
+#include <Representations/Infrastructure/Image.h>
 #include "Representations/Motion/MotionStatus.h"
 
 // Tools
 #include "Tools/CameraGeometry.h"
+#include <Tools/ImageProcessing/ImagePrimitives.h>
+#include <Tools/ImageProcessing/ColorModelConversions.h>
+#include "Tools/naoth_opencv.h"
+
+#include "tensorflow/lite/c/c_api.h"
+#include "tensorflow/lite/delegates/xnnpack/xnnpack_delegate.h"
+
 
 BEGIN_DECLARE_MODULE(PoseDetector)
-  REQUIRE(CameraInfo)
-  REQUIRE(CameraMatrix)
-  REQUIRE(BallDetectorIntegralImage)
-  REQUIRE(MotionStatus)
+  REQUIRE(ImageTop)
 
   PROVIDE(DebugRequest)
   PROVIDE(DebugModify)
   PROVIDE(DebugDrawings)
+  PROVIDE(StopwatchManager)
   PROVIDE(DebugParameterList)
 END_DECLARE_MODULE(PoseDetector)
+
+// TFlite C API based on HTWK Implementation
+// https://github.com/NaoHTWK/HTWKVision/
+#define MY_ASSERT_NE(v, e)                                                                        \
+    do {                                                                                          \
+        if ((v) == (e)) {                                                                         \
+            fprintf(stderr, "%s (%s:%d): This is kaputt! Exit!\n", __func__, __FILE__, __LINE__); \
+            fflush(stderr);                                                                       \
+            exit(1);                                                                              \
+        }                                                                                         \
+    } while (0)
+
+#define MY_ASSERT_EQ(v, e)                                                                        \
+    do {                                                                                          \
+        if ((v) != (e)) {                                                                         \
+            fprintf(stderr, "%s (%s:%d): This is kaputt! Exit!\n", __func__, __FILE__, __LINE__); \
+            fflush(stderr);                                                                       \
+            exit(1);                                                                              \
+        }                                                                                         \
+    } while (0)
+    
+
+
+struct TfLiteInterpreter;
+struct TfLiteDelegate;
+
 
 class PoseDetector: public PoseDetectorBase
 {
 public:
-  CameraInfo::CameraID cameraID;
-
   PoseDetector();
   ~PoseDetector();
+  static void error_reporter(void* /*user_data*/, const char* format, va_list args) {
+    vfprintf(stderr, format, args);
+    fprintf(stderr, "\n");
+  }
 
-private:
+  virtual void execute();
+
   class Parameters: public ParameterList
   {
   public:
     Parameters() : ParameterList("PoseDetector")
     {
-
+      PARAMETER_REGISTER(tflite_model_file) = "posenet-mobilenet.tflite";
       syncWithConfig();
     }
-
+    std::string tflite_model_file;
   } params;
+  private:
+    int numThreads = 2;
+    std::vector<int> input_dims = {1, 192, 192, 3};
+    TfLiteTensor* inputTensor;
+    std::vector<float> result;
+    TfLiteInterpreter* interpreter = nullptr;
+    TfLiteDelegate* delegate = nullptr;
 };
 
 #endif // POSEDETECTOR_H
