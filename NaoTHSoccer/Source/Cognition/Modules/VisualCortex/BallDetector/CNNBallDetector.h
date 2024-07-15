ac1e276c4ea7873e9e826b2f1f7c03740cdbe9ef
Max Patzelt <max.patzelt@gmail.com>
2024-07-15 12:04:23 +0200

    Fix merge conflict

--- NaoTHSoccer/Source/Cognition/Modules/VisualCortex/BallDetector/CNNBallDetector.h
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/BallDetector/CNNBallDetector.h
@@ -136,10 +136,6 @@ private:
   {
     Parameters() : ParameterList("CNNBallDetector")
     {
-      PARAMETER_REGISTER(keyDetector.borderRadiusFactorClose) = 0.5;
-      PARAMETER_REGISTER(keyDetector.borderRadiusFactorFar) = 0.8;
-      PARAMETER_REGISTER(keyDetector.maxInnerGreenDensitiy) = 0.5;
-      
       PARAMETER_REGISTER(cnn.threshold) = 0.99;
       PARAMETER_REGISTER(cnn.thresholdClose) = 0.99;
       // Constant offset added to the input of the CNN. < 0 darker, > 0 brighter
