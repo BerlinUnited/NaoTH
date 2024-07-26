5ffe32b57abf0e8fecb5fe8adfaf0d2543fc6b06
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-07-26 18:24:56 +0200

    allow for buggy jpeg log parsing

--- Utils/py/naoth/naoth/log/_experimental_reader.py
+++ Utils/py/naoth/naoth/log/_experimental_reader.py
@@ -186,11 +186,13 @@ class Frame:
         :param size: of payload data in the log file
         """
 
-        if name in self._fields:
-            raise ValueError(
-                f"Frame already contains member {name}: "
-                f"{self._fields[name]} [current] vs ({position, size}) [incoming]"
-            )
+        # TODO: fix this when recording jpeg logs, probably async related
+        print(f"Frame already contains member {name}: , HACK: ignoring so that jpeg logs can parse")
+        
+        # raise ValueError(
+        #     f"Frame already contains member {name}: "
+        #     f"{self._fields[name]} [current] vs ({position, size}) [incoming]"
+        # )
 
         self._fields[name] = position, size
         self.size += Scanner.size_of_field(name, size)
