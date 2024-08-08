c584b79226dd18ca78f13c725d29b5779547f8c0
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2024-08-08 18:21:04 +0200

    improve pipeline for naoth pip package creation slightly

--- Utils/py/naoth/package_naoth_pypi.sh
+++ Utils/py/naoth/package_naoth_pypi.sh
@@ -11,6 +11,3 @@ EOF
 
 echo "publish naoth package to pypi"
 python3 -m twine upload dist/* --config-file .pypirc
-
-echo ""
-echo "If you got errors it might be because you are trying to overwrite an existing version. Try incrementing the version"
