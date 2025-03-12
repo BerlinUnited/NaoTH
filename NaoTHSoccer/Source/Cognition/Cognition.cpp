0be7334b2854845fd3574bf9a7cd650aa143d64c
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2025-03-12 20:40:55 +0100

    simulation might use StaticDebugModelProvider

--- NaoTHSoccer/Source/Cognition/Cognition.cpp
+++ NaoTHSoccer/Source/Cognition/Cognition.cpp
@@ -288,9 +288,9 @@ void Cognition::init(naoth::ProcessInterface& platformInterface, const naoth::Pl
    * END ROLE DECISIONS
    */
 
+  REGISTER_MODULE(StaticDebugModelProvider);
   REGISTER_MODULE(KickDirectionSimulator);
   REGISTER_MODULE(Simulation);
-  REGISTER_MODULE(StaticDebugModelProvider);
   REGISTER_MODULE(AttentionEngine);
 
   // behavior
