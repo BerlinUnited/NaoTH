f83c73a071d0566ec5e0334ea7f991a0eeb07199
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2025-04-03 15:51:53 +0200

    changed pushing_free_kick to penalty_kick

--- NaoTHSoccer/Source/Cognition/Modules/Infrastructure/GameController/GameController.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/Infrastructure/GameController/GameController.cpp
@@ -229,8 +229,8 @@ void GameController::execute()
       // the ball is in the own half
       getPlayerInfo().kickoff = (globalBallPos.x < 0);
   }
-  // TODO: make sure that pushing_free_kick actually works like this
-  if (getPlayerInfo().robotSetPlay == PlayerInfo::corner_kick || getPlayerInfo().robotSetPlay == PlayerInfo::pushing_free_kick) {
+  
+  if (getPlayerInfo().robotSetPlay == PlayerInfo::corner_kick || getPlayerInfo().robotSetPlay == PlayerInfo::penalty_kick) {
       const Vector2d globalBallPos = (getRobotPose() * getBallModel().position);
       // the ball is in the opponent half
       getPlayerInfo().kickoff = (globalBallPos.x > 0);
