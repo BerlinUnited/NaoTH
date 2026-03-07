b2dc37ff57235bc619bbde4de636fd8226d45738
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2026-03-07 15:24:09 +0100

    convenience scripts for compilation

--- NaoTHSoccer/Make/compileBooster.sh
+++ NaoTHSoccer/Make/compileBooster.sh
@@ -7,10 +7,18 @@ set -e
 # catch the EXIT and print a message if the exit code is not zero
 trap '[ $? -eq 0 ] || echo "!!!! FAILURE WITH CODE $? !!!!"' EXIT
 
-echo "###### COMPILE ######"
-./compileNao.sh BoosterRobot $@
+# first parameter should be a build target,
+# compile NaoRobot if no argument is provided
+# or if the first argument is an optional argument
+if [ "${1::1}" == "-" ] || [ "${#}" -eq 0 ]; then
+  ACTION=BoosterRobot
+fi
 
-echo "###### XABSL ######"
-./compileBehavior.sh
+echo "###### GENERATE MAKE FILES ######"
+premake5 --platform=Booster --Test gmake2
+
+echo "###### COMPILE ######"
+# run make in the corresponding build dir
+(cd ../build/Booster/ && make config=optdebug_booster $ACTION $@)
 
 echo "###### FINISH ######"
