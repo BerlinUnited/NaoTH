05e306c89925cdbf04250928cdc08edf7787a230
Lelax <lelax@hier.email>
2026-03-12 10:40:38 +0100

    add network script for booster

---
+++ Utils/Booster-K1/NetworkStick/brainwash.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+echo "INFO: Beginning with network script"
+
+WLAN_SSID=NAONET
+WLAN_PW=a1b0a1b0a1
+WLAN_IP=10.0.4
+WLAN_SUBNET_MASK=16
+
+# TODO: how to get this on the booster
+NR=41
+
+IFNAME=wlP1p1s0
+if [[ $(ip a | grep $IFNAME) == "" ]]; then
+    echo "ERROR: Wifi interface name changed/ does not exist! Exiting."
+    exit 1
+fi
+
+echo "INFO: Setting SSID=$WLAN_SSID, PW=$WLAN_PW, IP=$WLAN_IP.$NR/$WLAN_SUBNET_MASK, IF=$IFNAME"
+
+# NOTE: NO NOT change the name of the connection file
+cat - <<EOF >/etc/NetworkManager/system-connections/WIFI.nmconnection
+[connection]
+id=WIFI
+uuid=975cebb3-4286-40fb-8856-80336335e3b1
+type=wifi
+interface-name=$IFNAME
+autoconnect=true
+
+[wifi]
+mode=infrastructure
+ssid=$WLAN_SSID
+cloned-mac-address=permanent
+
+[wifi-security]
+key-mgmt=wpa-psk
+psk=$WLAN_PW
+
+[ipv4]
+address1=$WLAN_IP.$NR/$WLAN_SUBNET_MASK
+dns=$WLAN_IP.1;1.1.1.1;8.8.8.8;
+gateway=$WLAN_IP.1
+method=manual
+
+[ipv6]
+addr-gen-mode=stable-privacy
+method=disabled
+
+[proxy]
+EOF
+
+# To avoid "failed to load connection: File permissions (100644) are insecure"
+sudo chown root:root /etc/NetworkManager/system-connections/*
+sudo chmod 600 /etc/NetworkManager/system-connections/*
+
+sudo systemctl restart NetworkManager
+sudo nmcli connection modify 975cebb3-4286-40fb-8856-80336335e3b1 wifi.cloned-mac-address permanent
+
+echo "INFO: Done with network script"
