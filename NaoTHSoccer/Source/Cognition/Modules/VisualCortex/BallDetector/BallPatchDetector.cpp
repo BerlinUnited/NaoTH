81b9c3e486b0676afdffde24bff45f21f7c4c0fd
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2026-05-21 21:07:02 +0200

    cleanup: move keypoint functions to the cpp file

--- NaoTHSoccer/Source/Cognition/Modules/VisualCortex/BallDetector/BallPatchDetector.cpp
+++ NaoTHSoccer/Source/Cognition/Modules/VisualCortex/BallDetector/BallPatchDetector.cpp
@@ -40,6 +40,271 @@ void BallPatchDetector::execute(const CameraInfo::CameraID id)
   );
 }
 
+template<class ImageType>
+void BallPatchDetector::calculateKeyPoints(const ImageType& integralImage, BestPatchList& best) const
+{
+  //
+  // STEP I: find the maximal height minY to be scanned in the image
+  //
+  if(!getFieldPercept().valid) {
+    return;
+  }
+
+  // find the top point of the polygon
+  int minY = getFieldPercept().getMinY();
+
+  // double check: polygon is empty
+  if(minY == (int)getImage().height() || minY < 0) {
+    return;
+  }
+
+  // todo needs a better place
+  const int32_t FACTOR = integralImage.FACTOR;
+
+  Vector2<unsigned int> point;
+
+  for(point.y = minY/FACTOR; point.y < integralImage.getHeight(); ++point.y)
+  {
+    double estimatedRadius = CameraGeometry::estimatedBallRadius(
+      getCameraMatrix(), getCameraInfo(), getFieldInfo().ballRadius,
+      static_cast<int>(point.x)*FACTOR, static_cast<int>(point.y)*FACTOR);
+
+    double radius = std::max(6.0, estimatedRadius);
+    unsigned int size   = (unsigned int)(radius*2.0/FACTOR+0.5);
+    unsigned int border = (unsigned int)(radius*params.borderRadiusFactorClose/FACTOR+0.5);
+
+    // HACK: different parameters depending on size
+    if(size < 40/FACTOR) {
+      border = (unsigned int)(radius*params.borderRadiusFactorFar/FACTOR+0.5);
+    }
+    border = std::max( 2u, border);
+
+    // smallest ball size == 3 => ball size == FACTOR*3 == 12
+    if (point.y <= border || point.y+size+border >= integralImage.getHeight()) {
+      continue;
+    }
+
+    for(point.x = border; point.x+size+border < integralImage.getWidth(); ++point.x)
+    {
+      evaluatePatch(integralImage, best, point, size, border);
+    }
+  }
+}
+
+
+template<class ImageType>
+void BallPatchDetector::calculateKeyPointsFast(const ImageType& integralImage, BestPatchList& best) const
+{
+  DEBUG_REQUEST("Vision:BallPatchDetector:draw_value",
+    CANVAS(((cameraID == CameraInfo::Top)?"ImageTop":"ImageBottom"));
+  );
+
+  //
+  // STEP I: find the maximal height minY to be scanned in the image
+  //
+  if(!getFieldPercept().valid) {
+    return;
+  }
+
+  // find the top point of the polygon
+  int minY = getFieldPercept().getMinY();
+
+  // double check: polygon is empty
+  if(minY == (int)getImage().height() || minY < 0) {
+    return;
+  }
+
+  // todo needs a better place
+  const int32_t FACTOR = integralImage.FACTOR;
+
+  Vector2<unsigned int> point;
+
+  const unsigned int height = integralImage.getHeight();
+  const unsigned int width = integralImage.getWidth();
+
+  for(point.y = minY/FACTOR; point.y < height; ++point.y)
+  {
+    double estimatedRadius = CameraGeometry::estimatedBallRadius(
+      getCameraMatrix(), getCameraInfo(), getFieldInfo().ballRadius,
+      static_cast<int>(getImage().width()/2), static_cast<int>(point.y)*FACTOR);
+
+    estimatedRadius = estimatedRadius / FACTOR + 0.5;
+    // Note: we have a minimal allowed radius
+    unsigned int radius = (estimatedRadius < 2.0) ? 2 : static_cast<unsigned int>(estimatedRadius);
+
+    // smalest ball size == 3 => ball size == FACTOR*3 == 12
+    if (point.y < radius || point.y + radius >= height) {
+      continue;
+    }
+
+    for(point.x = radius; point.x + radius < width; ++point.x)
+    {
+      //evaluatePatch(integralImage, best, point, size, border);
+
+      if(cameraID == CameraInfo::Bottom && //point.y+radius >(int)(integralImage.getHeight()/2) &&
+         getBodyContour().isOccupied(point.x*integralImage.FACTOR, (point.y+radius)*integralImage.FACTOR)) {
+        continue;
+      }
+
+      const unsigned int innerOffset = radius/2;
+      const unsigned int area = 4*radius*radius;
+
+      unsigned int inner = integralImage.getSumForRect(point.x-radius, point.y-radius, point.x+radius, point.y+radius, 0);
+      double greenInner  = integralImage.getDensityForRect(point.x-innerOffset, point.y-innerOffset, point.x+innerOffset, point.y+innerOffset, 1);
+
+      unsigned int below = 0;
+      unsigned int radius_below = radius*2;
+
+      if(point.y+radius_below < height) {
+        below = integralImage.getSumForRect(point.x-radius, point.y+radius, point.x+radius, point.y+radius_below, 0);
+      }
+
+      if (inner*2 > area && greenInner <= params.maxInnerGreenDensitiy && below*params.area_below_factor < area)
+      {
+        double value = ((double)inner)/((double)(area));
+        best.add(
+            static_cast<int>(point.x-radius)*integralImage.FACTOR,
+            static_cast<int>(point.y-radius)*integralImage.FACTOR,
+            static_cast<int>(point.x+radius)*integralImage.FACTOR,
+            static_cast<int>(point.y+radius)*integralImage.FACTOR,
+            value);
+      }
+
+      DEBUG_REQUEST("Vision:BallPatchDetector:draw_value",
+          double value = ((double)inner)/((double)(area));
+
+          value = Math::clamp(value / 200.0, 0.0,1.0);
+          PEN(Color(1.0,1.0-value,1.0-value,0.8),0.1);
+
+          FILLBOX((point.x)*integralImage.FACTOR - integralImage.FACTOR/2,
+                  (point.y)*integralImage.FACTOR - integralImage.FACTOR/2,
+                  (point.x)*integralImage.FACTOR + integralImage.FACTOR/2,
+                  (point.y)*integralImage.FACTOR + integralImage.FACTOR/2);
+      );
+    }
+  }
+}
+
+
+template<class ImageType>
+void BallPatchDetector::calculateKeyPointsFull(const ImageType& integralImage, BestPatchList& best) const
+{
+  DEBUG_REQUEST("Vision:BallPatchDetector:draw_value",
+    CANVAS(((cameraID == CameraInfo::Top)?"ImageTop":"ImageBottom"));
+  );
+
+  //
+  // STEP I: find the maximal height minY to be scanned in the image
+  //
+  if(!getFieldPercept().valid) {
+    return;
+  }
+
+  // find the top point of the polygon
+  int minY = getFieldPercept().getMinY();
+
+  // double check: polygon is empty
+  if(minY == (int)getImage().height() || minY < 0) {
+    return;
+  }
+
+  // todo needs a better place
+  const int32_t FACTOR = integralImage.FACTOR;
+
+  Vector2i point;
+
+  // TODO: this has to be made more general
+  for(int y = 1; y+1 < 480/4; ++y) {
+    for(int x = 1; x+1 < 640/4; ++x) {
+      values[x][y][0] = 0.0;
+    }
+  }
+
+  // TODO: faster reset?
+  //std::fill_n(&values[0][0][0], (480/4)*(640/4)*2, 0);
+
+  for(point.y = minY/FACTOR; point.y < (int)integralImage.getHeight(); ++point.y)
+  {
+    double estimatedRadius = CameraGeometry::estimatedBallRadius(
+      getCameraMatrix(), getCameraInfo(), getFieldInfo().ballRadius,
+      getImage().width()/2, point.y*FACTOR);
+
+
+    int radius = (int)(estimatedRadius / FACTOR + 0.5);
+
+    // smalest ball size == 3 => ball size == FACTOR*3 == 12
+    if (point.y < radius || point.y + radius >= (int)integralImage.getHeight()) {
+      continue;
+    }
+
+    for(point.x = radius; point.x+radius < (int)integralImage.getWidth(); ++point.x)
+    {
+      //evaluatePatch(integralImage, best, point, size, border);
+
+      if(cameraID == CameraInfo::Bottom && getBodyContour().isOccupied(point.x*integralImage.FACTOR, (point.y+radius)*integralImage.FACTOR)) {
+        values[point.x][point.y][0] = 0.0;
+        values[point.x][point.y][1] = radius;
+        continue;
+      }
+
+      int inner = integralImage.getSumForRect(point.x-radius, point.y-radius, point.x+radius, point.y+radius, 0);
+      double greeInner = integralImage.getDensityForRect(point.x-radius, point.y-radius, point.x+radius, point.y+radius, 1);
+      const int size = radius*2;
+
+      if (inner*2 > size*size && greeInner <= params.maxInnerGreenDensitiy)
+      {
+        double value = ((double)inner)/((double)(size)*(size));
+        values[point.x][point.y][0] = value;
+        values[point.x][point.y][1] = radius;
+      }
+      else
+      {
+        values[point.x][point.y][0] = 0.0;
+        values[point.x][point.y][1] = radius;
+      }
+
+      DEBUG_REQUEST("Vision:BallKeyPointExtractor:draw_value",
+          double value = ((double)inner)/((double)(size*size));
+
+          value = Math::clamp(value / 200.0, 0.0,1.0);
+          PEN(Color(1.0,1.0-value,1.0-value,0.8),0.1);
+
+          FILLBOX((point.x)*integralImage.FACTOR - integralImage.FACTOR/2,
+                  (point.y)*integralImage.FACTOR - integralImage.FACTOR/2,
+                  (point.x)*integralImage.FACTOR + integralImage.FACTOR/2,
+                  (point.y)*integralImage.FACTOR + integralImage.FACTOR/2);
+      );
+    }
+  }
+
+
+  for(int y = 1; y+1 < 480/4; ++y) {
+    for(int x = 1; x+1 < 640/4; ++x) {
+      if(values[x][y][0] > 0) {
+        if( values[x][y][0] > values[x-1][y][0] &&
+            values[x][y][0] > values[x+1][y][0] &&
+            values[x][y][0] > values[x  ][y-1][0] &&
+            values[x][y][0] > values[x  ][y+1][0] &&
+
+            values[x][y][0] > values[x-1][y-1][0] &&
+            values[x][y][0] > values[x+1][y-1][0] &&
+            values[x][y][0] > values[x-1][y+1][0] &&
+            values[x][y][0] > values[x+1][y+1][0])
+        {
+          double value = values[x][y][0];
+          int rad = (int)values[x][y][1];
+          best.add(
+            (x-rad)*integralImage.FACTOR,
+            (y-rad)*integralImage.FACTOR,
+            (x+rad)*integralImage.FACTOR,
+            (y+rad)*integralImage.FACTOR,
+            value);
+        }
+      }
+    }
+  }
+}
+
 BestPatchList::Patch BallPatchDetector::refineKeyPoint(const BestPatchList::Patch& patch) const
 {
   // todo needs a better place
