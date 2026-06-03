a46191687805645022525581ca2302004b9e684f
Lennart Keller <lennart.keller@hu-berlin.de>
2026-06-03 21:41:19 +0200

    change order

--- Utils/NetworkStickUbuntuBooster/installAutomation.sh
+++ Utils/NetworkStickUbuntuBooster/installAutomation.sh
@@ -1,10 +1,9 @@
 # turn off auto-mounting of usb sticks
 # gsettings set org.gnome.desktop.media-handling automount false gsettings set org.gnome.desktop.media-handling automount-open false
 
-sudo mv 99-new-network-berlin-united.rules /etc/udev/rules.d/
-sudo udevadm control --reload-rules
-
-sudo mv getNewNetworkOfUSBStick@.service /etc/systemd/system/
-sudo systemctl daemon-reload
-
 sudo mv getNewNetworkOfUSBStick.sh /usr/local/bin/
+sudo mv getNewNetworkOfUSBStick@.service /etc/systemd/system/
+sudo mv 99-new-network-berlin-united.rules /etc/udev/rules.d/
+
+sudo systemctl daemon-reload
+sudo udevadm control --reload-rules
