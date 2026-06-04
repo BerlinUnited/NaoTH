12e3f5cb5625999e75109723ae4d05b20699af92
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2026-06-04 22:24:50 +0200

    chore: template booster network script similar to NAO network script

--- Utils/NetworkStickBooster/startBrainwashing.sh
+++ Utils/NetworkStickBooster/startBrainwashing.sh
@@ -1,34 +1,44 @@
 #!/bin/bash
 set -x
 
-SSID="NAONET_5G"
-NETWORK_NAME="NAONET_5G"
+echo "Generate network configuration";
+WLAN_SSID="NAONET_5G"
+WLAN_PW="a1b0a1b0a1"
+WLAN_IP="10.0.4"
+WLAN_SUBNET_MASK="16"
+# TODO make DNS configurable (needed for HU networks)
+
+# e.g. for a competition set the following
+# WLAN_SSID="SPL_A"
+# WLAN_PW="Nao?!Nao?!"
+
+# NOTE: we dont set LAN IP on boosters for now, that means they will stay the same as in the docs
 
 # play sound
 /usr/bin/paplay $HOME/naoqi/Media/usb_start.wav"
 
 echo "Generate network configuration";
 
-sudo tee /etc/NetworkManager/system-connections/$NETWORK_NAME.nmconnection > /dev/null <<EOF
+sudo tee /etc/NetworkManager/system-connections/BU-WIFI.nmconnection > /dev/null <<EOF
 [connection]
-id=$NETWORK_NAME
+id=BU-WIFI
 type=wifi
 interface-name=wlP1p1s0
 autoconnect-priority=1
 
 [wifi]
 mode=infrastructure
-ssid=$SSID
+ssid=$WLAN_SSID
 
 [wifi-security]
 auth-alg=open
 key-mgmt=wpa-psk
-psk=a1b0a1b0a1
+psk=$WLAN_PW
 
 [ipv4]
 method=manual
-addresses=10.0.4.41/16
-gateway=10.0.4.1
+addresses=$WLAN_IP.41/$WLAN_SUBNET_MASK
+gateway=$WLAN_IP.1
 dns=8.8.8.8;1.1.1.1;
 
 [ipv6]
@@ -38,9 +48,9 @@ method=auto
 [proxy]
 EOF
 
-sudo chmod 600 /etc/NetworkManager/system-connections/$NETWORK_NAME.nmconnection
+sudo chmod 600 /etc/NetworkManager/system-connections/BU-WIFI.nmconnection
 
 
 sudo nmcli connection reload
 # sudo nmcli device disconnect wlP1p1s0 # if we encounter errors we might need to do this
-sudo nmcli connection up $NETWORK_NAME
+sudo nmcli connection up BU-WIFI
