3420a80c530df77ca08125fd8246d795ff8a32f2
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2026-06-04 18:31:47 +0200

    chore: add scripts needed for usb stick controlled behavior to k1 as well

--- Utils/NetworkStickUbuntuBooster/installAutomation.sh
+++
@@ -1,9 +0,0 @@
-# turn off auto-mounting of usb sticks
-# gsettings set org.gnome.desktop.media-handling automount false gsettings set org.gnome.desktop.media-handling automount-open false
-
-sudo mv getNewNetworkOfUSBStick.sh /usr/local/bin/
-sudo mv getNewNetworkOfUSBStick@.service /etc/systemd/system/
-sudo mv 99-new-network-berlin-united.rules /etc/udev/rules.d/
-
-sudo systemctl daemon-reload
-sudo udevadm control --reload-rules
